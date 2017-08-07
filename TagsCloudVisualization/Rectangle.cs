d979e19e469cb35bce8ef055586e6399c19f6dcb
digi <digi@skbkontur.ru>
2017-08-07 20:03:27 +0500

    анализ требований

---
+++ TagsCloudVisualization/Rectangle.cs
@@ -0,0 +1,10 @@
+﻿namespace TagsCloudVisualization
+{
+	struct Rectangle
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+		public double Width { get; set; }
+		public double Height { get; set; }
+	}
+}
