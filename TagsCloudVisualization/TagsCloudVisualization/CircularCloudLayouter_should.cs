6d869322675e55ae85cbf9bb5b132997036414fb
Степан <martyanovst@gmail.com>
2017-11-02 02:08:58 +0500

    first step on tdd road

---
+++ TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter_should.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    class CircularCloudLayouter_should
+    {
+        public CircularCloudLayouter cloud;
+
+        [SetUp]
+        public void Init()
+        {
+            var center = new Point(500, 500);
+            cloud = new CircularCloudLayouter(center);
+        }
+
+        [Test]
+        public void InitializeCloudCenter_AfterInitialization()
+        {
+            var center = new Point(400, 400);
+            var cloudLayouter = new CircularCloudLayouter(center);
+            cloudLayouter.center.ShouldBeEquivalentTo(center);
+        }
+
+        [Test]
+        public void NotChangeRectangleSize_AfterAddition()
+        {
+            var wordBox = new Size(100, 100);
+
+            var size = cloud.PutNextRectangle(wordBox).Size;
+
+            size.ShouldBeEquivalentTo(wordBox);
+        }
+
+        [TestCase(1, 450, 450, TestName = "Single addition")]
+        [TestCase(2, 550, 350, TestName = "Double addition")]
+        [TestCase(3, 450, 350, TestName = "Triple addition")]
+        [TestCase(4, 350, 350, TestName = "Quadruple addition")]
+        [TestCase(5, 350, 450, TestName = "Multiple addition")]
+        public void PutRectangleToTheCorrectPlace_AfterAddition(int count, int expectedX, int expectedY)
+        {
+            var wordBox = new Size(100, 100);
+
+            for (var i = 0; i < count - 1; i++)
+                cloud.PutNextRectangle(wordBox);
+            var fourthRectangle = cloud.PutNextRectangle(wordBox);
+
+            var expectedLocation = new Point(expectedX, expectedY);
+            fourthRectangle.Location.Should().Be(expectedLocation);
+        }
+
+        [TestCase(2, TestName = "When only two rectangles")]
+        [TestCase(10, TestName = "When myltiple rectangles")]
+        [TestCase(100, TestName = "When a lot of rectangles")]
+        public void PutNextRectangleWithoutIntersectionOfOtherRectangle_AfterDoubleAddition(int count)
+        {
+            var wordBox = new Size(100, 100);
+            var rectangles = new List<Rectangle>();
+            for (var i = 0; i < count; i++)
+                rectangles.Add(cloud.PutNextRectangle(wordBox));
+
+            rectangles.Select(x => rectangles.Any(y => x != y && y.IntersectsWith(x))).ShouldAllBeEquivalentTo(false);
+        }
+
+        [TestCase(1000), Timeout(1000)]
+        public void PutNextRectangle_LessThanOneSecond_AfterMultiplyAddition(int count)
+        {
+            var wordBox = new Size(100, 100);
+            for (var i = 0; i < count; i++)
+                cloud.PutNextRectangle(wordBox);
+        }
+
+        [Test]
+        public void ThrowArgumetException_AfterAdditionTheNotPositiveSize()
+        {
+            var incorrectSize = new Size(-100, 100);
+            Assert.Throws<ArgumentException>(() => cloud.PutNextRectangle(incorrectSize),
+                "Rectangle size must be a positive");
+        }
+    }
+}
---
+++ TagsCloudVisualization/TagsCloudVisualization/TagCloudVisualizationForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TagsCloudVisualization
+{
+    
+
+    internal class TagCloudVisualizationForm : Form
+    {
+        private readonly List<Rectangle> rectangles = new List<Rectangle>();
+        private readonly CircularCloudLayouter cloud;
+        public TagCloudVisualizationForm()
+        {
+            var center = new Point(900, 500);
+            cloud = new CircularCloudLayouter(center);
+            var random = new Random();
+            for (var i = 0; i < 200; i++)
+            {
+                var word = new Size(30*random.Next(1,4), 10 * random.Next(1,4));
+                rectangles.Add(cloud.PutNextRectangle(word));
+            }
+            var timer = new Timer { Interval = 10 };
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            DoubleBuffered = true;
+            WindowState = FormWindowState.Maximized;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            foreach (var rectangle in rectangles)
+            e.Graphics.DrawRectangle(new Pen(Color.Black), rectangle);
+            e.Graphics.DrawEllipse(new Pen(Color.Blue),cloud.center.X - (float)cloud.radius,
+                cloud.center.Y - (float)cloud.radius, (float)cloud.radius*2, (float)cloud.radius*2);
+            e.Graphics.DrawRectangle(new Pen(Color.Red), new Rectangle(cloud.center,new Size(1,1)));
+
+
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            Invalidate();
+            Update();
+        }
+    }
+}
