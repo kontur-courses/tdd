8c7aa38482ebe69116623d9401b4a424a1c8cf6b
Alexander Debelyi <debelyy@yandex.ru>
2018-11-04 01:34:37 +0500

    main task solution

---
+++ cs/TagsCloudVisualization/RectangleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    public static class RectangleExtensions
+    {
+        public static bool IntersectsWith(this Rectangle rectangle, ICollection<Rectangle> rectangles)
+        {
+            return rectangles.Any(rectangle.IntersectsWith);
+        }
+    }
+}
