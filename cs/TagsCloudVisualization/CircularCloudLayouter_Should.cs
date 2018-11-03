b8bff065b05c3849ff6bfb18c3d9a0603c22b9de
Avel7884 <Avel7884@users.noreply.github.com>
2018-11-04 01:54:27 +0500

    Stage2.Test

--- cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -14,6 +14,10 @@ namespace TagsCloudVisualization
     {
         private CircularCloudLayouter layouter;
         private Point center;
+        private readonly Random rnd = new Random();
+        private const int minHeight = 6;
+        private const int maxHeight = 100;
+        private const int maxWidthHeightRatio = 10;
 
         [SetUp]
         public void SetUp()
@@ -29,10 +33,38 @@ namespace TagsCloudVisualization
         }
 
         [Test]
+        public void MakeRectangleWithCorrectSize()
+        {
+            var size = RandomSize();
+            layouter.PutNextRectangle(size).Size.Should().BeEquivalentTo(size);
+        }
+
+        [Test]
         public void PutFirstRectangleCenterOnLayoutCenter()
         {
-            var rect = layouter.PutNextRectangle(new Size(48, 24));
+            var rect = layouter.PutNextRectangle(RandomSize());
             rect.Center().Should().BeEquivalentTo(center);
+        }
+
+        private Size RandomSize()
+        {
+            var height = rnd.Next(minHeight, maxHeight);
+            var width = rnd.Next(height, height* maxWidthHeightRatio);
+            return new Size(width, height);
+        }
+
+        private Rectangle[] GenerateRectangles(int count)=>
+            count.Times(RandomSize).Select(layouter.PutNextRectangle).ToArray();
+
+        [Test]
+        public void DoNotOverlap()
+        {
+            const int count = 100;
+            var rectangles = GenerateRectangles(count);
+
+            for (int i = 0; i < count; i++) 
+            for (int j = 0; j < i; j++)
+                Assert.False(rectangles[i].IntersectsWith(rectangles[j]));
         }
     }
 }
--- cs/TagsCloudVisualization/Extensions.cs
+++ cs/TagsCloudVisualization/Extensions.cs
@@ -15,5 +15,29 @@ namespace TagsCloudVisualization
         public static Point Center(this Rectangle rectangle)=>
             rectangle.Location + rectangle.Size.Divide(2);
 
+        public static Size HeightSize(this Size size) =>
+            new Size(0,size.Height);
+
+        public static Size WidhtSize(this Size size) =>
+            new Size(0, size.Width);
+
+        public static IEnumerable<Point> Points(this Rectangle rect)
+        {
+            yield return rect.Location;
+            yield return rect.Location + rect.Size.HeightSize();
+            yield return rect.Location + rect.Size.WidhtSize();
+            yield return rect.Location + rect.Size;
+        }
+        public static void Times(this int count, Action act)
+        {
+            for (int i = 0; i < count; i++)
+                act();
+        }
+        public static IEnumerable<T> Times<T>(this int count, Func<T> act)
+        {
+            for (int i = 0; i < count; i++)
+                yield return act();
+        }
+
     }
 }
