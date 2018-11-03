9785637674f85e33d2b2e9bdf47ab4c3c7eae4de
Alexander Debelyi <debelyy@yandex.ru>
2018-11-04 01:35:26 +0500

    Visualizer task solution

---
+++ cs/TagsCloudVisualization/Visualizator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    public class Visualizator : IVisualizator
+    {
+        private readonly Color penColor = Color.Blue;
+        private readonly Size imageSize;
+        private readonly ICollection<Rectangle> rectangles;
+
+        public Visualizator(Size imageSize, ICollection<Rectangle> rectangles)
+        {
+            if (imageSize.Width <= 0 || imageSize.Height <= 0)
+            {
+                throw new ArgumentException(
+                    "Width and height of image have to be > 0",
+                    nameof(imageSize));
+            }
+
+            this.imageSize = imageSize;
+            this.rectangles = rectangles ?? throw new ArgumentNullException(nameof(rectangles));
+        }
+
+        public Bitmap Generate()
+        {
+            var image = new Bitmap(imageSize.Width, imageSize.Height);
+            var graphics = Graphics.FromImage(image);
+            var pen = new Pen(penColor);
+            var center = new Point(image.Width / 2, image.Height / 2);
+
+            var rects = rectangles
+                .Select(rectangle =>
+                {
+                    rectangle.Location = new Point(
+                        rectangle.Location.X + center.X,
+                        rectangle.Location.Y + center.Y);
+
+                    return rectangle;
+                })
+                .ToArray();
+
+            if (rectangles.Count != 0)
+            {
+                graphics.DrawRectangles(pen, rects);
+            }
+
+            return image;
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Visualizator_Should.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    public class Visualizator_Should
+    {
+        #region InvalidSizes
+
+        public static IEnumerable<Size> OnInvalidSizes()
+        {
+            yield return new Size(0, 0);
+            yield return new Size(0, 1);
+            yield return new Size(1, 0);
+            yield return new Size(-1, 1);
+            yield return new Size(1, -1);
+            yield return new Size(-1, -1);
+        }
+
+        #endregion
+
+        [TestCaseSource(nameof(OnInvalidSizes))]
+        [Test]
+        public void Constructor_ThrowsException_OnInvalidImageSize(Size size)
+        {
+            Action action = () => new Visualizator(size, new List<Rectangle>());
+            action
+                .Should()
+                .Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_OnNullRectangles()
+        {
+            Action action = () => new Visualizator(
+                new Size(100, 100),
+                null);
+            action
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+    }
+}
