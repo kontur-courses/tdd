6451190c5dab00d89d451812cea39b0cfe5783b5
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-11-03 19:03:20 +0500

    circular cloud layouter done

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
-        private Point center;
+        private readonly Point center;
+        private readonly IPointsGenerator pointsGenerator;
 
         public CircularCloudLayouter(Point center, IPointsGenerator pointsGenerator)
         {
             this.center = center;
+            this.pointsGenerator = pointsGenerator;
         }
 
-        public List<Rectangle> Rectangles { get; private set; }
+        public List<Rectangle> Rectangles { get; } = new List<Rectangle>();
 
         public void PutNextRectangle(Size rectangleSize)
         {
-            throw new NotImplementedException();
+            if (rectangleSize.Width <= 0 || rectangleSize.Height <= 0)
+                throw new ArgumentException("Rectangle width and height should be positive numbers");
+            Rectangle newRectangle;
+            while (true)
+            {
+                var nextPoint = pointsGenerator.GetNextPoint();
+                var location = center + (Size)nextPoint;
+                newRectangle = new Rectangle(location, rectangleSize).MoveToHavePointInCenter(location);
+                if (Rectangles.All(rectangle => !rectangle.IntersectsWith(newRectangle)))
+                    break;
+            }
+            Rectangles.Add(newRectangle);
         }
     }
 }
--- cs/TagsCloudVisualization/RectangleExtensions.cs
+++ cs/TagsCloudVisualization/RectangleExtensions.cs
@@ -6,5 +6,11 @@ namespace TagsCloudVisualization
     {
         public static Point GetCenter(this Rectangle rectangle) =>
             new Point(rectangle.Left + rectangle.Width / 2, rectangle.Top + rectangle.Height / 2);
+
+        public static Rectangle MoveToHavePointInCenter(this Rectangle rectangle, Point center)
+        {
+            var newLocation = center - new Size(rectangle.Width / 2, rectangle.Height / 2);
+            return new Rectangle(newLocation, rectangle.Size);
+        }
     }
 }
