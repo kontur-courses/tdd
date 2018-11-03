d4f1555760ce7ccbbc52c9d842ac39100d3cc1c3
Avel7884 <Avel7884@users.noreply.github.com>
2018-11-04 00:06:43 +0500

    Stage1.Tests

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    internal class CircularCloudLayouter_Should
+    {
+        private CircularCloudLayouter layouter;
+        private Point center;
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(120,150);
+            layouter = new CircularCloudLayouter(center);
+        }
+
+        [Test]
+        public void HaveCorrectCenter()
+        {
+            layouter.Center.Should().BeEquivalentTo(center);
+        }
+
+        [Test]
+        public void PutFirstRectangleCenterOnLayoutCenter()
+        {
+            var rect = layouter.PutNextRectangle(new Size(48, 24));
+            var rectCenter = rect.Location + new Size(24,12);
+            rectCenter.Should().BeEquivalentTo(center);
+        }
+    }
+}
