654be95377bc558ad9a2a0ff77ed16523249640a
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2018-11-05 00:42:47 +0500

    First Task: Rectangles in spiral order, tests

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -9,14 +10,44 @@ namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
+        public Point Center { get; }
+        private List<Rectangle> rectangles = new List<Rectangle>();
+        private const int AngleDelta = 1;
+        private const int SpiralWidth = 1;
+        private int currentAngle = 0;
+
+
         public CircularCloudLayouter(Point center)
         {
-
+            Center = center;
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            return Rectangle.Empty;
+            var rect = new Rectangle();
+            if (rectangles.Count == 0)
+            {
+                rect = new Rectangle(Center, rectangleSize);
+            }
+            else
+            {
+                rect = new Rectangle(GetCurrentSpiralPosition(), rectangleSize);
+                while (rect.IntersectsWithAnyFrom(rectangles))
+                {
+                    currentAngle += AngleDelta;
+                    rect = new Rectangle(GetCurrentSpiralPosition(), rectangleSize);
+                }
+            }
+
+            rectangles.Add(rect);
+            return rect;
+        }
+
+        private Point GetCurrentSpiralPosition()
+        {
+            var x = Center.X + (int)(SpiralWidth * currentAngle * Math.Cos(currentAngle));
+            var y = Center.Y + (int)(SpiralWidth * currentAngle * Math.Sin(currentAngle));
+            return new Point(x, y);
         }
     }
 }
--- cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -1,4 +1,9 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using FluentAssertions;
 using NUnit.Framework;
 
 
@@ -7,6 +12,75 @@ namespace TagsCloudVisualization
     [TestFixture]
     public class CircularCloudLayouter_Should
     {
-        
+        private Point center;
+        private CircularCloudLayouter layouter;
+        private readonly Size defaultSize = new Size(2, 4);
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(0, 1);
+            layouter = new CircularCloudLayouter(center);
+        }
+
+        [Test]
+        public void HaveCenter_AfterCreation()
+        {
+            layouter.Center.Should().Be(center);
+        }
+
+        [Test]
+        public void NotChangeCenter_AfterPuttingNewRectangle()
+        {
+            layouter.PutNextRectangle(defaultSize);
+            layouter.Center.Should().Be(center);
+        }
+
+        [Test]
+        public void ReturnSameSizeRectangleAsGiven_WhenPuttingNewRectangle()
+        {
+            var rect = layouter.PutNextRectangle(defaultSize);
+            rect.Size.Should().Be(defaultSize);
+        }
+
+        [Test]
+        public void PutFirstRectangleInCenter()
+        {
+            var rect = layouter.PutNextRectangle(defaultSize);
+            rect.Location.Should().Be(center);
+        }
+
+        [Test]
+        public void NotHaveIntersectedRectangles()
+        {
+            var rects = AssignLayouter(Enumerable.Repeat(defaultSize, 10)).ToArray();
+
+            var rectPairs = rects
+                .Select((rectangle1, i) => rects
+                    .Skip(i + 1)
+                    .Select(rectangle2 => Tuple.Create(rectangle1, rectangle2)))
+                .SelectMany(rectanglePairs => rectanglePairs.ToArray());
+
+            foreach (var rectPair in rectPairs)
+                rectPair.Item1.IntersectsWith(rectPair.Item2).Should().BeFalse(rectPair.Item1 + rectPair.Item2.ToString());
+        }
+
+        [Test]
+        public void PlaceFirstRectInARelativeCenter()
+        {
+            var rects = AssignLayouter(Enumerable.Repeat(defaultSize, 30)).ToArray();
+            var middleX = (float)rects.Sum(rect => rect.X) / rects.Length;
+            var middleY = (float)rects.Sum(rect => rect.Y) / rects.Length;
+            var relativeCenter = new PointF(middleX, middleY);
+            var distanceToFirst = GetDistance(relativeCenter, rects.First().Location);
+            foreach (var rectangle in rects.Skip(1))
+                GetDistance(relativeCenter, rectangle.Location).Should().BeGreaterOrEqualTo(distanceToFirst);
+        }
+
+        private IEnumerable<Rectangle> AssignLayouter(IEnumerable<Size> sizes) =>
+            sizes.Select(size => layouter.PutNextRectangle(size));
+
+        private double GetDistance(PointF point1, Point point2) =>
+            Math.Sqrt((point1.X - point2.X) * (point1.X - point2.X) + (point1.Y - point2.Y) * (point1.Y - point2.Y));
     }
 }
