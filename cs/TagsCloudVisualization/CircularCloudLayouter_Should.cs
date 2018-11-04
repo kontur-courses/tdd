774d600bfbc69447bab25979924f60619613ee14
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2018-11-04 18:57:56 +0500

    Project created, CircularCloudLayouter added

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -0,0 +1,12 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        
+    }
+}
