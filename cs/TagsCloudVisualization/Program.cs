01abc149ad0345121543d50b37b6bf828d910a92
kadukm <mk.shark25@gmail.com>
2018-11-06 00:15:46 +0500

    Add RectanglesVisualizer and some examples of its work

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            var rectangles = new List<Rectangle>();
+            var random = new Random();
+            for (var i = 0; i < 100; i++)
+            {
+                var randomSize = new Size(random.Next(10, 20) + i, random.Next(20, 30) + i);
+                var newRectangle = layouter.PutNextRectangle(randomSize);
+                rectangles.Add(newRectangle);
+            }
+            var resBitmap = RectanglesVisualizer.Visualize(rectangles);
+            resBitmap.Save("100 rectangles from small to big.bmp");
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/RectanglesVisualizer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class RectanglesVisualizer
+    {
+        public static Bitmap Visualize(IReadOnlyCollection<Rectangle> rectangles)
+        {
+            var (bitmapSize, offsetFromCenter) = GetNecessarySizeForBitmapAndOffset(rectangles);
+            var center = new Point(bitmapSize.Width / 2, bitmapSize.Height / 2);
+            var offsetForRectangles = center.Add(offsetFromCenter);
+
+            var canvas = new Bitmap(bitmapSize.Width, bitmapSize.Height);
+            var penForRectangles = new Pen(Color.Blue, 1);
+            using (var graphics = Graphics.FromImage(canvas))
+            {
+                graphics.Clear(Color.Black);
+                foreach (var rectangle in rectangles)
+                {
+                    var newRectangle = new Rectangle(rectangle.Location.Add(offsetForRectangles), rectangle.Size);
+                    graphics.DrawRectangle(penForRectangles, newRectangle);
+                }
+            }
+
+            return canvas;
+        }
+
+        private static (Size, Point) GetNecessarySizeForBitmapAndOffset(IReadOnlyCollection<Rectangle> rectangles)
+        {
+            var leftTopCorner = new Point(int.MaxValue, int.MaxValue);
+            var rightBottomCorner = new Point(int.MinValue, int.MinValue);
+
+            foreach (var rectangle in rectangles)
+            {
+                if (rectangle.Bottom > rightBottomCorner.Y)
+                    rightBottomCorner.Y = rectangle.Bottom;
+                if (rectangle.Right > rightBottomCorner.X)
+                    rightBottomCorner.X = rectangle.Right;
+                if (rectangle.Top < leftTopCorner.Y)
+                    leftTopCorner.Y = rectangle.Top;
+                if (rectangle.Left < leftTopCorner.X)
+                    leftTopCorner.X = rectangle.Left;
+            }
+
+            var width = rightBottomCorner.X - leftTopCorner.X + 1;
+            var height = rightBottomCorner.Y - leftTopCorner.Y + 1;
+            var dx = (rightBottomCorner.X + leftTopCorner.X + 1) / 2;
+            var dy = (rightBottomCorner.Y + leftTopCorner.Y + 1) / 2;
+            return (new Size(width, height), new Point(-dx, -dy));
+        }
+    }
+}
