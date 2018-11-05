8a255ba9fe25261357326f5aca45f91ffcccf11f
insomnia* <budljanskiy@mail.ru>
2018-11-05 12:25:51 +0500

    Layout and Spiral logic fix;

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -9,18 +9,40 @@ namespace TagsCloudVisualization
     {
         private List<Rectangle> Rectangles;
         private readonly Spiral spiral;
+        private readonly Point center = new Point(0,0);
 
         public CircularCloudLayouter()
         {
             Rectangles = new List<Rectangle>();
-            spiral = new Spiral(this);
+            spiral = new Spiral();
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            var rectangle = spiral.GenerateNewRectangle(Rectangles, rectangleSize);
+            var rectangle = GenerateNewRectangle(Rectangles, rectangleSize);
             Rectangles.Add(rectangle);
             return rectangle;
+        }
+
+        private Rectangle GenerateNewRectangle(List<Rectangle> rectangles, Size rectangleSize)
+        {
+            Rectangle rectangle;
+            if (rectangles.Count == 0)
+                return new Rectangle(center.ShiftToLeftRectangleCorner(rectangleSize), rectangleSize);
+            while (true)
+            {
+                var rectangleCenterPointLocation = spiral.GenerateRectangleLocation();
+                var rectangleLocation = rectangleCenterPointLocation.ShiftToLeftRectangleCorner(rectangleSize);
+                rectangle = new Rectangle(rectangleLocation, rectangleSize);
+                if (RectanglesDoNotIntersect(rectangles, rectangle))
+                    break;
+            }
+            return rectangle;
+        }
+
+        private bool RectanglesDoNotIntersect(List<Rectangle> rectangles, Rectangle newRectangle)
+        {
+            return !(rectangles.Any(newRectangle.IntersectsWith));
         }
     }
 
--- cs/TagsCloudVisualization/Spiral.cs
+++ cs/TagsCloudVisualization/Spiral.cs
@@ -7,21 +7,14 @@ namespace TagsCloudVisualization
 {
     public class Spiral
     {
-        private CircularCloudLayouter layouter;
         private int currentSpiralAngle;
-        private readonly Point center = new Point(0, 0);
-
-        public Spiral(CircularCloudLayouter layouter)
-        {
-            this.layouter = layouter;
-        }
 
         private void IncreaseSpiralAngle()
         {
             currentSpiralAngle++;
         }
 
-        private Point GenerateRectangleLocation()
+        public Point GenerateRectangleLocation()
         {
             //For generating rectangle location (left-upper corner)
             //I'm using Archimedean Spiral
@@ -31,31 +24,8 @@ namespace TagsCloudVisualization
 
             var x = (int)(radius * Math.Cos(currentSpiralAngle));
             var y = (int)(radius * Math.Sin(currentSpiralAngle));
-
+            IncreaseSpiralAngle();
             return new Point(x,y);
-        }
-
-        public Rectangle GenerateNewRectangle(List<Rectangle> rectangles, Size rectangleSize)
-        {
-            Rectangle rectangle;
-            if (rectangles.Count == 0)
-                return new Rectangle(center.ShiftToLeftRectangleCorner(rectangleSize), rectangleSize);
-            while (true)
-            {
-                var rectangleCenterPointLocation = GenerateRectangleLocation();
-                var rectangleLocation = rectangleCenterPointLocation.ShiftToLeftRectangleCorner(rectangleSize);
-                rectangle = new Rectangle(rectangleLocation, rectangleSize);
-                if (RectanglesDoNotIntersect(rectangles, rectangle))
-                    break;
-                IncreaseSpiralAngle();
-            }
-            return rectangle;
-        }
-
-
-        private bool RectanglesDoNotIntersect(List<Rectangle> rectangles, Rectangle newRectangle)
-        {
-            return !(rectangles.Any(newRectangle.IntersectsWith));
         }
 
 
