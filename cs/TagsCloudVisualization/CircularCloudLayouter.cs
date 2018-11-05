c2b5429807b973929ccaaa124731eb66b9080840
Andrew <a.a.belousov@uspu.su>
2018-11-05 12:31:55 +0500

    Init TagsCloudVisualization project

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter : ICircularCloudLayouter
+    {
+
+        public CircularCloudLayouter(Point center)
+        {
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/ICircularCloudLayouter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    interface ICircularCloudLayouter
+    {
+        Rectangle PutNextRectangle(Size rectangleSize);
+    }
+}
