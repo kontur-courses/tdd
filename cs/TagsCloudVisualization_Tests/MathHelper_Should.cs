31034d822f4948920901b3a02dc951a3c39ac66a
Arina <pada190814@gmail.com>
2018-11-07 20:29:27 +0500

    added tests for MathHelper class

---
+++ cs/TagsCloudVisualization_Tests/MathHelper_Should.cs
@@ -0,0 +1,15 @@
+﻿using TagsCloudVisualization;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization_Tests
+{
+    [TestFixture]
+    public class MathHelper_Should
+    {
+        [TestCase(1, 2, ExpectedResult = 2, TestName = "Positive values")]
+        [TestCase(1, -2, ExpectedResult = -2, TestName = "Different sign values")]
+        [TestCase(-1, -2, ExpectedResult = -2, TestName = "Negative values")]
+        public int MaxAbs_ReturnCorrectVal(int val1, int val2) => MathHelper.MaxAbs(val1, val2);
+    }
+}
