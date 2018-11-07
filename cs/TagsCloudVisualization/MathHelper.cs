bcc0be39748f12180944d5b6bdd4b514ba1fabea
Arina <pada190814@gmail.com>
2018-11-07 20:30:14 +0500

    added MathHelper class

---
+++ cs/TagsCloudVisualization/MathHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public static class MathHelper
+    {
+        public static int MaxAbs(int val1, int val2) =>
+            Math.Abs(val1) == Math.Max(Math.Abs(val1), Math.Abs(val2)) ? val1 : val2;
+    }
+}
