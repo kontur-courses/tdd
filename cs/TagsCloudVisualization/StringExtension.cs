04509bba9776d9d5b7ba3adceb36f5dda0184740
Arina <pada190814@gmail.com>
2018-11-09 01:40:57 +0500

    added classes for words layout

---
+++ cs/TagsCloudVisualization/StringExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TagsCloudVisualization
+{
+    public static class StringExtension
+    {
+        public static Size GetSurroundingRectangleSize(this string text, Font font) => TextRenderer.MeasureText(text, font);
+
+        public static Dictionary<string, int> GetFrequency(this string text) =>
+            string.Concat(text
+                    .Where(c => char.IsLetter(c) || char.IsWhiteSpace(c)))
+                .Split(null)
+                .Where(s => s.Any())
+                .GroupBy(s => s)
+                .OrderByDescending(s => s.Count())
+                .ToDictionary(g => g.Key, g => g.Count());
+    }
+}
