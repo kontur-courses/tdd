87c2ced96e77f586e969a3f9b08d0f1cb0ddd4c6
Turin_nowadays <k.dobrovolsky@gmail.com>
2018-11-08 13:03:02 +0500

    Неверные тесты на CircularCloudLayouter. Подразумевается ограничение размеров изображения.

---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudlayouter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public Point Center { get; set; }
+
+        public CircularCloudLayouter(Point center, Size imageSize)
+        {
+            CheckInputData(center, imageSize);
+
+            Center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+
+            return new Rectangle();
+        }
+
+
+        private static void CheckInputData(Point center, Size imageSize)
+        {
+            if (imageSize.Width <= 0)
+                throw new ArgumentException("image width should be positive");
+            if (imageSize.Height <= 0)
+                throw new ArgumentException("image height should be positive");
+            if (center.X < 0 || center.Y < 0)
+                throw new ArgumentException("both center coordinates should be non-negative");
+            if (center.X >= imageSize.Width || center.Y >= imageSize.Height)
+                throw new ArgumentException("center coordinates are not inside the image");
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/Tests/CircularCloudLayouterTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace TagsCloudVisualization.Tests
+{
+    [TestFixture]
+    public class CircularCloudLayouterTests
+    {
+        [TestCase(1, 1, 0, 1, "image width should be positive",
+            TestName = "FallOn_NotPositiveImageWidth")]
+        [TestCase(1, 1, 1, -1, "image height should be positive",
+            TestName = "FallOn_NotPositiveImageHeight")]
+        [TestCase(-2, -3, 10, 10, "both center coordinates should be non-negative",
+            TestName = "FallOn_NegativeCoordinates")]
+        [TestCase(10, 10, 2, 4, "center coordinates are not inside the image",
+            TestName = "FallOn_CenterCoordinatesOutsideImage")]
+        public void ConstructorIncorrectInput(int centerX, int centerY, int imageWidth, int imageHight, string msg)
+        {
+            Action act = () => new CircularCloudLayouter(new Point(centerX, centerY), new Size(imageWidth, imageHight));
+
+            act.Should().Throw<ArgumentException>()
+                .WithMessage(msg);
+        }
+
+        [Test]
+        public void PutNextRectangle(Size rectangleSize)
+        {
+            
+        }
+    }
+
+
+}
