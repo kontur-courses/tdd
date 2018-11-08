9deb105b9eee9dfaf137cfad480cfc425541d37a
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-11-08 19:32:32 +0500

    Make out rotation angle step as constant

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -11,6 +11,7 @@ namespace TagsCloudVisualization
         private readonly List<Rectangle> placedRectangles = new List<Rectangle>();
         private readonly double shiftOnSpiral = 0.01;
         private int rotationAngle = 0;
+        private const int RotationAngleStep = 1;
 
         public CircularCloudLayouter(Point center) => Center = center;
 
@@ -34,7 +35,7 @@ namespace TagsCloudVisualization
             var dx = Math.Cos(rotationAngle) * rotationAngle * shiftOnSpiral;
             var dy = Math.Sin(rotationAngle) * rotationAngle * shiftOnSpiral;
 
-            rotationAngle += 1;
+            rotationAngle += RotationAngleStep;
 
             var nextX = Center.X + (int) dx;
             var nextY = Center.Y + (int) dy;
