5dc2ee4138d260ca8027d680fa8ca4161e230219
hukopo <hukopo@yandex.ru>
2018-11-10 23:03:10 +0500

    refactor

--- cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -99,9 +99,5 @@ namespace TagsCloudVisualization
             }
         }
     }
-    public static class PointExtensions
-    {
-        public static double CalculateDistanceToPoint(this Point p, Point center) =>
-            Math.Sqrt((p.X - center.X) * (p.X - center.X) + (p.Y - center.Y) * (p.Y - center.Y));
-    }
+    
 }
