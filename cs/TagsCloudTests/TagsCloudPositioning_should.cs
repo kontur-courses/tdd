421cac73a2d059ce4187af49af441e626d86dbfd
Unknown <lamax_99@mail.ru>
2018-11-10 23:57:44 +0500

    Solved Task 3

--- cs/TagsCloudTests/TagsCloudPositioning_should.cs
+++ cs/TagsCloudTests/TagsCloudPositioning_should.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace TagsCloudVisualization
 {
     [TestFixture]
     class TagsCloudPositioning_should
     {
+        private readonly string currentTestFolderPath = TestContext.CurrentContext.TestDirectory;
+        private CircularCloudLayouter currentCircularCloudLayouter;
+
         [Test]
         public void ReturnRectangleAtCenter_WhenAddFirstRectangle()
         {
             var origin = new Point(0, 0);
-            var circularCloudLayouter = new CircularCloudLayouter(origin);
+            currentCircularCloudLayouter = new CircularCloudLayouter(origin);
             var size = new Size(100, 100);
 
-            var rectangle = circularCloudLayouter.PutNextRectangle(size);
+            var rectangle = currentCircularCloudLayouter.PutNextRectangle(size);
 
             rectangle.Center.Should().BeEquivalentTo(origin);
         }
@@ -27,14 +30,14 @@ namespace TagsCloudVisualization
         public void NoOneRectangleShouldIntersectWithOthers_WhenAddMoreThenOne()
         {
             var origin = new Point(0, 0);
-            var circularCloudLayouter = new CircularCloudLayouter(origin);
+            currentCircularCloudLayouter = new CircularCloudLayouter(origin);
             var size = new Size(200, 100);
             var rectangles = new List<Rectangle>();
             var result = false;
 
             for (var i = 0; i < 10; i++)
             {
-                var rectangle = circularCloudLayouter.PutNextRectangle(size);
+                var rectangle = currentCircularCloudLayouter.PutNextRectangle(size);
                 if (result)
                     break;
                 result = rectangle.IsIntersectsWithAnyRect(rectangles);
@@ -48,14 +51,14 @@ namespace TagsCloudVisualization
         public void MustPositioningAsCircle()
         {
             var origin = new Point(0, 0);
-            var circularCloudLayouter = new CircularCloudLayouter(origin);
+            currentCircularCloudLayouter = new CircularCloudLayouter(origin);
             var size = new Size(1, 1);
             var rectangles = new List<Rectangle>();
             const int radius = 11;
 
             for (var i = 0; i < 10; i++)
             {
-                var rectangle = circularCloudLayouter.PutNextRectangle(size);
+                var rectangle = currentCircularCloudLayouter.PutNextRectangle(size);
                 rectangles.Add(rectangle);
             }
 
@@ -71,14 +74,14 @@ namespace TagsCloudVisualization
         public void DenseCloudFormed()
         {
             var origin = new Point(0, 0);
-            var circularCloudLayouter = new CircularCloudLayouter(origin);
+            currentCircularCloudLayouter = new CircularCloudLayouter(origin);
             var size = new Size(1, 1);
             var rectangles = new List<Rectangle>();
             const int radius = 11;
 
             for (var i = 0; i < 10; i++)
             {
-                var rectangle = circularCloudLayouter.PutNextRectangle(size);
+                var rectangle = currentCircularCloudLayouter.PutNextRectangle(size);
                 rectangles.Add(rectangle);
             }
 
@@ -88,6 +91,22 @@ namespace TagsCloudVisualization
                 .Select(Math.Abs)
                 .All(h => h <= radius);
             result.Should().BeTrue();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var testResult = TestContext.CurrentContext.Result.Outcome;
+            var testName = TestContext.CurrentContext.Test.Name;
+            if (Equals(testResult, ResultState.Failure)
+                || Equals(testResult, ResultState.Error))
+            {
+                var dateTime = DateTime.Today.ToString("dd.MM.yyyy HH.mm.ss");
+                var filename = $"[{dateTime}] {testName}.png";
+                var path = Path.Combine(currentTestFolderPath, filename);
+                currentCircularCloudLayouter.Visualize(path);
+                Console.WriteLine($"Tag cloud visualization saved to file {path}");
+            }
         }
     }
 }
