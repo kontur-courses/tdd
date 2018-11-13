e19ea3a8ef4824c88e4f62a00f73dbf54deb376c
Egor <egor.druzhinin97@gmail.com>
2018-11-13 21:14:35 +0500

    Tests for extension methods added

--- cs/TagsCloudVisualization/Extensions/SizeExtensions.cs
+++ cs/TagsCloudVisualization/Extensions/SizeExtensions.cs
@@ -8,8 +8,8 @@ namespace TagsCloudVisualization.Extensions
         public static Size SetRandom(this Size size, int minWidth, int maxWidth, int minHeight, int maxHeight)
         {
             var r = new Random();
-            size.Width = r.Next(3, maxWidth);
-            size.Height = r.Next(3, maxHeight);
+            size.Width = r.Next(minWidth, maxWidth);
+            size.Height = r.Next(minHeight, maxHeight);
             return size;
         }
     }
---
+++ cs/TagsCloudVisualizationTests/SizeShould.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+using TagsCloudVisualization.Extensions;
+
+namespace TagsCloudVisualizationTests
+{
+    [TestFixture]
+    public class SizeShould
+    {
+        [Test]
+        public void HaveRandomValueInBound()
+        {
+            for (var i = 0; i < 15; i++)
+            {
+                var size = new Size().SetRandom(-10, 11, -2, 13);
+
+                size.Width.Should().BeGreaterOrEqualTo(-10).And.BeLessThan(11);
+                size.Height.Should().BeGreaterOrEqualTo(-2).And.BeLessThan(13);
+            }
+        }
+    }
+}
