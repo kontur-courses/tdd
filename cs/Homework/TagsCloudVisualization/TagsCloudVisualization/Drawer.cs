c4eea9e806d79af8037f198046b4fac3bba55182
Ivan Plaksin <zero.netto.o@gmail.com>
2018-11-15 16:26:42 +0500

    Fix "using" problem

--- cs/Homework/TagsCloudVisualization/TagsCloudVisualization/Drawer.cs
+++ cs/Homework/TagsCloudVisualization/TagsCloudVisualization/Drawer.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace TagsCloudVisualization
 {
@@ -8,14 +10,22 @@ namespace TagsCloudVisualization
     {
         public static void DrawAndSaveRectangles(Size canvasSize, List<Rectangle> rectangles, string name, string path = "")
         {
-            var bitmap = new Bitmap(canvasSize.Height, canvasSize.Width);
-            var graphics = Graphics.FromImage(bitmap);
-            graphics.FillRectangle(Brushes.White, 0, 0, 1000, 1000);
-            foreach (var rect in rectangles)
-                graphics.DrawRectangle(Pens.Black, rect);
+            using (var bitmap = new Bitmap(canvasSize.Height, canvasSize.Width))
+            {
+                var graphics = Graphics.FromImage(bitmap);
+                graphics.FillRectangle(Brushes.White, 0, 0, 1000, 1000);
+                foreach (var rect in rectangles)
+                    graphics.DrawRectangle(Pens.Black, rect);
 
-            bitmap.Save($"{path}{name}", ImageFormat.Png);
-            bitmap.Dispose();
+                try
+                {
+                    bitmap.Save($"{path}{name}", ImageFormat.Png);
+                }
+                catch (ExternalException exception)
+                {
+                    Console.Error.WriteLine("Something gone wrong. Check the path correctness.");
+                }
+            }
         }
     }
 }
