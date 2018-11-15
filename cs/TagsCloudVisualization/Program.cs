a799961692b5f1670396dca5b772ece34a7ea306
root <root@DESKTOP-8B3MLSU.localdomain>
2018-11-15 23:41:58 +0500

    add visualize

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,16 @@ namespace TagsCloudVisualization
     {
         public static void Main()
         {
-
+            var layouter = new CircularCloudLayouter(new Point(50, 100));
+            var rectangleLayout = new[]
+            {
+                layouter.PutNextRectangle(new Size(100, 100)),
+                layouter.PutNextRectangle(new Size(100, 100)),
+                layouter.PutNextRectangle(new Size(100, 20))
+            };
+            var image = new RectangleLayoutVisualizer().Vizualize(new Size(700, 700), 
+                rectangleLayout, new Point(50, 100));
+            new ImageSaver().Save(image, "picture.png");
         }
     }
 }
---
+++ cs/TagsCloudVisualization/RectangleLayoutVisualizer.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class RectangleLayoutVisualizer
+    {
+        public Bitmap Vizualize(Size size, Rectangle[] layout, Point layoutCenter)
+        {
+            var imageCenter = new Point(size.Width / 2, size.Height / 2);
+            ShiftAboutImageCenter(layout, layoutCenter, imageCenter);
+
+            var image = new Bitmap(size.Width, size.Height);
+            var graphics = Graphics.FromImage(image);
+
+            graphics.Clear(Color.White);
+            graphics.DrawRectangles(new Pen(Brushes.Black), layout);
+
+            return image;
+        }
+
+        private void ShiftAboutImageCenter(Rectangle[] layout, Point layoutCenter,
+            Point imageCenter)
+        {
+            var offset = (X: imageCenter.X - layoutCenter.X,
+                Y: imageCenter.Y - layoutCenter.Y);
+            for(int i=0; i<layout.Length; i++)
+            {
+                layout[i].X += offset.X;
+                layout[i].Y += offset.Y;
+            }
+        }
+    }
+}
