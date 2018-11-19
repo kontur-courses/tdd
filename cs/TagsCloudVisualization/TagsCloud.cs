c1a8186a1badee550a16b6ee8ea4f2126aa807d1
Ilya Shakov <eliyy4hu@outlook.com>
2018-11-19 19:34:50 +0500

    add TagsCloud class

---
+++ cs/TagsCloudVisualization/TagsCloud.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class TagsCloud
+    {
+        public readonly Point Center;
+        public readonly List<Rectangle> AddedRectangles = new List<Rectangle>();
+
+        public TagsCloud(Point center)
+        {
+            var x = center.X;
+            var y = center.Y;
+            if (x < 0 || y < 0)
+                throw new ArgumentException("Center coordinates should not be negative");
+            Center = center;
+        }
+
+        public TagsCloud(Point center, List<Rectangle> rectangles)
+        {
+            var x = center.X;
+            var y = center.Y;
+            if (x < 0 || y < 0)
+                throw new ArgumentException("Center coordinates should not be negative");
+            AddedRectangles = rectangles;
+        }
+
+        public void AddRectangle(Rectangle rectangle)
+        {
+            AddedRectangles.Add(rectangle);
+        }
+    }
+}
