0baec1dbd5816684432d073154aecb995e7371f1
Gleb <rebrocast2@mail.ru>
2019-10-31 22:49:45 +0500

    Added TagsCloudVisualization project with the necessary basic infrastructure

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public CircularCloudLayouter(Point center)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouter_Should.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization.Tests
+{
+    class CircularCloudLayouter_Should
+    {
+    }
+}
