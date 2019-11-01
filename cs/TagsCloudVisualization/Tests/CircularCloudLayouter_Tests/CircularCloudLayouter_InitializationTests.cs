26d10584ef31a0637361446b624620b43479a890
Gleb <rebrocast2@mail.ru>
2019-11-01 12:59:02 +0500

    Added CircularCloudLayouter initialization tests

--- cs/TagsCloudVisualization/Tests/CircularCloudLayouter_Tests/CircularCloudLayouter_InitializationTests.cs
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouter_Tests/CircularCloudLayouter_InitializationTests.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
 
 namespace TagsCloudVisualization.Tests.CircularCloudLayouter_Tests
 {
     class CircularCloudLayouter_InitializationTests
     {
+        [TestCase(1000, 500)]
+        [TestCase(10000, 2000)]
+        [TestCase(-11000, 20)]
+        [TestCase(6, -12)]
+        [TestCase(-5, -500)]
+        [TestCase(0, 0)]
+        public void ShouldNotThrow_WithAnyArguments(int x, int y)
+        {
+            var point = new Point(x, y);
+            Action act = () => new CircularCloudLayouter(point);
+            act.Should().NotThrow();
+        }
     }
 }
