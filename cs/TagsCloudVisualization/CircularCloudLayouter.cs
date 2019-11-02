6843295827a890d97aaaf1e0b25fe66d720361ad
Sergey Subachev <56560526+SergeySubachev@users.noreply.github.com>
2019-11-02 23:08:30 +0500

    заготовки тестов, заглушки в классе

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,6 +1,29 @@
-﻿namespace TagsCloudVisualization
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
 {
     class CircularCloudLayouter
     {
+        private readonly Point center;
+        public readonly List<Rectangle> Items;
+
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+            Items = new List<Rectangle>();
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            var newItem = new Rectangle(default, rectangleSize);
+            Items.Add(newItem);
+            ReallocRectangles();
+            return newItem;
+        }
+
+        private void ReallocRectangles()
+        {
+        }
     }
 }
--- cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -1,9 +1,51 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace TagsCloudVisualization
 {
     [TestFixture]
     class CircularCloudLayouter_Should
     {
+        private CircularCloudLayouter ccl;
+        private List<Size> sizes;
+
+        [OneTimeSetUp]
+        public void SetUpFixture()
+        {
+            var rnd = new Random();
+
+            sizes = new List<Size>();
+            int count = rnd.Next(10, 50);
+            for (int i = 0; i < count; i++)
+            {
+                int h = rnd.Next(10, 20);
+                int w = h * (int)(2 + 8 * rnd.NextDouble());
+                sizes.Add(new Size(w, h));
+            }
+
+            var center = new Point(rnd.Next(-100, 100), rnd.Next(-100, 100));
+            ccl = new CircularCloudLayouter(center);
+            sizes.ForEach(s => ccl.PutNextRectangle(s));
+        }
+
+        [Test]
+        public void AllocateRectanglesWithoutIntersects()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void BeLikeCircle()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void BeCompact()
+        {
+            Assert.IsTrue(false);
+        }
     }
 }
