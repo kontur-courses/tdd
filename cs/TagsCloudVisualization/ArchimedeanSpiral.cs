fa3fb08aa357d6dfb5ec207ca264539e25564e81
JDreyka <belous860@gmail.com>
2019-11-03 17:29:06 +0500

    Create ArchimedeanSpiral

---
+++ cs/TagsCloudVisualization/ArchimedeanSpiral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    class ArchimedeanSpiral : ISpiral
+    {
+        public IEnumerable<Point> GetPoints()
+        {
+            double defaultStep = 0.05;
+            double t = 0;
+            double r = 1;
+            while (true)
+            {
+                t += defaultStep * 2 * r;
+                yield return new Point((int)(1.5 * t * Math.Cos(t)), (int)(t * Math.Sin(t)));
+            }
+        }
+    }
+}
