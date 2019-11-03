676274d08d41a7c9591d7527476727f55316dfb6
Gleb <rebrocast2@mail.ru>
2019-11-03 20:19:30 +0500

    Added Interfaces folder and added IFigure interface

---
+++ cs/TagsCloudVisualization/Interfaces/IFigure.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Interfaces
+{
+    interface IFigure
+    {
+        Point Center { get; set; }
+        Point Location { get; set; }
+        bool Contains(Point point);
+        bool IntersectsWith(IFigure figure);
+    }
+}
