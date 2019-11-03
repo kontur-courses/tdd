c1d6c7b5109f268a381b2cc8447dde2247cb6518
JDreyka <belous860@gmail.com>
2019-11-03 17:08:11 +0500

    Create RectangularSpiral
    Created  RectangularSpiral and tests for it

---
+++ cs/TagsCloudVisualization/RectangularSpiral.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    class RectangularSpiral : ISpiral
+    {
+        static Point[] directions = new Point[]
+        {
+            new Point(0, 1),
+            new Point(1, 0),
+            new Point(0, -1),
+            new Point(-1, 0),
+        };
+
+        public IEnumerable<Point> GetPoints()
+        {
+            var stepNumber = 0;
+            var curentPosition = new Point(0, 0);
+            yield return curentPosition;
+
+            while (true)
+            {
+                var countStepsInCurentDirection = (stepNumber / 2) + 1;
+                var curentDirection = directions[stepNumber % 4];
+                for (int i = 0; i < countStepsInCurentDirection; i++)
+                {
+                    curentPosition = new Point(curentPosition.X + curentDirection.X, curentPosition.Y + curentDirection.Y);
+                    yield return curentPosition;
+                }
+                stepNumber += 1;
+            }
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/RectangularSpiral_should.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    class RectangularSpiral_should
+    {
+        RectangularSpiral rectangularSpiral;
+
+        [SetUp]
+        public void SetUp() => rectangularSpiral = new RectangularSpiral();
+
+        [Test]
+        public void GetPoints_ShouldBeReturnFirstPosition_InZeroZero()
+        {
+            rectangularSpiral.GetPoints().First().Should().Be(new Point(0, 0));
+        }
+
+        [Test]
+        public void GetPoints_ShouldBeReturn_SecondPointOnUpDirection()
+        {
+            var upDirection = new Point(0, 1);
+            var firstSpiralPoint = rectangularSpiral.GetPoints().First();
+            var expectedSecondPosition = 
+                new Point(
+                    firstSpiralPoint.X + upDirection.X,
+                    firstSpiralPoint.Y + upDirection.Y
+                );
+
+            rectangularSpiral.GetPoints().Take(2).Last().Should().Be(expectedSecondPosition);
+        }
+
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        public void GetPoints_ShouldtBeReturnPoints_NotRepeated(int countPoints)
+        {
+            var spiralPoints = rectangularSpiral.GetPoints().Take(countPoints).ToList();
+
+            var points = new HashSet<Point>(spiralPoints);
+
+            points.Count.Should().Be(countPoints);
+        }
+
+        [Test]
+        public void GetPoints_ShouldtBeReturnPoints_OnСlockwiseDirections()
+        {
+            var expectedDirections = new List<Point>
+            {
+                new Point(0,1),
+                new Point(1,0),
+                new Point(0,-1),
+                new Point(-1, 0),
+            };
+
+            var actualDirections = new List<Point>();
+
+            Point previousPoint = Point.Empty;
+            Point curentPoint = Point.Empty;
+            foreach (var point in rectangularSpiral.GetPoints())
+            {
+                if(actualDirections.Count == 4) break;
+                if(previousPoint == curentPoint)
+                {
+                    curentPoint = point;
+                    continue;
+                }
+
+                previousPoint = curentPoint;
+                curentPoint = point;
+
+                var curentDirection = new Point(curentPoint.X - previousPoint.X, curentPoint.Y - previousPoint.Y);
+                if (!actualDirections.Contains(curentDirection)) actualDirections.Add(curentDirection);
+            }
+
+            actualDirections.Should().BeEquivalentTo(expectedDirections);
+        }
+    }
+}
