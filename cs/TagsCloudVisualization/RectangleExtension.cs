cd82d964d9594ff44ceb470aa1062fe5a81e82f1
Shcherbina99 <ej99@example.com>
2019-11-03 17:58:41 +0500

    feat: add GetCenter in Rectangle

---
+++ cs/TagsCloudVisualization/RectangleExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    static class RectangleExtension
+    {
+        public static Point GetCenter(this Rectangle rectangle)
+        {
+            return new Point(rectangle.Left + rectangle.Width/2, rectangle.Top + rectangle.Height / 2);
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/RectangleExtension_should.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization.Tests
+{
+    class RectangleExtension_should
+    {
+        [Test]
+        public void GetCenter_WhenRectangleIsPoint_ReturnThisPoint()
+        {
+            var point = new Point(5,5);
+            var rectangle = new Rectangle(point, new Size(0, 0));
+            rectangle.GetCenter().Should().Be(point);
+        }
+
+        [Test]
+        public void GetCenter_WhenRectangle_ReturnCenter()
+        {
+            var point = new Point(4, 4);
+            var rectangle = new Rectangle(point, new Size(10, 10));
+            rectangle.GetCenter().Should().Be(new Point(9, 9));
+        }
+
+        [Test]
+        public void DoSomething_WhenSomething()
+        {
+            var point = new Point(4, 4);
+            var rectangle = new Rectangle(point, new Size(0, 10));
+            rectangle.GetCenter().Should().Be(new Point(4, 9));
+        }
+    }
+}
