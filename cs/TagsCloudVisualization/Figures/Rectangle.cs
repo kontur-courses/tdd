f748ca8c8118718ba27493d188f96a245fda3f2a
Gleb <rebrocast2@mail.ru>
2019-11-03 20:20:41 +0500

    Added Figures folder and added Rectangle figure

---
+++ cs/TagsCloudVisualization/Figures/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization.Figures
+{
+    class Rectangle : IFigure
+    {
+        System.Drawing.Rectangle rect;
+
+        public Rectangle(Point location, Size size) => rect = new System.Drawing.Rectangle(location, size);
+        public Rectangle() => rect = new System.Drawing.Rectangle();
+
+
+        public Size Size { get => rect.Size; set => rect.Size = value; }
+        public Point Center
+        {
+            get => new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+            set => rect.Location = new Point(value.X - rect.Width / 2, value.Y - rect.Height / 2);
+        }
+        public Point Location { get => rect.Location; set => rect.Location = value; }
+
+        public bool Contains(Point point) => rect.Contains(point);
+
+        public bool IntersectsWith(IFigure figure)
+        {
+            if (figure is Rectangle rect)
+                return this.rect.IntersectsWith(rect.rect);
+
+            for (var y = this.rect.Top; y <= this.rect.Bottom; y++)
+                for (var x = this.rect.Left; x <= this.rect.Right; x++)
+                    if (figure.Contains(new Point(x, y)))
+                        return false;
+            return true;
+        }
+    }
+}
