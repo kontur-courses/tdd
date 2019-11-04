d959a095c56f14b7d0c4beaaec1c7e15f1ec3ec7
Shcherbina99 <ej99@example.com>
2019-11-04 17:15:00 +0500

    feat: add CircularCloudLayouter

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        public readonly Point Center;
+        private List<Rectangle> Rectangles { get; }
+        private readonly SpiralGenerator generator;
+
+        public CircularCloudLayouter(Point center)
+        {
+            if (center.X < 0 || center.Y < 0)
+                throw new ArgumentException();
+            Center = center;
+            Rectangles = new List<Rectangle>();
+            generator = new SpiralGenerator(Center);
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            if (rectangleSize.Height <= 0 || rectangleSize.Width <= 0)
+                throw new ArgumentException();
+            var newRectangle = PutRectangleOnSpiral(rectangleSize);
+            while (Rectangles.Any(x => x.IntersectsWith(newRectangle)))
+            {
+                newRectangle = PutRectangleOnSpiral(rectangleSize);
+            }
+
+            newRectangle = MoveToCenter(newRectangle);
+            Rectangles.Add(newRectangle);
+            return newRectangle;
+        }
+
+        private Rectangle PutRectangleOnSpiral(Size rectangleSize)
+        {
+            var point = generator.GetNextPoint();
+            return new Rectangle(new Point(point.X - rectangleSize.Width / 2, point.Y - rectangleSize.Height / 2), rectangleSize);
+        }
+
+        private Rectangle MoveToCenter(Rectangle rectangle)
+        {
+            while (true)
+            {
+                var direction = Center - (Size)rectangle.GetCenter();
+
+                var offsetRectangle = new Rectangle(rectangle.Location + new Size(Math.Sign(direction.X), 0), rectangle.Size);
+                if (Rectangles.Any(el => el.IntersectsWith(offsetRectangle)) || offsetRectangle.GetCenter() == Center)
+                    break;
+
+                rectangle = offsetRectangle;
+
+                offsetRectangle = new Rectangle(rectangle.Location + new Size(0, Math.Sign(direction.Y)), rectangle.Size);
+                if (Rectangles.Any(el => el.IntersectsWith(offsetRectangle)))
+                    break;
+
+                rectangle = offsetRectangle;
+            }
+
+            return rectangle;
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouter_should.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter_should
+    {
+
+        private static Point center;
+        private static CircularCloudLayouter layouter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(10, 10);
+            layouter = new CircularCloudLayouter(center);
+        }
+
+        [Test]
+        public void Constructor_WhenInitialized_CenterIsPointInInitialized()
+        {
+            layouter.Center.Should().Be(center);
+        }
+
+        [TestCase(-10, -10)]
+        [TestCase(-10, 10)]
+        [TestCase(10, -10)]
+        public void Constructor_WhenInitializedPointWithNegativeCoords_ThrowArgumentException(int x, int y)
+        {
+            var center = new Point(x, y);
+            Action action = () =>
+            {
+                // ReSharper disable once ObjectCreationAsStatement
+                new CircularCloudLayouter(center);
+            };
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(-5, -5)]
+        [TestCase(-5, 0)]
+        [TestCase(-5, 5)]
+        public void PutNextRectangle_WhenSizeIsNegative_ThrowArgumentException(int width, int height)
+        {
+            Action action = () =>
+            {
+                // ReSharper disable once ObjectCreationAsStatement
+                layouter.PutNextRectangle(new Size(width, height));
+            };
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [Test]
+        public void PutNextRectangle_WhenPutOneRectangle_RectanglesContainThisRectangle()
+        {
+            var size = new Size(4,4);
+            var rectangle = layouter.PutNextRectangle(size);
+            rectangle.GetCenter().Should().Be(center);
+            rectangle.Size.Should().Be(size);
+        }
+
+        [Test]
+        public void DoSomething_WhenSomething()
+        {
+            var size = new Size(5, 5);
+
+            var rectangle1 = layouter.PutNextRectangle(size);
+
+            rectangle1.GetCenter().Should().Be(center);
+            rectangle1.Size.Should().Be(size);
+
+            var rectangle2 = layouter.PutNextRectangle(size);
+
+            rectangle2.IntersectsWith(rectangle1).Should().BeFalse();
+        }
+    }
+}
