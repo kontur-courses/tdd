4afdfed4f8f71844eaf11ad5430f7121fdb8884a
Shcherbina99 <ej99@example.com>
2019-11-04 13:49:36 +0500

    feat: add SpiralGenerator

---
+++ cs/TagsCloudVisualization/SpiralGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class SpiralGenerator
+    {
+        public Point Center;
+        private IEnumerator<Point> Generator { get; }
+        private readonly HashSet<Point> prevPoints = new HashSet<Point>();
+
+        public SpiralGenerator(Point center)
+        {
+            if (center.X < 0 || center.Y < 0)
+                throw new ArgumentException();
+            Center = center;
+            Generator = GetNextPoints().GetEnumerator();
+        }
+
+        public Point GetNextPoint()
+        {
+            Generator.MoveNext();
+            return Generator.Current;
+        }
+
+        public IEnumerable<Point> GetNextPoints()
+        {
+            float angle = 0;
+            const float offsetAngle = (float)(5 * Math.PI / 180);
+            while (true)
+            {
+                var radius = 7 * angle;
+                var point = new Point(
+                    (int)(Center.X + radius * Math.Cos(angle)),
+                    (int)(Center.Y + radius * Math.Sin(angle)));
+                if (!prevPoints.Contains(point))
+                {
+                    prevPoints.Add(point);
+                    yield return point;
+                }
+                angle += offsetAngle;
+            }
+            // ReSharper disable once IteratorNeverReturns
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/SpiralGenerator_should.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization.Tests
+{
+    class SpiralGenerator_should
+    {
+        [TestCase(-1, -1)]
+        [TestCase(1, -1)]
+        [TestCase(-1, 1)]
+        public void Constructor_WhenPointWithNegativeCord_ThrowArgumentException(int x, int y)
+        {
+            var center = new Point(x, y);
+            Action action = () =>
+            {
+                // ReSharper disable once ObjectCreationAsStatement
+                new SpiralGenerator(center);
+            };
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        private static Point center;
+        private static SpiralGenerator generator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(5, 5);
+            generator = new SpiralGenerator(center);
+        }
+
+
+        [Test]
+        public void GetNextPoint_WhenGetOnePoint_ReturnCenterPoint()
+        {
+            generator.GetNextPoint().Should().Be(generator.Center);
+        }
+
+        [Test]
+        public void GetNextPoint_WhenGetTwoPoint_ReturnDifferentPoints()
+        {
+            var first = generator.GetNextPoint();
+            generator.GetNextPoint().Should().NotBe(first);
+        }
+
+        [Test]
+        public void GetNextPoint_WhenGetAnyPoint_ReturnDifferentPoints()
+        {
+            var hash = new HashSet<Point>();
+            for (var i = 0; i < 20; i++)
+            {
+                var point = generator.GetNextPoint();
+                hash.Should().NotContain(point);
+                hash.Add(point);
+            }
+        }
+    }
+}
