8a6ee695e51b8b40f77cd63be20bb5c0005df075
5line5 <sevander777@gmail.com>
2019-11-04 14:12:52 +0600

    visualization was added

---
+++ TagsCloudVisualization/CloudVisualization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    class CloudVisualization
+    {
+        private Bitmap image;
+        public CloudVisualization(int width, int height)
+        {
+            image = new Bitmap(width, height);
+        }
+
+        public Bitmap DrawRectangles(List<Rectangle> rectangles)
+        {
+            var random = new Random();
+            var drowPlace = Graphics.FromImage(image);
+            Color color;
+            foreach(var rectangle in rectangles)
+            {
+                color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                drowPlace.FillRectangle(new SolidBrush(color), rectangle);
+            }
+            return image;
+        }
+    }
+}
