b461facdfbba52c287c300a2fd4d5d1476f2f1cd
CRASHGRIM <32795833+CRASHGRIM@users.noreply.github.com>
2019-11-04 18:33:11 +0500

    silly algorithm begin, without inner angles

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -2,18 +2,76 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using System.Linq;
 
 namespace TagsCloudVisualization
 {
+
     public class CircularCloudLayouter:ITagsCloud
     {
-        CircularCloudLayouter(Point center)
+        private class Angle
         {
+            public Point coordinates;
+            public int xLen;
+            public int yLen;
+            public bool isSquareInAngle;
+            public Angle(Point coord, int xLen, int yLen, bool isSquareInAngle)
+            {
+                this.coordinates = coord;
+                this.xLen = xLen;
+                this.yLen = yLen;
+                this.isSquareInAngle = isSquareInAngle;
+            }
+        }
+
+        private Point center;
+        private List<Rectangle> rectangles;
+        private HashSet<Angle> angles;
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+            this.rectangles = new List<Rectangle>();
+            this.angles = new HashSet<Angle>();
+            angles.Add(new Angle(new Point(0,0), int.MaxValue, int.MaxValue, false));
+            angles.Add(new Angle(new Point(0, 0), int.MaxValue, int.MinValue+1, false));
+            angles.Add(new Angle(new Point(0, 0), int.MinValue + 1, int.MaxValue, false));
+            angles.Add(new Angle(new Point(0, 0), int.MinValue + 1, int.MinValue+1, false));
+            // added four initial angles
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            throw new NotImplementedException();
+            var properAngle = FindMostSuitableAngle(rectangleSize);
+            int xCoord;
+            int yCoord;
+            if (!properAngle.isSquareInAngle)
+            {
+                angles.RemoveWhere(a=>(a.coordinates==properAngle.coordinates && a.xLen==properAngle.xLen && a.yLen==properAngle.yLen));
+                if (properAngle.xLen < 0)
+                    xCoord = properAngle.coordinates.X - rectangleSize.Width;
+                else
+                    xCoord = properAngle.coordinates.X;
+                if (properAngle.yLen < 0)
+                    yCoord = properAngle.coordinates.Y - rectangleSize.Height;
+                else
+                    yCoord = properAngle.coordinates.Y;
+                return new Rectangle(new Point(xCoord, yCoord), rectangleSize);
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        private double distance(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow((a.X - b.X), 2) + Math.Pow((a.Y - center.Y), 2));
+        }
+
+        private Angle FindMostSuitableAngle(Size rectangleSize)
+        {
+            return angles
+                .Where(a=>(Math.Abs(a.xLen)>rectangleSize.Width && Math.Abs(a.yLen)>rectangleSize.Height))
+                .OrderBy(a => distance(a.coordinates, center))
+                .First();
         }
 
     }
--- TagsCloudVisualization/CircularCloudLayouterTests.cs
+++ TagsCloudVisualization/CircularCloudLayouterTests.cs
@@ -3,11 +3,57 @@ using System.Collections.Generic;
 using System.Text;
 using FluentAssertions;
 using NUnit.Framework;
+using System.Drawing;
 
 namespace TagsCloudVisualization
 {
     [TestFixture]
     class CircularCloudLayouterTests
     {
+        [Test]
+        public void Constructor_shouldNotThrowArgumentExceptionOnProperInput()
+        {
+            Action LayouterCreation = () => new CircularCloudLayouter(new Point(0, 0));
+            LayouterCreation.Should().NotThrow<Exception>();
+        }
+        [Test]
+        public void PutNextRectangle_shouldNotThrowArgumentExceptionOnProperInput()
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            Action putNext = ()=>layouter.PutNextRectangle(new Size(1, 1));
+            putNext.Should().NotThrow<Exception>();
+        }
+
+        [Test]
+        public void PutNextRectangleOnce_centerShouldBeInFirstRectangle()
+        {
+            var center = new Point(0, 0);
+            var layouter = new CircularCloudLayouter(center);
+            var rect = layouter.PutNextRectangle(new Size(10, 10));
+            rect.Contains(center).Should().BeTrue();
+        }
+        [Test]
+        public void AddingTwoRectangles_ShouldNotIntersect()
+        {
+            var center = new Point(0, 0);
+            var layouter = new CircularCloudLayouter(center);
+            var rect1 = layouter.PutNextRectangle(new Size(10, 10));
+            var rect2 = layouter.PutNextRectangle(new Size(10, 10));
+            rect1.IntersectsWith(rect2).Should().BeFalse();
+        }
+        [Test]
+        public void PuttingFourEqualRectangles_ShouldMakeRectangleLayout()
+        {
+            var expected = new Rectangle(new Point(-10, -10), new Size(20, 20));
+            var center = new Point(0, 0);
+            var layouter = new CircularCloudLayouter(center);
+            var rectangles = new List<Rectangle>();
+            for (int i=0; i<4;i++)
+                rectangles.Add(layouter.PutNextRectangle(new Size(10, 10)));
+            var actual = new Rectangle();
+            for (int i = 0; i < 4; i++)
+                actual = Rectangle.Union(actual, rectangles[i]);
+            actual.Should().Be(expected);
+        }
     }
 }
