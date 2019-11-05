e7411447dffc5c6f7a0f3a41d2870f0f849e9b6c
sc222 <v.a.shaternikov@gmail.com>
2019-11-05 23:50:31 +0500

    TagsCloudVisualization project setup

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public CircularCloudLayouter(Point center)
+        {
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/CircularCloudLayouter_should.cs
@@ -0,0 +1,10 @@
+using System;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouter_should
+    {
+    }
+}
