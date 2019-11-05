33d9fee7f7b53b78789d4558465245bd56a28344
Pasha0666 <soldatov00009@gmail.com>
2019-11-05 21:34:28 +0500

    Создал проект

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public CircularCloudLayouter(Point center)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            
+        }
+    }
+}
