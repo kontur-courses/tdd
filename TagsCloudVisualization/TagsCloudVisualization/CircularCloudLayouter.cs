d657dd17f38949d4ba1efee28833e7f2297ae999
Pasha0666 <soldatov00009@gmail.com>
2019-11-06 14:09:35 +0500

    Пересоздал проект, подключил все сборки, написал первые тесты

---
+++ TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public CircularCloudLayouter(Point center)
+        {
+            
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            return Rectangle.Empty;
+        }
+    }
+    
+    public class CircularCloudLayouterTests 
+    { 
+        [TestCaseSource(nameof(_CoordinateCenter))] 
+        public void Constructor_DoesNotThrow_WithСorrectСenter(Point center) 
+        { 
+            Action action = () => new CircularCloudLayouter(center); 
+            action.Should().NotThrow(); 
+        }
+
+        private static IEnumerable<TestCaseData> _CoordinateCenter = Enumerable 
+            .Range(-1, 3) 
+            .SelectMany(i => Enumerable 
+                .Range(-1, 3) 
+                .Select(j => new TestCaseData(new Point(i, j)).SetName("{m}: " + $"X = {i}, Y = {j}"))); 
+    }
+}
