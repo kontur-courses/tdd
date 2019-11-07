4b5788e3bd60dafc7687f69b9d24a54695499829
Shcherbina99 <ej99@example.com>
2019-11-07 18:42:09 +0500

    refactor: DrawExample

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -7,64 +7,64 @@ namespace TagsCloudVisualization
 {
     class Program
     {
+        private static readonly Random Random = new Random();
         static void Main(string[] args)
         {
-            DrawFirstExample();
-            DrawSecondExample();
-            DrawThirdExample();
+            DrawExample(FirstFillFunc, "example1");
+            DrawExample(SecondFillFunc, "example2");
+            DrawExample(ThirdFillFunc, "example3");
         }
 
-        public static void DrawFirstExample()
+        public static List<Rectangle> FirstFillFunc(CircularCloudLayouter layouter)
         {
-            var sizeWindow = new Size(1000, 1000);
-            var visualizer = new Visualizer(sizeWindow);
-            var layouter = new CircularCloudLayouter(new Point(sizeWindow.Width/2, sizeWindow.Height/2));
-            var random = new Random();
             var rectangles = new List<Rectangle>();
-            for (int i = 0; i < 300; i++)
+            for (var i = 0; i < 300; i++)
             {
-                var size = new Size(random.Next(10, 50), random.Next(10, 50));
+                var size = new Size(Random.Next(10, 50), Random.Next(10, 50));
                 rectangles.Add(layouter.PutNextRectangle(size));
-
             }
-            visualizer.DrawRectangles(rectangles);
-            visualizer.Save(Path.Combine(Directory.GetCurrentDirectory(), "example1.bmp"));
+
+            return rectangles;
         }
-        public static void DrawSecondExample()
+
+        public static List<Rectangle> SecondFillFunc(CircularCloudLayouter layouter)
         {
-            var sizeWindow = new Size(1400, 1200);
-            var visualizer = new Visualizer(sizeWindow);
-            var layouter = new CircularCloudLayouter(new Point(sizeWindow.Width / 2, sizeWindow.Height / 2));
             var width = 60;
-            var height = 30;
+            const int height = 30;
             var rectangles = new List<Rectangle>();
-            for (int i = 0; i < 500; i++)
+            for (var i = 0; i < 500; i++)
             {
                 if (i % 100 == 0)
                     width -= 5;
                 var size = new Size(width, height);
                 rectangles.Add(layouter.PutNextRectangle(size));
-
             }
-            visualizer.DrawRectangles(rectangles);
-            visualizer.Save(Path.Combine(Directory.GetCurrentDirectory(), "example2.bmp"));
+
+            return rectangles;
         }
 
-        public static void DrawThirdExample()
+        public static List<Rectangle> ThirdFillFunc(CircularCloudLayouter layouter)
         {
-            var sizeWindow = new Size(1000, 1000);
+            var rectangles = new List<Rectangle>();
+            for (var i = 0; i < 100; i++)
+            {
+                var size = new Size(Random.Next(5, 60), Random.Next(5, 60));
+                rectangles.Add(layouter.PutNextRectangle(size));
+            }
+
+            return rectangles;
+        }
+
+        public static void DrawExample(Func<CircularCloudLayouter, List<Rectangle>> fillFunc, string nameExample)
+        {
+            var sizeWindow = new Size(1200, 1200);
             var visualizer = new Visualizer(sizeWindow);
             var layouter = new CircularCloudLayouter(new Point(sizeWindow.Width / 2, sizeWindow.Height / 2));
-            var random = new Random();
-            var rectangles = new List<Rectangle>();
-            for (int i = 0; i < 100; i++)
-            {
-                var size = new Size(random.Next(5, 60), random.Next(5, 60));
-                rectangles.Add(layouter.PutNextRectangle(size));
 
-            }
+            var rectangles = fillFunc(layouter);
+
             visualizer.DrawRectangles(rectangles);
-            visualizer.Save(Path.Combine(Directory.GetCurrentDirectory(), "example3.bmp"));
+            visualizer.Save(Path.Combine(Directory.GetCurrentDirectory(), $"{nameExample}.bmp"));
         }
     }
 }
