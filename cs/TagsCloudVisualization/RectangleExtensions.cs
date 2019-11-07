80d8ce690b71edd1f2636404aed7abb1d0d16c77
Данил Панков <mistertimechip@mail.ru>
2019-11-07 11:54:48 +0300

    Added some handy rectangle extension

---
+++ cs/TagsCloudVisualization/RectangleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    public static class RectangleExtensions
+    {
+        public static Point GetRectangleCenteredLocation(this Rectangle rectangle, Point point)
+        {
+            return new Point(point.X - rectangle.Width / 2, point.Y - rectangle.Height / 2);
+        }
+
+        public static bool IntersectsWithAny(this Rectangle rectangle, IEnumerable<Rectangle> rectangles)
+        {
+            return rectangles.Any(rect => rect.IntersectsWith(rectangle));
+        }
+    }
+}
