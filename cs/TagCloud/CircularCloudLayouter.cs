11a0f97b581bfe6335ca6d7c307103413c0fdb48
IAlmostDeveloper <51601917+IAlmostDeveloper@users.noreply.github.com>
2019-11-07 06:38:02 +0300

    refactor code, added cloud fullness

--- cs/TagCloud/CircularCloudLayouter.cs
+++ cs/TagCloud/CircularCloudLayouter.cs
@@ -8,52 +8,94 @@ namespace TagCloud
 {
     public class ArchimedeanSpiral
     {
-        public ArchimedeanSpiral(int parameter = 1) => this.parameter = parameter;
-        private readonly int parameter;
-        public double Angle { get; set; }
-        public double Step = 1.0;
+        public ArchimedeanSpiral(Point center, int parameter = 1)
+        {
+            this.parameter = parameter;
+            angle = center.X * center.X + center.Y * center.Y;
+        }
 
-        public void IncrementAngle() => Angle += Step;
-        public int GetCurrentX() => (int) Math.Round(parameter * Angle * Math.Sin(Angle));
-        public int GetCurrentY() => (int) Math.Round(parameter * Angle * Math.Cos(Angle));
+        private readonly int parameter;
+        private double angle;
+        private int X => (int) Math.Round(parameter * angle * Math.Sin(angle));
+        private int Y => (int) Math.Round(parameter * angle * Math.Cos(angle));
+
+        public Point GetNewPoint()
+        {
+            Point result = new Point(X, Y);
+            angle += Math.PI / 4;
+            return result;
+        }
     }
 }
 
 public class CircularCloudLayouter
 {
+    private readonly Point center;
     public readonly ArchimedeanSpiral Spiral;
     public readonly HashSet<Rectangle> Rectangles;
-    public Point CurrentCoords;
 
     public CircularCloudLayouter(Point center)
     {
-        Spiral = new ArchimedeanSpiral();
-        CurrentCoords = center;
+        this.center = center;
+        Spiral = new ArchimedeanSpiral(center);
         Rectangles = new HashSet<Rectangle>();
     }
 
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        if (rectangleSize.Width <= 0 || rectangleSize.Height <= 0)
-            throw new ArgumentException();
-        var rect = new Rectangle(CurrentCoords, rectangleSize);
-        AddRectangleToCollection(rect);
-        UpdateCoordinates();
+        CheckCorrectSize(rectangleSize);
+        var rect = new Rectangle(Spiral.GetNewPoint(), rectangleSize);
+        while (!RectangleDoesNotIntersect(rect))
+            rect = new Rectangle(Spiral.GetNewPoint(), rectangleSize);
+        Rectangles.Add(rect);
         return rect;
     }
 
-    private void UpdateCoordinates()
+    private static void CheckCorrectSize(Size rectangleSize)
     {
-        Spiral.IncrementAngle();
-        CurrentCoords = new Point(Spiral.GetCurrentX(), Spiral.GetCurrentY());
-    }
-
-    private void AddRectangleToCollection(Rectangle rect)
-    {
-        if (RectangleDoesNotIntersect(rect))
-            Rectangles.Add(rect);
+        if (rectangleSize.Width <= 0 || rectangleSize.Height <= 0)
+            throw new ArgumentException();
     }
 
     private bool RectangleDoesNotIntersect(Rectangle rectToAdd) =>
         Rectangles.All(rectangle => Rectangle.Intersect(rectToAdd, rectangle).IsEmpty);
+
+    public double GetCloudFullnessPercent()
+    {
+        var radius = GetFurthestDistance();
+        double cloudCircleSquare = Math.PI * radius * radius;
+        double allRectanglesSquare = GetAllRectanglesSquare();
+        return allRectanglesSquare / cloudCircleSquare;
+    }
+
+    public double GetFurthestDistance()
+    {
+        var furthestDistance = 0.0;
+        foreach (var rect in Rectangles)
+        {
+            var currentDistance = GetDistanceToRectangle(center, rect);
+            if (currentDistance > furthestDistance)
+                furthestDistance = currentDistance;
+        }
+
+        return furthestDistance;
+    }
+
+    public double GetDistanceToRectangle(Point point, Rectangle rect)
+    {
+        var distances = new List<double>
+        {
+            GetDistanceBetweenPoints(point, new Point(rect.Left, rect.Top)),
+            GetDistanceBetweenPoints(point, new Point(rect.Left, rect.Bottom)),
+            GetDistanceBetweenPoints(point, new Point(rect.Right, rect.Top)),
+            GetDistanceBetweenPoints(point, new Point(rect.Right, rect.Bottom))
+        };
+        return distances.Max();
+    }
+
+    public double GetDistanceBetweenPoints(Point from, Point to) =>
+        Math.Sqrt((to.X - from.X) * (to.X - from.X) + (to.Y - from.Y) * (to.Y - from.Y));
+
+    private double GetAllRectanglesSquare()
+        => Rectangles.Sum(rect => rect.Width * rect.Height);
 }
--- cs/TagCloud/CircularCloudLayouter_Should.cs
+++ cs/TagCloud/CircularCloudLayouter_Should.cs
@@ -9,38 +9,26 @@ namespace TagCloud
     {
         [Test]
         public void ArchimedeanSpiral_GetCurrentX_ShouldReturnZeroAtStart() =>
-            new ArchimedeanSpiral().GetCurrentX().Should().Be(0);
+            new ArchimedeanSpiral(new Point(0, 0)).GetNewPoint().X.Should().Be(0);
 
         [Test]
         public void ArchimedeanSpiral_GetCurrentY_ShouldReturnZeroAtStart() =>
-            new ArchimedeanSpiral().GetCurrentY().Should().Be(0);
-
-        [Test]
-        public void ArchimedeanSpiral_Angle_ShouldBeZeroAtStart() =>
-            new ArchimedeanSpiral().Angle.Should().Be(0);
+            new ArchimedeanSpiral(new Point(0, 0)).GetNewPoint().Y.Should().Be(0);
 
         [Test]
         public void ArchimedeanSpiral_GetCurrentX_ShouldReturnCorrectX()
         {
-            var spiral = new ArchimedeanSpiral();
-            spiral.IncrementAngle();
-            spiral.GetCurrentX().Should().Be(1);
+            var spiral = new ArchimedeanSpiral(new Point(0, 0));
+            spiral.GetNewPoint();
+            spiral.GetNewPoint().X.Should().Be(1);
         }
 
         [Test]
         public void ArchimedeanSpiral_GetCurrentY_ShouldReturnCorrectY()
         {
-            var spiral = new ArchimedeanSpiral();
-            spiral.IncrementAngle();
-            spiral.GetCurrentY().Should().Be(1);
-        }
-
-        [Test]
-        public void ArchimedeanSpiral_IncrementAngle_ShouldIncrement()
-        {
-            var spiral = new ArchimedeanSpiral();
-            spiral.IncrementAngle();
-            spiral.Angle.Should().Be(spiral.Step);
+            var spiral = new ArchimedeanSpiral(new Point(0, 0));
+            spiral.GetNewPoint();
+            spiral.GetNewPoint().Y.Should().Be(1);
         }
 
         [Test]
@@ -78,28 +66,64 @@ namespace TagCloud
         }
 
         [Test]
-        public void PutNextRectangle_AddingRect_ShouldIncrementAngle()
-        {
-            var layouter = new CircularCloudLayouter(new Point(0, 0));
-            layouter.PutNextRectangle(new Size(5, 6));
-            layouter.Spiral.Angle.Should().Be(layouter.Spiral.Step);
-        }
-
-        [Test]
         public void PutNextRectangle_AddingRect_ShouldSetNewCurrentCoords()
         {
             var layouter = new CircularCloudLayouter(new Point(0, 0));
             layouter.PutNextRectangle(new Size(5, 6));
-            layouter.CurrentCoords.ShouldBeEquivalentTo(new Point(1, 1));
+            layouter.Spiral.GetNewPoint().ShouldBeEquivalentTo(new Point(1, 1));
         }
 
-        [Test]
-        public void PutNextRectangle_ShouldNotAddIntersectingRect()
+        [TestCase(0, 0, 0, 0, 0.0)]
+        [TestCase(0, 0, 2, 2, 2.8284271247461903)]
+        [TestCase(0, 0, 1, 3, 3.1622776601683795)]
+        [TestCase(0, 0, -3, 2, 3.6055512754639891)]
+        [TestCase(1, 1, 1, 1, 0)]
+        [TestCase(1, 1, 1, -1, 2)]
+        [TestCase(1, 1, 4, 1, 3)]
+        [TestCase(1, 1, 4, -1, 3.6055512754639891)]
+        [TestCase(-5, 13, 4, 6, 11.40175425099138)]
+        [TestCase(4, 6, -5, 13, 11.40175425099138)]
+        public void GetDistanceBetweenPoints_Should(int xFrom, int yFrom, int xTo, int yTo, double expected)
         {
             CircularCloudLayouter layouter = new CircularCloudLayouter(new Point(0, 0));
-            layouter.PutNextRectangle(new Size(10, 3));
-            layouter.PutNextRectangle(new Size(5, 4));
-            layouter.Rectangles.Count.Should().Be(1);
+            layouter.GetDistanceBetweenPoints(new Point(xFrom, yFrom), new Point(xTo, yTo)).Should().Be(expected);
+        }
+
+        [TestCase(3, 2, 3.6056)]
+        [TestCase(2, 3, 3.6056)]
+        [TestCase(5, 5, 7.0711)]
+        [TestCase(10, 1, 10.0499)]
+        public void GetFurthestDistance_Should(int width, int height, double expected)
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            layouter.PutNextRectangle(new Size(width, height));
+            Math.Round(layouter.GetFurthestDistance(), 4).Should().Be(expected);
+        }
+
+        [TestCase(1, 0.1469)]
+        [TestCase(10, 0.1528)]
+        public void GetCloudFullness_Should(int rectCount, double expected)
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            for (int i = 0; i < rectCount; i++)
+                layouter.PutNextRectangle(new Size(3, 2));
+            Math.Round(layouter.GetCloudFullnessPercent(), 4).Should().Be(expected);
+        }
+
+        [TestCase(0, 0, 0, 0, -3, -2, 3.6056)]
+        [TestCase(0, 0, 1, 1, -3, -2, 2.2361)]
+        [TestCase(-1, 1, 2, 2, -3, -2, 3.1623)]
+        [TestCase(1, 1, 0, 0, 3, 2, 2.2361)]
+        [TestCase(0, 0, 0, 0, 1, 1, 1.4142)]
+        public void GetDistanceToRectangle_Should(int xFrom, int yFrom, int xTo, int yTo, int width, int height,
+            double expected)
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            Math.Round(
+                    layouter.GetDistanceToRectangle(new Point(xFrom, yFrom),
+                        new Rectangle(new Point(xTo, yTo), new Size(width, height))),
+                    4)
+                .Should().Be(expected);
         }
     }
 }
