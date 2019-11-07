daad874cf25e5f7d15b18fe22cbcec37ab517393
Данил Панков <mistertimechip@mail.ru>
2019-11-07 14:17:32 +0300

    Added visualizer

---
+++ cs/TagsCloudVisualization/CircularCloudLayouterVisualizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouterVisualizer
+    {
+        public void SaveImage(IEnumerable<Rectangle> rectangles, string path)
+        {
+            var bitmap = new Bitmap(700, 700);
+
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                foreach (var rectangle in rectangles)
+                    g.DrawRectangle(Pens.Red, rectangle);
+            }
+            
+            bitmap.Save(path);
+        }
+    }
+}
