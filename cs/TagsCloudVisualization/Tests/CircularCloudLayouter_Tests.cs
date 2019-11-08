703f85688a8af03cf33585cd9d34cb3b1f4ecc1c
Olga Bursina <bursina.olya@yandex.ru>
2019-11-08 23:33:45 +0500

    Update circular cloud layouter

--- cs/TagsCloudVisualization/Tests/CircularCloudLayouter_Tests.cs
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouter_Tests.cs
@@ -1,23 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
+using static TagsCloudVisualization.Tests.CircularCloudLayouter_Tests_AuxiliaryTools;
 
 namespace TagsCloudVisualization.Tests
 {
     [TestFixture]
     public class CircularCloudLayouter_Tests
     {
-        private CircularCloudLayouter cloud;
-        
+        private CircularCloudLayouter Cloud;
+        private List<Rectangle> Rectangles;
+
         [SetUp]
         public void SetUp()
         {
-            cloud = new CircularCloudLayouter(new Point(50, 50));
+            Cloud = new CircularCloudLayouter(new Point(500, 500));
+            Rectangles = new List<Rectangle>();
         }
-        
+
         [Test]
         public void CircularCloudLayouterCtor_ValidParameter_ShouldNotThrowException()
         {
@@ -28,80 +33,86 @@ namespace TagsCloudVisualization.Tests
         [Test]
         public void PutNextRectangle_ValidSize_ShouldNotThrowException()
         {
-            Action act = () => cloud.PutNextRectangle(new Size(50, 50));
+            Action act = () => Cloud.PutNextRectangle(new Size(50, 50));
             act.Should().NotThrow();
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(-5, 1)]
+        [TestCase(10, -78)]
+        [TestCase(-100, -100)]
+        public void PutNextRectangle_InvalidSize_ShouldThrowException(int width, int height)
+        {
+            Action act = () => Cloud.PutNextRectangle(new Size(width, height));
+            act.Should().Throw<ArgumentException>();
         }
 
         [Test]
         public void PutNextRectangle_FirstRectangle_ShouldBeInCenter()
         {
-            var rectangle = cloud.PutNextRectangle(new Size(100, 50));
-            rectangle.Location.Should().Be(new Point(0, 25));
+            var rectangle = Cloud.PutNextRectangle(new Size(100, 50));
+            rectangle.Location.Should().Be(new Point(450, 475));
         }
 
-        [Test]
-        public void CircularCloudLayouter_RecatanglesBeforePutting_ShouldBeEmpty()
-        {
-            cloud.Recatangles.Should().BeEmpty();
-        }
-
-        [Test]
-        public void CircularCloudLayouter_RectanglesAfterPutting_ShouldNotBeEmpty()
-        {
-            cloud.PutNextRectangle(new Size(100, 50));
-            cloud.Recatangles.Should().NotBeEmpty();
-        }
-
-        [Test]
-        public void CircularCloudLayouter_RectanglesAfterSomePutting_ShouldContainsSomeElements()
-        {
-            cloud.PutNextRectangle(new Size(100, 50));
-            cloud.PutNextRectangle(new Size(10, 5));
-            cloud.PutNextRectangle(new Size(120, 30));
-            cloud.PutNextRectangle(new Size(170, 5));
-            cloud.PutNextRectangle(new Size(10, 40));
-            cloud.Recatangles.Count.Should().Be(5);
-        }
-        
-        
         [TestCase(10, 15, 15, TestName = "10EqualRectangles")]
         [TestCase(100, 15, 15, TestName = "100EqualRectangles")]
         [TestCase(500, 15, 15, TestName = "500EqualRectangles")]
-        public void CreateCloudWithEqualRectangles_ValidValues_ShouldCreateCloud(int rectanglesCount, int width, int height)
+        public void CreateCloudWithEqualRectangles_ValidValues_ShouldCreateCloud(int rectanglesCount, int width,
+            int height)
         {
-            cloud = new CircularCloudLayouter(new Point(500, 500));
-            
-            cloud.CreateCloudWithEqualRectangles(rectanglesCount, width, height);
-     
-            cloud.Recatangles.Count.Should().Be(rectanglesCount);
+            var rectangles = GetCloudWithEqualRectangles(rectanglesCount, width, height);
+            rectangles.Count.Should().Be(rectanglesCount);
         }
-        
+
         [TestCase(10, 10, 50, 10, 50, TestName = "10DifferentRectangles")]
         [TestCase(100, 10, 50, 10, 50, TestName = "100DifferentRectangles")]
         [TestCase(500, 10, 50, 10, 50, TestName = "500DifferentRectangles")]
         public void CreateCloudWithDifferentRectangles_ValidValues_ShouldCreateCloud(int rectanglesCount, int minWidth,
             int maxWidth, int minHeight, int maxHeight)
         {
-            cloud = new CircularCloudLayouter(new Point(500, 500));
-            
-            cloud.CreateCloudWithDifferentRectangles(rectanglesCount, minWidth, maxWidth, minHeight, maxHeight);
-
-            cloud.Recatangles.Count.Should().Be(rectanglesCount);
+            var rectangles = GetCloudWithDifferentRectangles(rectanglesCount, minWidth, maxWidth, minHeight, maxHeight);
+            rectangles.Count.Should().Be(rectanglesCount);
         }
-        
+
+        [TestCase(100, 10, 50, 10, 50)]
+        [TestCase(300, 15, 20, 10, 60)]
+        [TestCase(500, 10, 20, 20, 30)]
+        public void CircularCloudLayouter_CloudWidthAndHeight_ShouldBeSimilar(int rectanglesCount, int minWidth,
+            int maxWidth, int minHeight, int maxHeight)
+        {
+            var rectangles = GetCloudWithDifferentRectangles(rectanglesCount, minWidth, maxWidth, minHeight, maxHeight);
+            var cloudWidth = GetCloudWidth(rectangles);
+            var cloudHeight = GetCloudHeight(rectangles);
+            Math.Abs(cloudWidth - cloudHeight).Should().BeLessOrEqualTo(Math.Max(maxHeight, maxWidth));
+        }
+
+        [TestCase(100, 15, 25, 15, 25)]
+        [TestCase(100, 10, 50, 10, 50)]
+        [TestCase(300, 15, 20, 10, 60)]
+        [TestCase(500, 10, 20, 20, 30)]
+        public void CircularCloudLayouter_Rectangles_ShouldFormCircle(int rectanglesCount, int minWidth, int maxWidth,
+            int minHeight, int maxHeight)
+        {
+            var rectangles = GetCloudWithDifferentRectangles(rectanglesCount, minWidth, maxWidth, minHeight, maxHeight);
+            var cloudRadius = GetCloudRadius(rectangles);
+            var rectanglesOutOfCircleCount = rectangles.Count(rectangle =>
+                GetDistanceBetweenPoints(rectangle.Location, new Point(500, 500)) > cloudRadius);
+
+            rectanglesOutOfCircleCount.Should().BeLessOrEqualTo((int) (rectanglesCount * 0.03));
+        }
+
         [TearDown]
         public void TearDown()
         {
-            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
-            {
-                var testName = TestContext.CurrentContext.Test.Name;
-                var painter = new Painter(new Size(1000, 1000), cloud);
-                painter.GetMultiColorCloud();
-                var fileName = new StringBuilder(testName).Append("FAILED").ToString();
-                painter.saveImageToDefaultDirectory(fileName);
-                var path = painter.GetImagesPath(fileName);
-                Console.WriteLine($"Tag cloud visualization saved to file {path}");
-            }
+            if (TestContext.CurrentContext.Result.Outcome.Status != TestStatus.Failed)
+                return;
+            var testName = TestContext.CurrentContext.Test.Name;
+            var painter = new Painter(new Size(1000, 1000));
+            var image = painter.GetMultiColorCloud(Rectangles);
+            var fileName = new StringBuilder(testName).Append("FAILED").ToString();
+            Saving.SaveImageToDefaultDirectory(fileName, image);
+            var path = Saving.GetImagesPath(fileName);
+            Console.WriteLine($"Tag cloud visualization saved to file {path}");
         }
     }
 }
