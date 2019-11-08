b924e45dc0ce884a7feeba28d26008a8c8d1e041
Olga Bursina <bursina.olya@yandex.ru>
2019-11-08 23:28:05 +0500

    Added point of entry

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using TagsCloudVisualization.Tests;
+
+namespace TagsCloudVisualization
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var layouter = new CircularCloudLayouter(new Point(250, 250));
+            var rectangles = new List<Rectangle>();
+            for (var i = 0; i < 100; i++)
+                rectangles.Add(layouter.PutNextRectangle(new Size(15, 15)));
+            var painter = new Painter(new Size(500, 500));
+            var image = painter.GetMultiColorCloud(rectangles);
+            Saving.SaveImageToDefaultDirectory("example", image);
+        }
+    }
+}
