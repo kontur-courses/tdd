69173c11ead5d72caedda6a3341178be8b819a19
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-08 14:40:52 +0500

    Refactoring: rendered to file PointExtension

--- cs/TagsCloudVisualization/Axis.cs
+++ cs/TagsCloudVisualization/Axis.cs
@@ -1,17 +1,8 @@
-﻿using System.Drawing;
-
-namespace TagsCloudVisualization
+﻿namespace TagsCloudVisualization
 {
     enum Axis
     {
         X,
         Y
-    }
-
-    static class PointExtension
-    {
-        public static int SelectCoordinatePointAlongAxis(this Point point, Axis axis) => axis == Axis.X ? point.X : point.Y;
-
-        public static Point Add(this Point first, Point second) => new Point(first.X + second.X, first.Y + second.Y);
     }
 }
---
+++ cs/TagsCloudVisualization/PointExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+
+namespace TagsCloudVisualization
+{
+    static class PointExtension
+    {
+        public static int SelectCoordinatePointAlongAxis(this Point point, Axis axis) => axis == Axis.X ? point.X : point.Y;
+
+        public static Point Add(this Point first, Point second) => new Point(first.X + second.X, first.Y + second.Y);
+    }
+}
