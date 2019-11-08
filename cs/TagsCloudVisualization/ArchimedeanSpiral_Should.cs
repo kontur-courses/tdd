c82f374c9771766e72d95058f505e04775bbc95c
etozhedankor <41688602+etozhedankor@users.noreply.github.com>
2019-11-08 12:51:21 +0500

    tests added

--- cs/TagsCloudVisualization/ArchimedeanSpiral_Should.cs
+++ cs/TagsCloudVisualization/ArchimedeanSpiral_Should.cs
@@ -2,7 +2,6 @@
 using System.Drawing;
 using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 
 namespace TagsCloudVisualization
 {
@@ -11,16 +10,32 @@ namespace TagsCloudVisualization
     {
         [TestCase(0, 0)]
         [TestCase(500, 400)]
-        public void ReturnCenterPointFirst_WhenAisZero(int centerX, int centerY)
+        public void ReturnCenterPointFirst_WithZeroA(int centerX, int centerY)
         {
             var center = new Point(centerX, centerY);
-            var archimedeanSpiral = new ArchimedeanSpiral(center);
+            var archimedeanSpiral = new ArchimedeanSpiral(center, a: 0);
             archimedeanSpiral.GetNextPoint().Should().BeEquivalentTo(center);
         }
 
-        [TestCase(5000, 5000, 2, 2, Math.PI * 2, TestName = "onFullCircle")]
-        [TestCase(5000, 5000, 2, 2, Math.PI, TestName = "onHalfCircle")]
-        [TestCase(5000, 5000, 2, 2, -Math.PI, TestName = "onOppositeDirection")]
+        [TestCase(5000, 5000, 2, 4, Math.PI / 6, 5, TestName = "On 5 steps")]
+        [TestCase(5000, 5000, 2, 4, Math.PI / 6, 100, TestName = "On 100 steps")]
+        [TestCase(5000, 5000, 2, 4, Math.PI / 180, 720, TestName = "On 720 steps")]
+        public void ReturnCorrectPoints_OnNextSteps(int centerX, int centerY, double a, double b, double step, int stepsNumber)
+        {
+            var center = new Point(centerX, centerY);
+            var archimedeanSpiral = new ArchimedeanSpiral(center, step, a, b);
+            for (var i = 0; i < stepsNumber; i++)
+            {
+                var nextPoint = archimedeanSpiral.GetNextPoint();
+                var pointRadius = GetDistance(center, nextPoint);
+                var pointPhi = step * i;
+                pointRadius.Should().BeApproximately(GetArchimedeanSpiralRadius(a, b, pointPhi), 1);
+            }
+        }
+
+        [TestCase(5000, 5000, 2, 2, Math.PI * 2, TestName = "On full circle")]
+        [TestCase(5000, 5000, 2, 2, Math.PI, TestName = "On half circle")]
+        [TestCase(5000, 5000, 2, 2, -Math.PI, TestName = "On opposite direction")]
         public void ReturnPointWithGreaterRadius_OnNextStep(int centerX, int centerY, double a, double b, double step)
         {
             var center = new Point(centerX, centerY);
@@ -36,6 +51,11 @@ namespace TagsCloudVisualization
         {
             return Math.Sqrt((firstPoint.X - secondPoint.X) * (firstPoint.X - secondPoint.X) +
                              (firstPoint.Y - secondPoint.Y) * (firstPoint.Y - secondPoint.Y));
+        }
+
+        private static double GetArchimedeanSpiralRadius(double a, double b, double phi)
+        {
+            return a + b * phi;
         }
     }
 }
