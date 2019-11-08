d34d66d3363d160996896559b6a779c2a5068320
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2019-11-08 14:10:28 +0500

    Щур Всеволод

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public readonly Point Center;
+        private readonly List<Rectangle> rectangles = new List<Rectangle>();
+        public IEnumerable<Rectangle> Rectangles => new List<Rectangle>(rectangles);
+
+        private readonly Spiral spiral = new Spiral(0.1);
+        private double spiralParam;
+
+        public CircularCloudLayouter(Point center)
+        {
+            Center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            Rectangle rectangle;
+            if (rectangles.Count == 0)
+            {
+                var location = new Point(Center.X - rectangleSize.Width / 2,
+                    Center.Y - rectangleSize.Height / 2);
+                rectangle = new Rectangle(location, rectangleSize);
+            }
+            else
+            {
+                rectangle = new Rectangle(new Point(0, 0), rectangleSize);
+                PlaceOnSpiral(ref rectangle);
+                SealInLayout(ref rectangle);
+            }
+
+            rectangles.Add(rectangle);
+
+            return rectangle;
+        }
+
+        private void PlaceOnSpiral(ref Rectangle rectangle)
+        {
+            while (RectangleIntersectsWithLayout(rectangle))
+            {
+                var newRectCenter = spiral.Calculate(spiralParam);
+                rectangle.Location = new Point(newRectCenter.X - rectangle.Size.Width / 2,
+                                               newRectCenter.Y - rectangle.Size.Height / 2);
+                spiralParam += 0.1;
+            }
+        }
+
+        private void SealInLayout(ref Rectangle rectangle)
+        {
+            var rectCenter = rectangle.GetCenter();
+            var dx = Math.Sign(Center.X - rectCenter.X);
+            var dy = Math.Sign(Center.Y - rectCenter.Y);
+
+            while (true)
+            {
+                var prevLocation = rectangle.Location;
+                rectangle.X += dx;
+                rectangle.Y += dy;
+                if (!RectangleIntersectsWithLayout(rectangle))
+                    continue;
+                rectangle.Location = prevLocation;
+                break;
+            }
+        }
+
+        private bool RectangleIntersectsWithLayout(Rectangle rectangle)
+        {
+            return rectangles.Select(rectangle.IntersectsWith).Any(r => r);
+        }
+    }
+
+    public class Spiral
+    {
+        private readonly Func<double, Point> func;
+
+        public Spiral(double coefficient)
+        {
+            func = t => new Point(Convert.ToInt32(coefficient * t * Math.Sin(t)),
+                                  Convert.ToInt32(coefficient * t * Math.Cos(t)));
+        }
+
+        public Point Calculate(double param)
+        {
+            return func(param);
+        }
+    }
+
+    public static class RectangleExtensions
+    {
+        public static Point GetCenter(this Rectangle rect)
+        {
+            return new Point(rect.Left + rect.Width / 2,
+                rect.Top + rect.Height / 2);
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        private static CircularCloudLayouter layouter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            layouter = new CircularCloudLayouter(new Point(0, 0));
+        }
+
+        [TestCase(2, 2, ExpectedResult = "{X=-1,Y=-1}", TestName = "OnEvenSize")]
+        [TestCase(1, 1, ExpectedResult = "{X=0,Y=0}", TestName = "OnOddSize")]
+        public string PlaceFirstRectangle_InCenter(int width, int height)
+        {
+            var size = new Size(width, height);
+            var rect = layouter.PutNextRectangle(size);
+            return rect.Location.ToString();
+        }
+
+        [Test]
+        public void PlaceRectangles_WithoutIntersection()
+        {
+            var rect1 = layouter.PutNextRectangle(new Size(1, 1));
+            var rect2 = layouter.PutNextRectangle(new Size(1, 1));
+
+            rect1.IntersectsWith(rect2).Should().Be(false);
+        }
+
+        private static IEnumerable LayouterTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new List<Size>
+                {
+                    new Size(2, 2),
+                    new Size(2, 2),
+                    new Size(2, 2),
+                    new Size(2, 2),
+                    new Size(2, 2),
+                    new Size(2, 2),
+                    new Size(2, 2)
+                });
+            }
+        }
+
+        [Test, TestCaseSource(nameof(LayouterTestCases))]
+        public void PlaceRectangles_CloseToEachOther(IEnumerable<Size> rectSizes)
+        {
+            foreach (var size in rectSizes)
+            {
+                layouter.PutNextRectangle(size);
+            }
+
+            var circleArea = GetСircumscribedСircleArea(layouter);
+            var layoutArea = GetArea(layouter);
+
+            (layoutArea / circleArea).Should()
+                .BeGreaterThan(0.6, $"layout area = {layoutArea}, circle area = {circleArea}");
+        }
+
+        /// <summary>
+        /// Расчитывает площадь описаной вокруг всех прямоугольников окружности
+        /// </summary>
+        private static double GetСircumscribedСircleArea(CircularCloudLayouter layouter)
+        {
+            var maxRadius = layouter.Rectangles.SelectMany(GetCornerPoints)
+                .Select(point => GetDistance(layouter.Center, point))
+                .Max();
+            return Math.PI * maxRadius * maxRadius;
+        }
+
+        private static IEnumerable<Point> GetCornerPoints(Rectangle rectangle)
+        {
+            yield return new Point(rectangle.Left, rectangle.Top);
+            yield return new Point(rectangle.Left, rectangle.Bottom);
+            yield return new Point(rectangle.Right, rectangle.Top);
+            yield return new Point(rectangle.Right, rectangle.Bottom);
+        }
+
+        private static int GetArea(CircularCloudLayouter layouter)
+        {
+            return layouter.Rectangles.Select(rectangle => rectangle.Height * rectangle.Width).Sum();
+        }
+
+        private static double GetDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+    }
+}
