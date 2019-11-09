655ef532aca3a4baeffa501bfd14a0d7dd9365bb
Данил Панков <mistertimechip@mail.ru>
2019-11-09 15:47:44 +0300

    Visualizer now fits cloud to the image

--- cs/TagsCloudVisualization/CircularCloudLayouterVisualizer.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouterVisualizer.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 
 namespace TagsCloudVisualization
 {
@@ -9,10 +11,19 @@ namespace TagsCloudVisualization
         public void SaveImage(IEnumerable<Rectangle> rectangles, string path)
         {
             var bitmap = new Bitmap(700, 700);
+            var enumerable = rectangles.ToList();
+            var minX = enumerable.OrderBy(rect => rect.X).First().X;
+            var minY = enumerable.OrderBy(rect => rect.Y).First().Y;
+            var maxX = enumerable.OrderBy(rect => rect.Right).Last().Right;
+            var maxY = enumerable.OrderBy(rect => rect.Bottom).Last().Bottom;
+            var xDifference = Math.Abs(maxX - minX);
+            var yDifference = Math.Abs(maxY - minY);
 
-            using (Graphics g = Graphics.FromImage(bitmap))
+            using (var g = Graphics.FromImage(bitmap))
             {
-                foreach (var rectangle in rectangles)
+                g.ScaleTransform(700f / xDifference, 700f / yDifference);
+                g.TranslateTransform(-minX, -minY);
+                foreach (var rectangle in enumerable)
                     g.DrawRectangle(Pens.Red, rectangle);
             }
             
