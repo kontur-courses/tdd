97979b1422e42c2efa8da9a40569d551405170b6
Данил Панков <mistertimechip@mail.ru>
2019-11-09 15:49:52 +0300

    Rectangle now offsets to center for better density

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -9,9 +9,11 @@ namespace TagsCloudVisualization
         private readonly ArchimedesSpiral archimedesSpiral;
 
         public List<Rectangle> Rectangles { get; }
+        private Point center;
 
         public CircularCloudLayouter(Point center)
         {
+            this.center = center;
             archimedesSpiral = new ArchimedesSpiral(center, 0.1);
             Rectangles = new List<Rectangle>();
         }
@@ -27,13 +29,30 @@ namespace TagsCloudVisualization
 
         private Rectangle GetNextRectangle(Size rectangleSize)
         {
+            var rectangleToPlace = default(Rectangle);
             foreach (var point in archimedesSpiral.GetSpiralPoints())
             {
-                var rectangleToPlace = new Rectangle(point, rectangleSize).GetCentered(point);
-                if (!rectangleToPlace.IntersectsWithAny(Rectangles))
-                    return rectangleToPlace;
+                var possibleRectangle = new Rectangle(point, rectangleSize).GetCentered(point);
+                if (possibleRectangle.IntersectsWithAny(Rectangles)) continue;
+                rectangleToPlace = GetRectanglePushedCloserToCenter(possibleRectangle);
+                break;
             }
-            return default;
+            return rectangleToPlace;
+        }
+
+        private Rectangle GetRectanglePushedCloserToCenter(Rectangle rectangle)
+        {
+            var rectangleCenter = rectangle.GetCenter();
+            var xDirection = 0;
+            var yDirection = 0;
+            while (!rectangle.IntersectsWithAny(Rectangles) && center.GetDistanceTo(rectangleCenter) > 10)
+            {
+                xDirection = rectangleCenter.X >= center.X ? rectangleCenter.X == center.X ? 0 : -1 : 1;
+                yDirection = rectangleCenter.Y >= center.Y ? rectangleCenter.Y == center.Y ? 0 : -1 : 1;
+                rectangle.Offset(xDirection, yDirection);
+            }
+            rectangle.Offset(-xDirection, -yDirection);
+            return rectangle;
         }
     }
 }
