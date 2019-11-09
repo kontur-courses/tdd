f58e719733893b6d525136307092e83d593a6cbb
Данил Панков <mistertimechip@mail.ru>
2019-11-09 13:09:12 +0300

    Added GetCentered method and made some refactoring

--- cs/TagsCloudVisualization/RectangleExtensions.cs
+++ cs/TagsCloudVisualization/RectangleExtensions.cs
@@ -6,7 +6,7 @@ namespace TagsCloudVisualization
 {
     public static class RectangleExtensions
     {
-        public static Point GetRectangleCenteredLocation(this Rectangle rectangle, Point point)
+        public static Point GetCenteredLocation(this Rectangle rectangle, Point point)
         {
             return new Point(point.X - rectangle.Width / 2, point.Y - rectangle.Height / 2);
         }
@@ -14,6 +14,12 @@ namespace TagsCloudVisualization
         public static bool IntersectsWithAny(this Rectangle rectangle, IEnumerable<Rectangle> rectangles)
         {
             return rectangles.Any(rect => rect.IntersectsWith(rectangle));
+        }
+
+        public static Rectangle GetCentered(this Rectangle rectangle, Point point)
+        {
+            var centeredLocation = GetCenteredLocation(rectangle, point);
+            return new Rectangle(centeredLocation, rectangle.Size);
         }
     }
 }
