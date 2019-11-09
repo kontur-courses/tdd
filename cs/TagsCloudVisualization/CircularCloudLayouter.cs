efbb012dbb32830e7f8d02d1d81c09117b995237
Данил Панков <mistertimechip@mail.ru>
2019-11-09 13:26:21 +0300

    Refactored for better readability and encapsulation

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,48 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Linq;
-using FluentAssertions;
-using NUnit.Framework;
 
 namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
-        public ArchimedesSpiral ArchimedesSpiral { get; }
+        private readonly ArchimedesSpiral archimedesSpiral;
 
         public List<Rectangle> Rectangles { get; }
 
         public CircularCloudLayouter(Point center)
         {
-            ArchimedesSpiral = new ArchimedesSpiral(center, 0.1);
+            archimedesSpiral = new ArchimedesSpiral(center, 0.1);
             Rectangles = new List<Rectangle>();
         }
         
-
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
             if(rectangleSize.IsEmpty)
-                throw new ArgumentException();
-            var rect = new Rectangle(Point.Empty, rectangleSize);
-            var rectangleLocation = GetNextRectangleLocation(rectangleSize);
-            var centeredLocation = rect.GetRectangleCenteredLocation(rectangleLocation);
-            rect.Location = centeredLocation;
-            Rectangles.Add(rect);
-            return rect;
+                throw new ArgumentException("Empty rectangle size");
+            var rectangle = GetNextRectangle(rectangleSize);
+            Rectangles.Add(rectangle);
+            return rectangle;
         }
 
-        private Point GetNextRectangleLocation(Size rectangleSize)
+        private Rectangle GetNextRectangle(Size rectangleSize)
         {
-            foreach (var point in ArchimedesSpiral.GetSpiralPoints())
+            foreach (var point in archimedesSpiral.GetSpiralPoints())
             {
-                var rectangleToPlace = new Rectangle(Point.Empty, rectangleSize);
-                var centeredLocation = rectangleToPlace.GetRectangleCenteredLocation(point);
-                rectangleToPlace.Location = centeredLocation;
+                var rectangleToPlace = new Rectangle(point, rectangleSize).GetCentered(point);
                 if (!rectangleToPlace.IntersectsWithAny(Rectangles))
-                    return point;
+                    return rectangleToPlace;
             }
             return default;
         }
