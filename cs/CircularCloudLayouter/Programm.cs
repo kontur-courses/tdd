93256bf5f7f19af1ce36f52bdedad258395cf9b4
balandin228 <55559705+balandin228@users.noreply.github.com>
2019-11-11 18:00:34 +0500

    add reading from file

--- cs/CircularCloudLayouter/Programm.cs
+++ cs/CircularCloudLayouter/Programm.cs
@@ -1,28 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
 using NUnit.Framework;
-
 
 namespace TagsCloudVisualization
 
 {
     public class Programm
     {
-        public static void GetCloud(int count, int width, int height, int number)
+        public static void GetCloud(CircularCloudLayouter layouter, int width, int height, int number)
         {
             var cloud = new CloudVisualization(width, height);
-            var layouter = new CircularCloudLayouter(new Point(width / 2, height / 2));
-            Random random = new Random();
-            for (int i = 0; i < count; i++)
-                layouter.PutNextRectangle(new Size(random.Next(3, 50), random.Next(3, 50)));
             var result = cloud.DrawRectangles(layouter.Rectangles);
             result.Save(string.Format(@"../../layouter{0}.png", number));
         }
+
+        public static CircularCloudLayouter GetLayouter(int count, Point center)
+        {
+            var layouter = new CircularCloudLayouter(center);
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+                layouter.PutNextRectangle(new Size(random.Next(3, 50), random.Next(3, 50)));
+            return layouter;
+        }
         public static void Main()
         {
-            GetCloud(400,1000,1000,1);
-            GetCloud(200,500,500,2);
+
+            GetCloud(GetLayouter(400, new Point(500, 500)), 1000, 1000, 1);
+            GetCloud(GetLayouter(200, new Point(250, 250)), 500, 500, 2);
+            Console.WriteLine("Введиьте путь к файлу");
+            var path = Console.ReadLine();
+            var sizes = File.ReadAllLines(path)
+                .Select(l => l.Split(' ')
+                    .Select(s => int.Parse(s))
+                    .ToArray())
+                .Select(ar => Tuple.Create(ar[0], ar[1])).ToList();
+            var layouter = new CircularCloudLayouter(new Point(sizes.Count, sizes.Count));
+            foreach (var size in sizes)
+                layouter.PutNextRectangle(new Size(size.Item1, size.Item2));
+            GetCloud(layouter, sizes.Count * 2, sizes.Count * 2, 3);
         }
     }
 }
