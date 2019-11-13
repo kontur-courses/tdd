253d376869df4dd7e498ca521f4d65b0848a3bad
Denis Chernushchenko <NightQnEarth@gmail.com>
2019-11-14 04:38:17 +0500

    Implemented cloud compression.

--- cs/TagsCloudVisualization/CloudLayouters/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CloudLayouters/CircularCloudLayouter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using TagsCloudVisualization.Extensions;
 
 namespace TagsCloudVisualization.CloudLayouters
@@ -18,7 +19,7 @@ namespace TagsCloudVisualization.CloudLayouters
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
             if (rectangleSize.IsEmpty)
-                throw new ArgumentException("Got argument is empty.", nameof(rectangleSize));
+                throw new ArgumentException("Passed argument is empty.", nameof(rectangleSize));
 
             Rectangle newRectangle = Rectangle.Empty;
 
@@ -28,13 +29,13 @@ namespace TagsCloudVisualization.CloudLayouters
 
                 newRectangle = new Rectangle(newRectangleLocation, rectangleSize);
 
-                if (!newRectangle.IntersectsWithAnyOf(rectangles)) break;
+                if (!newRectangle.IntersectsWith(rectangles)) break;
 
                 if (currentArchimedeanSpiralPoint.X <= 0)
                 {
                     newRectangle.Offset(new Point(-rectangleSize.Width, -rectangleSize.Height));
 
-                    if (!newRectangle.IntersectsWithAnyOf(rectangles)) break;
+                    if (!newRectangle.IntersectsWith(rectangles)) break;
                 }
             }
 
@@ -45,9 +46,39 @@ namespace TagsCloudVisualization.CloudLayouters
                 newRectangle.Offset(firstRectangleOffset);
             }
 
+            newRectangle = MoveRectangleToOrigin(newRectangle, rectangles);
+
             rectangles.Add(newRectangle);
 
             return newRectangle.CreateMovedCopy(centralOffset);
+        }
+
+        private static Rectangle MoveRectangleToOrigin(Rectangle rectangle, IReadOnlyCollection<Rectangle> rectangles)
+        {
+            if (rectangle.Contains(0, 0)) return rectangle;
+
+            var xDelta = -Math.Sign(rectangle.X + rectangle.Width / 2);
+            var yDelta = -Math.Sign(rectangle.Y + rectangle.Height / 2);
+
+            var sizes = new[] { new Size(xDelta, 0), new Size(0, yDelta) }.Where(size => !size.IsEmpty);
+
+            for (int i = 0; i < 2; i++)
+                // ReSharper disable once PossibleMultipleEnumeration
+                foreach (var size in sizes)
+                    while (true)
+                    {
+                        var movedRectangle = rectangle.CreateMovedCopy(size);
+                        if (movedRectangle.IntersectsWith(rectangles) ||
+                            XDistanceToCenter(movedRectangle) == 0 && size.Width != 0 ||
+                            YDistanceToCenter(movedRectangle) == 0 && size.Height != 0)
+                            break;
+                        rectangle = movedRectangle;
+                    }
+
+            return rectangle;
+
+            static int XDistanceToCenter(Rectangle rect) => rect.X + rect.Width / 2;
+            static int YDistanceToCenter(Rectangle r) => r.Y + r.Height / 2;
         }
     }
 }
