0e99bd76b716de53e5125a08a229d6542ecf3886
5line5 <sevander777@gmail.com>
2019-11-13 23:49:11 +0600

    fix mistake in visualization

--- TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/CloudVisualization.cs
+++ TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/CloudVisualization.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace TagsCloudVisualization.TagsCloudVisualization
 {
     class CloudVisualization : ITagsCloudVisualization<Rectangle>
     {
-        public Bitmap Draw(IEnumerable<Rectangle> rectangles, int imageWidth, int imageHeight)
+        public Bitmap Draw(IEnumerable<Rectangle> figuresToDraw, int imageWidth, int imageHeight)
         {
             var image = new Bitmap(imageWidth, imageHeight);
             var random = new Random();
             using (var drawPlace = Graphics.FromImage(image))
             {
-                foreach (var rectangle in rectangles)
+                foreach (var rectangle in figuresToDraw.ToList())
                 {
                     var color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
                     drawPlace.FillRectangle(new SolidBrush(color), rectangle);
--- TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/DebugVisualization.cs
+++ TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/DebugVisualization.cs
@@ -6,13 +6,14 @@ namespace TagsCloudVisualization.TagsCloudVisualization
 {
     class DebugVisualization : ITagsCloudVisualization<Rectangle>
     {
-        public Bitmap Draw(IEnumerable<Rectangle> rectangles, int imageWidth, int imageHeight)
+        public Bitmap Draw(IEnumerable<Rectangle> figuresToDraw, int imageWidth, int imageHeight)
         {
             var image = new Bitmap(imageWidth, imageHeight);
             using (var drawPlace = Graphics.FromImage(image))
             {
                 var blackPen = new Pen(new SolidBrush(Color.Black), 3);
                 var redPen = new Pen(new SolidBrush(Color.Red), 3);
+                var rectangles = figuresToDraw.ToList();
                 foreach (var rectangle in rectangles)
                 {
                     var rectangleIntersectsAnother = rectangles.Any(rec => rec != rectangle
