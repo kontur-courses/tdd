3a1671257dba9f9b4392b12a15b4c12f5fa44fc4
Pasha0666 <soldatov00009@gmail.com>
2019-11-14 13:55:33 +0500

    Исправил 3 тест

--- TagsCloudVisualization/TagsCloudVisualization/ExamplesCircularCloud.cs
+++ TagsCloudVisualization/TagsCloudVisualization/ExamplesCircularCloud.cs
@@ -52,7 +52,7 @@ namespace TagsCloudVisualization
         public static void GenerateThirdTagCloud()
         {
             var enumerable = Enumerable.Repeat(new Tuple<string, Font>((6).ToString(), new Font(FONT_FAMILY_NAME, 15)), 5000);
-            GenerateTagCloud(enumerable, new Size(2000, 2000), "3.png");
+            GenerateTagCloud(enumerable, new Size(2400, 2400), "3.png");
         }
     }
 }
