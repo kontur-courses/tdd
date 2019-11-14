f43538fd4056c78eb514bdee527e18500f242c2b
Sergey Subachev <56560526+SergeySubachev@users.noreply.github.com>
2019-11-14 14:25:06 +0500

    public

--- cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -8,7 +8,7 @@ using System.Linq;
 namespace TagsCloudVisualization
 {
     [TestFixture]
-    class CircularCloudLayouter_Should
+    public class CircularCloudLayouter_Should
     {
         private CircularCloudLayouter layouter;
 
