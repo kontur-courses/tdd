08c23147e9cf50065538867bfc44872f3e482a80
neoezop <49761942+neoezop@users.noreply.github.com>
2019-11-18 18:36:04 +0500

    added visualization
    Added visualizer class, changed project type to console application to get pictures easily, added pictures

---
+++ TagsCloudVisualization/TagsCloudVisualization/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagsCloudVisualization
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var layouter = new CircularCloudLayouter(new Point(5000, 5000));
+            var visualizer = new Visualizer();
+            var rectangles = GenerateRectangles(layouter);
+
+            visualizer.Visualize(rectangles, new Size(10000, 10000))
+                .Save("Cloud.png", ImageFormat.Png);
+        }
+
+        private static List<Rectangle> GenerateRectangles(CircularCloudLayouter layouter)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 10000; ++i)
+            {
+                var size = new Size(rnd.Next(10, 100), rnd.Next(10, 100));
+                layouter.PutNextRectangle(size);
+            }
+            return layouter.GetRectangles();
+        }
+    }
+}
---
+++ TagsCloudVisualization/TagsCloudVisualization/Visualizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    class Visualizer
+    {
+        public Bitmap Visualize(List<Rectangle> rectangles, Size imageSize)
+        {
+            var bmp = new Bitmap(imageSize.Width, imageSize.Height);
+            var graphics = Graphics.FromImage(bmp);
+            var rnd = new Random();
+            foreach (var rectangle in rectangles)
+                graphics.FillRectangle(new SolidBrush(GetRandomColor(rnd)), rectangle);
+            return bmp;
+        }
+
+        private Color GetRandomColor(Random rnd)
+        {
+            return Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+        }
+    }
+}
