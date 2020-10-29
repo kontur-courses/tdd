665e47e32eaf266b61ef45d7313cd9a46e80d0a9
PShenica <polebota@gmail.com>
2020-10-29 19:36:39 +0500

    add new class ArchimedeanSpiral and class ArchimedeanSpiralTests

---
+++ cs/TagsCloudVisualization/ArchimedeanSpiral.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class ArchimedeanSpiral
+    {
+        public Point Center { get; }
+        public double Density { get; }
+
+        public ArchimedeanSpiral(Point center, double density)
+        {
+            Center = center;
+
+            if (density <= 0)
+            {
+                throw new ArgumentException("density cant be negative or zero");
+            }
+
+            Density = density;
+        }
+
+        public IEnumerator<PointF> GetNextPoint()
+        {
+            yield return new PointF();
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/ArchimedeanSpiralTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class ArchimedeanSpiralTests
+    {
+        [TestCase(0, 0, 0, TestName = "Density is zero")]
+        [TestCase(0, 0, -1.0, TestName = "Density is negative")]
+        public void ThrowException_When(int x, int y, double density)
+        {
+            var point = new Point(x, y);
+            Func<ArchimedeanSpiral> putRectangle = () => new ArchimedeanSpiral(point, density);
+
+            putRectangle.Should().Throw<ArgumentException>();
+        }
+    }
+}
