df5cc119dad159a29d91ec15bc1ddfb39f2b95df
PShenica <polebota@gmail.com>
2020-10-29 18:16:17 +0500

    add new class CircularCloudLayouterTests and few tests

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
+        private Point Center { get; }
+
+        public CircularCloudLayouter(Point center)
+        {
+            Center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            if (rectangleSize.Height <= 0 || rectangleSize.Width <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return new Rectangle();
+        }
     }
 }
---
+++ cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouterTests
+    {
+        private Point CentrePoint { get; set; }
+        private CircularCloudLayouter CloudLayouter { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CentrePoint = new Point(0, 0);
+            CloudLayouter = new CircularCloudLayouter(CentrePoint);
+        }
+
+        [TestCase(0, 9, TestName = "Width is zero")]
+        [TestCase(-1, 8, TestName = "Width is negative")]
+        [TestCase(10, 0, TestName = "Height is zero")]
+        [TestCase(3, -2, TestName = "Height is negative")]
+        public void PutNextRectangle_ThrowExeption_When(int width, int height)
+        {
+            var size = new Size(width, height);
+            Action putRectangle = () => CloudLayouter.PutNextRectangle(size);
+
+            putRectangle.Should().Throw<ArgumentException>();
+        }
+    }
+}
