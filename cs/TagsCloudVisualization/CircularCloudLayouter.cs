261423b0efc090d2d89c17e90b666779913954cf
ilya.belyaev.student@gmail.com <50026308+CaptainBelyash@users.noreply.github.com>
2020-10-30 15:24:10 +0500

    Create TagCloudVisualization project

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        public CircularCloudLayouter(Point center)
+        {
+            
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/CircularCloudLayouter_should.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouter_should
+    {
+        private CircularCloudLayouter layouter;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            layouter = new CircularCloudLayouter(new Point(10, 10));
+        }
+    }
+}
