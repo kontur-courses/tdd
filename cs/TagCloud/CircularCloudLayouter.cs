710ed28ac55f1a58acc7e477e570c948fe7f1657
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-10-30 23:33:40 +0500

    first realization circle layouter
    use spiral

---
+++ cs/TagCloud/CircularCloudLayouter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+namespace TagCloud
+{
+    class Spiral
+    {
+        private Point Center;
+        private int Step;
+        public Spiral(Point center, int step)
+        {
+            this.Center = center;
+            this.Step = step;
+        }
+
+        public IEnumerable<Point> GetNextPoint
+        {
+            get
+            {
+                var i = 1;
+                var newPoint = Center;
+                yield return Center;
+                while (true)
+                {
+                    var sign = 1;
+                    if (i % 2 == 0)
+                    {
+                        sign = -1;
+                    }
+                    for (var j = 0; j < i; j++)
+                    {
+                        newPoint = new Point(newPoint.X, newPoint.Y+ sign * Step);
+
+                        yield return newPoint;
+                    }
+                    for (var j = 0; j < i; j++)
+                    {
+                        newPoint = new Point(newPoint.X - sign * Step, newPoint.Y);
+
+                        yield return newPoint;
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+    class CircularCloudLayouter
+    {
+        private Point center;
+        private Spiral spiral;
+        private List<Rectangle> rectangles = new List<Rectangle>();
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            spiral = new Spiral(center, 1);
+            var isSuitiablePoint = true;
+            foreach (var point in spiral.GetNextPoint)
+            {
+                isSuitiablePoint = true;
+                var newRectangle = new Rectangle(point, rectangleSize);
+                foreach (var rectangle in rectangles)
+                {
+                    if (rectangle.IntersectsWith(newRectangle))
+                    {
+                        isSuitiablePoint = false;
+                        break;
+                    }
+                }
+
+                if (isSuitiablePoint)
+                {
+                    rectangles.Add(newRectangle);
+                    return newRectangle;
+                }
+            }
+            return new Rectangle(center, rectangleSize);
+        }
+    }
+}
---
+++ cs/TagCloud/CircularCloudLayouterShould.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagCloud
+{
+    [TestFixture]
+    public class CircularCloudLayouterShould
+    {
+        [Test]
+        public void PutNextRectangle_ReturnRectangleWithCorrectSize()
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            var size = new Size(10, 10);
+            layouter.PutNextRectangle(size).Size.Should().Be(size);
+        }
+        
+        [Test]
+        public void PutNextRectangle_ReturnNotIntersectedRectangles()
+        {
+            var layouter  = new CircularCloudLayouter(new Point(0, 0));
+            var firstRectangle = layouter.PutNextRectangle(new Size(10, 10));
+            var secondRectangle = layouter.PutNextRectangle(new Size(10, 10));
+            firstRectangle.IntersectsWith(secondRectangle).Should().BeFalse();
+        }
+
+        [Test]
+        public void PutNextRectangle_ManyRectangles_ReturnNotIntersectRectangles()
+        {
+            var layouter  = new CircularCloudLayouter(new Point(0, 0));
+            var size = new Size(2, 1);
+            var rectangles = new List<Rectangle>();
+            for (var i = 0; i < 10; i++)
+            {
+                rectangles.Add(layouter.PutNextRectangle(size));
+            }
+
+            for (var i = 0; i < rectangles.Count - 1; i++)
+            {
+                for (var j = i + 1; j < rectangles.Count; j++)
+                {
+                    rectangles[i].IntersectsWith(rectangles[j]).Should().BeFalse();
+                }
+            }
+        }
+
+        [Test]
+        public void PutNextRectangles_LayoutRectanglesInCircle()
+        {
+            var center = new Point(0, 0);
+            var layouter  = new CircularCloudLayouter(center);
+            var size = new Size(2, 1);
+            var rectangles = new List<Rectangle>();
+            for (var i = 0; i < 10; i++)
+            {
+                rectangles.Add(layouter.PutNextRectangle(size));
+            }
+
+            foreach (var rectangle in rectangles)
+            {
+                GetDistance(rectangle.Location, center).Should().BeLessThan(5);
+            }
+        }
+        
+        private double GetDistance(Point a, Point b)
+        {
+            return Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+        }
+    }
+}
