8be56c048730906a06158313f298c06799a65716
catsAndSuicide <anisa.hamidullina00@gmail.com>
2020-10-31 18:08:23 +0500

    Добавлен метод MoveToCenter. Теперь облако более плотное. Соответсвующая проверка добавлена.

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -11,6 +11,14 @@ namespace TagsCloudVisualization
         private readonly List<Rectangle> currentRectangles;
         private int currentRadius;
         private double currentAngle;
+        public readonly Tuple<int, int>[] Directions = new Tuple<int, int>[]
+            {
+                new Tuple<int, int>(0, -1),
+                new Tuple<int, int>(0, 1),
+                new Tuple<int, int>(-1, 0),
+                new Tuple<int, int>(1, 0)
+            };
+        public readonly int Shift = 2;
         public CircularCloudLayouter(Point center)
         {
             this.center = center;
@@ -36,7 +44,7 @@ namespace TagsCloudVisualization
                 point = GetNextPoint();
                 newRectangle = new Rectangle(point, rectangleSize);
             }
-            currentRectangles.Add(newRectangle);
+            currentRectangles.Add(MoveToCenter(newRectangle));
             return newRectangle;
         }
 
@@ -59,7 +67,7 @@ namespace TagsCloudVisualization
             return point;
         }
 
-        private bool IntersectWithPreviousRectangles(Rectangle newRectangle)
+        public bool IntersectWithPreviousRectangles(Rectangle newRectangle)
         {
             foreach (var rectangle in currentRectangles)
             {
@@ -67,6 +75,40 @@ namespace TagsCloudVisualization
                     return true;
             }
             return false;
+        }
+
+        public double GetDistanceToCenter(Rectangle rectangle)
+        {
+            var rectCenter = new Point(
+                rectangle.X + rectangle.Width / 2,
+                rectangle.Y + rectangle.Height / 2);
+            return Math.Sqrt((center.X - rectangle.X) ^ 2 + (center.Y - rectCenter.Y) ^ 2);
+        }
+
+        private Rectangle MoveToCenter(Rectangle rectangle)
+        {
+            var resultRectangle = new Rectangle(rectangle.Location, rectangle.Size);
+            Rectangle tempRectangle = new Rectangle(rectangle.Location, rectangle.Size); ;
+            while (true)
+            {
+                var flag = false;
+                foreach (var direction in Directions)
+                {
+                    tempRectangle.X = resultRectangle.X + direction.Item1 * Shift;
+                    tempRectangle.Y = resultRectangle.Y + direction.Item2 * Shift;
+                    if (!IntersectWithPreviousRectangles(tempRectangle) 
+                        && GetDistanceToCenter(tempRectangle) < GetDistanceToCenter(resultRectangle))
+                    {
+                        resultRectangle.X = tempRectangle.X;
+                        resultRectangle.Y = tempRectangle.Y;
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                    break;
+            }
+            return resultRectangle;
         }
     }
 }
--- cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
@@ -27,16 +27,39 @@ namespace TagsCloudVisualization
         public void PutNextRectangle_PutDifferentRectanglesInDifferentPlaces()
         {
             var firstRectangle = layouter.PutNextRectangle(new Size(50, 50));
-            var secondRrectangle = layouter.PutNextRectangle(new Size(50, 50));
-            firstRectangle.Location.Should().NotBeEquivalentTo(secondRrectangle.Location);
+            var secondRectangle = layouter.PutNextRectangle(new Size(50, 50));
+            firstRectangle.Location.Should().NotBeEquivalentTo(secondRectangle.Location);
         }
 
         [Test]
         public void PutNextRectangle_ReturnsNotIntersectedRectangles()
         {
             var firstRectangle = layouter.PutNextRectangle(new Size(50, 50));
-            var secondRrectangle = layouter.PutNextRectangle(new Size(50, 50));
-            firstRectangle.IntersectsWith(secondRrectangle).Should().BeFalse();
+            var secondRectangle = layouter.PutNextRectangle(new Size(50, 50));
+            firstRectangle.IntersectsWith(secondRectangle).Should().BeFalse();
+        }
+
+        [Test]
+        public void CircularCloudLayouter_CreateDenseCloud()
+        {
+            var firstRectangle = layouter.PutNextRectangle(new Size(50, 50));
+            var secondRectangle = layouter.PutNextRectangle(new Size(50, 50));
+            Rectangle tempRectangle = new Rectangle(0, 0, 50, 50);
+            foreach(var direction in layouter.Directions)
+            {
+                tempRectangle.X = firstRectangle.X + direction.Item1 * layouter.Shift;
+                tempRectangle.X = firstRectangle.Y + direction.Item2 * layouter.Shift;
+                if (layouter.GetDistanceToCenter(tempRectangle) < layouter.GetDistanceToCenter(firstRectangle))
+                {
+                    tempRectangle.IntersectsWith(secondRectangle).Should().BeTrue();
+                }
+                tempRectangle.X = secondRectangle.X + direction.Item1 * layouter.Shift;
+                tempRectangle.X = secondRectangle.Y + direction.Item2 * layouter.Shift;
+                if (layouter.GetDistanceToCenter(tempRectangle) < layouter.GetDistanceToCenter(secondRectangle))
+                {
+                    tempRectangle.IntersectsWith(firstRectangle).Should().BeTrue();
+                }
+            }
         }
     }
 }
