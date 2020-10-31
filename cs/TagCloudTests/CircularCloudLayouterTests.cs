66b109390ba352babf790216227329762cba02a4
Kisus228 <nik.babushkin.201408@gmail.com>
2020-10-31 19:43:29 +0500

    Added a test that checks the radius of the cloud. Minor fixes in previous tests.

--- cs/TagCloudTests/CircularCloudLayouterTests.cs
+++ cs/TagCloudTests/CircularCloudLayouterTests.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Drawing;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 using TagCloud;
@@ -16,6 +16,7 @@ namespace TagCloudTests
             var resultRect = cloudLayouter.PutNextRectangle(new Size(5, 5));
             resultRect.Location.Should().Be(center);
         }
+
         [Test]
         public void PutNextRectangle_RectanglesDoNotIntersect_AfterAddition()
         {
@@ -25,9 +26,25 @@ namespace TagCloudTests
 
             for (var i = 0; i < cloudLayouter.Rectangles.Count - 1; i++)
             for (var j = i + 1; j < cloudLayouter.Rectangles.Count; j++)
-                Assert.False(cloudLayouter.Rectangles[i].IntersectsWith(cloudLayouter.Rectangles[j]),
-                    $"First rect coords: {cloudLayouter.Rectangles[i]}\n" +
-                    $"Second rect coords: {cloudLayouter.Rectangles[j]}");
+                cloudLayouter.Rectangles[i].IntersectsWith(cloudLayouter.Rectangles[j]).Should().BeFalse();
+        }
+
+        [Test]
+        public void PutNextRectangle_RectanglesAreNotFarFromCenter_AfterAddition()
+        {
+            var center = new Point(50, 50);
+            var cloudLayouter = new CircularCloudLayouter(center);
+            var sizes = SizeCreator(5);
+            foreach (var size in sizes) cloudLayouter.PutNextRectangle(size);
+            var mostTopRect = cloudLayouter.Rectangles.OrderBy(rect => rect.Top).First();
+            var mostDownRect = cloudLayouter.Rectangles.OrderByDescending(rect => rect.Bottom).First();
+            var mostLeftRect = cloudLayouter.Rectangles.OrderBy(rect => rect.Left).First();
+            var mostRightRect = cloudLayouter.Rectangles.OrderByDescending(rect => rect.Right).First();
+            const int expectedRadius = 15;
+            (center.Y - mostTopRect.Top).Should().BeLessThan(expectedRadius);
+            (mostDownRect.Bottom - center.Y).Should().BeLessThan(expectedRadius);
+            (center.X - mostLeftRect.Left).Should().BeLessThan(expectedRadius);
+            (mostRightRect.X - center.X).Should().BeLessThan(expectedRadius);
         }
 
         private static Size[] SizeCreator(int amount)
