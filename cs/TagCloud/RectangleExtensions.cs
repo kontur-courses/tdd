7538dd029e98ef110bd5c90c02f1eb9a86943125
Kisus228 <nik.babushkin.201408@gmail.com>
2020-10-31 21:22:57 +0500

    Added RectangleExtensions

---
+++ cs/TagCloud/RectangleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagCloud
+{
+    public static class RectangleExtensions
+    {
+        public static bool IntersectsWith(this Rectangle sourceRect, IEnumerable<Rectangle> otherRects)
+        {
+            return otherRects.Any(sourceRect.IntersectsWith);
+        }
+    }
+}
