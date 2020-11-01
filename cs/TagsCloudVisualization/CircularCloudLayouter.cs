108c9c2ff7249f004074dcc29238413a71e6e701
PShenica <polebota@gmail.com>
2020-11-01 18:49:18 +0500

    implemented CircularCloudLayouter class and add new test for it

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Linq;
 
 namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
         private Point Center { get; }
+        private ArchimedeanSpiral Spiral { get; }
+        private List<Rectangle> Rectangles { get; }
+        private string Root { get; }
+        private int RightBorder { get; set; }
+        private int BottomBorder { get; set; }
 
         public CircularCloudLayouter(Point center)
         {
             Center = center;
+            Spiral = new ArchimedeanSpiral(Center, 0.2, 0);
+            Rectangles = new List<Rectangle>();
+            Root = new DirectoryInfo("..\\..\\..\\Data").FullName;
+            RightBorder = 0;
+            BottomBorder = 0;
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
@@ -20,7 +32,87 @@ namespace TagsCloudVisualization
                 throw new ArgumentException();
             }
 
-            return new Rectangle();
+            return GetNewRectangle(rectangleSize);
+        }
+
+        private Rectangle GetNewRectangle(Size rectangleSize)
+        {
+            var location = Spiral.GetNextPoint();
+            var rectangle = new Rectangle(location, rectangleSize);
+
+            while (Collided(rectangle))
+            {
+                location = Spiral.GetNextPoint();
+                rectangle = new Rectangle(location, rectangleSize);
+            }
+
+            if (rectangle.Left < 0 || rectangle.Top < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            rectangle = MoveToCenter(rectangle);
+            UpdateImageBorers(rectangle);
+            Rectangles.Add(rectangle);
+
+            return rectangle;
+        }
+
+        private Rectangle MoveToCenter(Rectangle rectangle)
+        {
+            var movedRectangle = rectangle;
+
+            while (!Collided(rectangle) &&
+                   rectangle.X != Center.X &&
+                   rectangle.Y != Center.Y)
+            {
+                movedRectangle = rectangle;
+                var deltaX = Center.X - rectangle.X < 0 ? -1 : 1;
+                var deltaY = Center.Y - rectangle.Y < 0 ? -1 : 1;
+
+                var position = new Point(rectangle.X + deltaX, rectangle.Y + deltaY);
+                rectangle = new Rectangle(position, rectangle.Size);
+            }
+
+            return movedRectangle;
+        }
+
+        private void UpdateImageBorers(Rectangle rectangle)
+        {
+            if (RightBorder < rectangle.Right)
+            {
+                RightBorder = rectangle.Right;
+            }
+
+            if (BottomBorder < rectangle.Bottom)
+            {
+                BottomBorder = rectangle.Bottom;
+            }
+        }
+
+        private bool Collided(Rectangle newRectangle) =>
+            Rectangles.Any(rectangle => rectangle.IntersectsWith(newRectangle));
+
+        public string GenerateNewFilePath()
+        {
+            var dateTime = DateTime.Now;
+            return $"{Root}\\{dateTime:MMddyy-HHmmssffffff}.jpg";
+        }
+
+        public void SaveDrawing(string savePath)
+        {
+            var pen = new Pen(Color.MediumVioletRed, 4);
+            var bitmap = new Bitmap(RightBorder + (int)pen.Width, BottomBorder + (int)pen.Width);
+            var graphics = Graphics.FromImage(bitmap);
+
+            graphics.DrawRectangles(pen, Rectangles.ToArray());
+
+            if (!Directory.Exists(savePath) || !savePath.EndsWith(".jpg"))
+            {
+                throw new ArgumentException();
+            }
+
+            bitmap.Save(savePath);
         }
     }
 }
--- cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
@@ -1,33 +1,112 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using FluentAssertions;
+using NUnit.Framework.Interfaces;
 
 namespace TagsCloudVisualization
 {
     [TestFixture]
     public class CircularCloudLayouterTests
     {
-        private Point CentrePoint { get; set; }
+        private Point Center { get; set; }
         private CircularCloudLayouter CloudLayouter { get; set; }
 
         [SetUp]
         public void SetUp()
         {
-            CentrePoint = new Point(0, 0);
-            CloudLayouter = new CircularCloudLayouter(CentrePoint);
+            Center = new Point(400, 400);
+            CloudLayouter = new CircularCloudLayouter(Center);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var testStatus = TestContext.CurrentContext.Result.Outcome.Status;
+
+            if (testStatus == TestStatus.Failed)
+            {
+                var path = CloudLayouter.GenerateNewFilePath();
+                CloudLayouter.SaveDrawing(path);
+                Console.WriteLine($"Tag cloud visualization saved to file {path}");
+            }
         }
 
         [TestCase(0, 9, TestName = "Width is zero")]
         [TestCase(-1, 8, TestName = "Width is negative")]
         [TestCase(10, 0, TestName = "Height is zero")]
         [TestCase(3, -2, TestName = "Height is negative")]
-        public void PutNextRectangle_ThrowExeption_When(int width, int height)
+        public void PutNextRectangle_ThrowException_When(int width, int height)
         {
             var size = new Size(width, height);
             Action putRectangle = () => CloudLayouter.PutNextRectangle(size);
 
             putRectangle.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void PutNextRectangle_ReturnExpectedSize_WhenSizeIsPositive()
+        {
+            var expectedSize = new Size(13, 11);
+            var rectangle = CloudLayouter.PutNextRectangle(expectedSize);
+
+            rectangle.Size.Should().Be(expectedSize);
+        }
+
+        [Test]
+        public void PutNextRectangle_ReturnDifferentPositions_WhenCalledTwoTimes()
+        {
+            var firstRectangle = CloudLayouter.PutNextRectangle(new Size(5, 5));
+            var secondRectangle = CloudLayouter.PutNextRectangle(new Size(10, 10));
+
+            secondRectangle.Location.Should().NotBe(firstRectangle.Location);
+        }
+
+        [Test]
+        public void PutNextRectangle_ReturnNotCollidedRectangles_WhenCalledTwoTimes()
+        {
+            var firstRectangle = CloudLayouter.PutNextRectangle(new Size(100, 100));
+            var secondRectangle = CloudLayouter.PutNextRectangle(new Size(100, 100));
+
+            secondRectangle.IntersectsWith(firstRectangle).Should().Be(false);
+        }
+
+        [Test]
+        public void PutNextRectangle_ThrowException_WhenRectanglesDoesntFitOnImage()
+        {
+            Center = new Point(0, 0);
+            var size = new Size(400, 200);
+
+            Action putRectangle = () =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    CloudLayouter.PutNextRectangle(size);
+                }
+            };
+
+            putRectangle.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase(@"C:\image.gif", TestName = "Wrong image format")]
+        [TestCase(@"S:Nonexistent\Dir\image.jpg", TestName = "Directory dont exist")]
+        public void SaveDrawing_ThrowException_When(string path)
+        {
+            Action getPath = () => CloudLayouter.SaveDrawing(path);
+
+            getPath.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GenerateNewFilePath_ReturnExistingDirectory_WhenCalled()
+        {
+            var path = CloudLayouter.GenerateNewFilePath().Split('\\');
+            path[^1] = "";
+            var directoryPath = String.Join('\\', path);
+
+            Directory.Exists(directoryPath).Should().Be(true);
         }
     }
 }
