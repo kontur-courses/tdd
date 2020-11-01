3fef3864434e9af6cf66efa2201e8d62201325f8
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2020-11-02 04:03:02 +0500

    Unused namespaces deleted

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using TagsCloudVisualization.Infrastructure;
 using TagsCloudVisualization.Infrastructure.Environment;
 using TagsCloudVisualization.Infrastructure.Layout;
 
