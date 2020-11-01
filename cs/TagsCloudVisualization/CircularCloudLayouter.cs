cab75ed9a68b671273cb0265204c50962b3f7c00
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-02 00:55:35 +0500

    Added class 'CircularCloudLayouter'

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    internal class CircularCloudLayouter
+    {
+        private readonly List<Rectangle> placedRectangles;
+        private readonly Spiral spiral;
+
+        public CircularCloudLayouter(Point center)
+        {
+            spiral = new Spiral(center);
+            placedRectangles = new List<Rectangle>();
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            if (rectangleSize.IsEmpty)
+                throw new ArgumentException();
+            foreach (var point in spiral)
+            {
+                var topLeftCorner = new Point();
+                topLeftCorner.X = point.X - rectangleSize.Width / 2;
+                topLeftCorner.Y = point.Y - rectangleSize.Height / 2;
+                var rectangle = new Rectangle(topLeftCorner, rectangleSize);
+                if (!IsRectangleIntersects(rectangle))
+                {
+                    placedRectangles.Add(rectangle);
+                    return rectangle;
+                }
+            }
+
+            return new Rectangle();
+        }
+
+        private bool IsRectangleIntersects(Rectangle rectangle)
+        {
+            foreach (var placedRectangle in placedRectangles)
+                if (rectangle.IntersectsWith(placedRectangle))
+                    return true;
+
+            return false;
+        }
+    }
+}
