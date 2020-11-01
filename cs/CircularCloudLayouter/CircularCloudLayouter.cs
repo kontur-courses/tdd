178c231e6190ad0b12f5ce945744ac0f691dd154
Dima <d.d.demin@urfu.me>
2020-11-01 10:15:51 +0500

    Created CircularCloudLayouter.cs (Ctor, fields, PutNextRectangle that throws an exception)

---
+++ cs/CircularCloudLayouter/CircularCloudLayouter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace CircularCloudLayouter
+{
+    public class CircularCloudLayouter
+    {
+        private readonly List<Rectangle> _rectangles;
+        public readonly Point Center;
+
+        public CircularCloudLayouter(Point center)
+        {
+            Center = center;
+            _rectangles = new List<Rectangle>();
+        }
+        
+        public Rectangle GetCurrentRectangle => _rectangles.Last();
+
+        public List<Rectangle> GetRectangles => _rectangles;
+
+        public void PutNextRectangle(Size rectangleSize)
+        {
+            if (rectangleSize.Height == 0 || rectangleSize.Width == 0)
+                throw new ArgumentException("Size width and height must be not zero");
+        }
+    }
+}
