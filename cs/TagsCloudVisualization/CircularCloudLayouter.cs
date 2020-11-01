bd975add8d920ca8e020b016c798123b9b8a0593
Danil <danil7953@mail.ru>
2020-11-02 04:09:40 +0500

    Создал начальный CircularCloudLayouter и тест к нему.

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        private Point _center;
+        public CircularCloudLayouter(Point center)
+        {
+            _center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            var startRectangle = new Point(_center.X - rectangleSize.Width / 2, _center.Y - rectangleSize.Height / 2);
+            return new Rectangle(startRectangle, rectangleSize);
+        }
+    }
+}
