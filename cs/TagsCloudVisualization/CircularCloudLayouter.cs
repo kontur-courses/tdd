0cb158e28cc097f8be32ac88b8ff4835d00611ac
Denis Bautin <denis020301@mail.ru>
2020-11-01 19:37:36 +0500

    Create class CircularCloudLayouter and tests for him
    Add file CircularCloudLayouter.cs
    Add file CircularCloudLayouterTests.cs

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public readonly Point Center;
+        private readonly Spiral spiral = new Spiral();
+        private readonly Surface surface = new Surface();
+
+        public CircularCloudLayouter(Point center)
+        {
+            Center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            var rect = new Rectangle(spiral.GetNextPoint(), rectangleSize);
+            while (surface.IsRectangleIntersect(rect))
+            {
+                rect.Location = spiral.GetNextPoint();
+            }
+
+            if (rect.Location != Point.Empty)
+                rect = GetShiftedToCenterRectangle(rect);
+            
+            surface.AddRectangle(rect);
+            spiral.Reset();
+            return rect;
+        }
+
+        private Rectangle GetShiftedToCenterRectangle(Rectangle rect)
+        {
+            while (true)
+            {
+                var movedRect = DoStepToCenter(rect);
+                if (surface.IsRectangleIntersect(movedRect))
+                    return rect;
+                rect = movedRect;
+            }
+        }
+
+        private static Rectangle DoStepToCenter(Rectangle rect)
+        {
+            var rectQuarters = Surface.FindQuartersForRectangle(rect);
+            foreach (var quarter in rectQuarters)
+            {
+                var (dx, dy) = GetDeltaForQuarter(quarter);
+                rect.Offset(dx, dy);
+            }
+
+            return rect;
+        }
+
+        private static (int dx, int dy) GetDeltaForQuarter(Surface.Quarters quarter)
+        {
+            return quarter switch
+            {
+                Surface.Quarters.First => (-1, 1),
+                Surface.Quarters.Second => (1, 1),
+                Surface.Quarters.Third => (1, -1),
+                Surface.Quarters.Fourth => (-1, -1),
+                _ => throw new ArgumentException()
+            };
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/CircularCloudLayouterTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using FluentAssertions;
+using NUnit.Framework.Interfaces;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouterTests
+    {
+        private CircularCloudLayouter layouter;
+        private List<Rectangle> rectangles;
+
+        [SetUp]
+        public void SetUp()
+        {
+            rectangles = new List<Rectangle>();
+            layouter = new CircularCloudLayouter(new Point(25, 25));
+        }
+
+        [Test]
+        public void PutNextRectangle_LocationOfFirstRectangleIsPointEmpty()
+        {
+            rectangles.Add(
+                layouter.PutNextRectangle(new Size(2, 2)));
+
+            rectangles.Should().ContainSingle(rectangle =>
+                rectangle.Location == Point.Empty);
+        }
+
+        [Test]
+        public void PutNextRectangle_AllRectanglesAreNotIntersect()
+        {
+            PutNextRectangleTest();
+        }
+
+        [TestCase(-5, -5, TestName = "Center at (-5, -5)")]
+        [TestCase(400, 400, TestName = "Center at (400, 400)")]
+        [TestCase(0, 0, TestName = "Center at (0, 0)")]
+        public void PutNextRectangle_CorrectWorkWithDifferentCenters(int xCenter, int yCenter)
+        {
+            layouter = new CircularCloudLayouter(new Point(xCenter, yCenter));
+            PutNextRectangleTest();
+        }
+
+        private void PutNextRectangleTest(int count = 100)
+        {
+            var random = new Random();
+            
+            for (int i = 0; i < count; i++)
+            {
+                var size = new Size(random.Next(5, 80), random.Next(5, 80));
+                rectangles.Add(layouter.PutNextRectangle(size));
+            }
+            
+            AreRectangleItersect(rectangles).Should().Be(false);
+        }
+
+        private static bool AreRectangleItersect(List<Rectangle> rects)
+        {
+            for (int i = 0; i < rects.Count - 1; i++)
+                if (rects.Skip(i + 1).Any(rectangle =>
+                    rectangle.IntersectsWith(rects[i])))
+                {
+                    return true;
+                }
+
+            return false;
+        }
+    }
+}
