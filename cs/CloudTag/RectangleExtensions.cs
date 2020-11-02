d57b14114cbe1b3f20108c3eab1a61fe9b97953f
yourname <yourname@site.com>
2020-11-02 14:38:07 +0500

    добавил RectangleExtensions с методом SetCenter

---
+++ cs/CloudTag/RectangleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace CloudTag
+{
+    public static class RectangleExtensions
+    {
+        public static void SetCenter(this ref Rectangle rectangle, Point centerPoint)
+        {
+            var x = centerPoint.X - rectangle.Width / 2;
+            var y = centerPoint.Y - rectangle.Height / 2;
+            rectangle.Location = new Point(x,y);
+        }
+    }
+}
