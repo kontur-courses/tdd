8b2436a3b697a93a27676b7c547bf02edbbc2daf
yourname <yourname@site.com>
2020-11-02 15:55:08 +0500

    добавил тестов

--- cs/CloudTag/CircularCloudLayouterTests.cs
+++ cs/CloudTag/CircularCloudLayouterTests.cs
@@ -1,44 +1,95 @@
-﻿using System.Configuration;
-using System.Drawing;
+﻿using System.Drawing;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace CloudTag
 {
-    [TestFixture]
     public class CircularCloudLayouterTests
     {
         private CircularCloudLayouter layouter;
-
-        [Test]
-        public void Constructor_DoesntThrow()
-        {
-            Assert.DoesNotThrow(() => new CircularCloudLayouter(new Point(0, 0)));
-        }
+        private readonly Point layouterCenter = new Point(0, 0);
 
         [SetUp]
         public void SetUp()
         {
-            layouter = new CircularCloudLayouter(new Point(0, 0));
+            layouter = new CircularCloudLayouter(layouterCenter);
+        }
+
+        [Test]
+        public void Constructor_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => new CircularCloudLayouter(new Point(0, 0)));
         }
 
         [Test]
         public void PutNextRectangle_ReturnsRectangle_FirstAdding()
         {
             var size = new Size(10, 10);
-            var rectangle = layouter.PutNextRectangle(size);
-            rectangle.Location.Should().Be(new Point(-5, -5));
-            rectangle.Size.Should().Be(size);
+            var actualRectangle = layouter.PutNextRectangle(size);
+            var expectedRectangle = new Rectangle(Point.Empty, size);
+            expectedRectangle.SetCenter(layouterCenter);
+            actualRectangle.Should().Be(expectedRectangle);
+        }
+
+        [TestCase(2, TestName = "2 rect")]
+        [TestCase(10, TestName = "10 rect")]
+        [TestCase(80, TestName = "80 rect")]
+        [TestCase(200, TestName = "200 rect")]
+        public void PutNextRectangle_ShouldNotIntersect_RectSameSize(int rectCount)
+        {
+            var size = new Size(10, 10);
+            for (var i = 0; i < rectCount; i++)
+                layouter.PutNextRectangle(size);
+
+            var rectsInLayout = layouter.GetRectangles().ToArray();
+            for (var i = 0; i < rectCount; i++)
+            {
+                rectsInLayout[i].Should().Match<Rectangle>(
+                    rect1 => !rectsInLayout
+                        .Skip(i + 1)
+                        .Any(rect1.IntersectsWith)
+                );
+            }
         }
 
         [Test]
-        public void PutNextRectangle_ShouldntIntersect_TwoRectSameSize()
+        public void PutNextRectangle_ReturnEmptyRect_SizeIsZero()
+        {
+            var actualRect = layouter.PutNextRectangle(new Size(0, 0));
+            actualRect.Should().Be(Rectangle.Empty);
+        }
+
+        [TestCase(1, TestName = "1 rect")]
+        [TestCase(20, TestName = "20 rect")]
+        public void GetRectangles_ReturnEmptyCollection_AddedRectsWithZeroSize(int rectCount)
+        {
+            var size = new Size(0,0);
+            for (var i = 0; i < rectCount; i++)
+                layouter.PutNextRectangle(size);
+
+            layouter.GetRectangles().Should().BeEmpty();
+        }
+
+        [Test]
+        public void PutNextRectangle_ReturnsEmptyRect_OneOfSidesIs0()
+        {
+            var actualRect1 = layouter.PutNextRectangle(new Size(0, 10));
+            var actualRect2 = layouter.PutNextRectangle(new Size(10, 0));
+
+            actualRect1.Should().Be(Rectangle.Empty);
+            actualRect2.Should().Be(Rectangle.Empty);
+        }
+        
+        [Timeout(1000)]
+        [Test]
+        public void PutNextRectangle_PerformanceTest_5000RectSameSize()
         {
             var size = new Size(10, 10);
-            var rect1 = layouter.PutNextRectangle(size);
-            var rect2 = layouter.PutNextRectangle(size);
-            
-            rect1.Should().Match<Rectangle>(rect => !rect.IntersectsWith(rect2));
+            for (var i = 0; i < 5000; i++)
+                layouter.PutNextRectangle(size);
         }
+        
+        
     }
 }
