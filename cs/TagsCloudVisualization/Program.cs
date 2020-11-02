7f3500478961f21f205e775d34a10dfc4f193a4a
Rinsz <zer0zaur@mail.ru>
2020-11-02 14:39:16 +0500

    Task 2. Program.cs random generator added

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 
 
@@ -10,7 +11,20 @@ namespace TagsCloudVisualization
     {
         static void Main(string[] args)
         {
+            var center = new Point(540, 540);
+            var random = new Random();
+            var layouter = new CircularCloudLayouter(center);
 
+            var sizes = new List<Size>();
+            for (var i = 7; i < 300; i++)
+                sizes.Add(new Size((int) (5 * random.NextDouble() * 1080 / i),
+                    (int) (5 * random.NextDouble() * 1080 / 4 / i)));
+
+            sizes = sizes.OrderByDescending(x => x.Height * x.Width).ToList();
+            foreach (var size in sizes)
+                layouter.PutNextRectangle(size);
+
+            layouter.CreateImage($"{Environment.CurrentDirectory}", "image");
         }
     }
 }
