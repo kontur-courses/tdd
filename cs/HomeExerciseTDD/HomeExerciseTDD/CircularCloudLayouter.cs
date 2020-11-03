5e816ad6f62bbfa0a3bc813a235792395671b98f
fool_like_you <lambda2712@gmail.com>
2020-11-03 17:45:34 +0500

    added layouter logic

---
+++ cs/HomeExerciseTDD/HomeExerciseTDD/CircularCloudLayouter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+
+namespace HomeExerciseTDD
+{
+    public class CircularCloudLayouter:ICircularCloudLayouter
+    {
+        private Point center;
+        private readonly List<Rectangle> rectanglesInCloud = new List<Rectangle>();
+        private readonly Spiral spiral;
+
+
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+            spiral = new Spiral(this.center);
+        }
+        private bool IsIntersect(Rectangle currentRectangle)
+        {
+            return rectanglesInCloud.Any(currentRectangle.IntersectsWith);
+        }
+        
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            while (true)
+            {
+                var rectangleLocation = spiral.GetNextPoint();
+                var currentRectangle = new Rectangle(rectangleLocation, rectangleSize);
+                if (!IsIntersect(currentRectangle))
+                {
+                    rectanglesInCloud.Add(currentRectangle);
+                    return currentRectangle;
+                }
+            }
+        }
+    }
+}
---
+++ cs/HomeExerciseTDD/HomeExerciseTDD/Spiral.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace HomeExerciseTDD
+{
+    public class Spiral
+    {
+        private Point center;
+        private const float step = 0.0005f;
+        private Point previousPoint;
+        private float previousAngle;
+
+        public Spiral(Point center)
+        {
+            previousPoint = center;
+            this.center = center;
+        }
+
+        public Point GetNextPoint()
+        {
+            while (true)
+            {
+                if (previousPoint == center)
+                {
+                    previousPoint = new Point(Size.Empty);
+                    return center;
+                }
+                var angle = previousAngle;
+                angle++;
+
+                var distance = angle * step;
+                var currentX = distance * (float) Math.Cos(angle) + center.X;
+                var currentY = distance * (float) Math.Sin(angle) + center.Y;
+
+                var currentPoint = new Point((int) currentX, (int) currentY);
+                previousAngle = angle;
+                if (previousPoint.Equals(currentPoint))
+                    continue;
+                
+                previousPoint = currentPoint;
+                return currentPoint;
+            }
+        }
+    }
+}
