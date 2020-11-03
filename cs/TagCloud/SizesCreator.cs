44a757c146ae7ada1cc1f4ff72c753187e5a0f10
Kisus228 <nik.babushkin.201408@gmail.com>
2020-11-03 15:49:49 +0500

    Вынес создание размеров в отдельный класс

---
+++ cs/TagCloud/SizesCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagCloud
+{
+    public static class SizesCreator
+    {
+        public static Size[] CreateSizesArray(int minWidth, int maxWidth, int minHeight, int maxHeight, int amount)
+        {
+            var rnd = new Random();
+            var sizes = new Size[amount];
+            for (var index = 0; index < amount; index++)
+            {
+                sizes[index] = new Size(rnd.Next(minWidth, maxWidth), rnd.Next(minHeight, maxHeight));
+            }
+
+            return sizes;
+        }
+    }
+}
