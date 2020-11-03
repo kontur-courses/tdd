afd282c81e828b8b3eaaa8b1af258e1abfbbb08d
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-04 01:00:13 +0500

    Added test for spiral

--- cs/TagCloud/TagCloudTest/SpiralTests.cs
+++ cs/TagCloud/TagCloudTest/SpiralTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using FluentAssertions;
 using NUnit.Framework;
 using TagCloud;
@@ -23,6 +24,24 @@ namespace TagCloudTest
             var shiftedSpiral = new Spiral(startingPoint);
 
             shiftedSpiral.CurrentPoint.Should().Be(startingPoint);
+        }
+
+        [Test]
+        public void DistanceBetween2PointsAfterFullSpin_ShouldIncrease()
+        {
+            spiral.Next();
+            var point1 = spiral.CurrentPoint;
+
+            for (var i = 0; i < 360 / spiral.AngleStep; i++)
+                spiral.Next();
+            var point2 = spiral.CurrentPoint;
+
+            for (var i = 0; i < 360 / spiral.AngleStep; i++)
+                spiral.Next();
+            var point3 = spiral.CurrentPoint;
+            int DistSquared(Point p1, Point p2) => (p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y);
+
+            DistSquared(point1, point2).Should().BeGreaterThan(DistSquared(point2, point3));
         }
     }
 }
