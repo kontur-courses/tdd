a842a15d7c33581f20ae733cf20827eab69422e7
Denis Bautin <denis020301@mail.ru>
2020-11-04 20:29:23 +0500

    Reduced nesting in TearDown, refactor AreRectangleIntersect method
    Change file CircularCloudLayouterTests.cs

--- cs/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
@@ -22,20 +22,20 @@ namespace TagsCloudVisualizationTests
             rectangles = new List<Rectangle>();
             layouter = new CircularCloudLayouter(new Point(25, 25));
         }
-        
+
         [TearDown]
         public void TearDown()
         {
-            if (TestContext.CurrentContext.Result.Outcome == ResultState.Failure)
-            {
-                Drawer.DrawRectangles(layouter.Center, rectangles);
-                
-                var fileName = $"{TestContext.CurrentContext.Test.FullName}.png";
-                var path = Path.Combine(Path.GetFullPath(@"..\..\..\"), fileName);
-                Drawer.SaveImage(path);
-                
-                Console.WriteLine($"Tag cloud visualization saved to file {path}");
-            }
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Failure)
+                return;
+
+            Drawer.DrawRectangles(layouter.Center, rectangles);
+
+            var fileName = $"{TestContext.CurrentContext.Test.FullName}.png";
+            var path = Path.Combine(Path.GetFullPath(@"..\..\..\"), fileName);
+            Drawer.SaveImage(path);
+
+            Console.WriteLine($"Tag cloud visualization saved to file {path}");
         }
 
         [Test]
@@ -66,26 +66,21 @@ namespace TagsCloudVisualizationTests
         private void PutNextRectangleTest(int count = 100)
         {
             var random = new Random();
-            
+
             for (int i = 0; i < count; i++)
             {
                 var size = new Size(random.Next(5, 80), random.Next(5, 80));
                 rectangles.Add(layouter.PutNextRectangle(size));
             }
-            
-            AreRectangleItersect(rectangles).Should().Be(false);
+
+            AreRectangleIntersect(rectangles).Should().Be(false);
         }
 
-        private static bool AreRectangleItersect(List<Rectangle> rects)
+        private static bool AreRectangleIntersect(List<Rectangle> rects)
         {
-            for (int i = 0; i < rects.Count - 1; i++)
-                if (rects.Skip(i + 1).Any(rectangle =>
-                    rectangle.IntersectsWith(rects[i])))
-                {
-                    return true;
-                }
-
-            return false;
+            return rects.Any(rectangle =>
+                rects.Where(otherRect => otherRect != rectangle)
+                    .Any(rectangle.IntersectsWith));
         }
     }
 }
