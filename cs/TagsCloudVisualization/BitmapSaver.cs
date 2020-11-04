9f5133ca21eef6762aebafb73030a2df57db2e1a
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-05 02:17:15 +0500

    Added class 'BitmapSaver'

---
+++ cs/TagsCloudVisualization/BitmapSaver.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace TagsCloudVisualization
+{
+    public class BitmapSaver
+    {
+        public static void Save(Bitmap bitmap, string filename)
+        {
+            var projectDirectory = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+            bitmap.Save(Path.Combine(projectDirectory.FullName, "pictures", filename));
+        }
+    }
+}
