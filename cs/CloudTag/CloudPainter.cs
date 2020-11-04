86b352e9471de82bdca9db1229e03acfbe90ff35
yourname <yourname@site.com>
2020-11-05 02:04:03 +0500

    добавил CloudPainter.cs

---
+++ cs/CloudTag/CloudPainter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace CloudTag
+{
+    public static class CloudPainter
+    {
+        public static Bitmap DrawTagCloud(Pen pen, CircularCloudLayouter layouter)
+        {
+            var rectangles = layouter.GetRectangles().ToArray();
+            
+            var width = rectangles.Max(rectangle => rectangle.Right) +
+                        Math.Abs(rectangles.Min(rectangle => rectangle.Left));
+
+            var height = rectangles.Max(rectangle => rectangle.Bottom) +
+                         Math.Abs(rectangles.Min(rectangle => rectangle.Top));
+            
+            var bitmap = new Bitmap(width, height);
+            Graphics.FromImage(bitmap).DrawRectangles(pen, rectangles);
+
+            return bitmap;
+        }
+    }
+}
