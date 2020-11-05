0d0073027d2a916a4c860fdff9f1a42fb5bf4ef1
PShenica <polebota@gmail.com>
2020-11-06 04:50:35 +0500

    add new class TagsVisualizator and TagsVisualizatorTests

---
+++ cs/TagsCloudVisualization/TagsVisualizator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace TagsCloudVisualization
+{
+    public class TagsVisualizator
+    {
+        private string Root { get; }
+        private int RightBorder { get; set; }
+        private int BottomBorder { get; set; }
+
+        public TagsVisualizator()
+        {
+            Root = new DirectoryInfo("..\\..\\..\\Sampels").FullName;
+            RightBorder = 0;
+            BottomBorder = 0;
+        }
+
+        public void SaveVisualization(List<Rectangle> rectangles, string savePath)
+        {
+            rectangles.ForEach(UpdateImageBorders);
+
+            var pen = new Pen(Color.MediumVioletRed, 4);
+            using var bitmap = new Bitmap(RightBorder + (int)pen.Width, BottomBorder + (int)pen.Width);
+
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.DrawRectangles(pen, rectangles.ToArray());
+
+            if (!Directory.Exists(savePath))
+            {
+                throw new ArgumentException("directory doesn't exist");
+            }
+
+            bitmap.Save(savePath, ImageFormat.Jpeg);
+        }
+
+        private void UpdateImageBorders(Rectangle rectangle)
+        {
+            if (rectangle.Left < 0 || rectangle.Top < 0)
+            {
+                throw new ArgumentException("rectangle out of image boundaries");
+            }
+
+            if (RightBorder < rectangle.Right)
+            {
+                RightBorder = rectangle.Right;
+            }
+
+            if (BottomBorder < rectangle.Bottom)
+            {
+                BottomBorder = rectangle.Bottom;
+            }
+        }
+
+        public string GenerateNewFilePath()
+        {
+            var dateTime = DateTime.Now;
+            return $"{Root}\\{dateTime:MMddyy-HHmmssffffff}.jpg";
+        }
+    }
+}
---
+++ cs/TagsCloudVisualizationTests/TagsVisualizatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualizationTests
+{
+    public class TagsVisualizatorTests
+    {
+        private TagsVisualizator Sut { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Sut = new TagsVisualizator();
+        }
+
+        [Test]
+        public void SaveVisualization_UpdateImageBordersThrowException_WhenRectangleOutOfBoundaries()
+        {
+            var path = Sut.GenerateNewFilePath();
+            var rectangles = new List<Rectangle>();
+            var location = new Point(-100, -100);
+            var size = new Size(50, 50);
+            rectangles.Add(new Rectangle(location, size));
+
+            Action saveImage = () => Sut.SaveVisualization(rectangles, path);
+
+            saveImage.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase(@"C:\image.gif", TestName = "Wrong image format")]
+        [TestCase(@"S:Nonexistent\Dir\image.jpg", TestName = "Directory dont exist")]
+        public void TagsVisualizator_ThrowException_When(string path)
+        {
+            Action saveImage = () => Sut.SaveVisualization(new List<Rectangle>(), path);
+
+            saveImage.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GenerateNewFilePath_ReturnExistingDirectory_WhenCalled()
+        {
+            var path = Sut.GenerateNewFilePath().Split('\\');
+            path[^1] = "";
+            var directoryPath = String.Join('\\', path);
+
+            Directory.Exists(directoryPath).Should().BeTrue();
+        }
+    }
+}
