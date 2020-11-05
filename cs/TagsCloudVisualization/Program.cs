2d0ca9f6795d43da57c17b0a6ec9f43f22204f25
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-06 03:04:53 +0500

    Updated project

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rectanglesSizes = new[]
+            {
+                new Size(50, 50),
+                new Size(20, 80),
+                new Size(70, 30),
+                new Size(10, 10),
+                new Size(30, 40),
+                new Size(20, 20)
+            };
+            var center = new Point(100, 100);
+            var layouter = new CircularCloudLayouter(center);
+            var placedRectangles =
+                rectanglesSizes.Select(x => layouter.PutNextRectangle(x)).ToList();
+            var bitmap = CircularCloudDrawer.GetBitmap(placedRectangles, center);
+            BitmapSaver.Save(bitmap, "random.jpg");
+        }
+    }
+}
