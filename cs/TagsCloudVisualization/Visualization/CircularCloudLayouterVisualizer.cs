fc85273527685b9aeb2d38254eca6941ed395a83
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-11-06 01:50:15 +0500

    Add VisualizerSettings and work with it into CloudVisualizer

--- cs/TagsCloudVisualization/Visualization/CircularCloudLayouterVisualizer.cs
+++ cs/TagsCloudVisualization/Visualization/CircularCloudLayouterVisualizer.cs
@@ -4,26 +4,24 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using TagsCloudVisualization.Core;
+using TagsCloudVisualization.VisualizationSettings;
 
 namespace TagsCloudVisualization.Visualization
 {
     public class CircularCloudLayouterVisualizer
     {
         private CircularCloudLayouter CloudLayouter { get; }
-        private string WorkDirectory { get; }
+        private VisualizerSettings Settings { get; }
         private readonly Brush brushForRect = Brushes.Yellow;
         private readonly Pen penForFrame = Pens.Black;
 
         public CircularCloudLayouterVisualizer(CircularCloudLayouter cloudLayouter)
         {
             CloudLayouter = cloudLayouter ?? throw new ArgumentNullException(nameof(cloudLayouter));
-            var workDir = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\TagClouds";
-            if (!Directory.Exists(workDir))
-                Directory.CreateDirectory(workDir);
-            WorkDirectory = workDir;
+            Settings = VisualizerSettings.ReadSettingsFromConfig();
         }
 
-        public void SaveInFile(string filename = "last_cloud.jpg", int imageWidth = 720, int imageHeight = 720)
+        public void SaveInFile(string filename = "last_cloud.jpg")
         {
             if (string.IsNullOrWhiteSpace(filename))
                 throw new ArgumentException("Filename shouldn`t be null, empty or white space!");
@@ -33,17 +31,18 @@ namespace TagsCloudVisualization.Visualization
             if (!filename.EndsWith(".jpg"))
                 filename += ".jpg";
 
-            var bmp = new Bitmap(imageWidth, imageHeight);
+            var bmp = new Bitmap(Settings.ImageWidth, Settings.ImageWidth);
             var graphics = Graphics.FromImage(bmp);
 
-            graphics.FillRectangle(Brushes.White, 0, 0, imageWidth, imageHeight);
+            graphics.FillRectangle(Brushes.White, 0, 0, Settings.ImageWidth, Settings.ImageHeight);
             foreach (var rectangle in CloudLayouter.Rectangles)
             {
                 graphics.FillRectangle(brushForRect, rectangle);
                 graphics.DrawRectangle(penForFrame, rectangle);
             }
 
-            var path = Path.Combine(WorkDirectory, filename);
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                Settings.WorkDirectory, filename);
             bmp.Save(path, ImageFormat.Jpeg);
 
             Console.WriteLine($"Tag cloud visualization saved to file {path}");
---
+++ cs/TagsCloudVisualization/VisualizationSettings/VisualizerSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace TagsCloudVisualization.VisualizationSettings
+{
+    public class VisualizerSettings
+    {
+        private static readonly string ConfigPath =
+            Path.Combine(Directory.GetCurrentDirectory(), "VisualizationSettings", "config.json");
+
+        public string WorkDirectory { get; set; }
+        public int ImageWidth { get; set; }
+        public int ImageHeight { get; set; }
+
+        public static VisualizerSettings ReadSettingsFromConfig()
+        {
+            var jsonString = File.ReadAllText(ConfigPath);
+            return JsonSerializer.Deserialize<VisualizerSettings>(jsonString);
+        }
+
+        public void SaveSettingsIntoConfig(string workDir = "TagsCloud", int imageWidth = 1280, int imageHeight = 720)
+        {
+            WorkDirectory = workDir;
+            ImageWidth = imageWidth;
+            ImageHeight = imageHeight;
+            
+            var jsonString = JsonSerializer.Serialize(this);
+            File.WriteAllText(jsonString, ConfigPath);
+        }
+    }
+}
