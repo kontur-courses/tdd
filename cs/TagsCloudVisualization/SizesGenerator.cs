16ab6cfe1e08defcc44d89d47a8b8cbded1da46b
ilya.belyaev.student@gmail.com <50026308+CaptainBelyash@users.noreply.github.com>
2020-11-05 17:33:48 +0500

    Add Sizes generator

---
+++ cs/TagsCloudVisualization/SizesGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class SizesGenerator
+    {
+        public static List<Size> GenerateSizesList(int count, Size minSize, Size maxSize)
+        {
+            if (minSize.Height >= maxSize.Height 
+                || minSize.Width >= maxSize.Width)
+                throw new ArgumentException("minSizes fields must be less than maxSizes fields");
+            
+            var random = new Random();
+            var sizes = new List<Size>();
+            for (var i = 0; i < count; i++)
+            {
+                var newSize = new Size(random.Next(minSize.Width, maxSize.Width),
+                    random.Next(minSize.Height, maxSize.Height));
+                sizes.Add(newSize);
+            }
+
+            return sizes;
+        }
+    }
+}
