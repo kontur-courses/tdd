b652e6859881184446d922533d518a8bfcad893e
ilya.belyaev.student@gmail.com <50026308+CaptainBelyash@users.noreply.github.com>
2020-11-06 18:25:41 +0500

    Move support classes in test project

--- cs/TagsCloudVisualization_Tests/SizesGenerator.cs
+++ cs/TagsCloudVisualization_Tests/SizesGenerator.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 
-namespace TagsCloudVisualization
+namespace TagsCloudVisualization_Tests
 {
     public class SizesGenerator
     {
