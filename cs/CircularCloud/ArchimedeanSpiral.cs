7d7928541c2ea58c8d4f9fa834a6cb4cc72b6e84
xsitin <m9ckom@gmail.com>
2020-11-06 14:51:53 +0500

    fixed typo

--- cs/CircularCloud/ArchimedeanSpiral.cs
+++ cs/CircularCloud/ArchimedeanSpiral.cs
@@ -7,7 +7,7 @@ namespace CircularCloud
     {
         public Point Center { get; }
         protected const double Distance = 0.5;
-        protected double Angel;
+        protected double Angle;
         protected Point? Previous;
 
         public ArchimedeanSpiral(Point center) => Center = center;
@@ -17,10 +17,10 @@ namespace CircularCloud
             Point result;
             do
             {
-                var radius = Distance * Angel;
-                var x = radius * Math.Cos(Angel);
-                var y = radius * Math.Sin(Angel);
-                Angel += 0.0005;
+                var radius = Distance * Angle;
+                var x = radius * Math.Cos(Angle);
+                var y = radius * Math.Sin(Angle);
+                Angle += 0.0005;
                 result = new Point((int) Math.Round(x) + Center.X, (int) Math.Round(y) + Center.Y);
             } while (result == Previous);
 
