4a354479ccef9b1ddbe396395a448ff42c2bfc5a
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-07 02:04:07 +0500

    Added examples generation

--- cs/TagCloud/TagCloud/Program.cs
+++ cs/TagCloud/TagCloud/Program.cs
@@ -7,13 +7,13 @@ namespace TagCloud
     {
         public static void Main()
         {
-            var tagCloud = new CircularCloudLayouter(new Point(1920 / 2, 1080 / 2));
+            var tagCloud = new CircularCloudLayouter(new Point(1920 / 2, 1080 / 2),1);
             var random = new Random();
-            for (var i = 0; i < 500; i++)
+            for (var i = 0; i < 100; i++)
                 tagCloud.PutNextRectangle(new Size(random.Next() % 100 + 1, random.Next() % 100 + 1));
             var vis = new TagCloudVisualizer(tagCloud);
             var image = vis.CreateBitMap(1920, 1080);
-            image.Save($"testCloud.jpg");
+            image.Save($"100RectanglesDensity1.jpg");
         }
     }
 }
