12a2d430d338b8322b177851c6ee3a29d2713a2d
PShenica <polebota@gmail.com>
2020-11-07 21:52:05 +0500

    add new class BitmapSaverTests and BitmapSaver

---
+++ cs/TagsCloudVisualization/BitmapSaver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TagsCloudVisualization
+{
+    public class BitmapSaver
+    {
+        private Bitmap ImageBitmap { get; }
+
+        public BitmapSaver(Bitmap imageBitmap)
+        {
+            ImageBitmap = imageBitmap;
+        }
+
+        public void SaveBitmapToDirectory(string savePath)
+        {
+            if (!PathInRightFormat(savePath))
+            {
+                throw new ArgumentException("wrong path format");
+            }
+
+            using (ImageBitmap)
+            {
+                ImageBitmap.Save(savePath, ImageFormat.Jpeg);
+            }
+        }
+
+        private static bool PathInRightFormat(string path)
+        {
+            var pattern = @"((?:[^\\]*\\)*)(.*[.].+)";
+            var match = Regex.Match(path, pattern);
+            var directoryPath = match.Groups[1].ToString();
+
+            return Directory.Exists(directoryPath) && match.Groups[2].Success;
+        }
+    }
+}
---
+++ cs/TagsCloudVisualizationTests/BitmapSaverTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualizationTests
+{
+    public class BitmapSaverTests
+    {
+        private BitmapSaver Sut { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            const int height = 10;
+            const int width = 10;
+            Sut = new BitmapSaver(new Bitmap(width, height));
+        }
+
+        [TestCase(@"<html></html>", TestName = "Directory dont exist")]
+        [TestCase(@"C:/Dir/image.jpg", TestName = "Not backslash separator")]
+        [TestCase(@"C:\image jpg", TestName = "Doesnt have dot separator")]
+        [TestCase(@"C:\image.", TestName = "Doesnt have filename extension")]
+        public void SaveBitmapToDirectory_ThrowException_When(string path)
+        {
+            Action saveImage = () => Sut.SaveBitmapToDirectory(path);
+
+            saveImage.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase(@"C:\image.jpg", TestName = "Relative path")]
+        [TestCase(@"..\image.jpg", TestName = "Absolute path")]
+        public void SaveBitmapToDirectory_DoesntThrowException_When(string path)
+        {
+            Action saveImage = () => Sut.SaveBitmapToDirectory(path);
+
+            saveImage.Should().NotThrow<ArgumentException>();
+        }
+    }
+}
