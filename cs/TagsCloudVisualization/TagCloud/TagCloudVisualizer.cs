1a79cb2e63289ae0ddcf4bde941dbf12cf57406e
VladSavitskiy <cyberl1fe@yandex.ru>
2020-11-08 20:55:11 +0500

    Refactored TagCloudVisualizer.cs

--- cs/TagsCloudVisualization/TagCloud/TagCloudVisualizer.cs
+++ cs/TagsCloudVisualization/TagCloud/TagCloudVisualizer.cs
@@ -1,26 +1,69 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 
 namespace TagsCloudVisualization.TagCloud
 {
     public static class TagCloudVisualizer
     {
-        public static void PrintTagCloud(ICloudLayouter cloudLayouter, ImageFormat imageFormat,
-            string pathToFile = @"..\..\..\Images\", string fileName = "TagCloud")
+        public static void PrintTagCloud(
+            List<Rectangle> rectangles,
+            Point rectanglesCenter,
+            int horizontalEdgeIndent,
+            int verticalEdgeIndent,
+            string pathToFile = @"..\..\..\Images\",
+            string fileName = "TagCloud")
         {
-            var bitmap = new Bitmap(cloudLayouter.PointGenerator.CanvasSize.Height,
-                cloudLayouter.PointGenerator.CanvasSize.Width);
-            var graphics = Graphics.FromImage(bitmap);
-            var colors = new[]
-                {Color.MediumVioletRed, Color.Orange, Color.White, Color.Red, Color.LawnGreen, Color.SpringGreen};
+            var imageSize = GetImageSizeToDraw(rectangles, horizontalEdgeIndent, verticalEdgeIndent);
+            var image = new Bitmap(imageSize.Height, imageSize.Width);
+            rectangles = CenterTheTagCloud(rectangles, image, rectanglesCenter);
+            DrawRectangles(rectangles, image,
+                new[]
+                {
+                    Color.MediumVioletRed, Color.Orange, Color.White,
+                    Color.Red, Color.LawnGreen, Color.SpringGreen
+                });
+            image.Save($"{pathToFile}{fileName}.png", ImageFormat.Png);
+        }
+        
+        private static Size GetImageSizeToDraw(List<Rectangle> rectangles, 
+            int horizontalOffset, int verticalOffset)
+        {
+            var maxX = rectangles.Max(rect => rect.Right);
+            var minX = rectangles.Min(rect => rect.Left);
+            var maxY = rectangles.Max(rect => rect.Bottom);
+            var minY = rectangles.Min(rect => rect.Top);
 
-            graphics.FillRectangle(new SolidBrush(Color.Black), new Rectangle(0, 0, cloudLayouter.PointGenerator.CanvasSize.Width, cloudLayouter.PointGenerator.CanvasSize.Height));
-            for (int i = 0; i < cloudLayouter.AddedRectanglesCount; i++)
-                graphics.DrawRectangle(new Pen(colors[i % colors.Length]),
-                    cloudLayouter.GetAddedRectangle(i));
+            return new Size(maxX - minX + 2 * horizontalOffset, maxY - minY + 2 * verticalOffset);
+        }
+        
+        private static void DrawRectangles(IEnumerable<Rectangle> rectangles, Bitmap image, Color[] colors)
+        {
+            var graphics = Graphics.FromImage(image);
+            graphics.FillRectangle(new SolidBrush(Color.Black),
+                new Rectangle(0, 0, image.Width, image.Height));
 
-            bitmap.Save(pathToFile + fileName + "." + imageFormat.ToString().ToLower(), imageFormat);
+            var iterationCount = 0;
+            var pen = new Pen(Color.White);
+            foreach (var rectangle in rectangles)
+            {
+                pen.Color = colors[iterationCount % colors.Length];
+                graphics.DrawRectangle(pen, rectangle);
+                iterationCount++;
+            }
+        }
+
+        private static List<Rectangle> CenterTheTagCloud(
+            IEnumerable<Rectangle> rectangles, Bitmap image, Point rectanglesCenter)
+        {
+            var shiftX = image.Width / 2 - rectanglesCenter.X;
+            var shiftY = image.Height / 2 - rectanglesCenter.Y;
+
+            return rectangles
+                .Select(rectangle => new Rectangle(
+                    new Point(rectangle.X + shiftX, rectangle.Y + shiftY), rectangle.Size))
+                .ToList();
         }
     }
 }
