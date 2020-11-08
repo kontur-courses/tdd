e2ccfcd441c9ce8205daf147d14dfe57312bb6d8
MaxCross <dan25001@gmail.com>
2020-11-09 00:50:03 +0500

    Рефакторинг тестов CircularCloudLayouter

--- cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -4,12 +4,85 @@ using System.IO;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace TagsCloudVisualization
 {
     [TestFixture]
     public class CircularCloudLayouter_Should
     {
+        public static readonly Size[][] FourRectanglesCases =
+        {
+            new[]
+            {
+                new Size(1, 1),
+                new Size(1, 1),
+                new Size(1, 1),
+                new Size(1, 1),
+            },
+            new[]
+            {
+                new Size(7, 3),
+                new Size(5, 8),
+                new Size(3, 6),
+                new Size(2, 1),
+            },
+        };
+
+        public static readonly Size[][] ALotRectanglesCases =
+        {
+            new[]
+            {
+                new Size(7, 3),
+                new Size(5, 8),
+                new Size(3, 6),
+                new Size(2, 1),
+                new Size(7, 5),
+                new Size(3, 10),
+                new Size(15, 1),
+                new Size(31, 10),
+                new Size(11, 26),
+                new Size(12, 25),
+                new Size(13, 24),
+                new Size(14, 23),
+                new Size(15, 22),
+                new Size(16, 21),
+                new Size(17, 20),
+                new Size(18, 19),
+                new Size(31, 10),
+                new Size(11, 26),
+                new Size(12, 25),
+                new Size(13, 24),
+                new Size(14, 23),
+                new Size(15, 22),
+                new Size(16, 21),
+                new Size(17, 20),
+                new Size(18, 19),
+                new Size(31, 10),
+                new Size(11, 26),
+                new Size(12, 25),
+                new Size(13, 24),
+                new Size(14, 23),
+                new Size(15, 22),
+                new Size(16, 21),
+                new Size(17, 20),
+                new Size(18, 19),
+                new Size(31, 10),
+                new Size(11, 26),
+                new Size(12, 25),
+                new Size(13, 24),
+                new Size(14, 23),
+                new Size(15, 22),
+                new Size(16, 21),
+                new Size(17, 20),
+                new Size(18, 19),
+                new Size(31, 10),
+                new Size(11, 26),
+                new Size(12, 25),
+                new Size(13, 24),
+            }
+        };
+        
         [Test]
         public void ThrowException_WhenPutNotPositiveSize()
         {
@@ -19,121 +92,60 @@ namespace TagsCloudVisualization
             act.Should().Throw<ArgumentException>();
         }
 
-        [Test]
-        public void PlaceFirstFourRectanglesAroundCenter()
+        public DirectoryInfo ImageOutputDirectory => new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory)
+            .Parent?.Parent?
+            .CreateSubdirectory("Images") ?? new DirectoryInfo(".");
+        
+
+        [TestCaseSource(nameof(FourRectanglesCases))]
+        public void PlaceFirstFourRectanglesAroundCenter(Size[] rectangleSizes)
         {
             var center = new Point(7, 10);
             var layouter = new CircularCloudLayouter(center);
-
-            var rectangles = new[]
-            {
-                new Size(7, 3),
-                new Size(5, 8),
-                new Size(3, 6),
-                new Size(2, 1),
-            };
             
-            var rect = layouter.PutNextRectangle(rectangles[0]);
-            rect.Location.Should().Be(center);
+            var rectangle = layouter.PutNextRectangle(rectangleSizes[0]);
+            rectangle.Location.Should().Be(center);
 
-            rect = layouter.PutNextRectangle(rectangles[1]);
-            rect.X.Should().Be(center.X);
-            rect.Bottom.Should().Be(center.Y);
+            rectangle = layouter.PutNextRectangle(rectangleSizes[1]);
+            rectangle.X.Should().Be(center.X);
+            rectangle.Bottom.Should().Be(center.Y);
 
-            rect = layouter.PutNextRectangle(rectangles[2]);
-            rect.Right.Should().Be(center.X);
-            rect.Bottom.Should().Be(center.Y);
+            rectangle = layouter.PutNextRectangle(rectangleSizes[2]);
+            rectangle.Right.Should().Be(center.X);
+            rectangle.Bottom.Should().Be(center.Y);
             
-            rect = layouter.PutNextRectangle(rectangles[3]);
-            rect.Right.Should().Be(center.X);
-            rect.Y.Should().Be(center.Y);
+            rectangle = layouter.PutNextRectangle(rectangleSizes[3]);
+            rectangle.Right.Should().Be(center.X);
+            rectangle.Y.Should().Be(center.Y);
         }
 
-        [Test]
-        public void NotHaveIntersections_WithFourRectangles()
+        [TestCaseSource(nameof(FourRectanglesCases))]
+        public void NotHaveIntersections_WithFourRectangles(Size[] rectangleSizes)
         {
             var center = new Point(7, 10);
             var layouter = new CircularCloudLayouter(center);
-
-            var rectangles = new[]
-            {
-                new Size(7, 3),
-                new Size(5, 8),
-                new Size(3, 6),
-                new Size(2, 1),
-            };
-            var result = rectangles.Select(r => layouter.PutNextRectangle(r)).ToArray();
+            
+            var result = rectangleSizes.Select(r => layouter.PutNextRectangle(r)).ToArray();
             result
                 .Where(r => result.Where(rr => rr != r).Any(r.IntersectsWith)).Should().BeEmpty();
         }
         
-        [Test]
-        public void NotHaveIntersections_WithALotRectangles()
+        [TestCaseSource(nameof(ALotRectanglesCases))]
+        public void NotHaveIntersections_WithALotRectangles(Size[] rectangleSizes)
         {
             var center = new Point(7, 10);
             var layouter = new CircularCloudLayouter(center);
-
-            var rectangleSizes = new[]
-            {
-                new Size(7, 3),
-                new Size(5, 8),
-                new Size(3, 6),
-                new Size(2, 1),
-                new Size(7, 5),
-                new Size(3, 10),
-                new Size(15, 1),
-                new Size(31, 10),
-                new Size(11, 26),
-                new Size(12, 25),
-                new Size(13, 24),
-                new Size(14, 23),
-                new Size(15, 22),
-                new Size(16, 21),
-                new Size(17, 20),
-                new Size(18, 19),
-                new Size(31, 10),
-                new Size(11, 26),
-                new Size(12, 25),
-                new Size(13, 24),
-                new Size(14, 23),
-                new Size(15, 22),
-                new Size(16, 21),
-                new Size(17, 20),
-                new Size(18, 19),
-                new Size(31, 10),
-                new Size(11, 26),
-                new Size(12, 25),
-                new Size(13, 24),
-                new Size(14, 23),
-                new Size(15, 22),
-                new Size(16, 21),
-                new Size(17, 20),
-                new Size(18, 19),
-                new Size(31, 10),
-                new Size(11, 26),
-                new Size(12, 25),
-                new Size(13, 24),
-                new Size(14, 23),
-                new Size(15, 22),
-                new Size(16, 21),
-                new Size(17, 20),
-                new Size(18, 19),
-                new Size(31, 10),
-                new Size(11, 26),
-                new Size(12, 25),
-                new Size(13, 24),
-            }.Select(s => new Size(s.Width * 5, s.Height * 5)).ToArray();
             var rectangles = rectangleSizes.Select(r => layouter.PutNextRectangle(r)).ToArray();
+            
             var intersectionIndices = Enumerable.Range(0, rectangles.Length)
                 .Where(i => rectangles.Any(r => r != rectangles[i] && r.IntersectsWith(rectangles[i])))
                 .ToArray();
+            
             if (intersectionIndices.Length != 0)
             {
-                var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory)
-                    .Parent?.Parent?
-                    .CreateSubdirectory("Images") ?? new DirectoryInfo(".");
-                var outputFile = $"{directory.FullName}\\intersections_fail.bmp";
-                ImageGenerator.GenerateImageWithRectanglesFromLayouter(rectangleSizes, outputFile,
+                var outputFile = $"{ImageOutputDirectory.FullName}\\intersections_fail.bmp";
+                var rectangleSizesScaled = rectangleSizes.Select(s => new Size(s.Width * 5, s.Height * 5)).ToArray();
+                ImageGenerator.GenerateImageWithRectanglesFromLayouter(rectangleSizesScaled, outputFile,
                     (i) => intersectionIndices.Contains(i)
                         ? Color.Red
                         : Color.Green);
@@ -142,51 +154,45 @@ namespace TagsCloudVisualization
             }
         }
         
-        [Test]
-        public void NotChangeSize_WithFourRectangles()
+        [TestCaseSource(nameof(FourRectanglesCases))]
+        public void NotChangeSize_WithFourRectangles(Size[] rectangleSizes)
         {
             var center = new Point(7, 10);
             var layouter = new CircularCloudLayouter(center);
-
-            var rectangles = new[]
-            {
-                new Size(7, 3),
-                new Size(5, 8),
-                new Size(3, 6),
-                new Size(2, 1),
-            };
-            var result = rectangles.Select(r => layouter.PutNextRectangle(r)).ToArray();
-            result.Select(r => r.Size).Should().BeEquivalentTo(rectangles, options => options.WithStrictOrdering());
+            
+            var result = rectangleSizes.Select(r => layouter.PutNextRectangle(r)).ToArray();
+            result.Select(r => r.Size).Should().BeEquivalentTo(rectangleSizes, options => options.WithStrictOrdering());
         }
         
-        [Test]
-        public void NotChangeSize_WithALotRectangles()
+        [TestCaseSource(nameof(ALotRectanglesCases))]
+        public void NotChangeSize_WithALotRectangles(Size[] rectangleSizes)
         {
             var center = new Point(7, 10);
             var layouter = new CircularCloudLayouter(center);
-
-            var rectangles = new[]
-            {
-                new Size(7, 3),
-                new Size(5, 8),
-                new Size(3, 6),
-                new Size(2, 1),
-                new Size(7, 5),
-                new Size(3, 10),
-                new Size(15, 1),
-                new Size(31, 10),
-                new Size(40, 12),
-                new Size(11, 26),
-                new Size(12, 25),
-                new Size(13, 24),
-                new Size(14, 23),
-                new Size(15, 22),
-                new Size(16, 21),
-                new Size(17, 20),
-                new Size(18, 19),
-            };
-            var result = rectangles.Select(r => layouter.PutNextRectangle(r)).ToArray();
-            result.Select(r => r.Size).Should().BeEquivalentTo(rectangles, options => options.WithStrictOrdering());
+            
+            var result = rectangleSizes.Select(r => layouter.PutNextRectangle(r)).ToArray();
+            result.Select(r => r.Size).Should().BeEquivalentTo(rectangleSizes, options => options.WithStrictOrdering());
         }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var context = TestContext.CurrentContext;
+            var arguments = context.Test.Arguments;
+            if (context.Result.Outcome == ResultState.Failure)
+            {
+                var rectangleSizes = arguments.Length > 0
+                    ? arguments[0] as Size[]
+                    : null;
+                if (rectangleSizes != null)
+                {
+                    rectangleSizes = rectangleSizes.Select(s => new Size(s.Width * 5, s.Height * 5)).ToArray();
+                    var outputFile = $"{ImageOutputDirectory.FullName}\\test_output.bmp";
+                    ImageGenerator.GenerateImageWithRectanglesFromLayouter(rectangleSizes, outputFile);
+                    TestContext.Out.WriteLine($"Tag cloud visualization saved to file \"{outputFile}\"");
+                }
+            }
+        }
+
     }
 }
