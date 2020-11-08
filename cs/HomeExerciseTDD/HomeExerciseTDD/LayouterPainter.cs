4887ab5bdd7186e4791ac796a7ee278772d9a952
fool_like_you <lambda2712@gmail.com>
2020-11-08 13:49:11 +0500

    added LayouterPainter.cs

---
+++ cs/HomeExerciseTDD/HomeExerciseTDD/LayouterPainter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+
+namespace HomeExerciseTDD
+{
+    public class LayouterPainter: IPainter<Rectangle>
+    {
+        private readonly Bitmap bitmap;
+        private readonly Graphics graphics;
+        private Pen pen;
+        private Color color;
+        private readonly Random randomazer;
+        private readonly List<Rectangle> rectangles;
+        private readonly string fileName;
+        private readonly ImageFormat format;
+        private readonly int offsetX;
+        private readonly int offsetY;
+        
+        public LayouterPainter(int width, int height, List<Rectangle> rectangles, string fileName, ImageFormat format)
+        {
+            offsetX = width/2;
+            offsetY = height/2;
+            this.format = format;
+            this.fileName = fileName;
+            this.rectangles = rectangles;
+            randomazer= new Random();
+            color = new Color();
+            bitmap = new Bitmap(width, height);
+            graphics = Graphics.FromImage(bitmap);
+        }
+
+        public void DrawFigures()
+        {
+            foreach (var rectangle in rectangles)
+            {
+                var center = rectangles.First();
+                var newX= rectangle.X + offsetX - center.X;
+                var newY = rectangle.Y + offsetY - center.Y;
+                var point = new Point(newX, newY);
+                var newRectangle = new Rectangle(point, rectangle.Size);
+
+                color = GetColor();
+                pen = new Pen(color);
+                graphics.DrawRectangle(pen, newRectangle);
+            }
+            
+            bitmap.Save(fileName, format);
+        }
+
+        private Color GetColor()
+        {
+            var R = randomazer.Next(0, 255);
+            var G = randomazer.Next(0, 255);
+            var B = randomazer.Next(0, 255);
+            
+            return Color.FromArgb(R, G, B);
+        }
+    }
+}
