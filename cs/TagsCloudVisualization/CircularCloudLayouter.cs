9862e6b585f62d3ad3bc7550734550983a9fede4
MaxCross <dan25001@gmail.com>
2020-11-09 00:53:15 +0500

    Рефакторинг CircularCloudLayouter

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -32,8 +32,8 @@ namespace TagsCloudVisualization
         private class Spiral
         {
             private Directions currentDirection = Directions.Up;
-            private Rectangle sizeRectangle;
-            private Rectangle previousSizeRectangle;
+            private Rectangle borders;
+            private Rectangle previousBorders;
             
             private readonly List<Rectangle>[] lines = new List<Rectangle>[4];
             private List<Rectangle> currentLine;
@@ -41,24 +41,7 @@ namespace TagsCloudVisualization
             public Spiral(Size firstRectangleSize)
             {
                 currentLine = new List<Rectangle> {new Rectangle(Point.Empty, firstRectangleSize)};
-                sizeRectangle = new Rectangle(Point.Empty, firstRectangleSize);
-            }
-
-            private void Turn()
-            {
-                lines[(int) currentDirection] = currentLine;
-
-                var lastRectangle = currentLine[currentLine.Count - 1];
-                currentLine = new List<Rectangle> {lastRectangle};
-                
-                previousSizeRectangle = sizeRectangle;
-                
-                var lastRectangleBorder = lastRectangle.BorderInDirection(currentDirection);
-                var sizeRectangleBorder = sizeRectangle.BorderInDirection(currentDirection);
-                if (currentDirection.CompareInDirection(lastRectangleBorder, sizeRectangleBorder) > 0)
-                    sizeRectangle = sizeRectangle.ResizedToBorderInDirection(currentDirection, lastRectangleBorder);
-
-                currentDirection = currentDirection.CounterClockwise();
+                borders = new Rectangle(Point.Empty, firstRectangleSize);
             }
 
             public Rectangle PutNextRectangle(Size size)
@@ -71,7 +54,7 @@ namespace TagsCloudVisualization
 
                 currentRectangle = currentRectangle
                     .ShiftedToRectangleBorder(currentDirection, lastRectangle)
-                    .ShiftedToRectangleBorder(fromCenterDirection, previousSizeRectangle);
+                    .ShiftedToRectangleBorder(fromCenterDirection, previousBorders);
 
                 var previousLine = lines[(int) currentDirection];
                 var intersectedRectangles = previousLine
@@ -79,7 +62,7 @@ namespace TagsCloudVisualization
                     ?? Array.Empty<Rectangle>();
                 var max = intersectedRectangles.Length > 0
                     ? intersectedRectangles.MaxBorderInDirection(fromCenterDirection)
-                    : previousSizeRectangle.BorderInDirection(fromCenterDirection);
+                    : previousBorders.BorderInDirection(fromCenterDirection);
                 currentRectangle = currentRectangle.ShiftedToBorderInDirection(toCenterDirection, max);
 
                 intersectedRectangles = lines[(int) fromCenterDirection]
@@ -93,16 +76,33 @@ namespace TagsCloudVisualization
                 currentLine.Add(currentRectangle);
                 
                 var currentRectangleBorder = currentRectangle.BorderInDirection(fromCenterDirection);
-                var sizeRectangleBorder = sizeRectangle.BorderInDirection(fromCenterDirection);
-                if(fromCenterDirection.CompareInDirection(currentRectangleBorder, sizeRectangleBorder) > 0)
-                    sizeRectangle = sizeRectangle.ResizedToBorderInDirection(fromCenterDirection, currentRectangleBorder);
+                var border = borders.BorderInDirection(fromCenterDirection);
+                if(fromCenterDirection.CompareInDirection(currentRectangleBorder, border) > 0)
+                    borders = borders.ResizedToBorderInDirection(fromCenterDirection, currentRectangleBorder);
 
                 if (currentDirection.CompareInDirection(
                     currentRectangle.BorderInDirection(currentDirection),
-                    previousSizeRectangle.BorderInDirection(currentDirection)) > 0)
+                    previousBorders.BorderInDirection(currentDirection)) > 0)
                     Turn();
 
                 return currentRectangle;
+            }
+
+            private void Turn()
+            {
+                lines[(int) currentDirection] = currentLine;
+
+                var lastRectangle = currentLine[currentLine.Count - 1];
+                currentLine = new List<Rectangle> {lastRectangle};
+                
+                previousBorders = borders;
+                
+                var lastRectangleBorder = lastRectangle.BorderInDirection(currentDirection);
+                var border = borders.BorderInDirection(currentDirection);
+                if (currentDirection.CompareInDirection(lastRectangleBorder, border) > 0)
+                    borders = borders.ResizedToBorderInDirection(currentDirection, lastRectangleBorder);
+
+                currentDirection = currentDirection.CounterClockwise();
             }
         }
     }
