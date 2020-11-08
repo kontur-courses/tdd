f302dd4914381bf05464d7a3faf46c510fab983e
fool_like_you <lambda2712@gmail.com>
2020-11-08 13:52:04 +0500

    added new tests in SpiralTests.cs

--- cs/HomeExerciseTDD/HomeExerciseTDDTests/SpiralTests.cs
+++ cs/HomeExerciseTDD/HomeExerciseTDDTests/SpiralTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Drawing;
 using FluentAssertions;
 using HomeExerciseTDD;
@@ -9,7 +10,7 @@ namespace TestProject1
     public class SpiralTests
     {
         [Test]
-        public void SpiralGetNextPoint_ReturnCenterPoint_WhenFirstCall()
+        public void GetNextPoint_ReturnCenterPoint_WhenFirstCall()
         {
             var center = new Point(22, 22);
             Spiral spiral = new Spiral(center);
@@ -18,16 +19,38 @@ namespace TestProject1
 
             nextPoint.Should().BeEquivalentTo(center);
         }
+        
         [Test]
-        public void SpiralGetNextPoint_ReturnOtherPoint_WhenNotFirstCall()
+        public void GetNextPoint_ReturnOtherPoint_WhenNotFirstCall()
         {
             var center = new Point(22, 22);
             Spiral spiral = new Spiral(center);
 
             var nextPoint = spiral.GetNextPoint();
-            nextPoint = spiral.GetNextPoint();
 
             nextPoint.Should().NotBeEquivalentTo(spiral.GetNextPoint());
+        }
+        
+        [Test]
+        public void GetNextPoint_ReturnPointСorrespondsSpiral_WhenManyCalls()
+        {
+            var spiralPoints = new List<Point>();
+            var center = new Point(22, 22);
+            Spiral spiral = new Spiral(center);
+            var expectedSpiralPoints = new List<Point>
+            {
+                new Point(22, 22),
+                new Point(21, 22),
+                new Point(21, 21),
+                new Point(22, 21)
+            };
+
+            spiralPoints.Add(spiral.GetNextPoint());
+            spiralPoints.Add(spiral.GetNextPoint());
+            spiralPoints.Add(spiral.GetNextPoint());
+            spiralPoints.Add(spiral.GetNextPoint());
+
+            spiralPoints.Should().BeEquivalentTo(expectedSpiralPoints);
         }
     }
 }
