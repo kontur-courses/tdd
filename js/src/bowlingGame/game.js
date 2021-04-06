f08a4d8a938f53b36ad37258d773f4662827887d
Прилепа Олег Андреевич <o.a.prilepa@skbkontur.ru>
2021-04-06 19:34:39 +0700

    план по тестам, код по итогу обсуждений в Discord

--- js/src/bowlingGame/game.js
+++ js/src/bowlingGame/game.js
@@ -1,24 +1,72 @@
 import "chai/register-should";
 import {beginAndEndWithReporting} from "./infrastructure/reportingTest";
 
-// cd js
-// npm install
-// npm run bowling
-
 class Game {
+    constructor() {
+        this.frameIndex = 0;
+        this.history = [
+            {rolls: []}
+        ];
+    }
+
     roll(pins) {
+        const rolls = this.history[this.frameIndex].rolls;
+        rolls.push(pins);
+        if (rolls.length > 1) {
+            this.frameUp();
+        }
+    }
+
+    frameUp() {
+        this.frameIndex++;
+        this.history[this.frameIndex] = {rolls: []};
     }
 
     getScore() {
-        throw new Error("Not implemented!");
+        return this.history.reduce((accumulator, currentValue)=>{
+            let current = currentValue.rolls.reduce((a, b) => a + b, 0);
+
+            if (currentValue.rolls.length === 2 && current === 10) { // spare
+                current ++;
+            }
+
+            return accumulator + current;
+        }, 0);
     }
 }
 
-describe("Game", () => {
-    it("should have zero score before any rolls", () => {
+describe("Боулинг", () => {
+    it("В начале игры должно быть 0 очков", () => {
         const game = new Game();
         game.getScore().should.be.eq(0);
     });
+
+    it("После броска в 10 очков должно быть 10 очков", () => {
+        const game = new Game();
+        game.roll(10);
+        game.getScore().should.be.eq(10);
+    });
+
+    it("После двух простых бросков на 2 и 5 сумма очков 7", () => {
+        const game = new Game();
+        game.roll(2);
+        game.roll(5);
+        game.getScore().should.be.eq(7);
+    });
+
+    it("После броска 2+8 (spare) и 3 во в следующем фрейме должно быть 16 очков", () => {
+        const game = new Game();
+        game.roll(2);
+        game.roll(8);
+        game.roll(3);
+        game.getScore().should.be.eq(16);
+    });
+
+    it("Два spare подряд");
+    it("strike");
+    it("double strike");
+    it("all strikes");
+    it("Третий бросок в последнем фрейме");
 
     beginAndEndWithReporting();
 });
