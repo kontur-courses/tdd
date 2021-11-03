f0a682d12b1c7f2d895315018eb81a5270e92836
SquirrelLeonid <rm200002@yandex.ru>
2021-11-04 01:11:46 +0500

    Created projects:
    --> TagCloudVisualization
    --> TagCloudVisualization_Tests
    __________________________________
    Implemented some functionality via TDD method
    --> CircularCloudLayouter able to put number of  rectangles without intersection. But they will be located diagonally.

---
+++ cs/TagCloud/CircularCloudLayouter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagCloud
+{
+    public class CircularCloudLayouter
+    {
+        private readonly Point CENTER;
+        private readonly List<Rectangle> rectangles;
+        private const int DEFAULT_SPACE_BETWEEN_RECTANGLES = 5;
+
+        public CircularCloudLayouter(Point center)
+        {
+            CENTER = center;
+            rectangles = new List<Rectangle>();
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            ThrowIfIncorrectSize(rectangleSize);
+            var rectangle = new Rectangle(Point.Empty, rectangleSize);
+
+            if (rectangles.Count == 0)
+                rectangle.Location = GetOffsetFromCenter(rectangleSize);
+            else
+            {
+                var prevRect = rectangles[rectangles.Count - 1];
+                rectangle.Location =
+                    new Point(prevRect.Location.X + prevRect.Width + DEFAULT_SPACE_BETWEEN_RECTANGLES,
+                        prevRect.Location.Y + prevRect.Height + DEFAULT_SPACE_BETWEEN_RECTANGLES);
+            }
+
+            rectangles.Add(rectangle);
+            return rectangle;
+        }
+
+        public Point GetCenterPoint()
+        {
+            return CENTER;
+        }
+
+        public Point GetOffsetFromCenter(Size rectangleSize)
+        {
+            return new Point(CENTER.X - rectangleSize.Width / 2, CENTER.Y - rectangleSize.Height / 2);
+        }
+
+        private void ThrowIfIncorrectSize(Size rectangleSize)
+        {
+            if (rectangleSize.Width <= 0 || rectangleSize.Height <= 0)
+                throw new ArgumentException("Width and height of rectangle must be a positive numbers");
+        }
+    }
+}
---
+++ cs/TagCloud_Tests/CircularLayouter_Should.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud;
+
+namespace TagCloud_Tests
+{
+    public class CircularLayouter_Should
+    {
+        [Test]
+        public void NotThrow_WhenCreated()
+        {
+            Action act = () => new CircularCloudLayouter(new Point(0, 0));
+            act.Should().NotThrow();
+        }
+
+        [Test]
+        public void Have_CenterPoint_AfterCreate()
+        {
+            var centerPoint = new Point(0, 0);
+
+            var layouter = new CircularCloudLayouter(centerPoint);
+
+            layouter.GetCenterPoint().Should().BeEquivalentTo(centerPoint);
+        }
+
+        [Test]
+        public void ReturnRectangle_WhenPutNext()
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+
+            var expectedRect = new Rectangle(new Point(0, 0), new Size(10, 10));
+            var actualRect = layouter.PutNextRectangle(new Size(10, 10));
+
+            actualRect.Size.Should().BeEquivalentTo(expectedRect.Size);
+        }
+
+        [Test]
+        public void Allow_NegativeCoordinates_ForCloudCenter()
+        {
+            var center = new Point(-1, -1);
+
+            var layouter = new CircularCloudLayouter(center);
+
+            layouter.GetCenterPoint().Should().BeEquivalentTo(center);
+        }
+
+        [Test]
+        public void ThrowArgumentException_WhenNotPositiveSize(
+            [ValueSource(nameof(IncorrectRectangleSizes))]
+            Size rectSize)
+        {
+            var layouter = new CircularCloudLayouter(new Point(0,0));
+
+            Action act = () => layouter.PutNextRectangle(rectSize);
+
+            act.Should().Throw<ArgumentException>("Negative size not allowed");
+        }
+
+        [Test]
+        public void PutFirstRectangle_InCenter()
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            var rectSize = new Size(10, 10);
+
+            var rect = layouter.PutNextRectangle(rectSize);
+
+            rect.Location
+                .Should()
+                .BeEquivalentTo(new Point(-5, -5), "because first rectangle should align by its middle point");
+        }
+
+        [Test]
+        public void NotIntersect_TwoRectangles()
+        {
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+
+            var firstRect = layouter.PutNextRectangle(new Size(10, 10));
+            var secondRect = layouter.PutNextRectangle(new Size(5, 15));
+
+            firstRect.IntersectsWith(secondRect).Should().BeFalse();
+        }
+
+        private static IEnumerable<Size> IncorrectRectangleSizes()
+        {
+            yield return new Size(-1, -1);
+            yield return new Size(-1, 0);
+            yield return new Size(0, -1);
+            yield return new Size(0, 0);
+        }
+    }
+}
