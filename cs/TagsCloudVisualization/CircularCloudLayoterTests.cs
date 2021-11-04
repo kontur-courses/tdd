8d91dc421294552facc0ec072225c7903f5224f3
SlivnyiArtem <slivnay1965@gmail.com>
2021-11-04 17:11:15 +0500

    Add first methods and tests

---
+++ cs/TagsCloudVisualization/CircularCloudLayoterTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayoterTests
+    {
+        [Test]
+        public void ShouldCreateCircularCloud()
+        {
+            var center = new Point(15, 20);
+            Action creating = () => new CircularCloudLayouter(center);
+            creating.Should().NotThrow();
+        }
+
+        [Test]
+        public void ShouldCreateCenterCorrectly()
+        {
+            var center = new Point(10, 30);
+            var layouter =  new CircularCloudLayouter(center);
+            layouter.Center.Should().Be(new Point(10,30));
+        }
+
+        [Test]
+        public void ShouldPutOneRectangleInRightLocation()
+        {
+            var rectangleSize = new Size(30, 10);
+            var layouterCenter = new Point(20, 10);
+            var layouter = new CircularCloudLayouter(layouterCenter);
+            var rectangle = layouter.PutNextRectangle(rectangleSize);
+            rectangle.Location.Should().Be(new Point(5, 5));
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter : ICircularCloudLayouter
+    {
+        public Point Center
+        { get; set; }
+        public CircularCloudLayouter(Point center)
+        {
+            Center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            Point rectangleLocation = new Point(Center.X - rectangleSize.Width/2, Center.Y - rectangleSize.Height/2);
+            var rectangle = new Rectangle(rectangleLocation, rectangleSize);
+            return rectangle;
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
