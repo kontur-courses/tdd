552da39682b2783d1bc4aa77daf210e0fe6fa74e
Aripov Sergey <aripovsergey3@gmail.com>
2021-11-04 22:35:49 +0500

    Исправление 2 задачи n1 + начало 3 задачи

--- cs/TagsCloudVisualization/Visualizer.cs
+++ cs/TagsCloudVisualization/Visualizer.cs
@@ -9,26 +9,40 @@ namespace TagsCloudVisualization
     {
         public static Bitmap GetRandomBitmap(int count, double scale, bool guidingCircle = true)
         {
-            var rectangles = GenerateRandomRectangles(count, scale, out var width, out var height);
-            var bitmap = CreateBitmap(rectangles, width, height, $"random-count{count}-scale{scale}", guidingCircle);
+            var rectangles = GenerateRandomRectangles(count, scale);
+            var (width, height) = FitRectangles(rectangles);
+            ReplaceRectangles(rectangles, width, height);
+
+            var bitmap = CreateBitmap(rectangles, width, height, $"random-count{count}-scale{scale}", guidingCircle, true);
 
             return bitmap;
         }
 
-        private static IEnumerable<Rectangle> GenerateRandomRectangles(int count, double scale, out int width, out int height)
+        private static void ReplaceRectangles(Rectangle[] rectangles, int width, int height)
         {
-            var layouter = new CircularCloudLayouter(Point.Empty);
-            var rnd = new Random();
-            var rectangles = new List<Rectangle>(count);
+            var offsetX = width / 2;
+            var offsetY = height / 2;
+            OffsetRectangles(rectangles, offsetX, offsetY);
+        }
+
+        public static Bitmap GetBitmapFromRectangles(Rectangle[] rectangles, bool guidingCircle = false)
+        {
+            var (width, height) = FitRectangles(rectangles);
+            ReplaceRectangles(rectangles, width, height);
+
+            var bitmap = CreateBitmap(rectangles, width, height, null, guidingCircle, false);
+
+            return bitmap;
+        }
+
+        private static (int width, int height) FitRectangles(Rectangle[] rectangles)
+        {
             var maxHeight = 0;
             var minHeight = 0;
             var maxWidth = 0;
             var minWidth = 0;
-            for (var i = 0; i < count; i++)
+            foreach (var rectangle in rectangles)
             {
-                var size = new Size((int)(rnd.Next(3, 100) * scale), (int)(rnd.Next(5, 20) * scale));
-                var rectangle = layouter.PutNextRectangle(size);
-                rectangles.Add(rectangle);
                 if (rectangle.Top < maxHeight)
                     maxHeight = rectangle.Top;
                 if (rectangle.Bottom > minHeight)
@@ -39,22 +53,34 @@ namespace TagsCloudVisualization
                     minWidth = rectangle.Left;
             }
 
-            width = Math.Abs(minWidth) + Math.Abs(maxWidth) + 100;
-            height = Math.Abs(minHeight) + Math.Abs(maxHeight) + 100;
-            var offsetX = width / 2;
-            var offsetY = height / 2;
+            var width = Math.Abs(minWidth) + Math.Abs(maxWidth) + 100;
+            var height = Math.Abs(minHeight) + Math.Abs(maxHeight) + 100;
 
-            return OffsetRectangles(rectangles.ToArray(), offsetX, offsetY);
+            return (width, height);
         }
 
-        private static Rectangle[] OffsetRectangles(Rectangle[] rectangles, int offsetX, int offsetY)
+        private static Rectangle[] GenerateRandomRectangles(int count, double scale)
+        {
+            var layouter = new CircularCloudLayouter(Point.Empty);
+            var rnd = new Random();
+            var rectangles = new List<Rectangle>(count);
+            for (var i = 0; i < count; i++)
+            {
+                var size = new Size((int)(rnd.Next(5, 100) * scale), (int)(rnd.Next(3, 30) * scale));
+                rectangles.Add(layouter.PutNextRectangle(size));
+            }
+
+            return rectangles.ToArray();
+        }
+
+        private static void OffsetRectangles(Rectangle[] rectangles, int offsetX, int offsetY)
         {
             for (var i = 0; i < rectangles.Length; i++)
                 rectangles[i].Offset(offsetX, offsetY);
-            return rectangles;
         }
 
-        private static Bitmap CreateBitmap(IEnumerable<Rectangle> rectangles, int width, int height, string name, bool guidingCircle = false)
+        private static Bitmap CreateBitmap(IEnumerable<Rectangle> rectangles, int width, int height, string name,
+            bool guidingCircle = false, bool autoSave = false)
         {
             var bitmap = new Bitmap(width, height);
             using (var bitmapGraphics = Graphics.FromImage(bitmap))
@@ -73,7 +99,8 @@ namespace TagsCloudVisualization
                 }
             }
 
-            bitmap.Save($"{name}.png", ImageFormat.Png);
+            if (autoSave)
+                bitmap.Save($"{name}.png", ImageFormat.Png);
             return bitmap;
         }
     }
