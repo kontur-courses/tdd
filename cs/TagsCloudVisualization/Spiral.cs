9d8d19bd32cd595179de5918ff4e9059122a6106
nazar_garipov <nazar.garipov475@gmail.com>
2021-11-05 21:48:32 +0500

    Remade spiral func

---
+++ cs/TagsCloudVisualization/Spiral.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class Spiral
+    {
+        private static Dictionary<Size, int> sizeToParameter = new();
+
+        public static IEnumerable<Point> GetCoordinates(Size tagSize, double spiralPitch)
+        {
+            var parameter = Math.PI / 2;
+            if (sizeToParameter.ContainsKey(tagSize))
+                parameter = sizeToParameter[tagSize];
+            while (true)
+            {
+                //yield return GetArchimedeanSpiral(parameter, spiralPitch);    
+                parameter += Math.PI / (((int)parameter + 1) * 4);
+            }
+        }
+
+        
+
+        private static void PolarToCartesian(float r, float theta,
+            out float x, out float y)
+        {
+            x = (float)(r * Math.Cos(theta));
+            y = (float)(r * Math.Sin(theta));
+        }
+
+        private static Point GetArchimedeanSpiral(double paramteter, double pitch)
+        {
+            var coefficient = pitch / 2 * Math.PI;
+            return new Point((int)(coefficient * paramteter * Math.Cos(paramteter)),
+                (int)(coefficient * paramteter * Math.Sin(paramteter)));
+        }
+    }
+}
