9618e53ec4db903e1c45bac9dceb11ed23ecb6e1
DaGnYMilleR <danil.lunyow@mail.ru>
2021-11-05 19:46:16 +0500

    small refactoring of drawers

--- cs/Homework/TagsCloud.Tests/TestDrawer.cs
+++ cs/Homework/TagsCloud.Tests/TestDrawer.cs
@@ -12,7 +12,10 @@ namespace TagsCloud.Tests
             var center = rectangles.First().GetCenter();
             var pen = new Pen(Color.Chocolate);
             graphics.DrawRectangles(pen, rectangles);
-            graphics.FillEllipse(Brushes.Red, new Rectangle(center.X, center.Y, 3, 3));
+            graphics.FillEllipse(Brushes.Red, InitRectangleFromCenter(center, 3, 3));
         }
+
+        private Rectangle InitRectangleFromCenter(Point center, int width, int height) 
+            => new(center.X - width / 2, center.Y - height / 2, width, height);
     }
 }
--- cs/Homework/TagsCloud.Visualization/Drawer/Drawer.cs
+++ cs/Homework/TagsCloud.Visualization/Drawer/Drawer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using TagsCloud.Visualization.Extensions;
@@ -7,15 +8,19 @@ namespace TagsCloud.Visualization.Drawer
 {
     public abstract class Drawer : IDrawer
     {
-        public Image Draw(Rectangle[] rectangles)
+        public Image Draw([NotNull] Rectangle[] rectangles)
         {
+            if (rectangles.Length == 0)
+                throw new ArgumentException($"rectangles array cannot be empty");
+            
             var (width, height) = GetWidthAndHeight(rectangles);
             var (widthWithOffset, heightWithOffset) = (width + 100, height + 100);
-            var center = rectangles.First().GetCenter(); //TODO extract new entity that will encapsulate cloud
+            var center = rectangles.First().GetCenter();
             var bitmap = new Bitmap(widthWithOffset, heightWithOffset);
             using var graphics = Graphics.FromImage(bitmap);
 
             graphics.TranslateTransform(center.X + widthWithOffset / 2, center.Y + heightWithOffset / 2);
+
             Transform(graphics, rectangles);
 
             return bitmap;
