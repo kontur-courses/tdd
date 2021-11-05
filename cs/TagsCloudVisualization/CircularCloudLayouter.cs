8dd1f26a596f2e493e5423b52b6c8c777a12d478
nazar_garipov <nazar.garipov475@gmail.com>
2021-11-05 21:48:07 +0500

    Implement CircularCloudLayouter

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        private readonly List<Rectangle> tagCloud = new();
+        private readonly Point center;
+        private int minRadius = 0;
+
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            var tag = new Rectangle(GetNextPosition(rectangleSize), rectangleSize);
+            tagCloud.Add(tag);
+            return tag;
+        }
+
+        public IEnumerable<Rectangle> GetCloud()
+        {
+            return tagCloud;
+        }
+
+        private Point GetNextPosition(Size newRectangleSize)
+        {
+            return GetNextNearestPositionForTag(newRectangleSize);
+        }
+
+        private Point GetNextNearestPositionForTag(Size size)
+        {
+            return GetPointsFrom(minRadius)
+                .Select(p => new Rectangle(p-size/2, size))
+                .First(r => !IsIntersectWithCloud(r)).Location;
+        }
+
+        private bool IsIntersectWithCloud(Rectangle newTag)
+        {
+            foreach (var tag in tagCloud)
+                if (tag.IntersectsWith(newTag))
+                    return true;
+            return false;
+
+        }
+
+        private IEnumerable<Point> GetPointsFrom(int radius)
+        {
+            while (true)
+            {
+                foreach (var p in GetPointsInRadius(radius++))
+                    yield return p;
+            }
+        }
+
+        private IEnumerable<Point> GetPointsInRadius(int radius)
+        {
+            return GetPointsInRadiusInZeroCenter(radius)
+                .Select(point => new Point(point.X + center.X, point.Y + center.Y));
+        }
+
+        private IEnumerable<Point> GetPointsInRadiusInZeroCenter(int radius)
+        {
+            yield return new Point(-radius, 0);
+            for (var x = -radius + 1; x < radius; x++)
+            {
+                yield return new Point(x, (int)Math.Sqrt(radius*radius - x*x));
+                yield return new Point(x, -(int)Math.Sqrt(radius*radius - x*x));
+            }
+            yield return new Point(radius, 0);
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    class CircularCloudLayouter_Tests
+    {
+        [TestCase(0, TestName = "return empty cloud? if we dont put")]
+        [TestCase(1, TestName = "Return one tag, if we put only one")]
+        [TestCase(10, TestName = "Return all tags, if we put multiple")]
+        public void GetCloud_ReturnAll_IfPutMultipleTags(int countOfTags)
+        {
+            var cloudLayouter = new CircularCloudLayouter(new Point(0, 0));
+            for (var i = 0; i < countOfTags; i++)
+                cloudLayouter.PutNextRectangle(new Size(1, 1));
+
+            var tagCloud = cloudLayouter.GetCloud();
+
+            tagCloud.Count().Should().Be(countOfTags);
+        }
+
+        [TestCase(0, 0, 2, 2, TestName = "Even width and height in zero center")]
+        [TestCase(0, 0, 9, 9, TestName = "Odd width and height in zero center")]
+        [TestCase(0, 1, 2, 5, TestName = "Different width and height")]
+        [TestCase(3, 3, 1, 1, TestName = "Width and height greater than center coordinates")]
+        [TestCase(3, 6, 9, 1, TestName = "Different parameters and different center coordinates")]
+        [TestCase(-4, -3, 2, 6, TestName = "Negative center coordinates")]
+        public void GetCloud_ReturnFirstTagInCenter_IfAddOneTag(int xCloudPosition, int yCloudPosition, int width,
+            int height)
+        {
+            var cloudLayouter = new CircularCloudLayouter(new Point(xCloudPosition, yCloudPosition));
+            cloudLayouter.PutNextRectangle(new Size(width, height));
+
+            var tag = cloudLayouter.GetCloud().First();
+
+            var xCenter = (tag.Left + tag.Right) / 2;
+            var yCenter = (tag.Top + tag.Bottom) / 2;
+            xCenter.Should().Be(xCloudPosition);
+            yCenter.Should().Be(yCloudPosition);
+        }
+
+        [TestCase(0, 0, 1, 1, 4, TestName = "Two squares in zero position")]
+        [TestCase(5, -9, 2, 6, 7, TestName = "Many squares in ")]
+        [TestCase(0, 0, 2, 2, 12, TestName = "Two squares s")]
+        public void GetCloud_СenterLiesWithinBoundariesOfTheCloud(int xCloudPosition,
+            int yCloudPosition, int sizeX, int sizeY, int count)
+        {
+            var center = new Point(xCloudPosition, yCloudPosition);
+            var cloudLayouter = new CircularCloudLayouter(center);
+            for (var i = 0; i < count; i++)
+                cloudLayouter.PutNextRectangle(new Size(sizeX, sizeY));
+
+            var cloud = cloudLayouter.GetCloud();
+            var bounds = GetInnerBounds(cloud.ToArray());
+
+            bounds.Contains(center).Should().BeTrue();
+        }
+
+        private Rectangle GetInnerBounds(Rectangle[] tags)
+        {
+            return tags.Aggregate(new Rectangle(), 
+                (current, tag) => Rectangle.Union(current, tag));
+        }
+    }
+}
