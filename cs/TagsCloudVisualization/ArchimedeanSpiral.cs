4a8e34b627f68510aaa0d4d5836839e12cce4113
Tripletri <Tripletri@yandex.ru>
2021-11-05 14:02:39 +0500

    Создан класс ArchimedeanSpiral.

---
+++ cs/TagsCloudVisualization/ArchimedeanSpiral.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class ArchimedeanSpiral
+    {
+        private readonly Point center;
+        private readonly int radius;
+
+        public ArchimedeanSpiral(Point center, int radius)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        public Point GetPoint(int degree)
+        {
+            var angle = degree * Math.PI / 180;
+            var length = radius * angle;
+            return new Point
+            {
+                X = (int)(length * Math.Cos(angle)) + center.X,
+                Y = -(int)(length * Math.Sin(angle)) + center.Y
+            };
+        }
+    }
+}
---
+++ cs/TagsCloudVisualizationTests/ArchimedeanSpiralTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using TagsCloudVisualization;
+using FluentAssertions;
+
+namespace TagsCloudVisualizationTests
+{
+    public class ArchimedeanSpiralTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 2)]
+        [TestCase(-1, -2)]
+        public void GetPoint_WithoutAngle_InCenter(int x, int y)
+        {
+            var center = new Point(x, y);
+            var spiral = new ArchimedeanSpiral(center, 1);
+
+            var actualPoint = spiral.GetPoint(0);
+
+            actualPoint.Should().Be(center);
+        }
+
+        [Test]
+        public void GetPoint_Every90DegreePeriod_YProportionalToRadius() =>
+            AssertProportionAlongAngle(90, period => -(0.5 + period * 2), point => point.Y);
+
+        [Test]
+        public void GetPoint_Every270DegreePeriod_YProportionalToRadius() =>
+            AssertProportionAlongAngle(270, period => 1.5 + period * 2, point => point.Y);
+
+        [Test]
+        public void GetPoint_Every0DegreePeriod_XProportionalToRadius() =>
+            AssertProportionAlongAngle(0, period => period * 2, point => point.X);
+
+        [Test]
+        public void GetPoint_Every180DegreePeriod_XProportionalToRadius() =>
+            AssertProportionAlongAngle(180, period => -(1 + period * 2), point => point.X);
+
+        private static void AssertProportionAlongAngle(int angleInDegrees, Func<int, double> getPeriodProportion,
+            Func<Point, int> selectCoordinateToAssert)
+        {
+            const int radius = 10;
+            var spiral = new ArchimedeanSpiral(new Point(0, 0), radius);
+
+            for (var period = 0; period < 100; period++)
+            {
+                var expectedProportion = getPeriodProportion(period);
+                var expected = expectedProportion * Math.PI * radius;
+                var degree = angleInDegrees + 360 * period;
+
+                var actual = spiral.GetPoint(degree);
+
+                ((double)selectCoordinateToAssert(actual)).Should().BeApproximately(expected, 1,
+                    $"period is '{period}', and proportion should be '{expectedProportion}'");
+            }
+        }
+    }
+}
