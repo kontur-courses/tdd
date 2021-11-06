ae8cd6b8b4520ea91bea2c80982c25055417f74f
makspanshin <makspanshin97@gmail.com>
2021-11-07 01:15:07 +0500

    Create Spiral class
    Класс реализует спираль Архимеда.

---
+++ cs/TagsCloudVisualization/Spiral.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    /// <summary>
+    /// Класс реалзует спираль Архимеда
+    /// </summary>
+    public class Spiral
+    {
+        private const double TwoPi = 2 * Math.PI;
+
+        private const double Step = 0.5;
+
+        private int countPoints;
+
+        private Point center;
+
+        public Spiral(Point center)
+        {
+            this.center = center;
+            countPoints = 0;
+        }
+
+        public Point CalcPointSpiral()
+        {
+            var p = Step * countPoints / TwoPi;
+            var x = (int)(p * Math.Cos(countPoints + 1)) + center.X;
+            var y = (int)(p * Math.Sin(countPoints + 1)) + center.Y;
+
+            countPoints++;
+
+            return new Point(x, y);
+        }
+    }
+}
