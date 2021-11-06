af8f4ffdbc52013909eb77784db64ce648f4ce0f
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-06 10:03:25 +0500

    Add xml-comment to GetRotationDirection method

--- TagsCloudVisualization/ConvexHullBuilder.cs
+++ TagsCloudVisualization/ConvexHullBuilder.cs
@@ -5,6 +5,18 @@ namespace TagsCloudVisualization
 {
     public static class ConvexHullBuilder
     {
+        /// <summary>
+        /// Returns the integer that indicates point location relative to the vector.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="point"></param>
+        /// <returns>A number that indicates <paramref name="point"/> location relative to the
+        /// <paramref name="vector"/>.
+        /// Return value meaning: -1 <paramref name="point"/> is located to the right
+        /// of the <paramref name="vector"/>.
+        /// 0 <paramref name="point"/> is located on the <paramref name="vector"/>.
+        /// 1 <paramref name="point"/> is located to the left of the <paramref name="vector"/>.
+        /// </returns>
         public static int GetRotationDirection(Vector vector, Point point)
         {
             var vectorProduct = (vector.End.X - vector.Begin.X) * (point.Y - vector.Begin.Y) 
