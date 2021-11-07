bb2115a4f1fa0c209fc098c33b0fd8013625cd0c
makspanshin <makspanshin97@gmail.com>
2021-11-07 13:28:28 +0500

    Implementation of the PainterOfRectangles
    Класс для отрисовки CircularCloudLayouter

--- cs/TagsCloudVisualization/PainterOfRectangles.cs
+++ cs/TagsCloudVisualization/PainterOfRectangles.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace TagsCloudVisualization
 {
-
-    // TODO:Реализовать класс для отрисовки прямоугольников
-    class PainterOfRectangles
+    public class PainterOfRectangles
     {
+        private Size pictSize;
+        public PainterOfRectangles(Size sizeOfPicture)
+        {
+            pictSize = sizeOfPicture;
+        }
+
+        public void CreateImageOfRectangles(List<Rectangle> rectangles,string filename = "rectangles")
+        {
+            Bitmap bmp = new Bitmap(pictSize.Width,pictSize.Height);
+
+            Graphics graphics = Graphics.FromImage(bmp);
+
+            foreach (var rectangle in rectangles)
+            {
+                graphics.DrawRectangle(new Pen(Color.Blue, .5f), rectangle);
+            }
+
+            bmp.Save(filename);
+
+            bmp.Dispose();
+            graphics.Dispose();
+        }
     }
 }
