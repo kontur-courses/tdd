39d4ed57eb0707bab878bdd2a397553cd4fc76ad
gODealOAple <maksim.math@gmail.com>
2021-11-07 18:32:11 +0500

    Rename DrawerSettings to TagsCloudGenerator and put some logic there from Program.cs

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using TagsCloudVisualization.ColorGenerators;
 
 // Disable warning https://docs.microsoft.com/ru-ru/dotnet/fundamentals/code-analysis/quality-rules/ca1416
@@ -15,56 +14,48 @@ namespace TagsCloudVisualization
     internal class Program
     {
         private static readonly Size GeneratedImageSize = new(1000, 1000);
-        private static readonly SizeF CloudScale = new(0.7f, 0.7f);
-        private static readonly Color BackgroundColor = Color.Black;
 
         private static void Main(string[] args)
         {
             var directory = Path.Combine(Directory.GetCurrentDirectory(), "GeneratedClouds");
             if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
-            foreach (var settings in GetSettings())
+            foreach (var generator in GetGenerators())
             {
                 using var bitmap = new Bitmap(GeneratedImageSize.Width, GeneratedImageSize.Height);
-                settings.Draw(bitmap, CloudScale);
+                generator.Generate(bitmap);
                 var filename = GenerateFileName();
                 var path = Path.Combine(directory, filename + ".bmp");
                 bitmap.Save(path, ImageFormat.Bmp);
             }
         }
 
-        private static IEnumerable<DrawerSettings> GetSettings()
+        private static IEnumerable<TagsCloudGenerator> GetGenerators()
         {
             var rnd = new Random();
-            yield return new DrawerSettings(GenerateRectangles(
-                    50,
-                    new CircularCloudLayouter(new Point()),
-                    () => new Size(rnd.Next(30, 50), rnd.Next(20, 30))),
-                new TagsCloudDrawer(BackgroundColor, new RandomColorGenerator(rnd)));
-            yield return new DrawerSettings(GenerateRectangles(
-                    100,
-                    new CircularCloudLayouter(new Point()),
-                    () => new Size(rnd.Next(40, 50), rnd.Next(20, 30))),
-                new TagsCloudDrawer(BackgroundColor, new RainbowColorGenerator(rnd)));
-            yield return new DrawerSettings(GenerateRectangles(
-                    1000,
-                    new CircularCloudLayouter(new Point()),
-                    () => new Size(rnd.Next(40, 50), rnd.Next(20, 30))),
-                new TagsCloudDrawer(BackgroundColor, new RainbowColorGenerator(rnd)));
-            yield return new DrawerSettings(GenerateRectangles(
-                    1000,
-                    new CircularCloudLayouter(new Point()),
-                    () => new Size(rnd.Next(10, 50), rnd.Next(10, 50))),
-                new TagsCloudDrawer(BackgroundColor, new GrayscaleColorGenerator(rnd)));
-        }
-
-        private static Rectangle[] GenerateRectangles(int count, CircularCloudLayouter layouter, Func<Size> sizeFactory)
-        {
-            if (layouter == null) throw new ArgumentNullException(nameof(layouter));
-            if (sizeFactory == null) throw new ArgumentNullException(nameof(sizeFactory));
-            if (count <= 0) throw new ArgumentException($"{nameof(count)} should be positive");
-            return Enumerable.Range(0, count)
-                .Select(x => layouter.PutNextRectangle(sizeFactory()))
-                .ToArray();
+            yield return new TagsCloudGenerator(
+                50,
+                new SizeF(0.7f, 0.7f),
+                new CircularCloudLayouter(new Point()),
+                () => new Size(rnd.Next(30, 50), rnd.Next(20, 30)),
+                new TagsCloudDrawer(Color.Gray, new RandomColorGenerator(rnd)));
+            yield return new TagsCloudGenerator(
+                100,
+                new SizeF(0.7f, 0.7f),
+                new CircularCloudLayouter(new Point()),
+                () => new Size(rnd.Next(40, 50), rnd.Next(20, 30)),
+                new TagsCloudDrawer(Color.Gray, new RainbowColorGenerator(rnd)));
+            yield return new TagsCloudGenerator(
+                1000,
+                new SizeF(0.7f, 0.7f),
+                new CircularCloudLayouter(new Point()),
+                () => new Size(rnd.Next(40, 50), rnd.Next(20, 30)),
+                new TagsCloudDrawer(Color.Gray, new RainbowColorGenerator(rnd)));
+            yield return new TagsCloudGenerator(
+                1000,
+                new SizeF(0.7f, 0.7f),
+                new CircularCloudLayouter(new Point()),
+                () => new Size(rnd.Next(10, 50), rnd.Next(10, 50)),
+                new TagsCloudDrawer(Color.Gray, new GrayscaleColorGenerator(rnd)));
         }
 
         private static string GenerateFileName()
---
+++ cs/TagsCloudVisualization/TagsCloudGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudVisualization
+{
+    public class TagsCloudGenerator
+    {
+        private readonly TagsCloudDrawer _drawer;
+        private readonly CircularCloudLayouter _layouter;
+        private readonly int _rectanglesCount;
+        private readonly SizeF _cloudScale;
+        private readonly Func<Size> _sizeFactory;
+
+        public TagsCloudGenerator(
+            int rectanglesCount, 
+            SizeF cloudScale,
+            CircularCloudLayouter layouter, 
+            Func<Size> sizeFactory,
+            TagsCloudDrawer drawer)
+        {
+            if (cloudScale.Width <= 0 || cloudScale.Height <= 0)
+                throw new ArgumentException(
+                    $"{nameof(cloudScale)} expected positive dimensions, but actually negative");
+            if (rectanglesCount <= 0) throw new ArgumentException(
+                $"{nameof(rectanglesCount)} should be positive");
+            _rectanglesCount = rectanglesCount;
+            _cloudScale = cloudScale;
+            _layouter = layouter ?? throw new ArgumentNullException(nameof(layouter));
+            _sizeFactory = sizeFactory ?? throw new ArgumentNullException(nameof(sizeFactory));
+            _drawer = drawer ?? throw new ArgumentNullException(nameof(drawer));
+        }
+
+        public void Generate(Bitmap bitmap)
+        {
+            if (bitmap == null) throw new ArgumentNullException(nameof(bitmap));
+            _drawer.Draw(bitmap, GenerateRectangles(), _cloudScale);
+        }
+
+        private Rectangle[] GenerateRectangles()
+        {
+            return Enumerable.Range(0, _rectanglesCount)
+                .Select(x => _layouter.PutNextRectangle(_sizeFactory()))
+                .ToArray();
+        }
+    }
+}
