aad996f8fff3e457342e0fb3d6b937cd3afb849d
makspanshin <makspanshin97@gmail.com>
2021-11-07 12:26:59 +0500

    Create PainterOfRectangles.cs

---
+++ cs/TagsCloudVisualization/PainterOfRectangles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+
+    // TODO:Реализовать класс для отрисовки прямоугольников
+    class PainterOfRectangles
+    {
+    }
+}
