caed02deda07142026630166218c4afdb44deb92
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-07 11:21:12 +0500

    Add vector initialization test

---
+++ TagsCloudVisualization/VectorShould.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    class VectorShould
+    {
+        [Test]
+        public void InitializeFieldsAfterInstanceCreation()
+        {
+            var begin = new Point(0, 0);
+            var end = new Point(1, 1);
+
+            var vector = new Vector(begin, end);
+
+            vector.Begin.Should().BeEquivalentTo(begin);
+            vector.End.Should().BeEquivalentTo(end);
+        }
+    }
+}
