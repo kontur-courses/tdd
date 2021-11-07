2d6df4511b6d70e46c503d54c43506eecf9c724f
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-07 11:32:27 +0500

    Add vector length test

--- TagsCloudVisualization/VectorShould.cs
+++ TagsCloudVisualization/VectorShould.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -17,6 +18,27 @@ namespace TagsCloudVisualization
 
             vector.Begin.Should().BeEquivalentTo(begin);
             vector.End.Should().BeEquivalentTo(end);
+        }
+
+        [TestCaseSource(nameof(VectorLengthTestData))]
+        public void CalculateCorrectLength(Point begin, Point end, double expectedLength)
+        {
+            var vectorLength = new Vector(begin, end).GetLength();
+
+            vectorLength.Should().Be(expectedLength);
+        }
+
+        private static IEnumerable<TestCaseData> VectorLengthTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new Point(0, 0), new Point(0,0), 0)
+                    .SetName("when the beginning and the end are the same");
+                yield return new TestCaseData(new Point(0, 0), new Point(-3, -4), 5)
+                    .SetName("when coordinates are negative");
+                yield return new TestCaseData(new Point(1, 1), new Point(4, 5), 5)
+                    .SetName("when common case is given");
+            }
         }
     }
 }
