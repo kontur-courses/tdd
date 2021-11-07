68d60afdc24ac6ed77126b2ad3dd0d9184f062fa
Lyalin_Artem <aalyalin22@gmail.com>
2021-11-07 18:32:30 +0500

    Added Drawer and SizeGenerator classes

---
+++ cs/TagsCloudVisualization/Drawer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public static class Drawer
+    {
+        public static Bitmap DrawRectangles(IEnumerable<Rectangle> rectangles)
+        {
+            var image = new Bitmap(700, 700);
+            var graph = Graphics.FromImage(image);
+
+            var random = new Random();
+            foreach (var rectangle in rectangles)
+            {
+                var pen = new Pen(GetRandomColor(random), 1);
+                graph.DrawRectangle(pen, rectangle);
+            }
+
+            return image;
+        }
+
+        private static Color GetRandomColor(Random random) =>
+            Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+    }
+}
---
+++ cs/TagsCloudVisualization/SizeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class SizeGenerator
+    {
+        private readonly Random random;
+        private readonly int maxHeight;
+        private readonly int maxWidth;
+        private readonly int minHeight;
+        private readonly int minWidth;
+
+        public SizeGenerator(int minWidth, int maxWidth, int minHeight, int maxHeight)
+        {
+            random = new Random();
+            this.minWidth = minWidth;
+            this.maxWidth = maxWidth;
+            this.minHeight = minHeight;
+            this.maxHeight = maxHeight;
+        }
+
+        public List<Size> GenerateSizes(int count)
+        {
+            var generatedSizes = new List<Size>();
+
+            for (var i = 0; i < count; i++)
+            {
+                generatedSizes.Add(new Size(random.Next(minWidth, maxWidth), random.Next(minHeight, maxHeight)));
+            }
+
+            return generatedSizes;
+        }
+    }
+}
