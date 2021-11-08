b645317e5e5cd9c956bdae7e0014d3e9fdfd6814
Karpov Arseny <jexinox@gmail.com>
2021-11-08 22:27:11 +0500

    Added Rectangle Extensions to check intersections Added Spiral IncreaseSize method and constructor

---
+++ cs/TagsCloudVisualization/RectangleExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    internal static class RectangleExtensions
+    {
+        public static bool Intersects(this Rectangle firstRect, Rectangle secondRect)
+        {
+            return !(firstRect.X + firstRect.Width < secondRect.X ||
+                     firstRect.X > secondRect.X + secondRect.Width ||
+                     firstRect.Y > secondRect.Y + secondRect.Height ||
+                     firstRect.Y + firstRect.Height < secondRect.Y);
+        }
+    }
+}
--- cs/TagsCloudVisualization/Spiral.cs
+++ cs/TagsCloudVisualization/Spiral.cs
@@ -14,7 +14,17 @@ namespace TagsCloudVisualization
             new PointD(
                 Math.Cos(angle) * radius + center.X,
                 Math.Sin(angle) * radius + center.Y);
-        
-        
+
+
+        public Spiral(Point center)
+        {
+            this.center = center;
+        }
+
+        public void IncreaseSize(double radiusIncreaseValue = 0.1, double angleIncreaseValue = 0.1)
+        {
+            radius += radiusIncreaseValue;
+            angle += angleIncreaseValue;
+        }
     }
 }
