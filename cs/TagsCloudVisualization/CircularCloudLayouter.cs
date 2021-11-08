43c7e2afa0e8d924f17f77accdc28501fc759837
Georgy Grantsev <mrxenonmr@gmail.com>
2021-11-08 19:10:19 +0500

    Внес исправления в класс CircularCloudLayouter соответствии с ревью

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -7,49 +7,49 @@ namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
-        private readonly List<Rectangle> _rectangles = new();
-        private readonly Spiral _spiral;
-        public Point Center => _spiral.Center;
+        private readonly List<Rectangle> rectangles;
+        private readonly Spiral spiral;
+        public Point Center => spiral.Center;
 
         public CircularCloudLayouter(Point center = default)
         {
-            _spiral = new Spiral(center);
+            rectangles = new List<Rectangle>();
+            spiral = new Spiral(center);
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
             if (rectangleSize.Width <= 0 || rectangleSize.Height <= 0)
-                throw new ArgumentException("Rectangle sizes must be great than zero");
+                throw new ArgumentException("Rectangle sizes must be greater than zero");
 
             Rectangle rectangle;
 
             do
             {
-                var point = _spiral.GetNextPoint() - rectangleSize / 2;
+                var point = spiral.GetNextPoint() - rectangleSize / 2;
                 rectangle = new Rectangle(point, rectangleSize);
             } while (IsLayoutIntersectWith(rectangle));
 
             rectangle = MoveToCenter(rectangle);
 
-            _rectangles.Add(rectangle);
+            rectangles.Add(rectangle);
 
             return rectangle;
         }
 
-        private bool IsLayoutIntersectWith(Rectangle rectangle)
-        {
-            return _rectangles.Any(rectangle.IntersectsWith);
-        }
-
-
         public Rectangle[] GetLayout()
         {
-            return _rectangles.ToArray();
+            return rectangles.ToArray();
+        }
+
+        private bool IsLayoutIntersectWith(Rectangle rectangle)
+        {
+            return rectangles.Any(rectangle.IntersectsWith);
         }
 
         private Rectangle MoveToCenter(Rectangle rectangle)
         {
-            if (_rectangles.Count == 0)
+            if (rectangles.Count == 0)
                 return rectangle;
 
             rectangle = ShiftToCenter(rectangle, false);
@@ -61,15 +61,18 @@ namespace TagsCloudVisualization
 
         private Rectangle ShiftToCenter(Rectangle rectangle, bool isVertical)
         {
-            var oldDistance = (rectangle.Location + rectangle.Size / 2).GetDistance(_spiral.Center);
+            var oldDistance = (rectangle.Location + rectangle.Size / 2).GetDistance(spiral.Center);
             var oldLocation = rectangle.Location;
             while (true)
             {
                 var newLocation = isVertical
-                    ? new Point(oldLocation.X, oldLocation.Y + Math.Sign(_spiral.Center.Y - rectangle.Location.Y - rectangle.Size.Height / 2))
-                    : new Point(oldLocation.X + Math.Sign(_spiral.Center.X - rectangle.Location.X - rectangle.Size.Width / 2), oldLocation.Y);
+                    ? new Point(oldLocation.X,
+                        oldLocation.Y + Math.Sign(spiral.Center.Y - rectangle.Location.Y - rectangle.Size.Height / 2))
+                    : new Point(
+                        oldLocation.X + Math.Sign(spiral.Center.X - rectangle.Location.X - rectangle.Size.Width / 2),
+                        oldLocation.Y);
 
-                var newDistance = (newLocation + rectangle.Size / 2).GetDistance(_spiral.Center);
+                var newDistance = (newLocation + rectangle.Size / 2).GetDistance(spiral.Center);
                 var newRectangle = new Rectangle(newLocation, rectangle.Size);
 
                 if (!IsLayoutIntersectWith(newRectangle))
