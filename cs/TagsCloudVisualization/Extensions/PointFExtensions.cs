a7c422bb23829a4babfd3de3dcb5ef345d9a244d
Pavel_Lediankin <a.lediankin@yandex.ru>
2021-11-09 12:51:48 +0500

    Extensions
    Extensions разбиты на несколько файлов

---
+++ cs/TagsCloudVisualization/Extensions/PointFExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace TagsCloudVisualization
+{
+    public static class PointFExtensions
+    {
+        public static Vector2 ToVector(this PointF p)
+            => new Vector2(p.X, p.Y);
+    }
+}
---
+++ cs/TagsCloudVisualization/Extensions/Vector2Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace TagsCloudVisualization
+{
+    public static class Vector2Extensions
+    {
+        public static PointF ToPoint(this Vector2 v)
+            => new PointF(v.X, v.Y);
+
+
+
+        public static double GetDistanseTo(this Vector2 v, PointF point)
+            => (point.ToVector() - v).Length();
+    }
+}
