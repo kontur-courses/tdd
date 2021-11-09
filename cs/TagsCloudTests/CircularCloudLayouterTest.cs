ca317cfef2665cffc991e221d780921180ddc284
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-11-09 11:40:05 +0500

    add first task

---
+++ cs/TagsCloudTests/CircularCloudLayouterTest.cs
@@ -0,0 +1,99 @@
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudTests
+{
+    [TestFixture]
+    public class CircularCloudLayouterTest
+    {
+        private Point center;
+        private CircularCloudLayouter sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            center = new Point(0, 0);
+            sut = new CircularCloudLayouter(center);
+        }
+
+        [TestCase(0, 1, TestName = "0 width")]
+        [TestCase(1, 0, TestName = "0 height")]
+        [TestCase(-1, 1, TestName = "negative width")]
+        [TestCase(1, -1, TestName = "negative height")]
+        public void PutNextRectangle_ShouldThrowArgumentException(int width, int height)
+        {
+            var size = new Size(width, height);
+
+            FluentActions.Invoking(
+                    () => sut.PutNextRectangle(size))
+                .Should().Throw<ArgumentException>();
+        }
+
+
+        [Test]
+        public void PutNextRectangle_ShouldReturnRectangleInCenter_WhenCalledFirstTime()
+        {
+            var rectangle = sut.PutNextRectangle(new Size(10, 10));
+
+            rectangle.GetCenter().Should().Be(center);
+        }
+
+        [TestCase(100)]
+        [TestCase(500)]
+        public void PutNextRectangle_ShouldReturnNotIntersectRectangles_WhenRandomSize(int count)
+        {
+            var rnd = new Random();
+            var sizeFactory = new Func<Size>(() =>
+                new Size(rnd.Next(20, 50), rnd.Next(10, 20)));
+            
+            var rectangles = GetRectangles(count, sizeFactory);
+            
+            rectangles
+                .IsAnyInCollectionIntersects()
+                .Should().BeFalse();
+        }
+        
+        [TestCase(100)]
+        [TestCase(500)]
+        public void PutNextRectangle_ShouldReturnNotIntersectRectangles_WhenSameSize(int count)
+        {
+            var rectangles = GetRectangles(count, () => new Size(20, 40));
+            
+            rectangles
+                .IsAnyInCollectionIntersects()
+                .Should().BeFalse();
+        }
+        
+
+        [TestCase(10, 200, 300, 100, TestName = "small count of big rectangles")]
+        [TestCase(100, 20, 30, 10, TestName = "big count of small rectangles")]
+        public void PutNextRectangle_ShouldReturnRectanglesThatFormACircle(int count, int maxWidth, int maxHeight, int precision)
+        {
+            var rnd = new Random();
+            var width = rnd.Next(1, maxWidth);
+            var height = rnd.Next(1, maxWidth);
+            var layoutArea = count * width * height;
+            var expectedRadius = Math.Sqrt(layoutArea / Math.PI);
+            var sizeFactory = new Func<Size>(() => new Size(width, height));
+
+            var rectangles = GetRectangles(count, sizeFactory);
+            var distanceX = (double)rectangles.Select(rectangle => rectangle.GetCenter().X).Max() - center.X;
+            var distanceY = (double)rectangles.Select(rectangle => rectangle.GetCenter().Y).Max() - center.Y;
+
+            Math.Abs(distanceX - expectedRadius).Should().BeLessThan(precision);
+            Math.Abs(distanceY - expectedRadius).Should().BeLessThan(precision);
+        }
+        
+        private List<Rectangle> GetRectangles(int count, Func<Size> sizeFactory)
+        {
+            return Enumerable.Range(0, count).Select(x =>
+                sut.PutNextRectangle(sizeFactory.Invoke())).ToList();
+        }
+    }
+}
---
+++ cs/TagsCloudTests/PointGeneratorTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudTests
+{
+    [TestFixture]
+    public class PointGeneratorTest
+    {
+        private Point start;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            start = new Point(0, 0);
+        }
+
+        [Test]
+        public void GetNextPoint_ShouldReturnStartPoint_WhenCalledFirstTime()
+        {
+            var sut = new PointGenerator(start);
+
+            var actualPoint = sut.GetNextPoint();
+
+            actualPoint.Should().BeEquivalentTo(start);
+        }
+        
+        [Test]
+        public void GetNextPoint_ShouldReturnPointsWithIncreasingDistanceToStart()
+        {
+            var sut = new PointGenerator(start);
+            var points = Enumerable.Range(0, 100).Select(x => sut.GetNextPoint()).ToList();
+            
+            var actualRadius = points.Select(point => point.DistanceTo(start)).ToList();
+
+            var expectedRadius = actualRadius.OrderBy(x => x);
+
+            actualRadius.Should().BeEquivalentTo(expectedRadius);
+        }
+    }
+}
