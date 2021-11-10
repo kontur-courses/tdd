a429d06ab4c34677f349dfce6acffafbaf64cdba
Karpov Arseny <jexinox@gmail.com>
2021-11-10 23:37:10 +0500

    Added constants Added methods for counting expected spiral X and Y

--- Homework/TagsCloudVisualizationTests/Spiral_Should.cs
+++ Homework/TagsCloudVisualizationTests/Spiral_Should.cs
@@ -10,14 +10,18 @@ namespace TagsCloudVisualizationTests
     [TestFixture]
     public class Spiral_Should
     {
+        private const double Epsilon = 1e-9;
+        private const double DefaultRadiusIncreaseValue = 0.1;
+        private const double DefaultAngleIncreaseValue = 0.1;
+
         [Test]
         [TestCaseSource(nameof(CenterGenerator))]
         public void StartPoint_EqualsCenter_WithCustomCenter(Point center)
         {
             var spiral = new Spiral(center);
             
-            spiral.CurrentPoint.X.Should().BeApproximately(center.X, 1e-9);
-            spiral.CurrentPoint.Y.Should().BeApproximately(center.Y, 1e-9);
+            spiral.CurrentPoint.X.Should().BeApproximately(center.X, Epsilon);
+            spiral.CurrentPoint.Y.Should().BeApproximately(center.Y, Epsilon);
         }
 
         [Test]
@@ -27,9 +31,15 @@ namespace TagsCloudVisualizationTests
             var spiral = new Spiral(center);
             
             spiral.IncreaseSize();
+            
+            var currentRadius = DefaultRadiusIncreaseValue;
+            var currentAngle = DefaultAngleIncreaseValue;
 
-            spiral.CurrentPoint.X.Should().BeApproximately(Math.Cos(0.1) * 0.1 + center.X, 1e-9);
-            spiral.CurrentPoint.Y.Should().BeApproximately(Math.Sin(0.1) * 0.1 + center.Y, 1e-9);
+            var expectedX = CountX(currentRadius, currentAngle, center.X);
+            var expectedY = CountY(currentRadius, currentAngle, center.Y);
+
+            spiral.CurrentPoint.X.Should().BeApproximately(expectedX, Epsilon);
+            spiral.CurrentPoint.Y.Should().BeApproximately(expectedY, Epsilon);
         }
 
         [Test]
@@ -40,8 +50,14 @@ namespace TagsCloudVisualizationTests
             
             spiral.IncreaseSize(1, 1);
             
-            spiral.CurrentPoint.X.Should().BeApproximately(Math.Cos(1) * 1 + center.X, 1e-9);
-            spiral.CurrentPoint.Y.Should().BeApproximately(Math.Sin(1) * 1 + center.Y, 1e-9);
+            var currentRadius = 1;
+            var currentAngle = 1;
+
+            var expectedX = CountX(currentRadius, currentAngle, center.X);
+            var expectedY = CountY(currentRadius, currentAngle, center.Y);
+            
+            spiral.CurrentPoint.X.Should().BeApproximately(expectedX, Epsilon);
+            spiral.CurrentPoint.Y.Should().BeApproximately(expectedY, Epsilon);
         }
 
         [Test]
@@ -53,9 +69,15 @@ namespace TagsCloudVisualizationTests
             spiral.IncreaseSize();
             spiral.IncreaseSize();
             spiral.IncreaseSize();
+
+            var currentRadius = DefaultRadiusIncreaseValue * 3;
+            var currentAngle = DefaultAngleIncreaseValue * 3;
+
+            var expectedX = CountX(currentRadius, currentAngle, center.X);
+            var expectedY = CountY(currentRadius, currentAngle, center.Y);
             
-            spiral.CurrentPoint.X.Should().BeApproximately(Math.Cos(0.3) * 0.3 + center.X, 1e-9);
-            spiral.CurrentPoint.Y.Should().BeApproximately(Math.Sin(0.3) * 0.3 + center.Y, 1e-9);
+            spiral.CurrentPoint.X.Should().BeApproximately(expectedX, Epsilon);
+            spiral.CurrentPoint.Y.Should().BeApproximately(expectedY, Epsilon);
         }
         
         [Test]
@@ -67,9 +89,25 @@ namespace TagsCloudVisualizationTests
             spiral.IncreaseSize(1, 2);
             spiral.IncreaseSize(3, 4);
             spiral.IncreaseSize(5, 6);
+
+            var currentRadius = 1 + 3 + 5;
+            var currentAngle = 2 + 4 + 6;
+
+            var expectedX = CountX(currentRadius, currentAngle, center.X);
+            var expectedY = CountY(currentRadius, currentAngle, center.Y);
             
-            spiral.CurrentPoint.X.Should().BeApproximately(Math.Cos(12) * 9 + center.X, 1e-9);
-            spiral.CurrentPoint.Y.Should().BeApproximately(Math.Sin(12) * 9 + center.Y, 1e-9);
+            spiral.CurrentPoint.X.Should().BeApproximately(expectedX, Epsilon);
+            spiral.CurrentPoint.Y.Should().BeApproximately(expectedY, Epsilon);
+        }
+
+        private double CountX(double radius, double angle, double currentX)
+        {
+            return Math.Cos(angle) * radius + currentX;
+        }
+
+        private double CountY(double radius, double angle, double currentY)
+        {
+            return Math.Sin(angle) * radius + currentY;
         }
 
         private static IEnumerable<Point> CenterGenerator()
