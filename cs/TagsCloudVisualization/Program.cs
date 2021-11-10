32aec62c4236bca2a23a27e8cc9c1fe83118cbcb
Alex <udovi4@gmail.com>
2021-11-10 13:37:43 +0500

    Visualisation and test project

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace TagsCloudVisualization
 {
-    /*class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var visualiser = new TagsCloudVisualiser(new Point(10, 10));
+            visualiser.PutRectangle(new Size(4, 2));
+            visualiser.PutRectangle(new Size(2, 2));
+            visualiser.PutRectangle(new Size(2, 2));
+            //visualiser.PutRectangle(new Size(4, 4));
+            var image = visualiser.DrawCloud(new Point(), new Size(20, 20), new Size(800, 800));
+            image.Save("testimage.png", ImageFormat.Png);
         }
-    }*/
+    }
 }
---
+++ cs/TagsCloudVisualization/TagsCloudVisualiser.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class TagsCloudVisualiser
+    {
+        private CircularCloudLayouter layouter;
+
+        public TagsCloudVisualiser(Point center)
+        {
+            layouter = new CircularCloudLayouter(center);
+        }
+
+        public void PutRectangle(Size size)
+        {
+            layouter.PutNextRectangle(size);
+        }
+
+        public Bitmap DrawCloud(Point frameLocation, Size frameSize, Size bitmapSize)
+        {
+            var image = new Bitmap(bitmapSize.Width, bitmapSize.Height);
+            var graphics = Graphics.FromImage(image);
+            graphics.TranslateTransform(frameLocation.X, frameLocation.Y);
+            graphics.ScaleTransform((float)bitmapSize.Width / frameSize.Width, 
+                                    (float)bitmapSize.Height / frameSize.Height);
+            graphics.FillRectangles(Brushes.Red, layouter.Rectangles.ToArray());
+            graphics.DrawRectangles(new Pen(Color.Black, (float)frameSize.Width / bitmapSize.Width * 4), layouter.Rectangles.ToArray());
+            return image;
+        }
+    }
+}
