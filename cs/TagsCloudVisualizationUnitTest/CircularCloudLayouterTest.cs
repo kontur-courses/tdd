0630de0ba73c74f7d43f7f01a88a1dac5db93190
makspanshin <makspanshin97@gmail.com>
2021-11-11 22:52:08 +0500

    Update CircularCloudLayouterTest.cs

--- cs/TagsCloudVisualizationUnitTest/CircularCloudLayouterTest.cs
+++ cs/TagsCloudVisualizationUnitTest/CircularCloudLayouterTest.cs
@@ -1,57 +1,109 @@
 using System;
 using System.Drawing;
+using System.IO;
 using NUnit.Framework;
 using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework.Interfaces;
 using TagsCloudVisualization;
 
 namespace TagsCloudVisualizationUnitTest
 {
     public class TagsCloudVisualizationTest
     {
+        private CircularCloudLayouter circularCloudLayouter;
+
+        [OneTimeSetUp]
+        public void CreateFolderForFailedTests()
+        {
+        }
+
+        [SetUp]
+        public void InitCircularCloudLayouter()
+        {
+            circularCloudLayouter = new CircularCloudLayouter(new Point(500, 500));
+        }
+
+        [TearDown]
+        public void SaveImageWithFailCircularCloudLayouter()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status is TestStatus.Failed)
+            {
+                var painterOfRectangles = new PainterOfRectangles(new Size(1000, 1000));
+
+                painterOfRectangles.CreateImage(circularCloudLayouter.Rectangles,
+                    TestContext.CurrentContext.Test.Name + "CircularCloudLayouter.png");
+            }
+        }
+
         [TestCase(-10, -10, TestName = "Negative size")]
         [TestCase(0, 0, TestName = "Size is Empty")]
         [TestCase(10, 0, TestName = "Weight = zero")]
         public void PutNextRectangle_ShouldBeThrowWhen(int height, int width)
         {
-            var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0));
-
             Action act = () => circularCloudLayouter.PutNextRectangle(new Size(width, height));
 
             act.Should().Throw<Exception>();
         }
 
-        [TestCase(10, 10, TestName = "Usual param")]
-        public void PutNextRectangle_ShouldNotThrowWhen(int height, int width)
-        {
-            var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0));
-
-            Action act = () => circularCloudLayouter.PutNextRectangle(new Size(width, height));
-
-            act.Should().NotThrow();
-        }
-
-        [TestCase(33, 33, 10, 10, ExpectedResult = false, TestName = "ShouldBeFalse")]
-        [TestCase(5, 5, 10, 10, ExpectedResult = true, TestName = "ShouldBeTrue")]
-        public bool IsIntersectsRectangles(int x, int y, int height, int width)
-        {
-            var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0));
-
-            circularCloudLayouter.PutNextRectangle(new Size(10, 10));
-
-            return circularCloudLayouter.IsIntersects(new Rectangle(x, y, width, height));
-        }
-
         [Test]
-        public void PutNextRectangle_RectanglesShouldBeHave_UniqueCoordinates()
+        public void PutNextRectangle_RectanglesShouldBeHaveUniqueCoordinates()
         {
-            var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0));
-
             for (int i = 0; i < 1000; i++)
             {
                 circularCloudLayouter.PutNextRectangle(new Size(10, 10));
             }
 
             circularCloudLayouter.Rectangles.Should().OnlyHaveUniqueItems();
+        }
+
+        [TestCase(10, TestName = "10 Rectangles")]
+        [TestCase(1000, TestName = "1000 Rectangles")]
+        [TestCase(2000, TestName = "2000 Rectangles")]
+        public void PutNextRectangle_ShouldBeOptimized(int countRectangle)
+        {
+            Action act = () =>
+            {
+                for (int i = 0; i < countRectangle; i++)
+                {
+                    circularCloudLayouter.PutNextRectangle(new Size(10, 10));
+                }
+            };
+
+            act.ExecutionTime().Should().BeLessThanOrEqualTo(1.Seconds());
+        }
+
+        [TestCase(10, TestName = "10 Rectangles")]
+        [TestCase(1000, TestName = "1000 Rectangles")]
+        [TestCase(2000, TestName = "2000 Rectangles")]
+        public void DensityCloud_ShouldBeBig(int countRectangle)
+        {
+            for (var i = 0; i < countRectangle; i++)
+            {
+                circularCloudLayouter.PutNextRectangle(new Size(10, 20));
+            }
+
+            double square = Math.Pow(circularCloudLayouter.Radius(), 2) * Math.PI;
+
+            (circularCloudLayouter.SquareRectangles / square * 100).Should().BeGreaterThan(70);
+        }
+
+        [TestCase(10, TestName = "10 Rectangles")]
+        [TestCase(1000, TestName = "1000 Rectangles")]
+        [TestCase(2000, TestName = "2000 Rectangles")]
+        public void DensityCloud_Random_Size_ShouldBeBig(int countRectangle)
+        {
+            Random rnd = new Random();
+
+            for (var i = 0; i < countRectangle; i++)
+            {
+                circularCloudLayouter.PutNextRectangle(new Size(rnd.Next(10, 20), rnd.Next(10, 20)));
+            }
+
+            double square = Math.Pow(circularCloudLayouter.Radius(), 2) * Math.PI;
+
+
+            (circularCloudLayouter.SquareRectangles / square * 100).Should().BeGreaterThan(65);
         }
     }
 }
