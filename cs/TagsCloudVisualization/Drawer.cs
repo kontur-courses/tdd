ec9787a5851d922b715d5a3c381feac78d463f08
Lyalin_Artem <aalyalin22@gmail.com>
2021-11-11 22:34:58 +0500

    Fixed Drawer.cs

--- cs/TagsCloudVisualization/Drawer.cs
+++ cs/TagsCloudVisualization/Drawer.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace TagsCloudVisualization
 {
     public static class Drawer
     {
-        public static Bitmap DrawRectangles(IEnumerable<Rectangle> rectangles)
+        public static void DrawRectangles(IEnumerable<Rectangle> rectangles, string path)
         {
-            var image = new Bitmap(700, 700);
-            var graph = Graphics.FromImage(image);
+            using var image = new Bitmap(700, 700);
+            using var graph = Graphics.FromImage(image);
+            var center = rectangles.First().Location;
+            graph.TranslateTransform(image.Width / 2 - center.X, image.Height / 2 - center.Y);
 
             var random = new Random();
             foreach (var rectangle in rectangles)
@@ -18,7 +21,7 @@ namespace TagsCloudVisualization
                 graph.DrawRectangle(pen, rectangle);
             }
 
-            return image;
+            image.Save(path);
         }
 
         private static Color GetRandomColor(Random random) =>
