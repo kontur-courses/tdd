6dc47c9fe8e73a7fbe88f265c06d0a2f93b00685
MaxxLorge <MaxxLorge@gmail.com>
2021-11-12 00:10:08 +0500

    добавил конструкцию using для Bitmap в Program.cs

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -17,16 +17,17 @@ namespace TagsCloudVisualization
             var rectangles = isSameSized
                 ? RectanglesGenerator.GenerateSameRectanglesWithSize(count, minRectSize, maxRectSize)
                 : RectanglesGenerator.GenerateDifferentRectangles(count, minRectSize, maxRectSize);
-            var bmpVisualizer = new BitmapVisualizer(rectangles);
-            bmpVisualizer.DrawRectangles(Color.Black, Color.White);
             var isSameSizedString = isSameSized ? "SameSized" : "Different";
             var fileName = $@"{count}{isSameSizedString}RectanglesWithSizesFrom{minRectSize}To{maxRectSize}.png";
             var dirPath = @"../../../ExamplePictures";
-            var scaleCoeff = bmpVisualizer.Width > 1920 || bmpVisualizer.Height > 1080
-                ? (1920d / bmpVisualizer.Width + 1080d / bmpVisualizer.Height) / 2
-                : 1;
-            bmpVisualizer.Save(fileName, scaleCoeff, new DirectoryInfo(dirPath));
-            bmpVisualizer.Dispose();
+            using (var bmpVisualizer = new BitmapVisualizer(rectangles))
+            {
+                bmpVisualizer.DrawRectangles(Color.Black, Color.White);
+                var scaleCoeff = bmpVisualizer.Width > 1920 || bmpVisualizer.Height > 1080
+                    ? (1920d / bmpVisualizer.Width + 1080d / bmpVisualizer.Height) / 2
+                    : 1;
+                bmpVisualizer.Save(fileName, scaleCoeff, new DirectoryInfo(dirPath));
+            }
         }
     }
 }
