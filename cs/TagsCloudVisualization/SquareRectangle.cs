08014cb0b83e4d606ba2868c0cae774eea64125a
makspanshin <makspanshin97@gmail.com>
2021-11-11 22:51:53 +0500

    Create SquareRectangle.cs

---
+++ cs/TagsCloudVisualization/SquareRectangle.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public static class SquareRectangle
+    {
+        public static double Square(this Rectangle rectangle)
+        {
+            return rectangle.Height * rectangle.Width;
+        }
+    }
+}
