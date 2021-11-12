f7b9f6efdb303e0a1a35b721e679b19c1c445999
makspanshin <makspanshin97@gmail.com>
2021-11-13 01:39:05 +0500

    Create SaverImage.cs

---
+++ cs/TagsCloudVisualization/SaverImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization
+{
+    public class SaverImage : ICommandImage
+    {
+        private string filename;
+
+        public SaverImage(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public void Execute(Image image)
+        {
+            Save(image);
+        }
+
+        private void Save(Image image)
+        {
+            if (image == null)
+                throw new ArgumentException("Image is null");
+
+            if (string.IsNullOrWhiteSpace(filename))
+                throw new ArgumentException("Не корректное название файла");
+
+            image.Save(filename);
+            image.Dispose();
+        }
+    }
+}
