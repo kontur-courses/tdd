188d719df6bfb0196c7ddbbe017e5885dc4a0f9b
makspanshin <makspanshin97@gmail.com>
2021-11-12 12:48:36 +0500

    Update PainterOfRectangles.cs

--- cs/TagsCloudVisualization/PainterOfRectangles.cs
+++ cs/TagsCloudVisualization/PainterOfRectangles.cs
@@ -6,6 +6,7 @@ namespace TagsCloudVisualization
     public class PainterOfRectangles
     {
         private Size pictSize;
+
         public PainterOfRectangles(Size sizeOfPicture)
         {
             pictSize = sizeOfPicture;
@@ -13,7 +14,7 @@ namespace TagsCloudVisualization
 
         public void CreateImage(List<Rectangle> rectangles, string filename = "rectangles")
         {
-            using Bitmap bmp = new Bitmap(pictSize.Width,pictSize.Height);
+            using Bitmap bmp = new Bitmap(pictSize.Width, pictSize.Height);
 
             using Graphics graphics = Graphics.FromImage(bmp);
 
