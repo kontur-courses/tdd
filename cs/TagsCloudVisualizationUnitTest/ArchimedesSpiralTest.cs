8cac20989604f9c7189ca8286be383066757f70d
makspanshin <makspanshin97@gmail.com>
2021-11-13 01:38:27 +0500

    Create ArchimedesSpiralTest.cs

---
+++ cs/TagsCloudVisualizationUnitTest/ArchimedesSpiralTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualizationUnitTest
+{
+    class ArchimedesSpiralTest
+    {
+        [Test]
+        public void CalculateFirstPoint_ShouldBe_In_Centre()
+        {
+            var centre = new Point(100, 100);
+            var spiral = new ArchimedesSpiral(centre);
+
+            spiral.CalculatePoint().Should().Be(centre);
+        }
+    }
+}
