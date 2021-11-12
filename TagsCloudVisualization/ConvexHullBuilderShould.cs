36d01011e39f511bdf1ff31f958156f58961dd33
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-12 23:53:22 +0500

    MinimalConvexHullTestData and RotationDirectionTestData are methods now

--- TagsCloudVisualization/ConvexHullBuilderShould.cs
+++ TagsCloudVisualization/ConvexHullBuilderShould.cs
@@ -47,76 +47,70 @@ namespace TagsCloudVisualization
 
         }
 
-        private static IEnumerable<TestCaseData> MinimalConvexHullTestData
+        private static IEnumerable<TestCaseData> MinimalConvexHullTestData()
         {
-            get
-            {
-                yield return new TestCaseData(
-                    new List<Point>
-                    {
-                        new Point(0, 0)
-                    },
-                    new List<Point>
-                    {
-                        new Point(0,0)
-                    })
-                    .SetName("when one point is given");
-                yield return new TestCaseData(
-                    new List<Point>
-                    {
-                        new Point(0, 0), new Point(1,1)
-                    },
-                    new List<Point>
-                    {
-                        new Point(0,0), new Point(1,1)
-                    })
-                    .SetName("when two poinst are given");
-                yield return new TestCaseData(
-                    new List<Point>
-                    {
-                        new Point(0, 0), new Point(1,1), new Point(2,2)
-                    },
-                    new List<Point>
-                    {
-                        new Point(0,0), new Point(1,1), new Point(2,2)
-                    })
-                    .SetName("when three poinst are given");
-                yield return new TestCaseData(
-                    new List<Point>
-                    {
-                        new Point(2, 1), new Point(4, 1), new Point(6, 2), new Point(8, 4),
-                        new Point(5, 5), new Point(3, 4), new Point(2, 3), new Point(4, 3),
-                        new Point(6, 4), new Point(5, 3), new Point(5, 2), new Point(3, 2)
-                    },
-                    new List<Point>
-                    {
-                        new Point(2, 1), new Point(4, 1), new Point(6, 2), new Point(8, 4),
-                        new Point(5, 5), new Point(3, 4), new Point(2, 3),
-                    })
-                    .SetName("when points count more than three");
-            }
+            yield return new TestCaseData(
+                new List<Point>
+                {
+                    new Point(0, 0)
+                },
+                new List<Point>
+                {
+                    new Point(0,0)
+                })
+                .SetName("when one point is given");
+            yield return new TestCaseData(
+                new List<Point>
+                {
+                    new Point(0, 0), new Point(1,1)
+                },
+                new List<Point>
+                {
+                    new Point(0,0), new Point(1,1)
+                })
+                .SetName("when two poinst are given");
+            yield return new TestCaseData(
+                new List<Point>
+                {
+                    new Point(0, 0), new Point(1,1), new Point(2,2)
+                },
+                new List<Point>
+                {
+                    new Point(0,0), new Point(1,1), new Point(2,2)
+                })
+                .SetName("when three poinst are given");
+            yield return new TestCaseData(
+                new List<Point>
+                {
+                    new Point(2, 1), new Point(4, 1), new Point(6, 2), new Point(8, 4),
+                    new Point(5, 5), new Point(3, 4), new Point(2, 3), new Point(4, 3),
+                    new Point(6, 4), new Point(5, 3), new Point(5, 2), new Point(3, 2)
+                },
+                new List<Point>
+                {
+                    new Point(2, 1), new Point(4, 1), new Point(6, 2), new Point(8, 4),
+                    new Point(5, 5), new Point(3, 4), new Point(2, 3),
+                })
+                .SetName("when points count more than three");
         }
 
-        private static IEnumerable<TestCaseData> RotationDirectionTestData
+        private static IEnumerable<TestCaseData> RotationDirectionTestData()
         {
-            get
-            {
-                yield return new TestCaseData(
-                    new Vector(new Point(1,1), new Point(4,3)),
-                    new Point(3, 5),
-                    1)
-                    .SetName("when point is located to the left of the vector");
-                yield return new TestCaseData(
-                    new Vector(new Point(0, 0), new Point(3, 6)),
-                    new Point(4, -2),
-                    -1)
-                    .SetName("when point is located to the right of the vector");
-                yield return new TestCaseData(
-                    new Vector(new Point(0, 0), new Point(5, 5)),
-                    new Point(3, 3),
-                    0)
-                    .SetName("when point is located on the vector");
-            }
+            yield return new TestCaseData(
+                new Vector(new Point(1,1), new Point(4,3)),
+                new Point(3, 5),
+                1)
+                .SetName("when point is located to the left of the vector");
+            yield return new TestCaseData(
+                new Vector(new Point(0, 0), new Point(3, 6)),
+                new Point(4, -2),
+                -1)
+                .SetName("when point is located to the right of the vector");
+            yield return new TestCaseData(
+                new Vector(new Point(0, 0), new Point(5, 5)),
+                new Point(3, 3),
+                0)
+                .SetName("when point is located on the vector");
         }
     }
 }
