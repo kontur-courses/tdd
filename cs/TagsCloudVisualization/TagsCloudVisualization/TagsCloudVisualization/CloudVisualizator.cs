6edec505691898a26c6f48d8381f785327ebd113
KirillKalutsky <kefirchik.2000@mail.ru>
2021-11-12 23:20:06 +0500

    визуализатор облака для 2 задания

---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/CloudVisualizator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public class CloudVisualizator : ICloudVisualizator
+    {
+        private Bitmap bitmap;
+        private Graphics graphic;
+        private Point imageCenter;
+        private Point cloudCenter;
+        public CloudVisualizator(Size imageSize, Point cloudCenter)
+        {
+            bitmap = new Bitmap(imageSize.Width, imageSize.Height);
+            graphic = Graphics.FromImage(bitmap);
+            imageCenter = new Point(imageSize.Width / 2, imageSize.Height / 2);
+            this.cloudCenter = cloudCenter;
+        }
+
+        public void DrawRectangle(Pen pen, Rectangle rectangle)
+        {
+            rectangle.X += imageCenter.X - cloudCenter.X;
+            rectangle.Y += imageCenter.Y - cloudCenter.Y;
+            graphic.DrawRectangle(pen, rectangle);
+        }
+
+        public void DrawRectangles(Pen pen, List<Rectangle> rectangles)
+        {
+            foreach (var rectangle in rectangles)
+                DrawRectangle(pen, rectangle);
+        }
+
+        public void SaveImage(string path)
+        {
+            bitmap.Save(path);
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/ICloudVisualizator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public interface ICloudVisualizator
+    {
+        void DrawRectangle(Pen pen, Rectangle rectangle);
+        void DrawRectangles(Pen pen, List<Rectangle> rectangles);
+        void SaveImage(string path);
+    }
+}
