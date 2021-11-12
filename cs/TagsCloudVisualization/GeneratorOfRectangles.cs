465f28b3c3e3ab18cbe8fe9fa780af899aeb6b2f
makspanshin <makspanshin97@gmail.com>
2021-11-13 01:39:20 +0500

    Update GeneratorOfRectangles.cs

--- cs/TagsCloudVisualization/GeneratorOfRectangles.cs
+++ cs/TagsCloudVisualization/GeneratorOfRectangles.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Drawing;
+using TagsCloudVisualization.Interfaces;
 
 namespace TagsCloudVisualization
 {
-    public class GeneratorOfRectangles
+    public class GeneratorOfRectangles : IGenerataorFigure
     {
         private Random rnd = new Random();
 
-        public Size GetRectangleSize(int minValue, int maxValue)
+        public Size GetSize(int minValue, int maxValue)
         {
             return new Size(rnd.Next(minValue, maxValue), rnd.Next(minValue, maxValue));
         }
