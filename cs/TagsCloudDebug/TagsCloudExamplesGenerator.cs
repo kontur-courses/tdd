afd41091d1937d0827eed6b77f89bde5e783e08f
Lyalin_Artem <aalyalin22@gmail.com>
2021-11-13 06:00:52 +0500

    Added new project TagsCloudDebug.csproj for debugging and updated TagsCloudVisualization.csproj

---
+++ cs/TagsCloudDebug/TagsCloudExamplesGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using TagsCloudVisualization;
+
+namespace TagsCloudDebug
+{
+    static class TagsCloudExamplesGenerator
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                GenerateImage();
+            }
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                GenerateImage(int.Parse(args[i]), $"Example{i}.bmp");
+            }
+        }
+
+        private static void GenerateImage(int rectanglesCount = 150, string fileName = "Example.bmp")
+        {
+            var cloudLayouter = new CircularCloudLayouter(new ArchimedeanSpiral(Point.Empty));
+            var generator = new SizeGenerator(10, 50, 10, 50);
+            var drawer = new Drawer();
+            var saver = new ImageSaver();
+
+            var rectangles = generator.GenerateSizes(rectanglesCount)
+                .Select(size => cloudLayouter.PutNextRectangle(size));
+
+            using var image = drawer.DrawRectangles(rectangles, new Size(700, 700));
+            saver.SaveImage(image, fileName, ImageFormat.Png);
+        }
+    }
+}
