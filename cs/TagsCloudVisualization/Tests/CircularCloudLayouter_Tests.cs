ba7434a4cd7d5f47b3745afe62f8edd17c6c7f10
ivanarray <i.maliamow@yandex.ru>
2021-11-13 14:35:50 +0500

    Добавил tearDown метод, для отрисовки неудачного результата. Так-же написал тест, где добавляются прямоугольники разных размеров, и он валится)) допиливаю алгоритм
    review: @skylog
    #TagCloud

--- cs/TagsCloudVisualization/Tests/CircularCloudLayouter_Tests.cs
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouter_Tests.cs
@@ -1,13 +1,37 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace TagsCloudVisualization.Tests
 {
     [TestFixture]
     public class CircularCloudLayouter_Tests
     {
+        private CircularCloudLayouter cloudLayouter;
+
+        [SetUp]
+        public void SetCloudLayouter()
+        {
+            cloudLayouter = new CircularCloudLayouter(new Point(0, 0));
+        }
+
+        [TearDown]
+        public void RecordMistakes()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status != TestStatus.Failed) return;
+
+            var fileName = TestContext.CurrentContext.Test.Name +
+                           TestContext.CurrentContext.Result.FailCount;
+
+            Painter.Paint(cloudLayouter.GetRectangles(), fileName);
+            Console.WriteLine("Tag cloud visualization saved to file " +
+                              $"./TagsCloudVisualization/Tests/Tests_Images/{fileName}");
+        }
+
         [TestCase(0, 0)]
         [TestCase(1, 1)]
         [TestCase(-1, 1)]
@@ -17,7 +41,7 @@ namespace TagsCloudVisualization.Tests
         {
             var location = new Point(x, y);
 
-            var cloudLayouter = new CircularCloudLayouter(location);
+            cloudLayouter = new CircularCloudLayouter(location);
 
             cloudLayouter.Center.Should().Be(location,
                 "Constructor shouldn't change central point");
@@ -28,44 +52,72 @@ namespace TagsCloudVisualization.Tests
         [TestCase(0, 0, 0, 0, 0, 0)]
         [TestCase(3, 4, 5, 7, 0, 0)]
         public void PutNextRectangle_FirstRectangle_ShouldPutOnCentre
-            (int cX, int cY, int width, int height, int expX, int expY)
+        (int cX, int cY, int width, int height, int expX,
+            int expY)
         {
             var center = new Point(cX, cY);
-            var cloud = new CircularCloudLayouter(center);
+            cloudLayouter = new CircularCloudLayouter(center);
             var size = new Size(width, height);
+
             var expectedLocation =
                 new Point(expX, expY);
 
-            var actualRectangle = cloud.PutNextRectangle(size);
+            var actualRectangle = cloudLayouter.PutNextRectangle(size);
 
             actualRectangle.Location.Should().Be(expectedLocation,
                 "The first rectangle should be in the centre");
         }
 
+
         [Test]
         public void PutNextRectangle_NextRectangles_ShouldNotHaveIntersectionsSimpleCase()
         {
-            var centre = new Point(0, 0);
-            var cloudLayouter = new CircularCloudLayouter(centre);
             var size = new Size(3, 2);
-            var expLocation1 = new Point(-2, -3);
-            var expLocation2 = new Point(-2, 1);
 
-            PutManiRectangles(cloudLayouter, size, 6);
+            PutManiRectangles(size, 20);
 
 
-            cloudLayouter.GetRectangles()[1].Location.Should().Be(expLocation1);
-            cloudLayouter.GetRectangles()[2].Location.Should().Be(expLocation2);
-            
             Assert.IsFalse(cloudLayouter.GetRectangles()
                 .SelectMany(x => cloudLayouter.GetRectangles()
                     .Select(y => (x, y))).Where(x => x.x != x.y)
-                .Any(pair=>pair.x.IntersectsWith(pair.y)));
+                .Any(pair => pair.x.IntersectsWith(pair.y)));
         }
 
-        private void PutManiRectangles(CircularCloudLayouter cloudLayouter, Size size, int count)
+
+        [Test]
+        public void PutNextRectangle_NextRectangles_ShouldNotHaveIntersectionsDifferentSizes()
+        {
+            var sizes = new[]
+            {
+                new Size(1, 2),
+                new Size(40, 60),
+                new Size(85, 50),
+                new Size(100, 100),
+                new Size(300, 450),
+                new Size(219, 168),
+                new Size(37, 27)
+            };
+
+
+            PutManiRectangles(sizes);
+
+            Assert.IsFalse(cloudLayouter.GetRectangles()
+                .SelectMany(x => cloudLayouter.GetRectangles()
+                    .Select(y => (x, y))).Where(x => x.x != x.y)
+                .Any(pair => pair.x.IntersectsWith(pair.y)));
+        }
+
+
+        private void PutManiRectangles(Size size, int count)
         {
             for (; count > 0; count--)
+                cloudLayouter.PutNextRectangle(size);
+        }
+
+
+        private void PutManiRectangles(IEnumerable<Size> sizes)
+        {
+            foreach (var size in sizes)
                 cloudLayouter.PutNextRectangle(size);
         }
     }
