53127ff6852a4f86c8a52a58c400d0915e0782f1
Lyalin_Artem <aalyalin22@gmail.com>
2021-11-13 06:04:49 +0500

    Updated Drawer.cs and created tests

--- cs/TagsCloudVisualization/Drawer.cs
+++ cs/TagsCloudVisualization/Drawer.cs
@@ -5,26 +5,47 @@ using System.Linq;
 
 namespace TagsCloudVisualization
 {
-    public static class Drawer
+    public class Drawer : IDrawer
     {
-        public static void DrawRectangles(IEnumerable<Rectangle> rectangles, string path)
+        public Bitmap DrawRectangles(IEnumerable<Rectangle> rectangles, Size imageSize)
         {
-            using var image = new Bitmap(700, 700);
+            var rectanglesList = rectangles.ToList();
+            var center = rectanglesList.First().Location;
+
+            CheckIfRectanglesFitInImage(rectanglesList.ToList(), center, imageSize);
+
+            var image = new Bitmap(imageSize.Width, imageSize.Height);
             using var graph = Graphics.FromImage(image);
-            var center = rectangles.First().Location;
-            graph.TranslateTransform(image.Width / 2 - center.X, image.Height / 2 - center.Y);
 
             var random = new Random();
-            foreach (var rectangle in rectangles)
+            using var pen = new Pen(GetRandomColor(random), 1);
+
+            graph.TranslateTransform(image.Width / 2 - center.X, image.Height / 2 - center.Y);
+
+            foreach (var rectangle in rectanglesList)
             {
-                var pen = new Pen(GetRandomColor(random), 1);
+                pen.Color = GetRandomColor(random);
                 graph.DrawRectangle(pen, rectangle);
             }
 
-            image.Save(path);
+            return image;
         }
 
         private static Color GetRandomColor(Random random) =>
             Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+        private void CheckIfRectanglesFitInImage(List<Rectangle> rectangles, Point center, Size imageSize)
+        {
+            var distanceFromTopToCenter = Math.Abs(rectangles.Max(rect => rect.Top) - center.Y);
+            var distanceFromRightToCenter = Math.Abs(rectangles.Max(rect => rect.Right) - center.X);
+            var distanceFromBottomToCenter = Math.Abs(rectangles.Min(rect => rect.Bottom) - center.Y);
+            var distanceFromLeftToCenter = Math.Abs(rectangles.Min(rect => rect.Left) - center.X);
+
+            if (distanceFromBottomToCenter >= imageSize.Height / 2d ||
+                distanceFromTopToCenter >= imageSize.Height / 2d ||
+                distanceFromLeftToCenter >= imageSize.Width / 2d ||
+                distanceFromRightToCenter >= imageSize.Width / 2d)
+                throw new ArgumentException("Rectangles do not fit in image size");
+        }
     }
 }
---
+++ cs/TagsCloudVisualizationTests/Drawer_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualizationTests
+{
+    public class Drawer_Tests
+    {
+        [Test] 
+        public void DrawRectangles_ThrowsArgumentException_WhenRectanglesDoNotFitInImage()
+        {
+            var drawer = new Drawer();
+            var bigRectangle = new Rectangle(Point.Empty, new Size(55, 55));
+            var imageSize = new Size(50, 50);
+
+            Action action = () => drawer.DrawRectangles(new[] { bigRectangle }, imageSize);
+            
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
