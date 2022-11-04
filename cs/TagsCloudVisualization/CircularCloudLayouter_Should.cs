6adac18c53c8e5bac22f16b9a0496bbb2827ff47
Anton Yaskov <yaskovanton333@gmail.com>
2022-11-05 01:15:52 +0500

    Create proj

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        private CircularCloudLayouter _cloud;
+
+    }
+}
