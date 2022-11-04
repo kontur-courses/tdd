d9ab75cd583b077a12a5e8a9c9f11fcb34f624b3
Igor Sotarev <igor@sotarev.ru>
2022-11-04 23:01:55 +0500

    Add FermatSpiral and implement CircularCloudLayouter

--- cs/TagCloudVisualizer/CloudLayouter/CircularCloudLayouter.cs
+++ cs/TagCloudVisualizer/CloudLayouter/CircularCloudLayouter.cs
@@ -4,13 +4,38 @@ namespace TagCloudVisualizer.CloudLayouter;
 
 public class CircularCloudLayouter : ICloudLayouter
 {
+    private readonly Point center;
+    private readonly FermatSpiral fermatSpiral;
+    private readonly List<Rectangle> rectangles = new ();
+    public IReadOnlyList<Rectangle> Rectangles => rectangles;
+
     public CircularCloudLayouter(Point center)
     {
-        throw new NotImplementedException();
+        this.center = center;
+        fermatSpiral = new FermatSpiral(center);
     }
     
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        throw new NotImplementedException();
+        if (rectangleSize.Width < 0 || rectangleSize.Height < 0)
+            throw new ArgumentException($"Rectangle size should not be negative. Received Width: {rectangleSize.Width} and Height: {rectangleSize.Height}");
+
+        Rectangle result;
+
+        do
+        {
+            var rectangleCenter = fermatSpiral.GetNextPoint();
+            var anchorPoint = GetAnchorPoint(rectangleCenter, rectangleSize);
+            result = new Rectangle(anchorPoint, rectangleSize);
+        } while (rectangles.Any(r => r.IntersectsWith(result)));
+        
+        rectangles.Add(result);
+
+        return result;
+    }
+
+    private Point GetAnchorPoint(Point rectangleCenter, Size rectangleSize)
+    {
+        return new Point(rectangleCenter.X - rectangleSize.Width / 2, rectangleCenter.Y - rectangleSize.Height / 2);
     }
 }
---
+++ cs/TagCloudVisualizer/CloudLayouter/FermatSpiral.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+
+namespace TagCloudVisualizer.CloudLayouter;
+
+public class FermatSpiral
+{
+    private readonly int step;
+    private readonly Point center;
+    private int phi;
+    private int a;
+
+    public FermatSpiral(Point center, int a = 1, int step = 1)
+    {
+        this.a = a;
+        this.step = step;
+        this.center = center;
+        phi = 0;
+    }
+
+    public Point GetNextPoint()
+    {
+        if (phi == 0)
+        {
+            phi += step;
+            return center;
+        }
+        
+        var result = new Point((int)(a * Math.Sqrt(phi) * Math.Cos(phi) + center.X),
+            (int)(a * Math.Sqrt(phi) * Math.Sin(phi)) + center.Y);
+
+        if (a < 0)
+            phi += step;
+        a = -a;
+        return result;
+    }
+}
