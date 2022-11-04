bd1803f21ef1731d1d71fefe35a1a4709c5e232d
Igor Sotarev <igor@sotarev.ru>
2022-11-04 23:03:37 +0500

    Add TagCloudImageGenerator, Program.cs

---
+++ cs/TagCloudVisualizer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using TagCloudVisualizer.CloudLayouter;
+using TagCloudVisualizer.TagCloudImageGenerator;
+
+var rectangleCount = 300;
+
+if (args.Length > 0)
+    int.TryParse(args[0], out rectangleCount);
+
+var canvasSize = new Size(500, 500);
+var layouter = new CircularCloudLayouter(new Point(canvasSize.Width / 2, canvasSize.Height / 2));
+var random = new Random();
+
+for (var i = 0; i < rectangleCount; i++)
+{
+    layouter.PutNextRectangle(new Size(random.Next(10, 30), random.Next(10, 30)));
+}
+
+var path = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\TagCloud\\";
+
+if (!Directory.Exists(path))
+    Directory.CreateDirectory(path);
+
+var now = DateTime.Now;
+var filename = $"TagCloud_{now:yyyy-MM-dd-HH-mm-ss}.png";
+
+var fullFilePath = path + filename;
+
+var image = TagCloudImageGenerator.GenerateImage(layouter.Rectangles.ToArray(), canvasSize);
+image.Save(fullFilePath, ImageFormat.Png);
+
+Console.WriteLine($"Image saved as {fullFilePath}");
---
+++ cs/TagCloudVisualizer/TagCloudImageGenerator/TagCloudImageGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace TagCloudVisualizer.TagCloudImageGenerator;
+
+public class TagCloudImageGenerator
+{
+    public static Bitmap GenerateImage(Rectangle[] rectangles, Size canvasSize)
+    {
+        var canvas = new Bitmap(canvasSize.Width, canvasSize.Height);
+        var graphics = Graphics.FromImage(canvas);
+
+        var backgrouldBrush = Brushes.White;
+        var rectanglePen = Pens.Black;
+
+        graphics.FillRectangle(backgrouldBrush,0, 0, canvas.Width, canvas.Height);
+        foreach (var rectangle in rectangles)
+        {
+            graphics.DrawRectangle(rectanglePen, rectangle);
+        }
+
+        return canvas;
+    }
+}
