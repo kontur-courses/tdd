f73c237bdea14703e5afc1e6c4e9e52bdc4b905d
O-Evgeniy <evgeniyorlov94@mail.ru>
2022-11-05 15:57:34 +0500

    add Spiral.cs and SpiralTests.cs

---
+++ cs/TagsCloudVisualization.Tests/SpiralTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization.Tests;
+
+public class SpiralTests
+{
+    [TestCase(0, 0.2f, TestName = "{m}ZeroParameter")]
+    [TestCase(2, 0f, TestName = "{m}ZeroStep")]
+    public void Constructor_ThrowArgumentException_On(int parameter, float step)
+    {
+        Action act = () => new Spiral(Point.Empty, parameter, step);
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Constructor_DoesntThrowArgumentException_OnCorrectArguments()
+    {
+        Action act = () => new Spiral(Point.Empty, 1, 0.2f);
+
+        act.Should().NotThrow<ArgumentException>();
+    }
+    
+    [TestCaseSource(nameof(Cases))]
+    public void Next_ShouldReturnCorrectNextPoint_With(int parameter, float step, Point[] expectedPoints)
+    {
+        var spiral = new Spiral(new Point(100, 100), parameter, step);
+
+        var actual = new List<Point>();
+        for (int i = 0; i < expectedPoints.Length; i++)
+        {
+            actual.Add(spiral.Next());
+        }
+
+        actual.Should().BeEquivalentTo(expectedPoints);
+    }
+
+    private static object[] Cases =
+    {
+        new TestCaseData(2, 0.4f, new Point[]
+        {
+            new Point(100, 100), new Point(100, 100), new Point(102, 102), new Point(100, 102)
+        }).SetName("{m}PositiveArguments"),
+        new TestCaseData(-2, 0.4f, new Point[]
+        {
+            new Point(100, 100), new Point(100, 100), new Point(98, 98), new Point(100, 98)
+        }).SetName("{m}NegativeParameter"),
+        new TestCaseData(2, -0.4f, new Point[]
+        {
+            new Point(100, 100), new Point(100, 100), new Point(98, 102), new Point(100, 102)
+        }).SetName("{m}NegativeStep"),
+        new TestCaseData(-2, -0.4f, new Point[]
+        {
+            new Point(100, 100), new Point(100, 100), new Point(102, 98), new Point(100, 98)
+        }).SetName("{m}NegativeParameters"),
+    };
+}
---
+++ cs/TagsCloudVisualization/Spiral.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class Spiral
+{
+    private readonly Point center;
+    private readonly float step;
+    private readonly int parameter;
+    private float angle;
+
+    public Spiral(Point center, int parameter = 2, float step = 1f)
+    {
+        if (parameter == 0)
+            throw new ArgumentException("Parameter should be not equal zero");
+        if (step == 0.0f)
+            throw new ArgumentException("Step should be not equal zero");
+        this.center = center;
+        this.step = step;
+        this.parameter = parameter;
+    }
+
+    public Point Next()
+    {
+        if (angle == 0.0f)
+        {
+            angle += step;
+            return center;
+        }
+
+        int x = parameter * (int)Math.Round(angle * Math.Cos(angle)) + center.X;
+        int y = parameter * (int)Math.Round(angle * Math.Sin(angle)) + center.Y;
+
+        var nextPoint = new Point(x, y);
+        angle += step;
+
+        return nextPoint;
+    }
+}
