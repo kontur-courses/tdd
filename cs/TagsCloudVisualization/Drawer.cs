ca5b6be8fe4bc17a1dc3b999a81472bb3e0f8c30
Anton Yaskov <yaskovanton333@gmail.com>
2022-11-05 22:57:52 +0500

    Drawer and Program, where generate pics

---
+++ cs/TagsCloudVisualization/Drawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class Drawer
+    {
+        public static void CreateImage(int width, int height, IEnumerable<Rectangle> rectangles, string filename)
+        {
+            var b = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.Clear(Color.White);
+                foreach (var r in rectangles)
+                {
+                    g.DrawRectangle(new Pen(Color.Red), r);
+                }
+            }
+
+            b.Save(filename + ".bmp");
+        }
+    }
+}
--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.Drawing;
+using TagsCloudVisualization;
+
+var rnd = new Random();
+var cloud = new CircularCloudLayouter(new Point(750, 750));
+for (int i = 0; i < 25; i++)
+{
+    cloud.PutNextRectangle(new Size(rnd.Next(50, 150), rnd.Next(50, 150)));
+}
+Drawer.CreateImage(1500, 1500, cloud.Rectangles, "first layout");
