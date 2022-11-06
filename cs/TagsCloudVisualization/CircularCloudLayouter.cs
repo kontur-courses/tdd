74ea6d00bb867369448075c5b63cb1c3e147db77
Фирстова_Александра <navasylava@mail.ru>
2022-11-06 21:55:16 +0500

    Создала TagsCloudVisualization с CircularCloudLayouter

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        CircularCloudLayouter(Point center)
+        {
+        }
+
+        Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            return new Rectangle();
+        }
+    }
+}
