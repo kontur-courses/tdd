d5e3a8cc465dbb00fd40ea66ff3d080d8ef8ef2f
Владимир Недугов <MegaRs1@yandex.ru>
2022-11-06 22:28:05 +0400

    Added images and and two test image generations

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 
 namespace TagsCloudVisualization
 {
     class Program
     {
+        private const string ImagesFolder = "../../../result/";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var pen1 = new Pen(Brushes.DeepSkyBlue);
+            var pen2 = new Pen(Brushes.Red);
+
+            var painter = new Painter(new Size(500, 500), pen1, pen2);
+            painter.CreatePicture(100, ImagesFolder + "100.png");
+            painter = new Painter(new Size(1000, 1000), pen1, pen2);
+            painter.CreatePicture(300, ImagesFolder + "300.png");
         }
     }
 }
