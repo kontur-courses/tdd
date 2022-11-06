9f869185198a0fdccf37d3e5839fd0cb264e1485
Фирстова_Александра <navasylava@mail.ru>
2022-11-06 22:38:05 +0500

    Добавила RectangleExtensions с тестами

---
+++ cs/TagsCloudVisualization/RectangleExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace TagsCloudVisualization
+{
+    public static class RectangleExtensions
+    {
+        public static Point Center(this Rectangle rect)
+        {
+            return new Point(rect.Left + rect.Width / 2,rect.Top + rect.Height / 2);
+        }
+
+        public static bool AreIntersected(this Rectangle r1, Rectangle r2)
+        {
+            return (r1.Left >= r2.Left || r1.Right >= r2.Left)
+                   && (r1.Top >= r2.Top || r1.Top + r1.Height >= r2.Top)
+                   && (r2.Left >= r1.Left || r2.Right >= r1.Left)
+                   && (r2.Top >= r1.Top || r2.Top + r2.Height >= r1.Top);
+        }
+
+        public static bool AreIntersected(this Rectangle rectangle, List<Rectangle> rectangles)
+        {
+            if (rectangles is null) return false;
+            return rectangles.Any(rect => AreIntersected(rectangle, rect));
+        }
+
+        public static bool AreIntersected(this List<Rectangle> rectangles)
+        {
+            for (var i = 0; i < rectangles.Count - 1; i++)
+            {
+                for (var j = i + 1; j < rectangles.Count; j++)
+                {
+                    if (AreIntersected(rectangles[i], rectangles[j]))
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
