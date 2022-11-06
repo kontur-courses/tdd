84dff2d993081a197f5e072b8b82265cfff72b0c
Seekard <maks_byx@mail.ru>
2022-11-07 02:13:26 +0300

    CircularCloudLayouter methods were implemented.

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -2,39 +2,64 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 
 namespace TagsCloudVisualization
 {
-    internal class CircularCloudLayouter
+    public class CircularCloudLayouter
     {
-        public Point Center { get; private set; }
-        private List<Rectangle> _rectangles = new List<Rectangle>();
-        private IReadOnlyList<Rectangle> Rectangles => _rectangles;
+        private readonly ArchimedeanSpiral _spiral;
+        private readonly List<Rectangle> _rectangles = new List<Rectangle>();
+        private readonly double _angleStep = 0.01;
+        private double _angle = 0;
+        public Point Center { get; }
+        public IReadOnlyList<Rectangle> Rectangles => _rectangles;
 
         public CircularCloudLayouter(Point center)
         {
             Center = center;
+            _spiral = new ArchimedeanSpiral(center, 0, 0.5);
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            throw new Exception();
+            Rectangle rectangle = new Rectangle(Point.Empty, rectangleSize);
+            PlaceRectangle(ref rectangle);
+            ShiftRectangleToCenter(ref rectangle);
+            _rectangles.Add(rectangle);
+            return rectangle;
         }
 
-        public List<Rectangle> GetArrangedRectangles()
+        private void PlaceRectangle(ref Rectangle rectangle)
         {
-            throw new Exception();
+            do {
+                rectangle.Location = _spiral.GetPoint(_angle);
+                _angle += _angleStep;
+            } while (rectangle.IntersectsWith(_rectangles));
         }
-
-        // Refactor to another class
-        public Bitmap MakeVisualisation()
+        
+        private void ShiftRectangleToCenter(ref Rectangle rectangle)
         {
-            throw new Exception();
+            int dx = (rectangle.GetCenter().X < Center.X) ? 1 : -1;
+            ShiftRectangle(ref rectangle, dx, 0);
+            int dy = (rectangle.GetCenter().Y < Center.Y) ? 1 : -1;
+            ShiftRectangle(ref rectangle, 0, dy);
         }
-
-        public void Save()
+        
+        private void ShiftRectangle(ref Rectangle rectangle, int dx, int dy)
         {
-            throw new Exception();
+            Size offset = new Size(dx, dy);
+            while (rectangle.IntersectsWith(_rectangles) == false && 
+                   rectangle.GetCenter().X != Center.X &&
+                   rectangle.GetCenter().Y != Center.Y)
+            {
+                rectangle.Location += offset;
+            }
+
+            if (rectangle.IntersectsWith(_rectangles))
+            {
+                rectangle.Location -= offset;
+            }
         }
     }
 }
--- cs/TagsCloudVisualization/Tests/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouterTests.cs
@@ -1,12 +1,76 @@
-﻿using NUnit;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using NUnit;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace TagsCloudVisualization.Tests
 {
     [TestFixture]
     public class CircularCloudLayouterTests
     {
-        
+        private CircularCloudLayouter _layouter;
+        [SetUp]
+        public void SetUp()
+        {
+            _layouter = new CircularCloudLayouter(new Point(500, 500));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                CircularCloudLayouterVisualisator visualisator =
+                    new CircularCloudLayouterVisualisator(_layouter);
+                Bitmap image = visualisator.CreateImage();
+                visualisator.Save(image, TestContext.CurrentContext.Test.Name);
+            }
+        }
+
+        private static IEnumerable<TestCaseData> SizesOfRectangles
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new List<Size>()
+                    {
+                        new Size(100, 100),
+                        new Size(100, 100),
+                        new Size(100, 100),
+                        new Size(100, 100),
+                        new Size(100, 100),
+                    });
+                yield return new TestCaseData(
+                    new List<Size>()
+                    {
+                        new Size(100, 100),
+                        new Size(200, 100),
+                        new Size(100, 200),
+                        new Size(1000, 1000),
+                        new Size(100, 100),
+                    });
+            }
+        }
+
+        [TestCaseSource(nameof(SizesOfRectangles))]
+        public void PutNextRectangle_ShouldNotMakeIntersections(IReadOnlyList<Size> sizesOfRectangles)
+        {
+            foreach (Size rectangleSize in sizesOfRectangles)
+            {
+                _layouter.PutNextRectangle(rectangleSize);
+                foreach (Rectangle rectangle1 in _layouter.Rectangles)
+                {
+                    foreach (Rectangle rectangle2 in _layouter.Rectangles)
+                    {
+                        if (rectangle1 == rectangle2)
+                            continue;
+                        rectangle1.IntersectsWith(rectangle2).Should().BeFalse();
+                    }
+                }
+            }
+        }
     }
 }
