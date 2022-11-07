c60a298775b8458640d167e3a1d4e67f2385768d
Art <bartalamu@list.ru>
2022-11-07 11:55:10 +0500

    csproj + RectangleExtension

---
+++ cs/TagsCloudVisualization/RectangleExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public static class RectangleExtension
+    {
+        public static Point GetCenter(this Rectangle rectangle) => new((rectangle.X + rectangle.Right) / 2,
+            (rectangle.Y + rectangle.Bottom) / 2);
+
+        public static int GetArea(this Rectangle rectangle) => rectangle.Height * rectangle.Width;
+    }
+}
