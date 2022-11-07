87fda92734b0f60a4d155cedc87c53a6832342eb
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2022-11-07 17:47:11 +0500

    v1.1

--- cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,19 +1,65 @@
 ﻿using System.Drawing;
 using System.Numerics;
+using System.Runtime.Intrinsics.X86;
 
 namespace TagsCloudVisualization;
 
-class CircularCloudLayouter
+public class CircularCloudLayouter
 {
-    private RectanglesNet _rectanglesNet;
+    private const float alpha = 0.1f;
+    
+    private Point _center;
+    private List<Rectangle> _rectangles;
+    private Spiral _spiral;
+
+    public List<Rectangle> Rectangles
+    {
+        get
+        {
+            return _rectangles;
+        }
+    }
+    
+    public Point Center
+    {
+        get { return _center; }
+    }
 
     public CircularCloudLayouter(Point center)
     {
-        _rectanglesNet = new RectanglesNet(center);
+        _center = center;
+        _rectangles = new();
+        _spiral = new(alpha, center);
     }
 
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        return _rectanglesNet.AddRectToNet(rectangleSize);
+        _spiral.SetNewRandomStartAngle();
+        Rectangle rect = new(_center, rectangleSize);
+        while (IsIntersectedWithExistingRectangles(rect))
+            UpdateCoord(ref rect);
+        
+        UpdateRectangleList(rect);
+        return rect;
+    }
+
+    public void UpdateCoord(ref Rectangle r)
+    {
+        _spiral.IncreaseAngle(Math.Min(r.Width, r.Height));
+        r = new Rectangle(
+            _spiral.ToCartesian(),
+            r.Size
+            );
+    }
+
+    public bool IsIntersectedWithExistingRectangles(Rectangle rect)
+    {
+        return _rectangles
+            .Any(r => r.IntersectsWith(rect));
+    }
+
+    private void UpdateRectangleList(Rectangle rect)
+    {
+        _rectangles.Add(rect);
     }
 }
--- cs/TagsCloudVisualization/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/Program.cs
@@ -3,8 +3,6 @@ using System.Drawing.Imaging;
 using System.Net.Mime;
 using TagsCloudVisualization;
 
-// Console.WriteLine("Hello, World!");
-
 var layouter = new CircularCloudLayouter(new Point(150, 150));
 var bitmap = new Bitmap(300, 300);
 var g = Graphics.FromImage(bitmap);
@@ -13,10 +11,6 @@ var pen2 = new Pen(Color.Red, 2);
 var rand = new Random();
 for (int i = 0; i < 100; i++)
 {
-    g.DrawRectangle(pen,layouter.PutNextRectangle(new Size(10 + rand.Next(-5, 5), 20+ rand.Next(-10, 10))));
-    //g.DrawLine(pen2, layouter.Center, layouter.CenterMass);
-    //g.DrawLine(pen2, layouter.CenterMass, Point.Empty);
+    g.DrawRectangle(pen,layouter.PutNextRectangle(new Size( 20+ rand.Next(-10, 10),20+ rand.Next(-10, 10))));
 }
-    // g.DrawRectangle(pen, layouter.PutNextRectangle(new Size(10, 5)));
-    
-bitmap.Save("..\\..\\..\\Results\\result.bmp");
+bitmap.Save("..\\..\\..\\..\\..\\..\\Results\\result2.bmp");
