d4067d93c6160a4b8377df488279543896f8dd03
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-11-08 03:49:16 +0500

    added Program.cs, ICloudLayouter

---
+++ cs/TagsCloudVisualization/ICloudLayouter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    internal interface ICloudLayouter
+    {
+        Rectangle PutNextRectangle(Size rectangleSize);
+    }
+}
---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    internal class Program
+    {
+        public static int rectanglesCount = 100;
+        public static int maxRectanglesHeight = 100;
+        public static int maxRectanglesWidth = 100;
+
+        static void Main()
+        {
+            var center = new Point(400, 400);
+            var rectangleSizes = RectanglesRandomizer.GetSortedRectangles(
+                maxRectanglesWidth, maxRectanglesHeight, rectanglesCount);
+
+            var layouter = new CircularCloudLayouter(center, new Spiral(center));
+            var rectangles = layouter.GetRectangles(rectangleSizes);
+        }
+    }
+}
