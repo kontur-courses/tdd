ad000afe664f541e23251c5ffbc2431bda084680
Art <bartalamu@list.ru>
2022-11-07 13:59:43 +0500

    Некоторые упрощения

--- cs/TagsCloudVisualization/CircularCloudLayout.cs
+++ cs/TagsCloudVisualization/CircularCloudLayout.cs
@@ -41,12 +41,13 @@ namespace TagsCloudVisualization
         {
             double angle = 0;
             double adjustAngle = 1;
-            while (center.X + Math.Cos(angle) * angle / (0.3 * Math.PI) > 0
-                   && center.Y + Math.Sin(angle) * angle / (0.3 * Math.PI) > 0)
+            while (center.X + Math.Cos(angle) * angle / 0.95 > 0
+                   && center.Y + Math.Sin(angle) * angle / 0.95 > 0)
             {
-                pointer = new Point((int)(center.X + Math.Cos(angle) * angle / (0.3 * Math.PI)),
-                    (int)(center.Y + Math.Sin(angle) * angle / (0.3 * Math.PI)));
-                adjustAngle = adjustAngle <= 0.017 ? 0.017 : Math.PI / 4 / (int)(angle / (2 * Math.PI) + 1);
+                pointer = new Point((int)(center.X + Math.Cos(angle) * angle / 0.95),
+                    (int)(center.Y + Math.Sin(angle) * angle / 0.95));
+                var coilCount = (int)(angle / (2 * Math.PI) + 1);
+                adjustAngle = adjustAngle <= 0.017 ? 0.017 : Math.PI / 4 / coilCount;
                 angle += adjustAngle;
                 if (PointLiesInRectangles(pointer))
                     continue;
