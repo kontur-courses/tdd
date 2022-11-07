b8f02bb66ddebb6557120b63e1f03cb09bc7f510
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-11-08 03:44:35 +0500

    added RectanglesRandomizer

---
+++ cs/TagsCloudVisualization/RectanglesRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public static class RectanglesRandomizer
+    {
+        public static List<Size> GetSortedRectangles(int maxWidth, int maxHeight, int count)
+        {
+            if (maxHeight <= 0 || maxWidth <= 0 || count <= 0)
+                throw new ArgumentException();
+
+            var rectangles = new List<Size>();
+            var random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                rectangles.Add(new Size(random.Next(1, maxWidth), random.Next(1, maxHeight)));
+            }
+
+            return rectangles.OrderByDescending(rect => rect.Height * rect.Width).ToList();
+        }
+    }
+}
