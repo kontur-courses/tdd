423839a99dde3f976a36004a85df36a8ca71dd0f
Ilya <ilyapol92@gmail.com>
2022-11-07 23:31:31 +0500

    add visualization and tests

---
+++ cs/TagCloud/TagCloudVisualization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TagCloud
+{
+    public class TagCloudVisualization
+    {
+        private static Random random = new Random();
+
+        public static void SaveAsBitmap(CircularCloudLayouter tagCloud, string file)
+        {
+            var bitmap = new Bitmap( tagCloud.GetWidth()+1, tagCloud.GetHeight()+1);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                foreach (var reactangle in tagCloud.Reactangles)
+                {
+                    graphics.DrawRectangle(new Pen(GetRandomBrush()), reactangle);
+                }
+            }
+            bitmap.Save(file);
+        }
+
+        protected static Brush GetRandomBrush()
+        {
+            return new SolidBrush(GetRandomColor());
+        }
+
+        protected static Color GetRandomColor()
+        {
+            var knownColors = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            var randomColorName = knownColors[random.Next(knownColors.Length)];
+            return Color.FromKnownColor(randomColorName);
+        }
+    }
+}
---
+++ cs/TagCloud/TagCloudVisualizationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using NUnit.Framework;
+
+namespace TagCloud
+{
+    public class TagCloudVisualizationTests : TagCloudVisualization
+    {
+        [Test]
+        public void GetRandomBrush_GenerateRandomBrush()
+        {
+            var brushes = new HashSet<Brush>();
+
+            for (int i = 0; i < 10; i++)
+                brushes.Add(GetRandomBrush());
+
+            brushes.Count.Should().BeGreaterThan(7);
+        }
+
+        [Test]
+        public void SaveAsBitmap_TagCloudInFile_Success()
+        {
+            var tagCloud = new CircularCloudLayouter(new Point(5, 5));
+            var tempBmpFile = "temp.bmp";
+
+            File.Delete(tempBmpFile);
+            tagCloud.PutNextRectangle(new Size(200, 100));
+            SaveAsBitmap(tagCloud, tempBmpFile);
+
+            File.Exists(tempBmpFile).Should().BeTrue();
+        }
+    }
+}
