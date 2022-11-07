607f3085dac2457a241c5d7c49ad2e6b99c1b3c9
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-11-08 03:47:16 +0500

    added CircularCloudLayouterTests

---
+++ cs/TagsCloudVisualization/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization.TagsCloudVisualizationTests
+{
+    [TestFixture]
+    public class CircularCloudLayouterTests
+    {
+        private Point center;
+        private Spiral spr;
+        private List<Rectangle> addedRectangles;
+        private CircularCloudLayouter layouter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(400, 400);
+            spr = new Spiral(center);
+            addedRectangles = new List<Rectangle>();
+            layouter = new CircularCloudLayouter(center, spr);
+        }
+
+        [Test]
+        public void CloudInitialization_ThrowsNullReferenceException_OnNullSpiral()
+        {
+            Assert.Throws<NullReferenceException>(() => new CircularCloudLayouter(center, null));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(100)]
+        public void PutNextRectangle_ReturnPutRectangles_CorrectCount(int expectedCount)
+        {
+            for (int i = 0; i < expectedCount; i++)
+                addedRectangles.Add(layouter.PutNextRectangle(new Size(400, 400)));
+
+            addedRectangles.Count().Should().Be(expectedCount);
+        }
+
+        [TestCase(1, -1)]
+        [TestCase(-1, 1)]
+        [TestCase(1, 0)]
+        [TestCase(0, 1)]
+        [TestCase(-1, -1)]
+        [TestCase(0, 0)]
+        public void PutNextRectangle_ThrowsArgumentException_IncorrectArguments(int width, int height)
+        {
+            Assert.Throws<ArgumentException>(() => layouter.PutNextRectangle(new Size(width, height)));
+        }
+    }
+}
+
