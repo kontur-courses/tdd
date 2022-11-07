bcdb645ff672bf112d0a2226007067a1334cd13a
Art <bartalamu@list.ru>
2022-11-07 11:56:59 +0500

    SizeBuilder + Program.cs (визуализация)

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using static TagsCloudVisualization.SizeListBulder;
+
+namespace TagsCloudVisualization
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var layout = new CircularCloudLayout(new Point(200, 200));
+            var sizes = GetCustomSizes();
+            var rectanglesOutput = new List<Rectangle>();
+            sizes.ForEach(x =>
+            {
+                if (layout.PutNextRectangle(x, out Rectangle rectangle))
+                    rectanglesOutput.Add(rectangle);
+            });
+
+            Bitmap picture = new(410, 410);
+            Graphics g = Graphics.FromImage(picture);
+            g.Clear(Color.White);
+            rectanglesOutput.ForEach(x => g.DrawRectangle(Pens.Black, x));
+            g.DrawEllipse(Pens.Black, 0, 0, 400, 400);
+            picture.Save(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\Layout.bmp"));
+        }
+
+    }
+
+}
---
+++ cs/TagsCloudVisualization/SizeListBulder.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class SizeListBulder
+    {
+        public static List<Size> GetCustomSizes()
+        {
+            var list = new List<Size>();
+            list.Add(new Size(100, 75));
+            Enumerable.Range(0, 3).ToList().ForEach(x => list.Add(new Size(70, 55)));
+            Enumerable.Range(0, 11).ToList().ForEach(x => list.Add(new Size(50, 30)));
+            Enumerable.Range(0, 55).ToList().ForEach(x => list.Add(new Size(20, 15)));
+            Enumerable.Range(0, 75).ToList().ForEach(x => list.Add(new Size(20, 15)));
+            Enumerable.Range(0, 265).ToList().ForEach(x => list.Add(new Size(15, 10)));
+            return list;
+        }
+    }
+}
