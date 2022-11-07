9ac207cce6d6b6fbf476e82b339e8845d7b9d15c
angst-storm <serezha.kiprin@gmail.com>
2022-11-07 10:32:03 +0500

    create projects for CircularCloudLayouter and tests

---
+++ cs/TagCloud/CircularCloudLayouter.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace TagCloud;
+
+public class CircularCloudLayouter
+{
+    public CircularCloudLayouter(Point center)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Rectangle PutNextRectangle(Size rectangleSize)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ cs/TagCloudTests/TagCloud_Should.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace TagCloudTests;
+
+public class TagCloud_Should
+{
+    [Test]
+    public void Test()
+    {
+        Assert.Pass();
+    }
+}
