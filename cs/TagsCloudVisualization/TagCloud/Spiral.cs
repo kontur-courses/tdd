02989f21621e8f5ec66279f20428ce41d456b528
SVDyakov <svdyakov@yandex.ru>
2022-11-07 23:39:23 +0700

    Формирование арифметической спирали

---
+++ cs/TagsCloudVisualization/TagCloud/Spiral.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization.TagCloud
+{
+    /// <summary>
+    /// Арифметическая спираль
+    /// </summary>
+    public class Spiral
+    {
+        private const double defaultDelta = Math.PI / 360;
+        private const double defaultDensity = 1.7;
+        private const double defaultAngle = 0;
+
+        private readonly Point center;
+        private double angle;
+        private readonly double density;
+        private readonly double delta;
+
+        public Spiral(Point center, double angle = defaultAngle, double density = defaultDensity, double delta = defaultDelta)
+        {
+            this.center = center;
+            this.angle = angle;
+            this.density = density;
+            this.delta = delta;
+        }
+
+        public Point GetNewPoint()
+        {
+            var radius = density * angle;
+
+            var point = new Point(center.X, center.Y);
+            point.X += (int)(radius * Math.Cos(angle));
+            point.Y += (int)(radius * Math.Sin(angle));
+            angle += delta;
+            return point;
+        }
+    }
+}
