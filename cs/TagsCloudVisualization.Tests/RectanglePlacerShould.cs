3c4778b0bb2658deb0032f77f3a3ccdb70af806d
Zuguki <guskov.zahar20@gmail.com>
2022-11-07 16:30:00 +0300

    Rectangle tests done

---
+++ cs/TagsCloudVisualization.Tests/RectanglePlacerShould.cs
@@ -0,0 +1,21 @@
+using System.Drawing;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization.Tests
+{
+    [TestFixture]
+    public class RectanglePlacerShould
+    {
+        private RectanglePlacer rectanglePlacer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            rectanglePlacer = new RectanglePlacer();
+        }
+
+        [TestCaseSource(typeof(TestData), nameof(TestData.DefaultPointsAndSizeForPlace))]
+        public Rectangle Place_DefaultPointsAndSize_MiddlePlace(int x, int y, int width, int height) =>
+            rectanglePlacer.Place(new Point(x, y), new Size(width, height));
+    }
+}
--- cs/TagsCloudVisualization.Tests/TestData.cs
+++ cs/TagsCloudVisualization.Tests/TestData.cs
@@ -1,9 +1,17 @@
+using System.Drawing;
 using NUnit.Framework;
 
 namespace TagsCloudVisualization.Tests
 {
     public class TestData
     {
+        public static TestCaseData[] DefaultPointsAndSizeForPlace =
+        {
+            new TestCaseData(0, 0, 4, 4).Returns(new Rectangle(new Point(-2, -2), new Size(4, 4))),
+            new TestCaseData(-2, -2, 4, 4).Returns(new Rectangle(new Point(-4, -4), new Size(4, 4))),
+            new TestCaseData(0, 0, 10, 10).Returns(new Rectangle(new Point(-5, -5), new Size(10, 10))),
+        };
+
         public static TestCaseData[] IncorrectStepCount =
         {
             new TestCaseData(0).SetName("Set zero in Step"),
