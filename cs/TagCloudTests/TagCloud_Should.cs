08d780fe352a3152a871836a6391f4abf2387110
angst-storm <serezha.kiprin@gmail.com>
2022-11-08 20:02:21 +0500

    added informative test fail

--- cs/TagCloudTests/TagCloud_Should.cs
+++ cs/TagCloudTests/TagCloud_Should.cs
@@ -1,12 +1,30 @@
 using System.Drawing;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using TagCloud;
 
 namespace TagCloudTests;
 
 public class TagCloud_Should
 {
+    private CircularCloudLayouter layouter;
+
+    [SetUp]
+    public void SetUp()
+    {
+        layouter = new CircularCloudLayouter(new Point(400, 300));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (TestContext.CurrentContext.Result.Outcome != ResultState.Failure) return;
+        var filename = $"{TestContext.CurrentContext.Test.Name}.jpg";
+        layouter.SaveAsImage(filename, new Size(800, 600));
+        Console.WriteLine($"Tag cloud visualization saved to file {filename}");
+    }
+
     [TestCase(0, 0)]
     [TestCase(10, 10)]
     [TestCase(10, -10)]
@@ -17,24 +35,22 @@ public class TagCloud_Should
         Action act = () => _ = new CircularCloudLayouter(new Point(x, y));
         act.Should().NotThrow<Exception>();
     }
-    
+
     [TestCase(0, 0, TestName = "{m}isEmpty")]
     [TestCase(10, 0, TestName = "{m}WithZeroWidth")]
     [TestCase(0, 10, TestName = "{m}WithZeroHeight")]
     public void PutNextRectangle_ThrowArgumentException_OnSize(int width, int height)
     {
-        var layouter = new CircularCloudLayouter(new Point(0, 0));
         var size = new Size(width, height);
         var act = () => _ = layouter.PutNextRectangle(size);
         act.Should().Throw<ArgumentException>().WithMessage("Area of rectangle can't be zero");
     }
-    
+
     [TestCase(1, TestName = "{m}CorrectSize")]
     [TestCase(2, TestName = "{m}TwoSizes")]
     [TestCase(10, TestName = "{m}ManySizes")]
     public void PutNextRectangle_DontThrowException_On(int rectanglesCount)
     {
-        var layouter = new CircularCloudLayouter(new Point(0, 0));
         var size = new Size(10, 10);
         var act = () =>
         {
@@ -47,18 +63,31 @@ public class TagCloud_Should
     [Test]
     public void PutNextRectangle_ReturnedRectangle_HasTheInputSize()
     {
-        var layouter = new CircularCloudLayouter(new Point(0, 0));
         var size = new Size(10, 10);
         layouter.PutNextRectangle(size).Size.Should().Be(size);
     }
-    
+
     [Test]
     public void PutNextRectangle_ReturnRectangles_DontIntersect()
     {
-        var layouter = new CircularCloudLayouter(new Point(0, 0));
         var size = new Size(10, 10);
         var rect1 = layouter.PutNextRectangle(size);
         var rect2 = layouter.PutNextRectangle(size);
         rect1.IntersectsWith(rect2).Should().BeFalse();
+    }
+
+    [Test]
+    public void SaveAsImage_CreateFile()
+    {
+        const string filename = "filename.jpg";
+        layouter.SaveAsImage(filename, new Size(800, 600));
+        File.Exists(filename);
+    }
+
+    [Test]
+    public void FallingTest()
+    {
+        layouter.PutNextRectangle(new Size(50, 50));
+        Assert.Fail();
     }
 }
--- cs/TagCloudVisualization/Program.cs
+++ cs/TagCloudVisualization/Program.cs
@@ -6,28 +6,19 @@ void DrawTagCloud(string filename, IEnumerable<Size> sizes)
     var layouter = new CircularCloudLayouter(new Point(400, 300));
     foreach (var size in sizes)
         layouter.PutNextRectangle(size);
-
-    using var bitmap = new Bitmap(800, 600);
-    using var graphics = Graphics.FromImage(bitmap);
-    var pen = new Pen(Color.Black, 1);
-
-    graphics.Clear(Color.White);
-    foreach (var rectangle in layouter.Rectangles)
-        graphics.DrawRectangle(pen, rectangle);
-
-    bitmap.Save($"{filename}.jpg");
+    layouter.SaveAsImage(filename, new Size(800, 600));
 }
 
 var squareSizes = Enumerable.Range(1, 200)
     .Select(n => new Size(30, 30))
     .ToArray();
-  
-DrawTagCloud("squares", squareSizes);
+
+DrawTagCloud("squares.jpg", squareSizes);
 
 var rnd = new Random();
 var randomSizes = Enumerable.Range(1, 200)
     .Select(n => new Size(rnd.Next(15, 31), rnd.Next(15, 31)))
     .ToArray();
 
-DrawTagCloud("random", randomSizes);
-DrawTagCloud("sorted-random", randomSizes.OrderByDescending(s => s.Width * s.Height));
+DrawTagCloud("random.jpg", randomSizes);
+DrawTagCloud("sorted-random.jpg", randomSizes.OrderByDescending(s => s.Width * s.Height));
