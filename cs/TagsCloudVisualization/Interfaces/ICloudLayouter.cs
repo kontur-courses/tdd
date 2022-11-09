ea7e53bb52fdf1522c421dbda3a32d6ca09115ca
Annie Ryannel <ryannel321@gmail.com>
2022-11-09 16:42:12 +0300

    interfaces added

---
+++ cs/TagsCloudVisualization/Interfaces/ICloudLayouter.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public interface ICloudLayouter
+{
+    Rectangle PutNextRectangle(Size rectangleSize);
+    ICollection<Rectangle> Rectangles();
+    Point Center();
+}
