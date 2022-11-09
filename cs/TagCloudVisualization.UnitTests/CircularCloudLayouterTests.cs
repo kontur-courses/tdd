dd87139db7fd3c1685088c516b4accca5540f58b
Alexander Yasinovskiy <alex2021cyberhuman@gmail.com>
2022-11-09 13:25:57 +0500

    Added constructor CircularCloudLayouter
    Added parameter constraints and tested it.

--- cs/TagCloudVisualization.UnitTests/CircularCloudLayouterTests.cs
+++ cs/TagCloudVisualization.UnitTests/CircularCloudLayouterTests.cs
@@ -1,6 +1,20 @@
+using System.Drawing;
+using FluentAssertions;
+using TagsCloudVisualization;
+
 namespace TagCloudVisualization.UnitTests;
 
+[TestFixture]
 public class CircularCloudLayouterTests
 {
-    
+    [TestCase(-1, -1)]
+    [TestCase(-1, 10)]
+    [TestCase(10, -1)]
+    [TestCase(0, 0)]
+    public void Constructor_Throws(int centerX, int centerY)
+    {
+        var action = () => { _ = new CircularCloudLayouter(new Point(centerX, centerY)); };
+
+        action.Should().Throw<ArgumentException>();
+    }
 }
--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -5,12 +5,17 @@ namespace TagsCloudVisualization;
 public class CircularCloudLayouter
 {
     private readonly Point center;
-
-    private readonly List<Rectangle> rectangles = new(50);
-
+    
     public CircularCloudLayouter(Point center)
     {
+        if (center.IsEmpty || center.X <= 0 || center.Y <= 0)
+            throw new ArgumentException("center point is invalid", nameof(center));
         this.center = center;
     }
-    
+
+
+    public Rectangle PutNextRectangle(Size rectangleSize)
+    {
+        throw new NotImplementedException();
+    }
 }
