cdda79ca0a45810a722e27c2050439947ecc0f79
Art <bartalamu@list.ru>
2022-11-09 12:41:18 +0500

    Вынес сетап в билдер чтобы не копипастить все это в тестах, отрисовка теперь так же зависит от его инициализации

--- cs/CircularCloudLayoutTests/CircularCloudLayoutTests.cs
+++ cs/CircularCloudLayoutTests/CircularCloudLayoutTests.cs
@@ -8,31 +8,16 @@ namespace CircularCloudLayoutTests
     [TestFixture]
     public class CircularCloudLayoutTests
     {
-        private int layoutRadius;
-        private Point center;
-        private CircularCloudLayout layout;
-        private List<Rectangle> placedRectangles;
+        private SetUpBuilder? setup;
         private Bitmap picture;
         private Graphics g;
 
-        [SetUp]
-        public void SetupLayout()
-        {
-            center = new Point(200, 200);
-            placedRectangles = new();
-            layoutRadius = center.X < center.Y ? center.X : center.Y;
-            layout = new CircularCloudLayout(center);
-            SizeListBulder.GetCustomSizes().ForEach(x =>
-            {
-                if (layout.PutNextRectangle(x, out Rectangle rect))
-                    placedRectangles.Add(rect);
-            });
-        }
-
         [Test]
-        [Category("ToDraw")]
         public void PlacedRectangles_Should_Fill_80Percent_OfCircleSpace()
         {
+            setup = new SetUpBuilder().WithCenter(new Point(200, 200)).WithCustomSizes();
+            var layoutRadius = setup.GetRadius();
+            var placedRectangles = setup.GetRectangles();
             var area = 0;
             var expectedCoveredArea = (int)(Math.PI * layoutRadius * layoutRadius * 0.8);
 
@@ -60,9 +45,10 @@ namespace CircularCloudLayoutTests
         }
 
         [Test]
-        [Category("ToDraw")]
         public void PlacedRectangles_Should_Not_IntersectEachOther()
         {
+            setup = new SetUpBuilder().WithCenter(new Point(200, 200)).WithCustomSizes();
+            var placedRectangles = setup.GetRectangles();
             var doIntersects = false;
 
             for (int i = 0; i < placedRectangles.Count - 1; i++)
@@ -77,9 +63,11 @@ namespace CircularCloudLayoutTests
         }
 
         [Test]
-        [Category("ToDraw")]
         public void PlacedRectangles_ActualCenter_ShouldNotDeviate_MoreThanFivePercent_From_CenterPoint()
         {
+            setup = new SetUpBuilder().WithCenter(new Point(200, 200)).WithCustomSizes();
+            var center = setup.GetCenter();
+            var placedRectangles = setup.GetRectangles();
             var maxX = placedRectangles.Select(x => x.Right).Max();
             var maxY = placedRectangles.Select(x => x.Bottom).Max();
             var minX = placedRectangles.Select(x => x.Left).Min();
@@ -138,10 +126,13 @@ namespace CircularCloudLayoutTests
         }
 
         [Test]
-        [Category("ToDraw")]
         [Description("Circle based on center point")]
         public void PlacedRectangles_Should_LieInLimitingCircle()
         {
+            setup = new SetUpBuilder().WithCenter(new Point(200, 200)).WithCustomSizes();
+            var layoutRadius = setup.GetRadius();
+            var placedRectangles = setup.GetRectangles();
+            var center = setup.GetCenter();
             var isOutside = false;
 
             foreach (var rectangle in placedRectangles)
@@ -163,15 +154,14 @@ namespace CircularCloudLayoutTests
         public void Cleanup()
         {
             var context = TestContext.CurrentContext;
-            string? category = (string)TestContext.CurrentContext.Test.Properties.Get("Category");
-            if (!(context.Result.FailCount > 0) || category != "ToDraw")
+            if (!(context.Result.FailCount > 0) || setup == null)
                 return;
-            picture = new(center.X * 2 + 5, center.Y * 2 + 5);
+            picture = new(setup.GetCenter().X * 2 + 5, setup.GetCenter().Y * 2 + 5);
             g = Graphics.FromImage(picture);
             g.Clear(Color.White);
-            placedRectangles.ForEach(x => g.DrawRectangle(Pens.Black, x));
-            g.DrawEllipse(Pens.Black, center.X - layoutRadius, center.Y - layoutRadius,
-                layoutRadius * 2, layoutRadius * 2);
+            setup.GetRectangles().ForEach(x => g.DrawRectangle(Pens.Black, x));
+            g.DrawEllipse(Pens.Black, setup.GetCenter().X - setup.GetRadius(), setup.GetCenter().Y - setup.GetRadius(),
+                setup.GetRadius() * 2, setup.GetRadius() * 2);
             var time = DateTime.Now.ToString("dd/MM/yyyy_HH-mm-ss");
             var path = Path.Combine(context.WorkDirectory,
                 $"..\\..\\..\\CircularCloudLayout_{context.Test.Name}_{time}.bmp");
---
+++ cs/CircularCloudLayoutTests/SetUpBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using TagsCloudVisualization;
+
+namespace CircularCloudLayoutTests
+{
+    public class SetUpBuilder
+    {
+        private int layoutRadius;
+        private Point center;
+        private CircularCloudLayout layout;
+        private List<Rectangle> placedRectangles;
+        public SetUpBuilder WithCenter(Point center)
+        {
+            this.center = center;
+            layoutRadius = center.X < center.Y ? center.X : center.Y;
+            layout = new CircularCloudLayout(center);
+            placedRectangles = new();
+            return this;
+        }
+        public SetUpBuilder WithCustomSizes()
+        {
+            SizeListBulder.GetCustomSizes().ForEach(x =>
+            {
+                if (layout.PutNextRectangle(x, out Rectangle rect))
+                    placedRectangles.Add(rect);
+            });
+            return this;
+        }
+
+        public Point GetCenter() => center;
+        public int GetRadius() => layoutRadius;
+        public CircularCloudLayout GetLayout() => layout;
+        public List<Rectangle> GetRectangles() => placedRectangles;
+    }
+}
