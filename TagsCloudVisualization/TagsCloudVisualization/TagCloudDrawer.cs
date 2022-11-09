2e7d17ea2bf382ee5768f797e94017b8de8b16f2
FoxDictator <mega.y1979@gmail.com>
2022-11-09 22:30:57 +0500

    Created img drawer

---
+++ TagsCloudVisualization/TagsCloudVisualization/TagCloudDrawer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace TagsCloudVisualization
+{
+    internal class TagCloudDrawer
+    {
+        private Bitmap bitmap;
+        private Graphics graphics;
+
+        public string SavePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).ToString(),
+            "SavedImages", "img.jpg");
+        public Pen pen;
+        private int scale = 1;
+
+        public TagCloudDrawer(Size bitmapSize, int scale)
+        {
+            bitmap = new Bitmap(bitmapSize.Width, bitmapSize.Height);
+            graphics = Graphics.FromImage(bitmap);
+            pen = new Pen(Color.DarkRed, 1);
+            this.scale = scale;
+        }
+
+        public void DrawRectangle(Rectangle rectangle)
+        {
+            rectangle.X *= scale;
+            rectangle.X -= bitmap.Width / 2 * (scale - 1);
+            rectangle.Y *= scale;
+            rectangle.Y -= bitmap.Height / 2 * (scale - 1);
+            rectangle.Width *= scale;
+            rectangle.Height *= scale;
+            graphics.FillRectangle(Brushes.Blue, rectangle);
+            graphics.DrawRectangle(pen, rectangle);
+        }
+
+        public void SaveImage()
+        {
+            bitmap.Save(SavePath, ImageFormat.Jpeg);
+        }
+        
+    }
+}
--- TagsCloudVisualization/TagsCloudVisualization/UnitTest1.cs
+++ TagsCloudVisualization/TagsCloudVisualization/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Drawing;
 using FluentAssertions;
 using NUnit.Framework;
@@ -57,6 +59,63 @@ namespace TagsCloudVisualization
             layout.PutNextRectangle(new Size(1, 8))
                 .Should().Be(new Rectangle(7, -3, 1, 8));
         }
+
+    }
+
+    public class DrawingTests
+    {
+        private TagCloudDrawer drawer;
+        private CircularCloudLayouter layout;
+
+        [SetUp]
+        public void SetUp()
+        {
+            drawer = new TagCloudDrawer(new Size(2000, 2000), 10);
+            layout = new CircularCloudLayouter(new Point(1000, 1000));
+        }
+
+        [Test]
+        public void DrawDefaultCloud()
+        {
+            List<Rectangle> rects = new List<Rectangle>(new[]
+            {
+                layout.PutNextRectangle(new Size(6, 2)),
+                layout.PutNextRectangle(new Size(2, 4)),
+                layout.PutNextRectangle(new Size(3, 2)),
+                layout.PutNextRectangle(new Size(6, 1)),
+                layout.PutNextRectangle(new Size(3, 2)),
+                layout.PutNextRectangle(new Size(2, 2)),
+                layout.PutNextRectangle(new Size(8, 2)),
+
+                layout.PutNextRectangle(new Size(2, 4)),
+                layout.PutNextRectangle(new Size(2, 1)),
+                layout.PutNextRectangle(new Size(3, 2)),
+                layout.PutNextRectangle(new Size(2, 3)),
+                layout.PutNextRectangle(new Size(14, 1)),
+                layout.PutNextRectangle(new Size(1, 7)),
+                layout.PutNextRectangle(new Size(1, 5)),
+                layout.PutNextRectangle(new Size(16, 1)),
+                layout.PutNextRectangle(new Size(1, 8))
+            });
+
+            foreach (var r in rects)
+            {
+                drawer.DrawRectangle(r);
+            }
+            drawer.SaveImage();
+        }
+
+        [Test]
+        public void DrawRandomCloud()
+        {
+            Random r = new Random();
+            for(int i=0;i<100;i++)
+            {
+                drawer.DrawRectangle(layout.PutNextRectangle(new Size(r.Next(10, 20), r.Next(1, 10))));
+            }
+            drawer.SaveImage();
+        }
+
 
     }
 }
