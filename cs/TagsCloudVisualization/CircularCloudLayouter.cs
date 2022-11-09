778d7fc492052b9934db9cf1bb6e3c68ce9c92f5
massshasch <massshasch@gmail.com>
2022-11-09 18:26:03 +0500

    Fixed CircularCloudLayouter

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -2,55 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using TagsCloudVisualization;
 
-internal class CircularCloudLayouter
+public class CircularCloudLayouter
 {
-    private readonly List<Rectangle> rectangles;
-    private readonly Point center;
+    public readonly List<Rectangle> rectangles;
+    private Spiral spiral;
 
     public CircularCloudLayouter(Point center)
     {
         rectangles = new List<Rectangle>();
-        this.center = center;
+        spiral = new Spiral(center);
     }
 
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        if (rectangleSize.Height < 0 || rectangleSize.Width < 0) throw new ArgumentException();
-        foreach (var point in GetNextSpiralPoint(0.1, 0.001))
-        {
-            var coordinatesOfRectangle = FindCoordinatesOfRectangle(point, rectangleSize);
-            var rectangle = new Rectangle(coordinatesOfRectangle, rectangleSize);
-            if (IntersectsWithOtherRectangles(rectangle)) continue;
-            rectangles.Add(rectangle);
-            return rectangle;
-        }
-
-        throw new Exception();
+        if (rectangleSize.Height < 0 || rectangleSize.Width < 0)
+            throw new ArgumentException("Wrong size of rectangle");
+        var rect = spiral.GetPoints()
+            .Select(point =>
+            {
+                var coordinatesOfRectangle = CalculateRectangleCoordinates(point, rectangleSize);
+                return new Rectangle(coordinatesOfRectangle, rectangleSize);
+            })
+            .First(rectangle => !IntersectsWithOtherRectangles(rectangle));
+        rectangles.Add(rect);
+        return rect;
     }
 
-    private Point FindCoordinatesOfRectangle(Point point, Size size)
+    private Point CalculateRectangleCoordinates(Point rectangleCenter, Size rectangleSize)
     {
-        return new Point(point.X - size.Width / 2, point.Y - size.Height / 2);
+        return new Point(rectangleCenter.X - rectangleSize.Width / 2, rectangleCenter.Y - rectangleSize.Height / 2);
     }
 
     private bool IntersectsWithOtherRectangles(Rectangle rectangle)
     {
-        return rectangles.Where(r => r.IntersectsWith(rectangle)).ToList().Count != 0;
-    }
-
-    public IEnumerable<Point> GetNextSpiralPoint(double angleOffset, double radiusOffset)
-    {
-        double angle = 0;
-
-        double radius = 0;
-        if (angle == 0 && radius == 0) yield return center;
-        while (true)
-        {
-            yield return new Point(center.X + (int) Math.Round(radius * Math.Cos(angle)),
-                center.Y + (int) Math.Round(radius * Math.Sin(angle)));
-            radius += radiusOffset;
-            angle += angleOffset;
-        }
+        return rectangles.Any(r => r.IntersectsWith(rectangle));
     }
 }
