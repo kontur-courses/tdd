170e6be321c6962a7041cdacc981cb93c85f5a9d
Annie Ryannel <ryannel321@gmail.com>
2022-11-09 16:45:46 +0300

    spiral classes added

---
+++ cs/TagsCloudVisualization/ArchimedeanSpiral.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class ArchimedeanSpiral : ISpiral
+{
+    private const double DefaultCoefficient = 1d; 
+    
+    private readonly Point _center;
+    private readonly double _coefficient;
+
+    public ArchimedeanSpiral(Point center = default, double coefficient = DefaultCoefficient)
+    {
+        if (coefficient <= 0)
+            throw new ArgumentException("coefficient should be positive", nameof(coefficient));
+
+        _center = center;
+        _coefficient = coefficient;
+    }
+
+    public Point GetCartesianPoint(int degree)
+    {
+        if (degree < 0)
+            throw new ArgumentException("degree should be positive", nameof(degree));
+
+        var radians = Math.PI * degree / 180;
+        var radius = _coefficient * radians;
+
+        var x = (int)(radius * Math.Cos(radians)) + _center.X;
+        var y = (int)(radius * Math.Sin(radians)) + _center.Y;
+        return new Point(x, y);
+    }
+}
---
+++ cs/TagsCloudVisualization/ArchimedeanSpiralIterator.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class ArchimedeanSpiralIterator : ISpiralIterator
+{
+    private int _currentAngle;
+    private int _step;
+    private readonly ISpiral _spiral;
+
+    public ArchimedeanSpiralIterator(ISpiral spiral, int startAngle = 0, int step = 1)
+    {
+        if (startAngle < 0)
+        {
+            throw new ArgumentException("Angle should be not negative", nameof(startAngle));
+        }
+
+        if (step <= 0)
+        {
+            throw new ArgumentException("Step should be positive", nameof(step));
+        }
+        _spiral = spiral;
+        _currentAngle = startAngle;
+        _step = step;
+    }
+
+    public Point Next()
+    {
+        var nextPoint = _spiral.GetCartesianPoint(_currentAngle);
+        _currentAngle += _step;
+        return nextPoint;
+    }
+}
