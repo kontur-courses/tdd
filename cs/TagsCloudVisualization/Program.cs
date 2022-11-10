4f837e66c8dae5e0c72f0e2384c79d8d031d3010
MeleshinDA <meleshindemid@yandex.ru>
2022-11-10 18:27:28 +0300

    Вторая задача.

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class Program
+    {
+        private static CloudGenerator GetCloudGenerator()
+        {
+            var minSize = new Size(50, 50);
+            var maxSize = new Size(200, 200);
+            var center = new Point(1500, 1500);
+            return new CloudGenerator(100, minSize, maxSize, center, 4, 4, 0);
+        }
+        
+        public static void Main(string[] args)
+        {
+            var cg = GetCloudGenerator();
+            LayoutDrawer layoutDrawer = new LayoutDrawer(3000, 3000, Color.Black, 2);
+            layoutDrawer.DrawLayout(cg);
+            
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Readme.md
@@ -0,0 +1,8 @@
+На каждом изображении 100 прямоугольников.
+
+1. step = 0.1, density = 0.1, 
+![image](https://github.com/MeleshinDA/VKFriendsAPI/blob/master/example/example1.png)
+2. step = 2, density = 2
+![image](https://github.com/MeleshinDA/VKFriendsAPI/blob/master/example/example1.png)
+3. step = 4, density = 4
+![image](https://github.com/MeleshinDA/VKFriendsAPI/blob/master/example/example1.png)
