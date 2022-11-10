f7057edb3fc50dbeea44e0aff0c70039c3c84a67
Igor Sotarev <igor@sotarev.ru>
2022-11-10 20:09:50 +0500

    Refactor Program and TagCloudImageGenerator

--- cs/TagCloudVisualizer/Program.cs
+++ cs/TagCloudVisualizer/Program.cs
@@ -11,23 +11,24 @@ if (args.Length > 0)
 var canvasSize = new Size(500, 500);
 var layouter = new CircularCloudLayouter(new Point(canvasSize.Width / 2, canvasSize.Height / 2));
 var random = new Random();
+var rectangles = new Rectangle[rectangleCount];
 
 for (var i = 0; i < rectangleCount; i++)
 {
-    layouter.PutNextRectangle(new Size(random.Next(10, 30), random.Next(10, 30)));
+    rectangles[i] = layouter.PutNextRectangle(new Size(random.Next(10, 30), random.Next(10, 30)));
 }
 
-var path = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\TagCloud\\";
+var path = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+var folderName = "TagCloud";
+var now = DateTime.Now;
+var filename = $"TagCloud_{now:yyyy-MM-dd-HH-mm-ss}.png";
 
 if (!Directory.Exists(path))
     Directory.CreateDirectory(path);
 
-var now = DateTime.Now;
-var filename = $"TagCloud_{now:yyyy-MM-dd-HH-mm-ss}.png";
+var fullFilePath = Path.Combine(path, folderName, filename);
 
-var fullFilePath = path + filename;
-
-var image = TagCloudImageGenerator.GenerateImage(layouter.Rectangles.ToArray(), canvasSize);
+var image = TagCloudImageGenerator.GenerateImage(rectangles, canvasSize);
 image.Save(fullFilePath, ImageFormat.Png);
 
 Console.WriteLine($"Image saved as {fullFilePath}");
--- cs/TagCloudVisualizer/TagCloudImageGenerator/TagCloudImageGenerator.cs
+++ cs/TagCloudVisualizer/TagCloudImageGenerator/TagCloudImageGenerator.cs
@@ -2,17 +2,17 @@
 
 namespace TagCloudVisualizer.TagCloudImageGenerator;
 
-public class TagCloudImageGenerator
+public static class TagCloudImageGenerator
 {
     public static Bitmap GenerateImage(Rectangle[] rectangles, Size canvasSize)
     {
         var canvas = new Bitmap(canvasSize.Width, canvasSize.Height);
-        var graphics = Graphics.FromImage(canvas);
 
-        var backgrouldBrush = Brushes.White;
-        var rectanglePen = Pens.Black;
-
-        graphics.FillRectangle(backgrouldBrush,0, 0, canvas.Width, canvas.Height);
+        using var graphics = Graphics.FromImage(canvas);
+        using var backgroundBrush = new SolidBrush(Color.White);
+        using var rectanglePen = new Pen(Color.Black);
+        
+        graphics.FillRectangle(backgroundBrush,0, 0, canvas.Width, canvas.Height);
         foreach (var rectangle in rectangles)
         {
             graphics.DrawRectangle(rectanglePen, rectangle);
