f2e645b6ec557ef7e9fa678473b196091bade03a
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2022-11-10 15:18:53 +0500

    add TagCloudDrawer class and 2 cloud examples

--- cs/TagsCloudVisualization/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/Program.cs
@@ -1,16 +1,8 @@
 ﻿using System.Drawing;
-using System.Drawing.Imaging;
-using System.Net.Mime;
 using TagsCloudVisualization;
 
-var layouter = new CircularCloudLayouter(new Point(150, 150));
-var bitmap = new Bitmap(300, 300);
-var g = Graphics.FromImage(bitmap);
-var pen = new Pen(Color.Black, 2);
-var pen2 = new Pen(Color.Red, 2);
-var rand = new Random();
-for (int i = 0; i < 100; i++)
-{
-    g.DrawRectangle(pen,layouter.PutNextRectangle(new Size( 20+ rand.Next(-10, 10),20+ rand.Next(-10, 10))));
-}
-bitmap.Save("..\\..\\..\\..\\..\\..\\Results\\result2.bmp");
+var drawer = new TagCloudDrawer(300, 300, 1, Color.Black);
+drawer.DrawRandomRectangles(200,
+    new Size(1, 1),
+    new Size(10, 10));
+drawer.SaveImage();
