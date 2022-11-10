794b4189d241ffe41a006842402076efcfd0b412
angst-storm <serezha.kiprin@gmail.com>
2022-11-10 20:48:38 +0500

    fixed test

--- cs/TagCloudTests/SpiralPointGenerator_Should.cs
+++ cs/TagCloudTests/SpiralPointGenerator_Should.cs
@@ -21,13 +21,19 @@ public class SpiralPointGenerator_Should
     [Test]
     public void Generate_GeneratedLine_IntersectsCoordinateAxesAtCorrectPoints()
     {
-        var generator = new SpiralPointGenerator(1, Math.PI / 3);
+        var generator = new SpiralPointGenerator(1, 45 / (180 / Math.PI));
 
         var points = generator.Generate(new Point(0, 0)).Take(600).ToArray();
-        var intersectXAxe = points.Where(p => p.Y == 0).Select((p, i) => (x: Math.Abs(p.X), r: i * 3));
-        var intersectYAxe = points.Where(p => p.X == 0).Select((p, i) => (y: Math.Abs(p.Y), r: i));
+        var intersectXAxe = points
+            .Skip(1)
+            .Select((point, i) => (point, r: i + 2))
+            .Where(t => t.point.Y == 0);
+        var intersectYAxe = points
+            .Skip(2)
+            .Select((point, i) => (point, r: i + 3))
+            .Where(t => t.point.X == 0);
 
-        intersectXAxe.Should().AllSatisfy(t => t.x.Should().Be(t.r));
-        intersectYAxe.Should().AllSatisfy(t => t.y.Should().Be(t.r));
+        intersectXAxe.Should().AllSatisfy(t => Math.Abs(t.point.X).Should().Be(t.r));
+        intersectYAxe.Should().AllSatisfy(t => Math.Abs(t.point.Y).Should().Be(t.r));
     }
 }
