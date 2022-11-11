8b2deb0fef6c99f33f6eb20f722613582d94e840
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-11-12 01:44:13 +0500

    CircularCloudLayouterTests has been refactored

--- cs/TagsCloudVisualization/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Drawing;
-using System.Security.Cryptography.X509Certificates;
 using NUnit.Framework;
 using FluentAssertions;
 
@@ -19,13 +18,15 @@ namespace TagsCloudVisualization.TagsCloudVisualizationTests
             center = new Point(400, 400);
             spr = new Spiral(center);
             addedRectangles = new List<Rectangle>();
-            layouter = new CircularCloudLayouter(center, spr);
+            layouter = new CircularCloudLayouter(spr);
         }
 
         [Test]
         public void CloudInitialization_ThrowsNullReferenceException_OnNullSpiral()
         {
-            Assert.Throws<NullReferenceException>(() => new CircularCloudLayouter(center, null));
+            var action = () => new CircularCloudLayouter(null);
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("Spiral should not be null");
         }
 
         [TestCase(0)]
@@ -33,22 +34,26 @@ namespace TagsCloudVisualization.TagsCloudVisualizationTests
         [TestCase(100)]
         public void PutNextRectangle_ReturnPutRectangles_CorrectCount(int expectedCount)
         {
+            var size = new Size(100, 100);
+
             for (int i = 0; i < expectedCount; i++)
-                addedRectangles.Add(layouter.PutNextRectangle(new Size(400, 400)));
+            {
+                var rectangle = layouter.PutNextRectangle(size);
+                addedRectangles.Add(rectangle);
+            }
 
             addedRectangles.Count().Should().Be(expectedCount);
         }
 
-        [TestCase(1, -1)]
-        [TestCase(-1, 1)]
-        [TestCase(1, 0)]
-        [TestCase(0, 1)]
-        [TestCase(-1, -1)]
-        [TestCase(0, 0)]
+        [TestCase(1, -1, TestName = "Negative height, correct width")]
+        [TestCase(-1, 1, TestName = "Negative width, correct height")]
+        [TestCase(1, 0, TestName = "Zero height, correct width")]
+        [TestCase(0, 1, TestName = "Zero width, correct height")]
         public void PutNextRectangle_ThrowsArgumentException_IncorrectArguments(int width, int height)
         {
-            Assert.Throws<ArgumentException>(() => layouter.PutNextRectangle(new Size(width, height)));
+            var action = () => layouter.PutNextRectangle(new Size(width, height));
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("Sides of the rectangle should not be non-positive");
         }
     }
 }
-
