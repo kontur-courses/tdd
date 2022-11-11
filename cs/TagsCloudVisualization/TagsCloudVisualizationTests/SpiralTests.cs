526c0eeeb67990daf2d570ef326e37fe0b8846c8
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-11-12 01:49:08 +0500

    SpiralTests has been refactored

--- cs/TagsCloudVisualization/TagsCloudVisualizationTests/SpiralTests.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualizationTests/SpiralTests.cs
@@ -8,48 +8,31 @@ namespace TagsCloudVisualization.TagsCloudVisualizationTests
     internal class SpiralTests
     {
         [Test]
-        public void spiralInitialization_PropertyCenterEqualParamCenter_OnCorrectValue()
+        public void SpiralInitialization_ThrowsArgumentException_OnZeroAngleStep()
         {
-            var center = new Point(500, 500);
-            var spr = new Spiral(center);
-            spr.Center.Should().BeEquivalentTo(center);
+            var action = () => new Spiral(new Point(4, 4), 0);
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("Angle step should not be zero");
         }
 
-        [TestCase(0.0, 0.0)]
-        [TestCase(0, 0)]
-        [TestCase(0, 1)]
-        [TestCase(0.1, 0)]
-
-        public void spiralInitialization_ThrowsArgumentException_OnIncorrectValue(
-            double step, double parameter)
-        {
-            Assert.Throws<ArgumentException>(() => new Spiral(new Point(4, 4), step, parameter));
-        }
-
-        [TestCase(0.1, 2, new int[]
-        { 400, 399, 399, 399, 400, 400, 401, 401, 402, 402 }, new int[]
-        { 401, 401, 400, 399, 399, 398, 398, 399, 399, 400 })]
-
-        [TestCase(0.2, 1, new int[]
-        { 399, 400, 400, 401, 401, 401, 400, 399, 399, 398 }, new int[]
-        { 400, 400, 399, 399, 400, 401, 401, 401, 400, 400 })]
-
-        [TestCase(-10, Math.PI, new int[]
-        { 404, 396, 398, 413, 376, 429, 378, 404, 420, 357 }, new int[]
-        { 397, 409, 385, 415, 393, 391, 427, 360, 440, 375 })]
+        [TestCase(0.1,
+            new[] { 401, 401, 400, 400 },
+            new[] { 400, 401, 401, 402 })]
+        [TestCase(-0.1,
+            new[] { 399, 399, 400, 400 },
+            new[] { 400, 401, 401, 402 })]
         public void GetPoints_ShouldReturnSpiralPoints_OnCorrectValue(
-            double step, double parameter, int[] x, int[] y)
+            double step, int[] x, int[] y)
         {
-            var points = new List<Point>();
-            var expectedPoints = new Point[10];
+            var expectedPoints = new Point[x.Length];
 
             for (int i = 0; i < x.Length; i++)
             {
                 expectedPoints[i] = new Point(x[i], y[i]);
             }
 
-            var spiral = new Spiral(new Point(400, 400), step, parameter);
-            spiral.GetPoints(10).Should().BeEquivalentTo(expectedPoints);
+            var spiral = new Spiral(new Point(400, 400), step);
+            spiral.GetPoints(x.Length).Should().BeEquivalentTo(expectedPoints);
         }
     }
 }
