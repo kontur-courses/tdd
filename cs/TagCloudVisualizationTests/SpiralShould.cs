5e7a9a589b1890ec6fda929b72977f6be4219a20
IuxMorti <zdracte21@bk.ru>
2022-11-13 14:16:43 +0500

    add tests in SpiralShould.cs

--- cs/TagCloudVisualizationTests/SpiralShould.cs
+++ cs/TagCloudVisualizationTests/SpiralShould.cs
@@ -11,6 +11,21 @@ namespace TagCloudVisualizationTests
     public class SpiralShould
     {
 
+        [Test]
+        public void ThrowException_IncorrectDistanceBetweenLoops()
+        {
+            Action action = () => new Spiral(0, Point.Empty);
+            action.Should().Throw<ArgumentException>();
+        }
+        
+        [Test]
+        public void ThrowException_IncorrectIncrement()
+        {
+            Action action = () => new Spiral(1, Point.Empty, 0);
+            action.Should().Throw<ArgumentException>();
+        }
+
+
         [TestCase(0, 0)]
         [TestCase(-1000, -1000)]
         [TestCase(1000, 1000)]
@@ -20,7 +35,6 @@ namespace TagCloudVisualizationTests
             var spiral = new Spiral(1, expectedCenter);
 
             spiral.Center.Should().Be(expectedCenter);
-
         }
 
         [Test]
@@ -37,19 +51,19 @@ namespace TagCloudVisualizationTests
         [Test]
         public void ReturnDistanceBetweenLoops_EqualDistanceBetweenLoops()
         {
-            var center = new Point(0,0);
+            var center = new Point(0, 0);
             var spiral = new Spiral(
                 1,
                 center,
                 (float)Math.PI * 2);
 
-            var previsionPoint = spiral.First();
 
+            var previsionPoint = spiral.First();
             foreach (var point in spiral.Skip(1).Take(10))
             {
-                var distanceBetweenLoops = 
-                    Math.Sqrt(Math.Pow(previsionPoint.X - point.X, 2) 
-                                 + Math.Pow(previsionPoint.Y - point.Y, 2));
+                var distanceBetweenLoops =
+                    Math.Sqrt(Math.Pow(previsionPoint.X - point.X, 2)
+                              + Math.Pow(previsionPoint.Y - point.Y, 2));
                 previsionPoint = point;
                 distanceBetweenLoops.Should().BeInRange(1 - 1e-4, 1 + 1e-4);
             }
