66ef26b534492f1278fa28d5c37f3b5da7946fd8
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-11-13 18:56:15 +0500

    Added missing CloudVisualizer instance arguments

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -8,12 +8,12 @@ namespace TagsCloudVisualization
         {
             var center = new Point(400, 400);
             var rectangleSizes = RectanglesRandomizer.GetSortedRectangles(
-                100, 100, 100);
+                100, 100, 10);
 
             var layouter = new CircularCloudLayouter(new Spiral(center));
             var rectangles = layouter.GetRectangles(rectangleSizes);
 
-            var visualizer = new CloudVisualizer(center, rectangles);
+            var visualizer = new CloudVisualizer(center, rectangles, @"..\..\..\Images\", "Cloud 10");
             visualizer.CreateImage();
         }
     }
