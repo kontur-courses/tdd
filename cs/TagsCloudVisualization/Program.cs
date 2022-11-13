4940d071e08802ed6406bba6e50152b88dd05acd
glushakov.m <glushakovmax@mail.ru>
2022-11-14 02:31:07 +0300

    generate test images of layout

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing;
+using TagsCloudVisualization;
 
-Console.WriteLine("Hello, World!");
+if (!Directory.Exists("Images"))
+    Directory.CreateDirectory("Images");
+
+var random = new Random();
+var squareRectLayout = new CircularCloudLayouter(new Point(0, 0), 0.01);
+
+for (int i = 0; i < 100; i++)
+{
+    var squareWidth = random.Next(5, 100);
+    squareRectLayout.PutNextRectangle(new Size(squareWidth, squareWidth));
+}
+
+new BitmapTagsCloudVisualization().SaveTagsCloud(squareRectLayout, "Images\\SquareRectLayout.bmp");
+
+
+var horizontalRectLayout = new CircularCloudLayouter(new Point(0, 0), 0.01);
+
+for (int i = 0; i < 100; i++)
+{
+    var height = random.Next(5, 25);
+    var width = random.Next(50, 100);
+    horizontalRectLayout.PutNextRectangle(new Size(width, height));
+}
+
+new BitmapTagsCloudVisualization().SaveTagsCloud(horizontalRectLayout, "Images\\HorizontalRectLayout.bmp");
+
+var verticalRectLayout = new CircularCloudLayouter(new Point(0, 0), 0.01);
+
+for (int i = 0; i < 100; i++)
+{
+    var width = random.Next(5, 25);
+    var height = random.Next(50, 100);
+    verticalRectLayout.PutNextRectangle(new Size(width, height));
+}
+
+new BitmapTagsCloudVisualization().SaveTagsCloud(verticalRectLayout, "Images\\VerticalRectLayout.bmp");
+
+
+var singleSizeSquareRectLayout = new CircularCloudLayouter(new Point(0, 0), 0.01);
+var squareSingleWidth = random.Next(5, 100);
+
+for (int i = 0; i < 100; i++)
+{
+    singleSizeSquareRectLayout.PutNextRectangle(new Size(squareSingleWidth, squareSingleWidth));
+}
+
+new BitmapTagsCloudVisualization().SaveTagsCloud(singleSizeSquareRectLayout, "Images\\SingleSizeSquareRectLayout.bmp");
