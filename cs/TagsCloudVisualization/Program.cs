e8258cfac62128dbe2d212b0ae51a96562f3e506
massshasch <massshasch@gmail.com>
2022-11-13 21:09:36 +0500

    Changed method call

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -10,13 +10,14 @@ namespace TagsCloudVisualization
             var rnd = new Random();
             var canvasSize = new Size(800, 800);
             var visualisation = new CircularCloudVisualizator(canvasSize);
-            var layouter = new CircularCloudLayouter(new Point(canvasSize.Width / 2, canvasSize.Height / 2));
+            var center = new Point(canvasSize.Width / 2, canvasSize.Height / 2);
+            var layouter = new CircularCloudLayouter(center, new Spiral(center, 1, 1));
             for (int i = 0; i < 100; i++)
             {
                 layouter.PutNextRectangle(new Size(rnd.Next(10, 100), rnd.Next(10, 50)));
             }
 
-            visualisation.DrawRectangles(layouter.rectangles);
+            visualisation.DrawRectangles(layouter.Rectangles);
             visualisation.SaveCanvas("../visualization.jpg");
         }
     }
