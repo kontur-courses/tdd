f1daa339d0878339658eec94c6f4456c2545fa8c
massshasch <massshasch@gmail.com>
2022-11-13 21:05:30 +0500

    Fixed spiral

--- cs/TagsCloudVisualization/Spiral.cs
+++ cs/TagsCloudVisualization/Spiral.cs
@@ -9,43 +9,38 @@ namespace TagsCloudVisualization
         private readonly double angleOffset;
         private readonly double radiusOffset;
         private readonly Point center;
-        private readonly Dictionary<(double, double), Point> usedPoints;
+        private readonly List<Point> usedPoints;
         private double angle;
         private double radius;
 
         public Spiral(Point center, double angleOffset, double radiusOffset)
         {
             this.center = center;
-            usedPoints = new Dictionary<(double, double), Point>();
+            usedPoints = new List<Point>();
             this.angleOffset = angleOffset;
             this.radiusOffset = radiusOffset;
         }
 
         public IEnumerable<Point> GetPoints()
         {
-            (double, double) currentAngleAndRadius;
+            foreach (var point in usedPoints)
+                yield return point;
+
             if (angle == 0 && radius == 0)
             {
-                currentAngleAndRadius = (0, 0);
-                if (!usedPoints.ContainsKey(currentAngleAndRadius))
-                    usedPoints.Add(currentAngleAndRadius, center);
-                yield return usedPoints[currentAngleAndRadius];
+                if (!usedPoints.Contains(center))
+                    usedPoints.Add(center);
+                yield return center;
             }
 
             while (true)
             {
                 radius += radiusOffset;
                 angle += angleOffset;
-                currentAngleAndRadius = (angle, radius);
-                if (usedPoints.ContainsKey(currentAngleAndRadius))
-                    yield return usedPoints[currentAngleAndRadius];
-                else
-                {
-                    var x = center.X + (int) Math.Round(radius * Math.Cos(angle));
-                    var y = center.Y + (int) Math.Round(radius * Math.Sin(angle));
-                    usedPoints.Add(currentAngleAndRadius, new Point(x, y));
-                    yield return usedPoints[currentAngleAndRadius];
-                }
+                var x = center.X + (int) Math.Round(radius * Math.Cos(angle));
+                var y = center.Y + (int) Math.Round(radius * Math.Sin(angle));
+                usedPoints.Add(new Point(x, y));
+                yield return new Point(x, y);
             }
         }
     }
