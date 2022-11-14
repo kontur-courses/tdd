e8d923a52948fc0462b38e5399aecab013d95f8f
FoxDictator <mega.y1979@gmail.com>
2022-11-14 17:26:47 +0500

    Refactored tests using new package

--- TagsCloudVisualization/TagsCloudVisualization/UnitTest1.cs
+++ TagsCloudVisualization/TagsCloudVisualization/UnitTest1.cs
@@ -2,7 +2,11 @@ using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using System;
+using System.Diagnostics;
 using System.Drawing;
+using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
 
 namespace TagsCloudVisualization
 {
@@ -10,15 +14,17 @@ namespace TagsCloudVisualization
     public class PlacementTests
     {
         private BlockCloudLayouter layout;
+        private TestsHelper helper;
 
         [SetUp]
         public void SetUp()
         {
             layout = new BlockCloudLayouter(new Point(1500, 1500));
+            helper = new TestsHelper(layout);
         }
 
         [Test]
-        public void DefaultTest()
+        public void FailTest()
         {
             Random r = new Random();
             for (int i = 0; i < 100; i++)
@@ -34,99 +40,65 @@ namespace TagsCloudVisualization
         {
             if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
             {
-                DrawLayoutOnFailedTests();
+                helper.FailedTestsDrawImage();
             }
-        }
-
-        private void DrawLayoutOnFailedTests()
-        {
-            TagCloudDrawer drawer = new TagCloudDrawer(layout);
-            drawer.Scale = 10;
-            drawer.DrawImage();
-            drawer.SaveImage();
         }
     }
 
-    public class DrawingTests
+    [TestFixture]
+    [UseReporter(typeof(ApprovalTests.Reporters.VisualStudioReporter))]
+    [UseApprovalSubdirectory(@"Results\BlockLayoutDrawingTests")]
+    public class BlockLayoutDrawingTests
     {
-        private TagCloudDrawer drawer;
-        private BlockCloudLayouter layout;
+        private TestsHelper helper;
+        private BlockCloudLayouter layouter;
 
         [SetUp]
         public void SetUp()
         {
-            layout = new BlockCloudLayouter(new Point(1000, 1000));
-            drawer = new TagCloudDrawer(layout);
-            drawer.Scale = 10;
+            layouter = new BlockCloudLayouter(new Point(1000, 1000));
+            helper = new TestsHelper(layouter);
         }
 
         [Test]
         public void DrawDefaultCloud()
         {
-            layout.PutNextRectangle(new Size(6, 2));
-            layout.PutNextRectangle(new Size(2, 4));
-            layout.PutNextRectangle(new Size(3, 2));
-            layout.PutNextRectangle(new Size(6, 1));
-            layout.PutNextRectangle(new Size(3, 2));
-            layout.PutNextRectangle(new Size(2, 2));
-            layout.PutNextRectangle(new Size(8, 2));
-            layout.PutNextRectangle(new Size(2, 4));
-            layout.PutNextRectangle(new Size(2, 1));
-            layout.PutNextRectangle(new Size(3, 2));
-            layout.PutNextRectangle(new Size(2, 3));
+            layouter.PutNextRectangle(new Size(6, 2));
+            layouter.PutNextRectangle(new Size(2, 4));
+            layouter.PutNextRectangle(new Size(3, 2));
+            layouter.PutNextRectangle(new Size(6, 1));
+            layouter.PutNextRectangle(new Size(3, 2));
+            layouter.PutNextRectangle(new Size(2, 2));
+            layouter.PutNextRectangle(new Size(8, 2));
+            layouter.PutNextRectangle(new Size(2, 4));
+            layouter.PutNextRectangle(new Size(2, 1));
+            layouter.PutNextRectangle(new Size(3, 2));
+            layouter.PutNextRectangle(new Size(2, 3));
+
+            helper.Approve();
         }
 
         [Test]
         public void DrawRandomCloud()
         {
-            Random r = new Random();
+            Random r = new Random(156);
             for (int i = 0; i < 500; i++)
             {
-                layout.PutNextRectangle(new Size(r.Next(10, 80), r.Next(1, 10)));
+                layouter.PutNextRectangle(new Size(r.Next(10, 80), r.Next(1, 10)));
             }
+
+            helper.Approve();
         }
 
         [Test]
         public void DrawGrowingSizeRectanglesCloud()
         {
-            Random r = new Random();
             for (int i = 0; i < 100; i++)
             {
-                layout.PutNextRectangle(new Size(i, 2));
+                layouter.PutNextRectangle(new Size(i, 2));
             }
-        }
 
-        [Test]
-        public void DrawSimpleCloud()
-        {
-            for (int i = 0; i < 2000; i++)
-            {
-                layout.PutNextRectangle(new Size(4, 4));
-            }
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            if (TestContext.CurrentContext.Result.Outcome == ResultState.Success)
-            {
-                drawer.DrawImage();
-                drawer.SaveImage();
-            }
-        }
-    }
-
-    public class SpiralLayoutDrawingTests
-    {
-        private TagCloudDrawer drawer;
-        private SpiralCloudLayouter layouter;
-
-        [SetUp]
-        public void SetUp()
-        {
-            layouter = new SpiralCloudLayouter(new Point(1000, 1000));
-            drawer = new TagCloudDrawer(layouter);
-            drawer.Scale = 10;
+            helper.Approve();
         }
 
         [Test]
@@ -136,16 +108,55 @@ namespace TagsCloudVisualization
             {
                 layouter.PutNextRectangle(new Size(4, 4));
             }
+
+            helper.Approve();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                helper.FailedTestsDrawImage();
+            }
+        }
+    }
+
+    [TestFixture]
+    [UseReporter(typeof(ApprovalTests.Reporters.VisualStudioReporter))]
+    [UseApprovalSubdirectory(@"Results\SpiralLayoutDrawingTests")]
+    public class SpiralLayoutDrawingTests
+    {
+        private SpiralCloudLayouter layouter;
+        private TestsHelper helper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            layouter = new SpiralCloudLayouter(new Point(1000, 1000));
+            helper = new TestsHelper(layouter);
+        }
+
+        [Test]
+        public void DrawSimpleCloud()
+        {
+            for (int i = 0; i < 2000; i++)
+            {
+                layouter.PutNextRectangle(new Size(4, 4));
+            }
+
+            helper.Approve();
         }
 
         [Test]
         public void DrawGrowingSizeRectanglesCloud()
         {
-            Random r = new Random();
             for (int i = 0; i < 100; i++)
             {
                 layouter.PutNextRectangle(new Size(i, 2));
             }
+
+            helper.Approve();
         }
 
         [Test]
@@ -153,12 +164,14 @@ namespace TagsCloudVisualization
         {
             layouter.PutNextRectangle(new Size(1, 1));
             layouter.PutNextRectangle(new Size(5, 5));
+
+            helper.Approve();
         }
 
         [Test]
         public void DrawRandomCloud()
         {
-            Random r = new Random();
+            Random r = new Random(223);
             for (int i = 0; i < 500; i++)
             {
                 layouter.PutNextRectangle(new Size(r.Next(10, 80), r.Next(1, 10)));
@@ -168,11 +181,35 @@ namespace TagsCloudVisualization
         [TearDown]
         public void TearDown()
         {
-            if (TestContext.CurrentContext.Result.Outcome == ResultState.Success)
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
             {
-                drawer.DrawImage();
-                drawer.SaveImage();
+                helper.FailedTestsDrawImage();
             }
+        }
+    }
+
+    internal class TestsHelper
+    {
+        private ICloudLayouter layouter;
+
+        public TestsHelper(ICloudLayouter layouter)
+        {
+            this.layouter = layouter;
+        }
+
+        public void Approve()
+        {
+            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
+                "PlacedRectangles");
+        }
+
+        public void FailedTestsDrawImage()
+        {
+            TagCloudDrawer drawer = new TagCloudDrawer(layouter);
+            drawer.SaveName = TestContext.CurrentContext.Test.FullName + ".jpg";
+            drawer.Scale = 10;
+            drawer.DrawImage();
+            drawer.SaveImage();
         }
     }
 }
