843648375c224d98c481e2f6774ddb8e2ad57262
Everal <89090080110@mail.ru>
2022-11-14 16:03:46 +0500

    Add names for test in CoordinatesConverterTests

--- TagsCloudVisualization/TagsCloudVisualizationTests/CoordinatesConverterTests.cs
+++ TagsCloudVisualization/TagsCloudVisualizationTests/CoordinatesConverterTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Drawing;
 using FluentAssertions;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using NUnit.Framework;
 using TagsCloudVisualization.Helpers;
 
@@ -34,28 +33,40 @@ public class CoordinatesConverterTests
 
 	public static IEnumerable<TestCaseData> PointsSource()
 	{
-		yield return new TestCaseData(0, 0);
-		yield return new TestCaseData(1, 1);
-		yield return new TestCaseData(-1, -1);
-		yield return new TestCaseData(1, -1);
-		yield return new TestCaseData(-1, 1);
-		yield return new TestCaseData(5, 2);
-		yield return new TestCaseData(3, 5);
+		yield return new TestCaseData(0, 0).SetName("Zero point");
+		yield return new TestCaseData(5, 4).SetName("Point in the first quadrant");
+		yield return new TestCaseData(-9, 122).SetName("Point in the second quadrant");
+		yield return new TestCaseData(-1, -1).SetName("Point in the third quadrant");
+		yield return new TestCaseData(3, -7).SetName("Point in the fourth quadrant");
 	}
 
 	public static IEnumerable<TestCaseData> PolarPointsSource()
 	{
-		yield return new TestCaseData(5.66, 45 * Math.PI / 180).Returns(new Point(4, 4));
-		yield return new TestCaseData(5.66, 3 * 45 * Math.PI / 180).Returns(new Point(-4, 4));
-		yield return new TestCaseData(5.66, -3 * 45 * Math.PI / 180).Returns(new Point(-4, -4));
-		yield return new TestCaseData(5.66, -45 * Math.PI / 180).Returns(new Point(4, -4));
+		yield return new TestCaseData(5.66, Math.PI / 4).Returns(new Point(4, 4))
+			.SetName("Polar coordinate with angle Pi/4 should return point in the first quadrant");
+
+		yield return new TestCaseData(5.66, 3 * Math.PI / 4).Returns(new Point(-4, 4))
+			.SetName("Polar coordinate with angle 3Pi/4 should return point in the second quadrant");
+
+		yield return new TestCaseData(5.66, -3 * Math.PI / 4).Returns(new Point(-4, -4))
+			.SetName("Polar coordinate with angle -3Pi/4 should return point in the third quadrant");
+
+		yield return new TestCaseData(5.66, -Math.PI / 4).Returns(new Point(4, -4))
+			.SetName("Polar coordinate with angle -Pi/4 should return point in the fourth quadrant");
 	}
 
 	public static IEnumerable<TestCaseData> CartesianPointsSource()
 	{
-		yield return new TestCaseData(new Point(4, 4)).Returns((5.66, 45 * Math.PI / 180));
-		yield return new TestCaseData(new Point(-4, 4)).Returns((5.66, 3 * 45 * Math.PI / 180));
-		yield return new TestCaseData(new Point(-4, -4)).Returns((5.66, -3 * 45 * Math.PI / 180));
-		yield return new TestCaseData(new Point(4, -4)).Returns((5.66, -45 * Math.PI / 180));
+		yield return new TestCaseData(new Point(4, 4)).Returns((5.66, Math.PI / 4))
+			.SetName("Polar angle of point in the first quadrant should be positive");
+
+		yield return new TestCaseData(new Point(-4, 4)).Returns((5.66, 3 * Math.PI / 4))
+			.SetName("Polar angle of point in the second quadrant should be positive");
+
+		yield return new TestCaseData(new Point(-4, -4)).Returns((5.66, -3 * Math.PI / 4))
+			.SetName("Polar angle of point in the third quadrant should be negative");
+
+		yield return new TestCaseData(new Point(4, -4)).Returns((5.66, -Math.PI / 4))
+			.SetName("Polar angle of point in the fourth quadrant should be negative");
 	}
 }
