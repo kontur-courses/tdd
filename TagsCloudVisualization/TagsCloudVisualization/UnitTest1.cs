5d3ea619317a6ee217ecc4316831a4260b62b80e
FoxDictator <mega.y1979@gmail.com>
2022-11-14 18:43:14 +0500

    Revert "Deleted TestsHelper class"
    This reverts commit e06435cb23f1f5b76d252c609ed6e307aec0d6db.

--- TagsCloudVisualization/TagsCloudVisualization/UnitTest1.cs
+++ TagsCloudVisualization/TagsCloudVisualization/UnitTest1.cs
@@ -12,12 +12,26 @@ namespace TagsCloudVisualization
     [TestFixture]
     public class PlacementTests
     {
-        private BlockCloudLayouter layouter;
+        private BlockCloudLayouter layout;
+        private TestsHelper helper;
 
         [SetUp]
         public void SetUp()
         {
-            layouter = new BlockCloudLayouter(new Point(1500, 1500));
+            layout = new BlockCloudLayouter(new Point(1500, 1500));
+            helper = new TestsHelper(layout);
+        }
+
+        [Test]
+        public void FailTest()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                layout.PutNextRectangle(new Size(r.Next(10, 50), r.Next(1, 10)));
+            }
+
+            layout.PutNextRectangle(new Size(10, 10)).Should().Be(new Rectangle(0, 0, 0, 0));
         }
 
         [TearDown]
@@ -25,11 +39,7 @@ namespace TagsCloudVisualization
         {
             if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
             {
-                TagCloudDrawer drawer = new TagCloudDrawer(layouter);
-                drawer.SaveName = TestContext.CurrentContext.Test.FullName + ".jpg";
-                drawer.Scale = 10;
-                drawer.DrawImage();
-                drawer.SaveImage();
+                helper.FailedTestsDrawImage();
             }
         }
     }
@@ -39,12 +49,14 @@ namespace TagsCloudVisualization
     [UseApprovalSubdirectory(@"Results\BlockLayoutDrawingTests")]
     public class BlockLayoutDrawingTests
     {
+        private TestsHelper helper;
         private BlockCloudLayouter layouter;
 
         [SetUp]
         public void SetUp()
         {
             layouter = new BlockCloudLayouter(new Point(1000, 1000));
+            helper = new TestsHelper(layouter);
         }
 
         [Test]
@@ -62,8 +74,7 @@ namespace TagsCloudVisualization
             layouter.PutNextRectangle(new Size(3, 2));
             layouter.PutNextRectangle(new Size(2, 3));
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [Test]
@@ -75,8 +86,7 @@ namespace TagsCloudVisualization
                 layouter.PutNextRectangle(new Size(r.Next(10, 80), r.Next(1, 10)));
             }
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [Test]
@@ -87,8 +97,7 @@ namespace TagsCloudVisualization
                 layouter.PutNextRectangle(new Size(i, 2));
             }
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [Test]
@@ -99,8 +108,7 @@ namespace TagsCloudVisualization
                 layouter.PutNextRectangle(new Size(4, 4));
             }
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [TearDown]
@@ -108,11 +116,7 @@ namespace TagsCloudVisualization
         {
             if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
             {
-                TagCloudDrawer drawer = new TagCloudDrawer(layouter);
-                drawer.SaveName = TestContext.CurrentContext.Test.FullName + ".jpg";
-                drawer.Scale = 10;
-                drawer.DrawImage();
-                drawer.SaveImage();
+                helper.FailedTestsDrawImage();
             }
         }
     }
@@ -123,11 +127,13 @@ namespace TagsCloudVisualization
     public class SpiralLayoutDrawingTests
     {
         private SpiralCloudLayouter layouter;
+        private TestsHelper helper;
 
         [SetUp]
         public void SetUp()
         {
             layouter = new SpiralCloudLayouter(new Point(1000, 1000));
+            helper = new TestsHelper(layouter);
         }
 
         [Test]
@@ -138,8 +144,7 @@ namespace TagsCloudVisualization
                 layouter.PutNextRectangle(new Size(4, 4));
             }
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [Test]
@@ -150,8 +155,7 @@ namespace TagsCloudVisualization
                 layouter.PutNextRectangle(new Size(i, 2));
             }
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [Test]
@@ -160,8 +164,7 @@ namespace TagsCloudVisualization
             layouter.PutNextRectangle(new Size(1, 1));
             layouter.PutNextRectangle(new Size(5, 5));
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [Test]
@@ -173,8 +176,7 @@ namespace TagsCloudVisualization
                 layouter.PutNextRectangle(new Size(r.Next(10, 80), r.Next(1, 10)));
             }
 
-            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
-                "PlacedRectangles");
+            helper.Approve();
         }
 
         [TearDown]
@@ -182,12 +184,33 @@ namespace TagsCloudVisualization
         {
             if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
             {
-                TagCloudDrawer drawer = new TagCloudDrawer(layouter);
-                drawer.SaveName = TestContext.CurrentContext.Test.FullName + ".jpg";
-                drawer.Scale = 10;
-                drawer.DrawImage();
-                drawer.SaveImage();
+                helper.FailedTestsDrawImage();
             }
+        }
+    }
+
+    internal class TestsHelper
+    {
+        private ICloudLayouter layouter;
+
+        public TestsHelper(ICloudLayouter layouter)
+        {
+            this.layouter = layouter;
+        }
+
+        public void Approve()
+        {
+            Approvals.VerifyAll(TestContext.CurrentContext.Test.FullName, layouter.PlacedRectangles,
+                "PlacedRectangles");
+        }
+
+        public void FailedTestsDrawImage()
+        {
+            TagCloudDrawer drawer = new TagCloudDrawer(layouter);
+            drawer.SaveName = TestContext.CurrentContext.Test.FullName + ".jpg";
+            drawer.Scale = 10;
+            drawer.DrawImage();
+            drawer.SaveImage();
         }
     }
 }
