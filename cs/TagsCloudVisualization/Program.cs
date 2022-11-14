4b59b6a025a4e2faf7a6c1abfbab01cbe1b9cefd
Ruden <karimov.ruden@mail.ru>
2022-11-14 16:04:09 +0300

    добавил код и новые картинки для второго задания

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Drawing;
+using TagsCloudVisualization;
+
+var firstLayouter = new CircularCloudLayouter(new Point(250,250));
+for (int i = 0; i < 50; i++)
+    firstLayouter.PutNextRectangle(new Size(20, 5));
+
+var secondLayouter = new CircularCloudLayouter(new Point(100,100));
+for (int i = 0; i < 150; i++)
+{
+    secondLayouter.PutNextRectangle(new Size(20, 5));
+}
+
+var thridLayouter = new CircularCloudLayouter(new Point(250, 250));
+for (int i = 0; i < 500; i++)
+{
+    thridLayouter.PutNextRectangle(new Size(10, 5));
+}
+
+LayoutSaver.SaveFailedLayoutImageAsJpeg(@"C:\Users\harle\source\repos\tdd\cs\TagsCloudVisualization\Images\LayoutImage1.jpg",new Size(500,500),firstLayouter.Rectangles);
+LayoutSaver.SaveFailedLayoutImageAsJpeg(@"C:\Users\harle\source\repos\tdd\cs\TagsCloudVisualization\Images\LayoutImage2.jpg", new Size(500, 500), secondLayouter.Rectangles);
+LayoutSaver.SaveFailedLayoutImageAsJpeg(@"C:\Users\harle\source\repos\tdd\cs\TagsCloudVisualization\Images\LayoutImage3.jpg", new Size(500, 500), thridLayouter.Rectangles);
+
