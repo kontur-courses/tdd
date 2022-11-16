6eed9431beb2a17e451ba535b5f63437de4f9b06
Maksim Tsvetkov <atm.tsvetk@gmail.com>
2022-11-16 23:55:22 +0500

    Add close location rectangles test

--- cs/TagsCloudVisualization.Tests/Tests/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualization.Tests/Tests/CircularCloudLayouterTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 
@@ -26,28 +27,31 @@ namespace TagsCloudVisualization.Tests.Tests
         }
         
         [Test]
-        public void GenerateCloud_SuccessPath_ShouldAddRectangle()
+        [Description("GetNextRectangle must return new rectangle with correct position")]
+        public void GetNextRectangle_SuccessPath_ShouldAddNewRectangle()
         {
             var rectangleSize = new Size(100, 100);
 
-            cloudLayouter.PutNextRectangle(center, rectangles, rectangleSize);
+            rectangles.Add(cloudLayouter.GetNextRectangle(center, rectangles, rectangleSize));
             
-            Assert.AreEqual(1, rectangles.Count);
+            Assert.IsNotEmpty(rectangles);
         }
         
         [Test]
+        [Description("First rectangle must be located in the center")]
         public void PutNextRectangle_WithFirstRectangle_ShouldAddInCenter()
         {
             var rectangleSize = new Size(100, 100);
 
-            cloudLayouter.PutNextRectangle(center, rectangles, rectangleSize);
+            rectangles.Add(cloudLayouter.GetNextRectangle(center, rectangles, rectangleSize));
             
             Assert.AreEqual(center.X - rectangleSize.Width / 2, rectangles[^1].X);
             Assert.AreEqual(center.Y - rectangleSize.Height / 2, rectangles[^1].Y);
         }
         
         [Test]
-        public void GenerateCloud_SuccessPath_ShouldReturnRectangles()
+        [Description("GenerateCloud must return a list of rectangles with the length of a list of sizes")]
+        public void GenerateCloud_SuccessPath_ShouldReturnAmountRectangles()
         {
             var amount = 100;
             var listSize = TagCloudHelper.GenerateRandomListSize(amount);
@@ -58,6 +62,7 @@ namespace TagsCloudVisualization.Tests.Tests
         }
 
         [Test]
+        [Description("All rectangles must not intersect")]
         public void IsRectanglesIntersect_AllRectanglesNotIntersect_ShouldReturnTrue()
         {
             rectangles = cloudLayouter
@@ -66,6 +71,22 @@ namespace TagsCloudVisualization.Tests.Tests
             for (var i = 1; i < rectangles.Count; i++)
                 for (var j = 0; j < i; j++)
                     Assert.AreEqual(true, !rectangles[i].IntersectsWith(rectangles[j]));
+        }
+
+        [Test]
+        [Description("All rectangles must be placed in a circle closer to the center")]
+        public void CloseLocationRectangles_AllRectangleShiftToCenter_ShouldReturnTrue()
+        {
+            rectangles = cloudLayouter
+                .GenerateCloud(center, TagCloudHelper.GenerateRandomListSize(150));
+
+            var radius = Math.Max(rectangles.Sum(x => x.Width), rectangles.Sum(x => x.Height)) / 2;
+
+            foreach (var rectangle in rectangles)
+            {
+                Assert.AreEqual(true, 
+                    Math.Pow(rectangle.X - center.X, 2) + Math.Pow(rectangle.Y - center.Y, 2) < radius * radius);
+            }
         }
 
         [TearDown]
