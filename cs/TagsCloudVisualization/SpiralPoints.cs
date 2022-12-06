f81e8e6b9c5b7a311c48df9cf5cbf21fc561f997
nina_prokopova <nina30600@gmail.com>
2022-12-07 01:30:42 +0500

    Половина задачи 1 tag cloud

---
+++ cs/TagsCloudVisualization/SpiralPoints.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class SpiralPoints : IEnumerator<Point>
+    {
+        private Point spiralCenter;
+        private RadiusVector lastRadiusVector;
+        private int distanceBetweenPoints = 300;
+        private int spiralStep = 500;
+
+        public SpiralPoints(Point spiralCenter)
+        {
+            this.spiralCenter = spiralCenter;
+        }
+
+        // public IEnumerable<Point> NextPointGetter()
+        // {
+        //     GetNextRadiusVector();
+        //     yield return GetPointFromRadiusVector();
+        // }
+
+        private void GetNextRadiusVector()
+        {
+            if (lastRadiusVector == null)
+            {
+                lastRadiusVector = new RadiusVector(15,20);
+                return;
+            }
+
+            var tangensOfRotationAngle = distanceBetweenPoints / lastRadiusVector.Length;
+            var rotationAngel = Math.Atan(tangensOfRotationAngle);
+            var lengthAddition = spiralStep / 360 * rotationAngel;
+            var newRadiusVector = new RadiusVector(lengthAddition + lastRadiusVector.Length,
+                rotationAngel + lastRadiusVector.Angle);
+            lastRadiusVector = newRadiusVector;
+        }
+
+        private Point GetPointFromRadiusVector()
+        {
+            var alfa = lastRadiusVector.Angle;
+            var r = lastRadiusVector.Length;
+            var x = spiralCenter.X + r * Math.Cos(alfa);
+            var y = spiralCenter.Y + r * Math.Sin(alfa);
+            return new Point((int)x, (int)y);
+        }
+
+        public bool MoveNext()
+        {
+            GetNextRadiusVector();
+            
+            return true;
+        }
+
+        public void Reset()
+        {
+            lastRadiusVector = null;
+        }
+
+        object IEnumerator.Current => Current;
+
+        public Point Current
+        {
+            get { return GetPointFromRadiusVector(); }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/TagCloudVisualizer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using static System.Drawing.Bitmap;
+
+namespace TagsCloudVisualization
+{
+    public class TagCloudVisualizer
+    {
+        public void MakePicture()
+        {
+            //1. получение прямоугольников
+            var layouter = new CircularCloudLayouter(new Point(0, 0));
+            var size = new Size(50, 50);
+            var rectangleList = new List<Rectangle>();
+            for (int i = 0; i < 10; i++)
+            {
+                rectangleList.Add(layouter.PutNextRectangle(size));
+            }
+            
+            //2. Нахождение ширины и высоты
+            var minX = Int32.MaxValue;
+            var maxX = Int32.MinValue;
+            var minY = Int32.MaxValue;
+            var maxY = Int32.MinValue;
+            foreach (var rectangle in rectangleList)
+            {
+                minX = Math.Min(rectangle.Left, minX);
+                maxX = Math.Max(rectangle.Right, maxX);
+                minY = Math.Min(rectangle.Bottom, minY);
+                maxY = Math.Max(rectangle.Top, maxY);
+            }
+            var width = maxX - minX;
+            var height = maxY - minY;
+            
+            var bitmap = new Bitmap(1000, 1020);
+            Graphics.FromImage(bitmap);
+            bitmap.Save("D:\\шпора-2022\\tdd\\cs\\TagsCloudVisualization\\1.png");
+
+        }
+    }
+}
