d2d937a8c4f89d08b479ae9bc2899c7919305b7d
nina_prokopova <nina30600@gmail.com>
2022-12-09 22:18:13 +0500

    Tag Cloud 1 Эстетическая задача

--- cs/TagsCloudVisualization/TagCloudVisualizer.cs
+++ cs/TagsCloudVisualization/TagCloudVisualizer.cs
@@ -1,29 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using static System.Drawing.Bitmap;
 
 namespace TagsCloudVisualization
 {
     public class TagCloudVisualizer
     {
-        public void MakePicture()
+        public void MakePicture(List<Rectangle> rectangles, string fileName)
         {
-            //1. получение прямоугольников
-            var layouter = new CircularCloudLayouter(new Point(0, 0));
-            var size = new Size(50, 50);
-            var rectangleList = new List<Rectangle>();
-            for (int i = 0; i < 10; i++)
-            {
-                rectangleList.Add(layouter.PutNextRectangle(size));
-            }
-            
-            //2. Нахождение ширины и высоты
             var minX = Int32.MaxValue;
             var maxX = Int32.MinValue;
             var minY = Int32.MaxValue;
             var maxY = Int32.MinValue;
-            foreach (var rectangle in rectangleList)
+            foreach (var rectangle in rectangles)
             {
                 minX = Math.Min(rectangle.Left, minX);
                 maxX = Math.Max(rectangle.Right, maxX);
@@ -32,11 +21,46 @@ namespace TagsCloudVisualization
             }
             var width = maxX - minX;
             var height = maxY - minY;
-            
-            var bitmap = new Bitmap(1000, 1020);
-            Graphics.FromImage(bitmap);
-            bitmap.Save("D:\\шпора-2022\\tdd\\cs\\TagsCloudVisualization\\1.png");
+            var additive = 100;
+            var squareLength = Math.Max(width, height)+additive;
+            if (squareLength % 2 == 1)
+                squareLength += 1;
+            var bitmap = new Bitmap(squareLength, squareLength);
+            DrawBackground(bitmap, squareLength);
+            var blackColor = Color.FromArgb(0, 0, 0);
+            DrawRectangles(rectangles, bitmap, blackColor, squareLength/2);
+            bitmap.Save(fileName);
+        }
 
+        private static void DrawBackground(Bitmap bitmap, int squareLength)
+        {
+            for (var x = 0; x < squareLength; x++)
+            {
+                for (var y = 0; y < squareLength; y++)
+                {
+                    var whiteColor = Color.FromArgb(255, 255, 255);
+                    bitmap.SetPixel(x, y, whiteColor);
+                }
+            }
+        }
+
+        private static void DrawRectangles(List<Rectangle> rectangleList, Bitmap bitmap, 
+            Color blackColor, int halfSideLength)
+        {
+            foreach (var rectangle in rectangleList)
+            {
+                for (var x = rectangle.Left; x <= rectangle.Right; x++)
+                {
+                    bitmap.SetPixel(x + halfSideLength, rectangle.Top + halfSideLength, blackColor);
+                    bitmap.SetPixel(x + halfSideLength, rectangle.Bottom + halfSideLength, blackColor);
+                }
+
+                for (var y = rectangle.Top; y <= rectangle.Bottom; y++)
+                {
+                    bitmap.SetPixel(rectangle.Left + halfSideLength, y + halfSideLength, blackColor);
+                    bitmap.SetPixel(rectangle.Right + halfSideLength, y + halfSideLength, blackColor);
+                }
+            }
         }
     }
 }
