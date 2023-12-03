bb8077743dc5c9f8ed99879f49c2c31c1ea8e20c
Alexander Gorbatov <shantem965@mail.ru>
2023-12-04 00:08:36 +0500

    TagsCloudDrawer.cs has been implemented

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var random = new Random();
+
+        var layouter = new CircularCloudLayouter(new Point(-500, -500));
+        for (var i = 1; i <= 100; i++)
+        {
+            layouter.PutNextRectangle(new Size(10,10));
+        }
+
+        var drawer = new TagsCloudDrawer(layouter);
+        
+        var bitmap = drawer.DrawRectangles(new Pen(Color.Red, 1), 10);
+        TagsCloudDrawer.SaveImage(bitmap, Directory.GetCurrentDirectory(), "image.jpeg");
+    }
+}
---
+++ cs/TagsCloudVisualization/TagsCloudDrawer.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagsCloudVisualization;
+
+public class TagsCloudDrawer
+{
+    private readonly ICloudLayouter layouter;
+
+    public TagsCloudDrawer(ICloudLayouter layouter)
+    {
+        this.layouter = layouter;
+    }
+
+    public Bitmap DrawRectangles(Pen pen, int scale)
+    {
+        var borders = layouter.GetCloudBorders();
+        var bitmap = new Bitmap(borders.Width * scale, borders.Height * scale);
+        var graphics = Graphics.FromImage(bitmap);
+
+        var rectanglesWithShift = layouter.PlacedRectangles
+            .Select(r =>
+                new Rectangle(
+                    (r.X - borders.X) * scale,
+                    (r.Y - borders.Y) * scale, 
+                    r.Width * scale,
+                    r.Height * scale));
+        
+        foreach (var rectangle in rectanglesWithShift)
+            graphics.DrawRectangle(pen, rectangle);
+
+        return bitmap;
+    }
+
+    public static void SaveImage(Bitmap bitmap, string dirPath, string filename)
+    {
+        if (!Directory.Exists(dirPath))
+            Directory.CreateDirectory(dirPath);
+
+        bitmap.Save(Path.Combine(dirPath, filename), ImageFormat.Jpeg);
+    }
+}
