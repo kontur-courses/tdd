b67eb7a871cdc7aaa58d57454d83b7f62a1e91a8
Kholstinin Yegor <99079920+Yrwlcm@users.noreply.github.com>
2023-12-03 12:43:34 +0500

    Added image creating feature with examples

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,9 @@ namespace TagsCloudVisualization
             if (rectangleSize.Width < 0 || rectangleSize.Height < 0)
                 throw new ArgumentException("Rectangle can't have negative width or height");
 
+            // Необходимо чтобы значительно увеличить плотность, очень сильно жертвуем производительностью
+            // spiralGenerator.ResetSpiral();
+
             while (true)
             {
                 var nextPoint = spiralGenerator.GetNextPoint();
@@ -42,6 +46,34 @@ namespace TagsCloudVisualization
                 createdRectangles.Add(newRect);
                 return newRect;
             }
+        }
+
+        public void CreateImageOfLayout(string fileName, string? filePath = null)
+        {
+            var imageWidth = 2 * createdRectangles.Max(rect => Math.Max(Math.Abs(rect.Right), Math.Abs(rect.Left))) + 100;
+            var imageHeight = 2 * createdRectangles.Max(rect => Math.Max(Math.Abs(rect.Top), Math.Abs(rect.Bottom))) + 100;
+            var bitmap = new Bitmap(imageWidth, imageHeight);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.Wheat);
+            var blackPen = new Pen(Color.Black);
+            var offsettedRectangles = createdRectangles.ToArray();
+            for (var i = 0; i < offsettedRectangles.Length; i++)
+            {
+                offsettedRectangles[i].Offset(imageWidth / 2, imageHeight / 2);
+            }
+
+            filePath ??= AppDomain.CurrentDomain.BaseDirectory + @"\Images";
+
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            graphics.DrawRectangles(blackPen, offsettedRectangles);
+            bitmap.Save(filePath + @$"\{fileName}.png", ImageFormat.Png);
+
+            // Оставил это под комментом, использовал для удобства при запуске Main в LayoutExamples, но вдруг пригодится ;)
+            //Console.WriteLine($"Image is saved to {filePath}" + @$"\{fileName}.png");
         }
     }
 }
