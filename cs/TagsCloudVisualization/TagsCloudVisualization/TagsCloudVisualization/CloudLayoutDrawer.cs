2379b51632e46200c6603b1a89b7d8fd01444c3f
daHil <ssocmain@gmail.com>
2023-12-04 14:29:01 +0500

    added functionality for Draw of CloudLayoutDrawer so that tests go success

--- cs/TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/CloudLayoutDrawer.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/TagsCloudVisualization/CloudLayoutDrawer.cs
@@ -4,6 +4,16 @@ public static class CloudLayoutDrawer
 {
     public static Bitmap Draw(Rectangle[] rectanglesLayout, int width, int height)
     {
-        return null;
+        if (rectanglesLayout == null || rectanglesLayout.Length == 0)
+            throw new ArgumentNullException("rectangles are empty");
+        if (width <= 0 || height <= 0)
+            throw new ArgumentException("either width or height of rectangle size is not possitive");
+
+        var bitmap = new Bitmap(width, height);
+        var graphics = Graphics.FromImage(bitmap);
+        graphics.Clear(Color.White);
+        var pen = new Pen(Color.Black);
+        graphics.DrawRectangles(pen, rectanglesLayout);
+        return bitmap;
     }
 }
