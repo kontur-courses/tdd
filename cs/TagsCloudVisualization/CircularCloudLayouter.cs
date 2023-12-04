4562f97dcf4e9737b16e41ecf8dde33a6e2f20e3
Crazy1beatch <zhora.shevelev196@mail.ru>
2023-12-04 11:21:08 +0500

    Наметил примерно конструкцию проекта и тесты

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using TagsCloudVisualization;
+
+class CircularCloudLayouter
+{
+    public CircularCloudLayouter(Point center)
+    {
+            
+    }
+
+    public Rectangle PutNextRectangle(Size rectangleSize)
+    {
+        if (rectangleSize.Height <= 0 || rectangleSize.Width <= 0)
+            throw new ArgumentException("rectangleSize with zero or negative height or width is prohibited!");
+        throw new NotImplementedException();
+    }
+}
---
+++ cs/TagsCloudVisualization/UnitTests/CircularCloudLayouter_Should.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter_Should
+    {
+        [Test]
+        public void Create_Without_Exceptions()
+        {
+            var action = new Action(() => new CircularCloudLayouter(new Point()));
+            action.Should().NotThrow();
+        }
+
+        [TestCaseSource(typeof(TestData), nameof(TestData.ZeroOrLessHeightOrWidth_Size))]
+        public void Throw_WhenPutNewRectangle_WidthOrHeightLessEqualsZero(Size size)
+        {
+            var action = new Action(() => new CircularCloudLayouter(new Point()).PutNextRectangle(size));
+            action.Should().Throw<ArgumentException>()
+                .Which.Message.Should().Contain("zero or negative height or width");
+        }
+    }
+}
