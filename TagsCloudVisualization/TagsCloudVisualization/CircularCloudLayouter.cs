4c5e030661979b229f5f8f91502e8bdd14608309
tripples25 <92451706+tripples25@users.noreply.github.com>
2023-12-04 15:01:46 +0500

    PutNextRectangle now returns unique and non-overlapping rectangles

--- TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -5,12 +5,31 @@ namespace TagsCloudVisualization;
 
 public class CircularCloudLayouter
 {
+    private readonly Point center;
+    private readonly List<Rectangle> rectangles;
+    public IEnumerable<Rectangle> Rectangles => rectangles;
+
     public CircularCloudLayouter(Point center)
     {
+        this.center = center;
+        rectangles = new List<Rectangle>();
     }
-    
+
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        return new Rectangle(Point.Empty, rectangleSize);
+        var rectPosition = new Point(center.X - rectangleSize.Width / 2, center.Y - rectangleSize.Height / 2);
+
+        while (!IsPlaceSuitableForRectangle(new Rectangle(rectPosition, rectangleSize)))
+        {
+            rectPosition.Y += 1;
+        }
+        var rectangle = new Rectangle(rectPosition, rectangleSize);
+        rectangles.Add(rectangle);
+        return rectangle;
+    }
+
+    private bool IsPlaceSuitableForRectangle(Rectangle rectangle)
+    {
+        return rectangles.All(x => !x.IntersectsWith(rectangle));
     }
 }
--- TagsCloudVisualization/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
+++ TagsCloudVisualization/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
@@ -23,4 +23,48 @@ public class CircularCloudLayouterTests
 
         layouter.PutNextRectangle(rectSize).Size.Should().Be(rectSize);
     }
+    
+    [Test]
+    public void PutNextRectangle_ReturnsRectangleInCenter_OnFirstInvoke()
+    {
+        var center = Point.Empty;
+        var layouter = new CircularCloudLayouter(center);
+        var rectSize = new Size(2, 2);
+
+        layouter.PutNextRectangle(rectSize).Location.Should()
+            .Be(new Point(center.X - rectSize.Width / 2, center.Y - rectSize.Height / 2));
+    }
+
+    [Test]
+    public void PutNextRectangle_ReturnsUniqueRectangles()
+    {
+        var center = Point.Empty;
+        var layouter = new CircularCloudLayouter(center);
+        var rectSize = new Size(2, 2);
+        var iterations = 100;
+        var rectangles = new List<Rectangle>();
+
+        var f = () => layouter.PutNextRectangle(rectSize);
+        for (var i = 0; i < iterations; i++)
+        {
+            rectangles.Add(f.Invoke());
+        }
+
+        rectangles.ForEach(rect => rectangles.Count(x => x == rect).Should().Be(1));
+    }
+    
+    [Test]
+    public void PutNextRectangle_ReturnsRectangles_WithoutIntersections()
+    {
+        var center = Point.Empty;
+        var layouter = new CircularCloudLayouter(center);
+        var rectSize = new Size(2, 2);
+        var iterations = 100;
+        var f = () => layouter.PutNextRectangle(rectSize);
+        
+        for (var i = 0; i < iterations; i++)
+            f.Invoke();
+
+        layouter.Rectangles.All(rect => layouter.Rectangles.Except(new []{rect}).All(y => !y.IntersectsWith(rect))).Should().BeTrue();
+    }
 }
