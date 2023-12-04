664ae529aa819787a124273c532f30fa0584e699
RenatZubakin <zubakinr03@mail.ru>
2023-12-05 00:10:53 +0500

    create hometask solution and add simple tests

---
+++ cs/TagsCloudVisualization/TagsCloudVisualization.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class TagsCloudVisualization
+    {
+        public Point Center;
+        public List<Rectangle> WordPositions;
+        public double Radius;
+        public double Angle;
+
+        public TagsCloudVisualization(Point center)
+        {
+            Center = center;
+            Radius = 0;
+            Angle = 0;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+             throw new ArgumentException();
+        }
+
+    }
+
+    /*public class WordPosition
+    {
+        public Point Position;
+        public double Width;
+        public double Height;
+
+        public WordPosition(Point position, double width, double height)
+        {
+            Position = position;
+            Width = width;
+            Height = height;
+        }
+    }*/
+}
---
+++ cs/TagsCloudVisualization/TagsCloudVisualizationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class TagsCloudVisualizationTests
+    {
+
+        private TagsCloudVisualization tagsCloud;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tagsCloud = new TagsCloudVisualization(new Point(1, 2));
+        }
+
+        [Test]
+        public void TagsCloudVisualization_Initialize_Params()
+        {
+            Assert.AreEqual(0,tagsCloud.Radius);
+            Assert.AreEqual(0,tagsCloud.Angle);
+            Assert.AreEqual(null,tagsCloud.WordPositions);
+            Assert.AreEqual(new Point(1,2), tagsCloud.Center);
+        }
+
+        [TestCase(-1,2, TestName = "When_Width_Is_Negative")]
+        [TestCase(1, -2, TestName = "When_Height_Is_Negative")]
+        [TestCase(0,1, TestName = "When_Width_Is_Zero")]
+        [TestCase(1, 0, TestName = "When_Height_Is_Zero")]
+        public void PutNextRectangle_Throws_ArgumentException(int width,int height)
+        {
+            var size = new Size(width, height);
+            Assert.Throws<ArgumentException>(() => tagsCloud.PutNextRectangle(size));
+        }
+
+        [Test]
+        public void TagsCloudVisualization_First_ElementPosition_Should_Be_Equal_To_Center()
+        {
+            tagsCloud.PutNextRectangle(new Size(4, 5));
+            Assert.AreEqual(tagsCloud.Center, tagsCloud.WordPositions[0].Location);
+        }
+
+    }
+}
