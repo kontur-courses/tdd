b9c5ffe960535f80af0bc80c3273e87f84c7b65f
tripples25 <92451706+tripples25@users.noreply.github.com>
2023-12-05 07:00:01 +0500

    Added visualization for layouter

--- TagsCloudVisualization/TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/TagsCloudVisualization/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing;
+using TagsCloudVisualization;
 
-Console.WriteLine("Hello, World!");
+var center = new Point(512, 512);
+var layouter = new CircularCloudLayouter(center);
+var rectSize = new Size(100, 40);
+var iterations = 100;
+var f = () => layouter.PutNextRectangle(rectSize);
+var rand = new Random();
+for (var i = 0; i < iterations; i++)
+{
+    f.Invoke();
+    rectSize = new Size(rand.Next(60, 140), rand.Next(20, 80));
+}
+
+var radius = (int)layouter.Rectangles.Select(x => Math.Sqrt(Math.Pow(x.X - center.X, 2) + Math.Pow(x.Y - center.Y, 2))).Max();
+
+
+var image = new Bitmap(1024, 1024);
+var graphics = Graphics.FromImage(image);
+
+graphics.DrawRectangles(Pens.Aqua, layouter.Rectangles.ToArray());
+graphics.DrawEllipse(Pens.Brown, new Rectangle(new Point(center.X - radius, center.Y - radius), new Size(radius * 2, radius * 2)));
+graphics.Save();
+image.Save("abc.png");
