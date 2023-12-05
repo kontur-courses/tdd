d4e16edc20cf094ab2d70f579441d3cc1e063070
Anastasia <akinekimura@gmail.com>
2023-12-05 23:52:32 +0500

    Написала классы TagsCloudVisualization и PointsOnSpiral

---
+++ cs/TagsCloudVisualization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using TagsCloudVisualization;
+
+class CircularCloudLayouter
+{
+    private Point cloudCenter;
+    private List<Rectangle> rectangels = new List<Rectangle>();
+    private PointsOnSpiral points;
+
+    public CircularCloudLayouter(Point cloudCenter)
+    {
+        this.cloudCenter = cloudCenter;
+        points = new PointsOnSpiral(cloudCenter);
+    }
+
+    public Rectangle PutNextRectangle(Size rectangleSize)
+    {
+        foreach (var point in points.GetPointsOnSpiral())
+        {
+            var rectangle = new Rectangle(point, rectangleSize);
+            if (rectangels.Any(x => x.IntersectsWith(rectangle)))
+                continue;
+            rectangels.Add(rectangle);
+            return rectangle;
+        }
+
+        return new Rectangle(cloudCenter, rectangleSize);
+    }
+}
