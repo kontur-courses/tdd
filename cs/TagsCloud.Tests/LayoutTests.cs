142f027491a4918850f0b759452b0e56d7b247b4
luvairo-m <luvairo-228@yandex.ru>
2023-12-10 18:45:13 +0500

    Правки в тестах класса Layout

--- cs/TagsCloud.Tests/LayoutTests.cs
+++ cs/TagsCloud.Tests/LayoutTests.cs
@@ -1,7 +1,6 @@
 using FluentAssertions;
 using NUnit.Framework;
 using SixLabors.ImageSharp;
-using System.Reflection;
 using TagsCloudVisualization;
 using static TagsCloud.Tests.TestConfiguration;
 
@@ -10,18 +9,16 @@ namespace TagsCloud.Tests;
 [TestFixture]
 public class LayoutTests
 {
-    [OneTimeSetUp]
-    public void OneTimeSetUp()
-    {
-        random = new Random();
-    }
-
     [SetUp]
     public void SetUp()
     {
         var layoutFunction = new Spiral(random.Next(1, 25), random.NextSingle());
         var screenCenter = new PointF(WindowWidth / 2, WindowHeight / 2);
         layout = new Layout(layoutFunction, screenCenter);
+
+        // Clear currentRectangles between tests to update running context.
+        currentRectangles.Clear();
+        currentRectangles.TrimExcess();
     }
 
     [TearDown]
@@ -36,23 +33,20 @@ public class LayoutTests
         var fileName = $"{TestContext.CurrentContext.Test.MethodName}-fail.png";
         var filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
 
-        var rects = typeof(Layout)
-            .GetField("placedRectangles", BindingFlags.Instance | BindingFlags.NonPublic)!
-            .GetValue(layout) as IList<RectangleF>;
-
         LayoutVisualizer.CreateVisualization(
-            rects!,
+            currentRectangles,
             ImageCenter,
-            Color.Red,
-            1f,
             Color.White,
+            1f,
+            Color.Red,
             filePath);
 
         writer.WriteLine($"Tag cloud visualization saved to file <{filePath}>");
     }
 
     private Layout layout;
-    private Random random;
+    private readonly Random random = new();
+    private readonly List<RectangleF> currentRectangles = new();
 
     [Test]
     public void PutNextRectangle_ShouldNot_SkipRectangles()
@@ -67,28 +61,24 @@ public class LayoutTests
     public void PlacedRectangles_ShouldNot_HaveIntersections()
     {
         var rectCount = random.Next(1, 250);
-        var rects = PutNRectanglesInLayout(rectCount);
+        PutNRectanglesInLayout(rectCount);
 
-        RectanglesHaveIntersections(rects).Should().Be(false);
+        CurrentRectanglesHaveIntersections().Should().Be(false);
     }
 
-    private IList<RectangleF> PutNRectanglesInLayout(int amount)
+    private void PutNRectanglesInLayout(int amount)
     {
-        var rects = new List<RectangleF>(amount);
-
         for (var i = 0; i < amount; i++)
         {
             var size = new SizeF(random.Next(1, 250), random.Next(1, 250));
-            rects.Add(layout.PutNextRectangle(size));
+            currentRectangles.Add(layout.PutNextRectangle(size));
         }
-
-        return rects;
     }
 
-    private static bool RectanglesHaveIntersections(IList<RectangleF> rectangles)
+    private bool CurrentRectanglesHaveIntersections()
     {
-        return (from current in rectangles
-            from another in rectangles
+        return (from current in currentRectangles
+            from another in currentRectangles
             where current != another
             where current.IntersectsWith(another)
             select current).Any();
