94943ce09338fccea3d8012eac665c9a7183eb2e
Anastasia <akinekimura@gmail.com>
2023-12-11 21:03:03 +0500

    Поправила недочеты, написала интерфейсы

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,29 +1,40 @@
 ﻿using System.Drawing;
+using System.Collections.Generic;
 using TagsCloudVisualization;
 
-class CircularCloudLayouter
+public class CircularCloudLayouter : ITagCloudLayouter
 {
-    private Point cloudCenter;
-    private List<Rectangle> rectangels = new List<Rectangle>();
-    private PointsOnSpiral points;
+    private readonly Point cloudCenter;
+    private readonly IList<Rectangle> _rectangels = new List<Rectangle>();
+    private readonly SpiralPointsGenerator spiralPointsGenerator;
+    
 
     public CircularCloudLayouter(Point cloudCenter)
     {
         this.cloudCenter = cloudCenter;
-        points = new PointsOnSpiral(cloudCenter);
+        spiralPointsGenerator = new SpiralPointsGenerator(cloudCenter);
     }
 
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        foreach (var point in points.GetPointsOnSpiral())
+        foreach (var point in spiralPointsGenerator.GetPoints())
         {
             var rectangle = new Rectangle(point, rectangleSize);
-            if (rectangels.Any(x => x.IntersectsWith(rectangle)))
+            if (_rectangels.Any(x => x.IntersectsWith(rectangle)))
                 continue;
-            rectangels.Add(rectangle);
+            
+            _rectangels.Add(rectangle);
             return rectangle;
         }
 
         return new Rectangle(cloudCenter, rectangleSize);
+    }
+    
+    static void Main(string[] args)
+    {
+        var myCenter = new Point(0, 0);
+        var test = new CircularCloudLayouter(myCenter);
+        var ans = test.spiralPointsGenerator.GetPoints().Take(100000).ToList();
+        //Console.WriteLine(test.spiralPointsGenerator.GetPoints().Take(100).ToList());
     }
 }
---
+++ cs/TagsCloudVisualization/SpiralPointsGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public sealed class SpiralPointsGenerator : IPointsGenerator
+{
+    private readonly Point center;
+    private readonly double radiusStep;
+
+    public SpiralPointsGenerator(Point center, double radiusStep = 1)
+    {
+        this.center = center;
+        this.radiusStep = radiusStep;
+    }
+
+    ///<exception cref="System.OverflowExeption">
+    /// Long point generation operation
+    /// </exception>
+    public IEnumerable<Point> GetPoints()
+    {
+        double radius = 0;
+        var term = radiusStep / 360;
+        while (true)
+        {
+            for (var pointNumber = 0; pointNumber < 360; pointNumber++)
+            {
+                var pointAngle = 2 * Math.PI * pointNumber / 360;
+                // yield return new Point((int)(Math.Cos(pointAngle) * radius) + center.X,
+                //     (int)(Math.Sin(pointAngle) * radius) + center.Y);
+                var currentPoint = new Point(center.X, center.Y);
+                currentPoint.Offset((int)(Math.Cos(pointAngle) * radius), (int)(Math.Sin(pointAngle) * radius));
+                yield return currentPoint;
+                radius += term;
+            }
+        }
+    }
+}
