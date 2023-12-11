28eb625a2d035564dccac8a373e5abeb943cd73d
Anastasia <akinekimura@gmail.com>
2023-12-12 03:20:53 +0500

    Сделала задачи 2,3

---
+++ cs/TagsCloudVisualization/CloudDrawer.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+
+namespace TagsCloudVisualization;
+
+public class CloudDrawer
+{
+    public static void DrawAndSaveCloud(Rectangle[] rectangles, string fileName)
+    {
+        var bitmap = new Bitmap(500, 500);
+        var gr = Graphics.FromImage(bitmap);
+        gr.FillRectangle(Brushes.Black, 0, 0, 500, 500);
+        gr.FillRectangles(Brushes.White, rectangles);
+        bitmap.Save(fileName);
+    }
+}
