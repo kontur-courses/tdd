87ce885179b5704148a13bdce3c99f839cb6fa95
Nikita_Trofimov <nikita174trofimov@gmail.com>
2023-12-11 18:18:24 +0500

    Удалил комментарии (Для самого себя)

--- cs/TagsCloudVisualization/GenerateRandomRectangles.cs
+++ cs/TagsCloudVisualization/GenerateRandomRectangles.cs
@@ -17,34 +17,3 @@ public class GenerateRandomRectangles
         return rectangles;
     }
 }
-
-
-// public class RectangleGenerator
-// {
-//     private readonly CircularCloudLayouter _layouter;
-//
-//     public RectangleGenerator(CircularCloudLayouter layouter)
-//     {
-//         _layouter = layouter ?? throw new ArgumentNullException(nameof(layouter));
-//     }
-//
-//     public List<Rectangle> GenerateRandomRectangles(int count)
-//     {
-//         if (count <= 0)
-//             throw new ArgumentOutOfRangeException(nameof(count), "Count must be a positive number");
-//
-//         var rectangles = new List<Rectangle>();
-//         var random = new Random(1);
-//
-//         for (var i = 0; i < count; i++)
-//         {
-//             var width = random.Next(40, 100);
-//             var height = random.Next(20, 60);
-//             var size = new Size(width, height);
-//             var rectangle = _layouter.PutNextRectangle(size);
-//             rectangles.Add(rectangle);
-//         }
-//
-//         return rectangles;
-//     }
-// }
