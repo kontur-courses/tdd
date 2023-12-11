8b58d8f0592e3592d28af7ce6bfe3c817a716d16
tripples25 <92451706+tripples25@users.noreply.github.com>
2023-12-11 20:14:34 +0500

    Added new sample images

--- TagsCloudVisualization/TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/TagsCloudVisualization/Program.cs
@@ -8,7 +8,7 @@ using TagsCloudVisualization.PointsProviders;
 var center = new Point(-1000, 1000);
 var layouter = new CircularCloudLayouter(new ArchimedeanSpiralPointsProvider(center));
 var rand = new Random();
-for (var i = 0; i < 1000; i++)
+for (var i = 0; i < 100; i++)
     layouter.PutNextRectangle(new Size(rand.Next(60, 140), rand.Next(20, 80)));
 
 var visualizator = new TagsCloudVisualizator(layouter);
